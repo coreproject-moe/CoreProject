79e0365282c062e433dfa665ca4c4a97ee4837a1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-01-10 18:55:26 +0600

    Add FAQ
    Former-commit-id: 587d6413f00d351c00841c2c965012fbfe600e5e

--- pages/faq/models.py
+++ pages/faq/models.py
@@ -4,3 +4,9 @@ from tinymce.models import HTMLField
 
 class PageModel(models.Model):
     content = HTMLField()
+
+    def __str__(self) -> str:
+        return "Content"
+
+    class Meta:
+        verbose_name = "Page"
