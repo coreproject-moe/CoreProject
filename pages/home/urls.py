298b829feba6cd5dd257d0812c7caa9128061b2e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-01-13 10:09:32 +0600

    Add Navbar.html
    Former-commit-id: d8a326a791443dacbd0be587366871b4274419bb

--- pages/home/urls.py
+++ pages/home/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [path("", home_page, name="home_page")]
+urlpatterns = [path("", home_page, name="home_page"),]
--- pages/home/views.py
+++ pages/home/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-async def home_page(request):
+def home_page(request):
     return render(request, "home/index.html")
