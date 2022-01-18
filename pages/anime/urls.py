2837b5a5fe1f54d8b77d575ccf006acb7b4fe706
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-01-19 02:38:02 +0600

    Add anime Home page

--- pages/anime/urls.py
+++ pages/anime/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',anime_home_page,name='anime_home_page'),
     path("/<str:anime_name>/<int:anime_episode>", anime_page, name="anime_page")
 ]
--- pages/anime/views.py
+++ pages/anime/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def anime_home_page(request) -> HttpResponse:
+    return render(request,'anime/index.html')
 
 def anime_page(request, anime_name, anime_episode) -> HttpResponse:
     pass
