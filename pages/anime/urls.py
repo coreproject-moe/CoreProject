e993f4e40dbdc4c80094a78486420ce19ced84cf
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-01-19 02:52:26 +0600

    Fix data href

--- pages/anime/urls.py
+++ pages/anime/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('',anime_home_page,name='anime_home_page'),
-    path("/<str:anime_name>/<int:anime_episode>", anime_page, name="anime_page")
+    path("<str:anime_name>/<int:anime_episode>", anime_page, name="anime_page")
 ]
