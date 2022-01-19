86c94c50c6df9f64c714398390167f1a08825cd9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-01-19 11:04:32 +0600

    Add Cards
    Former-commit-id: af412c09f9192cd519bf539b7857bbb3d530f4e9

--- pages/anime/views.py
+++ pages/anime/views.py
@@ -1,10 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required()
 def anime_home_page(request) -> HttpResponse:
-    return render(request,'anime/index.html')
+    return render(request, "anime/index.html")
+
 
 def anime_name_and_episode_page(request, anime_name, anime_episode) -> HttpResponse:
     pass
