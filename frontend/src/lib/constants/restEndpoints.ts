b67a650403e4b96269e37dbf83c05e1aee7d0b4e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-09 01:41:37 +0600

    Weird Bug

---
+++ frontend/src/lib/constants/restEndpoints.ts
@@ -0,0 +1,51 @@
+// Main endpoint url
+export const baseUrl = 'http://127.0.0.1:8000';
+
+/*
+    Avatar endpoint
+    Example = http://127.0.0.1:8000/api/v1/avatar/1/
+*/
+export const avatarUrl = `${baseUrl}/api/v1/avatar/`;
+
+/* 
+    Token obtain endpoint
+    Example = http://127.0.0.1:8000/api/v1/token/
+*/
+export const tokenObtainUrl = `${baseUrl}/api/v1/token/`;
+
+/* 
+    Token refresh endpoint
+    Example = http://127.0.0.1:8000/api/v1/token/refresh/
+*/
+export const tokenRefreshUrl = `${baseUrl}/api/v1/token/refresh/`;
+
+/* 
+    Token blacklist endpoint
+    Example = http://127.0.0.1:8000/api/v1/token/blacklist/
+*/
+export const tokenBlacklistUrl = `${baseUrl}/api/v1/token/blacklist/`;
+
+/*
+    User Info endpoint (JSON)
+    Example = http://127.0.0.1:8000/api/v1/users/info/
+*/
+export const userInfoUrl = `${baseUrl}/api/v1/users/info/`;
+
+/*
+    User login endpoint
+    Example = 127.0.0.1:8000/authentication/login/
+*/
+export const loginUrl = `${baseUrl}/authentication/login/`;
+
+/*
+    User login endpoint
+    Example = 127.0.0.1:8000/authentication/logout/
+*/
+export const logoutUrl = `${baseUrl}/authentication/logout/`;
+
+/*
+    User Signup endpoint
+    Example = http://127.0.0.1:8000/authentication/register/
+*/
+
+export const signupUrl = `${baseUrl}/authentication/register/`;
