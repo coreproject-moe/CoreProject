189fd468a80fab03e08240f1beec007007cd5974
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-09 11:08:18 +0600

    Update : Fixed user sync
    Finally fix the user sync.
    
    
    Former-commit-id: 5a4f30a4f7784f1e9c1bb35984fbe004825263f7

--- frontend/src/lib/store/users.ts
+++ frontend/src/lib/store/users.ts
@@ -1,28 +1,5 @@
 import { writable } from 'svelte/store';
-import { userInfoUrl } from '$lib/constants/restEndpoints';
 import { getCookie } from '$lib/functions/getCookie';
 import { browser } from '$app/env';
 
 export const isUserAuthenticated = writable(browser && getCookie('Authorization'));
-
-const getData = () => {
-	fetch(userInfoUrl, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Authorization: `Token ${browser && getCookie('Authorization')}`
-		}
-	}).then(async (res) => {
-		const data = await res.json();
-		userInfo.set(data);
-	});
-};
-
-getData();
-
-export const userInfo = writable({}, (set) => {
-	
-});
-
-// Finally works i guess, God i hate JS.
