cd60b04aa2f3adbefe903240eb1dd103994d3b71
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-09 17:55:41 +0600

    Remove unused codes
    Former-commit-id: 48bd9098cecfa414a63ae1bf09332cb8e9ccc851

--- backend/api/v1/_user/views.py
+++ backend/api/v1/_user/views.py
@@ -1,5 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import (
+    SessionAuthentication,
+    TokenAuthentication,
+)
 
 from django.contrib.auth import get_user_model
 
