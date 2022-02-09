7349ce67757710167a3f0a5b1bfc72b76a9ade1d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-09 17:54:33 +0600

    Update views.py

--- backend/api/v1/_user/views.py
+++ backend/api/v1/_user/views.py
@@ -1,9 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .serializers import (
     UserSerializer,
