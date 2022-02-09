d839cbfedde4cb40ecf099c0660e14409a93d957
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-09 17:23:41 +0600

    Export constants into constants/backend folder

---
+++ frontend/src/lib/constants/backend/baseUrl.ts
@@ -0,0 +1,2 @@
+// Main endpoint url | ( Django )
+export const baseUrl = 'http://127.0.0.1:8000';
--- frontend/src/lib/store/users.ts
+++ frontend/src/lib/store/users.ts
@@ -2,4 +2,10 @@ import { writable } from 'svelte/store';
 import { getCookie } from '$lib/functions/getCookie';
 import { browser } from '$app/env';
 
+// So we are getting the Authorization cookie.
+// I have set up this in the  backend so that,
+// When a user logs in, it will create the token.
+// On logout delete the cookie.
+// We can listen to the cookie changes to determine if a user is logged in or not.
+
 export const isUserAuthenticated = writable(browser && getCookie('Authorization'));
