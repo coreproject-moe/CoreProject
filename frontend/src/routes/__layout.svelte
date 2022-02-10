12be179a0741ad4b737d4ca8ae93a5c1baf50e75
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-10 22:44:21 +0600

    Update __layout.svelte
    more Rant

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -2,7 +2,9 @@
 	import { browser } from '$app/env';
 
 	export async function load({ fetch }) {
-		// For the love of GOD. Run this only in Browser
+		// For the love of GOD.
+		// Run this only in Browser.
+		// Wasted 2+ hours debugging this stupid shit.
 
 		if (browser) {
 			const res = await fetch(userInfoUrl, {
