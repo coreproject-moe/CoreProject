a2f70f0e2dcd0049a90a7b6dbe0a3fd79756bc96
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-13 00:49:29 +0600

    Finally everything seems to be working
    Chef's kiss

--- frontend/src/lib/store/users.ts
+++ frontend/src/lib/store/users.ts
@@ -6,7 +6,9 @@ export const userToken = writable(
 	(browser && JSON.parse(localStorage.getItem('tokens'))) || { refresh: '', access: '' }
 );
 
-export const isUserAuthenticated = writable(get(userToken).access !== '');
+export const isUserAuthenticated = writable(
+	get(userToken).access !== '' && get(userToken).access !== undefined
+);
 
 // Monitor changes and set it to localStorage
 userToken.subscribe((change: { access: string; refresh: string }) => {
@@ -15,22 +17,24 @@ userToken.subscribe((change: { access: string; refresh: string }) => {
 
 // Custom function to refresh tokens
 
-setInterval(async () => {
-	if (browser && localStorage.getItem('tokens')) {
-		const tokenObj: { access: string; refresh: string } = JSON.parse(
-			localStorage.getItem('tokens')
-		);
-		const res = await fetch(tokenRefreshUrl, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				refresh: tokenObj.refresh
-			})
-		});
-		const data = await res.json();
-		userToken.set({ refresh: tokenObj.refresh, access: data?.access });
-	}
-}, 5000);
+browser &&
+	get(isUserAuthenticated) &&
+	setInterval(async () => {
+		if (localStorage.getItem('tokens')) {
+			const tokenObj: { access: string; refresh: string } = JSON.parse(
+				localStorage.getItem('tokens')
+			);
+			const res = await fetch(tokenRefreshUrl, {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					refresh: tokenObj.refresh
+				})
+			});
+			const data = await res.json();
+			userToken.set({ refresh: tokenObj.refresh, access: data?.access });
+		}
+	}, 5000);
