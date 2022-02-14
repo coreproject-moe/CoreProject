d13751568ea89eede8e4d16098f09415a65568b5
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-14 08:40:44 +0600

    Update views.py
    Former-commit-id: 242577736ccdbf36bb25fdf56f31c8cc4a72d750

--- backend/api/v1/_user/views.py
+++ backend/api/v1/_user/views.py
@@ -21,11 +21,3 @@ class UserInfo(generics.ListAPIView):
         data = get_user_model().objects.get(id=request.user.id)
         serializer = UserSerializer(data, many=False)
         return Response(serializer.data)
-
-    # def post(self, request: HttpResponse):
-    #     serializer = UserSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
