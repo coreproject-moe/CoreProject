adcf489cc0ce3f01e5aef44aaeef506aba091c00
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-14 13:06:51 +0600

    WIP Add image.resize function
    Former-commit-id: 2402691fa7d61323273e008f270509a5f562fa8f

--- backend/custom/user/models.py
+++ backend/custom/user/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.files import File
+
+from io import BytesIO
+from PIL import Image
+import pillow_avif
 
 
 class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to="avatars", default=None, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        im = Image.open(self.avatar)
+
+        in_memory = BytesIO()
+        im.save(in_memory, format="avif")
+
+        self.avatar = File(in_memory)
+
+        super(CustomUser, self).save(*args, **kwargs)
