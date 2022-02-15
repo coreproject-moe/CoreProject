b979a4486e41a125c66d9af0b699d344e0ee22cb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-15 19:31:22 +0600

    Performance optimizations

--- backend/api/v1/_capture/views.py
+++ backend/api/v1/_capture/views.py
@@ -2,15 +2,17 @@ from rest_framework import status
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from django.http.response import HttpResponse
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.views.decorators.vary import vary_on_headers
 
 from .models import CaptureModel
 from .serializers import CaptureSerializer
-from django.http.response import HttpResponse
 
 # Create your views here.
 
@@ -26,7 +28,6 @@ class CaptureView(
         * SoundCore (music player) Volume
     """
 
-    parser_classes = [JSONParser]
     serializer_class = [CaptureSerializer]
     permission_classes = [IsAuthenticated]
     authentication_classes = [
@@ -34,6 +35,12 @@ class CaptureView(
         JWTAuthentication,
     ]
 
+    @method_decorator(cache_page(60 * 60 * 2))
+    @method_decorator(
+        vary_on_headers(
+            "Authorization",
+        )
+    )
     def get(self, request: HttpResponse) -> Response:
         data, _ = CaptureModel.objects.get_or_create(user=request.user)
         serializer = CaptureSerializer(data, many=False)
