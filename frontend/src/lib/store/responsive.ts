4a3e18c34416769ac22d77f4499a1c51674255d8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-19 11:38:28 +0600

    Frontend : Code optimization
    Former-commit-id: 28437275c45cbc16f1e1d83c13a9f32104003c7c

--- frontend/src/lib/store/responsive.ts
+++ frontend/src/lib/store/responsive.ts
@@ -1,36 +1,30 @@
 import { writable } from "svelte/store";
 import { browser } from "$app/env";
 
-function handleWindowChange(e: MediaQueryList) {
-	// Check if the media query is true
-	return e.matches;
-}
-
 /*
  	Mapped from here
  	https://bulma.io/documentation/helpers/visibility-helpers/
 */
 
-const MOBILE = browser && window.matchMedia("(max-width: 768px)");
-const TABLET = browser && window.matchMedia("(min-width: 769px) and (max-width: 1023px)");
-const DESKTOP = browser && window.matchMedia("(min-width: 1024px) and (max-width: 1215px)");
-const WIDESCREEN = browser && window.matchMedia("(min-width: 1216px) and (max-width: 1407px)");
-const FULLHD = browser && window.matchMedia("(min-width: 1408px )");
+const MOBILE = browser && window?.matchMedia("(max-width: 768px)");
+const TABLET = browser && window?.matchMedia("(min-width: 769px) and (max-width: 1023px)");
+const DESKTOP = browser && window?.matchMedia("(min-width: 1024px) and (max-width: 1215px)");
+const WIDESCREEN = browser && window?.matchMedia("(min-width: 1216px) and (max-width: 1407px)");
+const FULLHD = browser && window?.matchMedia("(min-width: 1408px )");
 
 // Callback function to handle changes
 
+const handleWindowChange = async (e: MediaQueryList) => {
+	// Check if the media query is true
+	return e.matches;
+};
+
 const checkMode = () => {
-	if (handleWindowChange(MOBILE)) {
-		return "mobile";
-	} else if (handleWindowChange(TABLET)) {
-		return "tablet";
-	} else if (handleWindowChange(DESKTOP)) {
-		return "desktop";
-	} else if (handleWindowChange(WIDESCREEN)) {
-		return "widescreen";
-	} else if (handleWindowChange(FULLHD)) {
-		return "fullhd";
-	}
+	if (handleWindowChange(MOBILE)) return "mobile";
+	else if (handleWindowChange(TABLET)) return "tablet";
+	else if (handleWindowChange(DESKTOP)) return "desktop";
+	else if (handleWindowChange(WIDESCREEN)) return "widescreen";
+	else if (handleWindowChange(FULLHD)) return "fullhd";
 };
 
 export const responsiveMode = writable(checkMode());
