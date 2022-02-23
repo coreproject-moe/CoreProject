e9c902a619d0f0268507037cc384bbbe3da7ad99
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-23 10:15:06 +0600

    Backend : Use Class based view in pages.authentication
    Former-commit-id: 8dc1b2bb8e1d358468ac6da38225378bcbb306fe

--- backend/pages/authentication/urls.py
+++ backend/pages/authentication/urls.py
@@ -2,7 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path("login/", login_page, name="login_page"),
+    path("login/", LoginPageView.as_view(), name="login_page"),
     path("logout/", logout, name="logout_page"),
-    path("register/", register_page, name="register_page"),
+    path("register/", RegisterPageView.as_view(), name="register_page"),
 ]
