43d78c26a70b56de77bb29433d313a68212e74a9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-23 18:37:35 +0600

    Backend : Refractor Codes
    Former-commit-id: f3ee10b183baa964e12e1aa6238ce8e6924302ce

--- backend/api/v1/_user/views.py
+++ backend/api/v1/_user/views.py
@@ -1,14 +1,16 @@
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.views.decorators.vary import vary_on_headers
+
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from django.http.response import HttpResponse
-from django.contrib.auth import get_user_model
-from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
-from django.views.decorators.vary import vary_on_headers
 
 from .serializers import UserSerializer
 
@@ -33,7 +35,7 @@ class UserInfo(generics.ListAPIView):
             "Authorization",
         )
     )
-    def get(self, request: HttpResponse) -> Response:
+    def get(self, request: HttpRequest) -> Response:
         data = get_user_model().objects.get(id=request.user.id)
         serializer = UserSerializer(data)
         return Response(serializer.data)
