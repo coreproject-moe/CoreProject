7107001b80505c874a33e9a19a96e962bad0186c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-24 09:54:20 +0600

    Backend : Add md5 email option.
    Former-commit-id: 8b533d907269bfe19145e8ef96eebb0c0f35216e

--- backend/custom/user/models.py
+++ backend/custom/user/models.py
@@ -1,4 +1,7 @@
 from uuid import uuid4
+from hashlib import md5
+from typing import NoReturn
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -8,7 +11,11 @@ from .mixins.resize import ResizeImageMixin
 class CustomUser(AbstractUser, ResizeImageMixin):
     avatar = models.ImageField(upload_to="avatars", default=None, blank=True, null=True)
 
-    def save(self, *args, **kwargs):
+    def email_md5(self) -> str:
+        email = self.email
+        return md5(email.encode("utf-8")).hexdigest()
+
+    def save(self, *args, **kwargs) -> NoReturn:
         if self.avatar:
             file = self.resize(self.avatar)
             self.avatar.save(f"{uuid4()}.avif", file, save=False)
