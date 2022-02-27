caaf30276b1b9683bb97c565d133cef5cb373a59
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-27 11:51:27 +0600

    Backend : Add models for page.upload

--- backend/pages/upload/admin.py
+++ backend/pages/upload/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import AnimeNameModel, EpisodeModel
+
 # Register your models here.
+
+admin.site.register(EpisodeModel)
+admin.site.register(AnimeNameModel)
