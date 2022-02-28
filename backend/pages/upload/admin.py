2a8fb148c2bae0c5fb825b32228c8c70f9c9f360
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-28 11:33:48 +0600

    Backend : Add placeholder for `anime_info_edit_page`
    Former-commit-id: ff9a132b2977cd4eeca84a4260da9e7042cbe954

--- backend/pages/upload/admin.py
+++ backend/pages/upload/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import AnimeNameModel, EpisodeModel
+from .models import AnimeInfoModel, EpisodeModel
 
 # Register your models here.
 
 admin.site.register(EpisodeModel)
-admin.site.register(AnimeNameModel)
+admin.site.register(AnimeInfoModel)
