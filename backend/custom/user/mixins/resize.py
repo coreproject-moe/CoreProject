4d5e9403cbf6ea4fb888d56578d93a2050b168ce
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-02-28 12:40:54 +0600

    Backend : Update Image cropping

--- backend/custom/user/mixins/resize.py
+++ backend/custom/user/mixins/resize.py
@@ -1,3 +1,6 @@
+# Thanks Random guy from here :
+# https://note.nkmk.me/en/python-pillow-image-crop-trimming/
+
 import pillow_avif
 from PIL import Image
 from io import BytesIO
@@ -5,12 +8,26 @@ from io import BytesIO
 from django.db.models import ImageField
 from django.core.files import File
 
+# Image height and width in pixel
+CROP_WIDTH = 512
+CROP_HEIGHT = 512
+
 
 class ResizeImageMixin(object):
     def resize(self, image: ImageField) -> File:
-        im = Image.open(image)
+        base_image = Image.open(image)
+        width, height = base_image.size
+
+        resized_image = base_image.crop(
+            (
+                (width - CROP_WIDTH) // 2,
+                (height - CROP_HEIGHT) // 2,
+                (width + CROP_WIDTH) // 2,
+                (height + CROP_HEIGHT) // 2,
+            )
+        )
 
         in_memory = BytesIO()
-        im.save(in_memory, format="avif")
+        resized_image.save(in_memory, format="avif")
 
         return File(in_memory)
