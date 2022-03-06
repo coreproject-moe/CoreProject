623e925179247eebdb26d181887996139ea802f6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-06 10:50:06 +0600

    [ AnimeCore ] Frontend : Remove @vimejs

--- AnimeCore/frontend/svelte.config.js
+++ AnimeCore/frontend/svelte.config.js
@@ -23,12 +23,29 @@ const config = {
 		// paths: {
 		// 	base: dev ? '' : '/static'
 		// }
+
 		vite: {
 			resolve: {
 				alias: {
 					// these are the aliases and paths to them
 					$store: path.resolve("./src/lib/store"),
 					$urls: path.resolve("./src/lib/constants/backend/urls")
+				}
+			},
+			build: {
+				rollupOptions: {
+					output: {
+						manualChunks: {
+							md5: ["md5"],
+							anime: ["animejs"],
+							dayjs: ["dayjs"],
+							swiper: ["swiper"],
+							"tippy.js": ["tippy.js"],
+							// VideoJs specific
+							"video.js": ["video.js"],
+							"videojs-vtt.js": ["videojs-vtt.js"]
+						}
+					}
 				}
 			}
 		}
