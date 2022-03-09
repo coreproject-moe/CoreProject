cae33f765b6f214c02b1c4b6a80c67a2637447ba
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-09 11:19:59 +0600

    [AnimeCore] Frontend : Update Refractor
    Former-commit-id: 05936c9aa281698cf88357ca2d310c8d214c33dd

--- AnimeCore/frontend/postcss.config.cjs
+++ AnimeCore/frontend/postcss.config.cjs
@@ -11,7 +11,7 @@ const config = {
         !dev &&
             purgecss({
                 content: ["./src/**/**/*.{svelte,html,js,ts}"],
-                defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+                defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
                 safelist: {
                     deep: [],
                     greedy: [/swiper/, /svelte/, /tippy/, /vm-player/]
