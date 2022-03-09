41c53f01b1de541264540b5c3f0112b9c4dbc3c8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-10 01:58:57 +0600

    [ AnimeCore ] Frontend : Refractor

---
+++ AnimeCore/frontend/src/hooks/users/logout.ts
@@ -0,0 +1,28 @@
+import { browser } from "$app/env";
+import { userToken } from "$store/users";
+import { tokenBlacklistUrl } from "$urls/restEndpoints";
+import { get } from "svelte/store";
+
+export const logoutUser = async (href: string = "") => {
+    try {
+        const res = await fetch(tokenBlacklistUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                refresh: get(userToken).refresh
+            })
+        });
+        if (res?.ok) {
+            userToken.set({ refresh: "", access: "" });
+            browser && localStorage.removeItem("tokens");
+            window.location.href = href;
+        }
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error(`Cannot POST to ${tokenBlacklistUrl} | Reason : ${err}`);
+        }
+    }
+};
