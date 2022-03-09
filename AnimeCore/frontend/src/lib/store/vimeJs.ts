713e5f05bd76894bf5e2abc0bb4690cae2d92228
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-09 11:20:15 +0600

    [ AnimeCore ] Frontend : Better logic for stores
    Former-commit-id: 9cc2ff319e6a57407b780e13ac1dc439cda2330e

---
+++ AnimeCore/frontend/src/lib/store/vimeJs.ts
@@ -0,0 +1,56 @@
+import { browser } from "$app/env";
+import { captureEndpoint } from "$urls/restEndpoints";
+import { get, writable } from "svelte/store";
+import { isUserAuthenticated, userToken } from "./users";
+
+export const vimeJSVolume = writable(50, function start(set) {
+    if (get(isUserAuthenticated)) {
+        (async (set) => {
+            try {
+                const res = await fetch(captureEndpoint, {
+                    method: "GET",
+                    headers: new Headers({
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${get(userToken).access}`
+                    })
+                });
+                const data = await res.json();
+                set(data?.video_volume);
+            } catch (err) {
+                if (err instanceof Error) {
+                    console.error(
+                        `Can't fetch from backend | Flushing Tokens | Reason : ${err?.message}`
+                    );
+                }
+            }
+        })(set);
+    } else {
+        const volume =
+            browser && parseInt(localStorage.getItem("vimejs-volume") ?? JSON.stringify(100));
+        set(volume);
+    }
+});
+
+// On change update the backend and localstorage.
+vimeJSVolume.subscribe((change: number) => {
+    // Localstorage update
+    browser && localStorage.setItem("vimejs-volume", JSON.stringify(~~change));
+
+    // Backend update
+    try {
+        fetch(captureEndpoint, {
+            method: "PATCH",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${get(userToken).access}`
+            },
+            body: JSON.stringify({
+                video_volume: ~~change
+            })
+        });
+    } catch {
+        console.error(`POST to ${captureEndpoint} Failed`);
+    }
+});
