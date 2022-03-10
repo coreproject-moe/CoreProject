36f53b95af07f2988ae693b9ef2fa7b962d30366
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-10 20:34:31 +0600

    [ AnimeCore ] Frontend : Remove unused endpoints
    Former-commit-id: bcac4cc4cda4674d20f26418325c6a24cf19eae0

--- AnimeCore/frontend/src/lib/constants/backend/urls/pageUrlEndpoints.ts
+++ AnimeCore/frontend/src/lib/constants/backend/urls/pageUrlEndpoints.ts
@@ -1,20 +1,8 @@
 import { baseUrl } from "./baseUrl";
 
 /*
-    User Logour Page endpoint
-    Example = 127.0.0.1:8000/authentication/logout/
-*/
-export const logoutPageUrl = `${baseUrl}/_authentication/logout/`;
-
-/*
     User Signup Page endpoint
     Example = http://127.0.0.1:8000/authentication/register/
 */
 
 export const signupPageUrl = `${baseUrl}/_authentication/register/`;
-
-/* 
-    User Edit Info Page 
-    Example = http://127.0.0.1:8000/edit/user_info/
-*/
-export const userEditInfoPageUrl = `${baseUrl}/edit/user_info/`;
