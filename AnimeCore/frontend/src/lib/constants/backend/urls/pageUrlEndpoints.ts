347c31e63b2741639a8960daa61da7839b81b3bb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-11 01:40:16 +0600

    [ AnimeCore ] Frontend : Remove `pageUrlEndpoints.ts`

--- AnimeCore/frontend/src/lib/constants/backend/urls/pageUrlEndpoints.ts
+++
@@ -1,8 +0,0 @@
-import { baseUrl } from "./baseUrl";
-
-/*
-    User Signup Page endpoint
-    Example = http://127.0.0.1:8000/authentication/register/
-*/
-
-export const signupPageUrl = `${baseUrl}/_authentication/register/`;
