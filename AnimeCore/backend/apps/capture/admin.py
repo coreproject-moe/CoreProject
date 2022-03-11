ee2fe632c9efe73d074766541ea1c607d8ccfb33
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-11 19:35:17 +0600

    [ AnimeCore ] Backend : Best logic

--- AnimeCore/backend/apps/capture/admin.py
+++ AnimeCore/backend/apps/capture/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import CaptureVideoModel
+from .models import CaptureVideoModel, CaptureEpisodeModel, CaptureAnimeNameModel
 
 # Register your models here.
 
 admin.site.register(CaptureVideoModel)
+admin.site.register(CaptureEpisodeModel)
+admin.site.register(CaptureAnimeNameModel)
--- AnimeCore/backend/apps/capture/models.py
+++ AnimeCore/backend/apps/capture/models.py
@@ -4,7 +4,32 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+from apps.upload.models import EpisodeModel, AnimeInfoModel
+
 # Create your models here.
+
+
+class CaptureEpisodeModel(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    episode = models.ForeignKey(EpisodeModel, on_delete=models.CASCADE)
+    timestamp = models.IntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(0),
+        ],
+    )
+
+    def __str__(self) -> str:
+        return f"Episode : {self.episode.episode_number} | Time stamp = {datetime.timedelta(seconds=self.timestamp)} | User : {self.user.username}"
+
+
+class CaptureAnimeNameModel(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    anime = models.ForeignKey(AnimeInfoModel, on_delete=models.CASCADE)
+    episdoes = models.ManyToManyField(CaptureEpisodeModel)
+
+    def __str__(self) -> str:
+        return f"Anime : {self.anime} | User : {self.user.username}"
 
 
 class CaptureVideoModel(models.Model):
@@ -16,15 +41,11 @@ class CaptureVideoModel(models.Model):
             MinValueValidator(0),
         ],
     )
-    video_timestamp = models.IntegerField(
-        default=0,
-        validators=[
-            MinValueValidator(0),
-        ],
-    )
+
+    timestamp = models.ManyToManyField(CaptureAnimeNameModel)
 
     def __str__(self) -> str:
-        return f"User = {self.user.username} | Video Volume = {self.video_volume} | Video Time stamp = {datetime.timedelta(self.video_volume)}"
+        return f"User = {self.user.username} | Video Volume = {self.video_volume}"
 
     class Meta:
         verbose_name = "User Video Capture Model"
