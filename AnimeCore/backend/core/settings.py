e0a7fa8d6ec60c160bd4f6b342c0359c2fbd58ac
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-11 11:36:09 +0600

    [ AnimeCore ] Backend : Remove `os` from `settings.py`
    Former-commit-id: 1d11300cd3104b9a3de858f192d93d89a72b8565

--- AnimeCore/backend/core/settings.py
+++ AnimeCore/backend/core/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -170,14 +169,14 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    Path(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = Path(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = Path(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
