22f1c29f8ee7d2825edb232e5befbf4cc8a415a9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-11 19:35:17 +0600

    [ AnimeCore ] Backend : Best logic
    Former-commit-id: ee2fe632c9efe73d074766541ea1c607d8ccfb33

--- AnimeCore/backend/apps/capture/serializers.py
+++ AnimeCore/backend/apps/capture/serializers.py
@@ -1,13 +1,33 @@
 from rest_framework import serializers
 
-from .models import CaptureVideoModel
+from .models import (
+    CaptureAnimeNameModel,
+    CaptureEpisodeModel,
+    CaptureVideoModel,
+)
+
+
+class CaptureEpisodeSerializer(serializers.ModelSerializer):
+    episode = serializers.CharField(source="episode.episode_name")
+
+    class Meta:
+        model = CaptureEpisodeModel
+        exclude = ("id", "user")
+
+
+class CaptureAnimeNameSerializer(serializers.ModelSerializer):
+    anime = serializers.CharField(source="anime.anime_name")
+    episdoes = CaptureEpisodeSerializer(many=True)
+
+    class Meta:
+        model = CaptureAnimeNameModel
+        exclude = ("id", "user")
 
 
 class CaptureVideoSerializer(serializers.ModelSerializer):
+    timestamp = CaptureAnimeNameSerializer(many=True)
+    user = serializers.CharField(source="user.username")
+
     class Meta:
         model = CaptureVideoModel
-        fields = (
-            "video_volume",
-            "video_timestamp"
-            # "user",
-        )
+        fields = "__all__"
