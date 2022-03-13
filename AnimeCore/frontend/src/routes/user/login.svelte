d8b2923d109633d4bb9a7e928577078d6f8bebf6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-14 02:55:51 +0600

    [ AnimeCore ] Frontend : Move `Login.svelte` to `felte`
    Former-commit-id: 40b09fcde65ee6804aec4f2ff323d64c01b61011

--- AnimeCore/frontend/src/routes/user/login.svelte
+++ AnimeCore/frontend/src/routes/user/login.svelte
@@ -1,57 +1,78 @@
 <script lang="ts">
     import { fade } from "svelte/transition";
 
+    import * as yup from "yup";
+    import { createForm } from "felte";
+
+    import reporter from "@felte/reporter-tippy";
+    import { validator } from "@felte/validator-yup";
+
     import { page } from "$app/stores";
     import { goto } from "$app/navigation";
 
     import { trapFocus } from "$lib/functions/trapFocus";
     import { projectName } from "$lib/constants/frontend/project";
-    import { isUserAuthenticated, userToken } from "$store/users";
 
     import { tokenObtainUrl } from "$urls/restEndpoints";
+    import { isUserAuthenticated, userToken } from "$store/users";
 
     // Bind it to show or hide passwords
     let passwordShown = false;
 
-    // Bindable variables.
-    let username = "";
-    let password = "";
-
     // Show error message if theres an error.
+
     let errorMessage = "";
 
-    const handleFormSubmit = async () => {
-        try {
-            const res = await fetch(tokenObtainUrl, {
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json"
-                },
-                method: "POST",
-                body: JSON.stringify({
-                    username: username,
-                    password: password
-                })
-            });
-            const data = await res.json();
-
-            if (res?.ok) {
-                $userToken = data;
-                $isUserAuthenticated = true;
-
-                // Goto Next page if it exists else redirect to home.
-                const next = $page.url.searchParams.get("next");
-                goto(next ? next : "/anime");
-            }
-            errorMessage = data.detail;
-        } catch (err) {
-            if (err instanceof Error) {
-                $userToken = { refresh: "", access: "" };
-                errorMessage = "Cannot POST to Backend | Is backend down ? 🤔";
-                console.error(`Can't POST to backend | Reason : ${err.message}`);
+    const schema = yup.object({
+        username_or_email: yup
+            ?.string()
+            ?.required("Did you type your <b>Email</b> or <b>Username</b> ? 🤔"),
+        password: yup
+            ?.string()
+            ?.required("How can <b>one</b> create <b>ones</b> account without <b>Password</b> ? 🤔")
+            ?.max(1024, "<b>Password</b> must be less than <b>1024 Characters<b/>")
+    });
+    const { form } = createForm<yup.InferType<typeof schema>>({
+        extend: [
+            validator({ schema }),
+            reporter({
+                tippyProps: {
+                    theme: "black",
+                    allowHTML: true
+                }
+            })
+        ],
+        onSubmit: async (values) => {
+            try {
+                const res = await fetch(tokenObtainUrl, {
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    method: "POST",
+                    body: JSON.stringify({
+                        username: values?.username_or_email,
+                        password: values?.password
+                    })
+                });
+                const data = await res.json();
+                if (res?.ok) {
+                    $userToken = data;
+                    $isUserAuthenticated = true;
+                    // Goto Next page if it exists else redirect to home.
+                    const next = $page.url.searchParams.get("next");
+                    goto(next ? next : "/anime");
+                }
+                errorMessage = data.detail;
+            } catch (err) {
+                if (err instanceof Error) {
+                    $userToken = { refresh: "", access: "" };
+                    errorMessage = "Cannot POST to Backend | Is backend down ? 🤔";
+                    console.error(`Can't POST to backend | Reason : ${err?.message}`);
+                }
             }
         }
-    };
+    });
 </script>
 
 <svelte:head>
@@ -76,19 +97,16 @@
         </p>
     </div>
 {:else}
-    <form on:submit|preventDefault={handleFormSubmit} method="POST" use:trapFocus transition:fade>
+    <form method="POST" use:form use:trapFocus transition:fade>
         <div class="items field is-horizontal pt-3">
             <div class="field-body">
                 <div class="field">
                     <p class="control is-expanded has-icons-left">
                         <input
                             type="text"
-                            name="username"
+                            name="username_or_email"
                             class="input is-font-face-ubuntu has-text-white has-background-black has-border-gray"
                             placeholder="Username / Email"
-                            maxlength={50}
-                            required={true}
-                            bind:value={username}
                         />
                         <span class="icon is-small is-left">
                             <ion-icon
@@ -109,12 +127,6 @@
                             name="password"
                             class="input is-font-face-ubuntu has-text-white has-background-black has-border-gray"
                             placeholder="Password"
-                            maxlength={1024}
-                            minlength={8}
-                            required={true}
-                            on:input|capture={(e) => {
-                                password = e?.currentTarget?.value;
-                            }}
                         />
                         <span
                             class="icon is-small is-right is-clickable is-unselectable"
@@ -144,7 +156,7 @@
     <div class="level is-mobile pt-4">
         <div class="level-left">
             <div class="level-item is-size-7">
-                <a class="has-text-white" rel="external" href="/"> Forgot password? </a>
+                <a class="has-text-white" href="/"> Forgot password? </a>
             </div>
         </div>
         <div class="level-right">
