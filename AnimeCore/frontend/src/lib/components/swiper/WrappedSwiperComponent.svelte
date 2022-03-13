221d07b9fc64e4e039f970c635cf13b6f5b518f2
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-13 21:07:24 +0600

    [ AnimeCore ] Frontend : Add some magic
    Former-commit-id: ea1c35c3ad2fa20ea1e1c8553ca8b169d5224b44

--- AnimeCore/frontend/src/lib/components/swiper/WrappedSwiperComponent.svelte
+++ AnimeCore/frontend/src/lib/components/swiper/WrappedSwiperComponent.svelte
@@ -1,26 +1,63 @@
 <script lang="ts">
     export let animeNumber: number | string = "∞";
-    export let animeName = "One Piece";
+    export let animeName =
+        "One Piece Hello world Hdassad fafdsa One Piece Hello world Hdassad fafdsa One Piece Hello world Hdassad fafdsa";
     export let animePosterImage = "https://cdn.myanimelist.net/images/anime/6/73245.jpg";
 
-    import tippy, { type Instance } from "tippy.js";
+    import tippy, { followCursor, animateFill, type Instance } from "tippy.js";
     import { SwiperSlide } from "swiper/svelte";
     import { onDestroy, onMount } from "svelte";
 
-    let tippyjsAnimeNumberElement: HTMLElement & { _tippy?: Instance };
+    let tippyJsAnimeNumberElement: HTMLElement & { _tippy?: Instance };
+    let tippyJsAnimeNameElement: HTMLElement & { _tippy?: Instance };
+    let tippyJsSubElement: HTMLElement & { _tippy?: Instance };
+    let tippyJsDubElement: HTMLElement & { _tippy?: Instance };
 
     onMount(async () => {
         if (animeNumber === "∞") {
-            tippy(tippyjsAnimeNumberElement, {
+            tippy(tippyJsAnimeNumberElement, {
                 content: "This anime is probably ongoing",
                 theme: "black",
                 maxWidth: 120,
-                placement: "top"
+                placement: "top",
+                animateFill: true,
+                followCursor: "horizontal",
+                plugins: [followCursor, animateFill]
             });
         }
+        tippy(tippyJsAnimeNameElement, {
+            content: animeName,
+            theme: "black",
+            maxWidth: 150,
+            placement: "top",
+            animateFill: true,
+            followCursor: "horizontal",
+            plugins: [followCursor, animateFill]
+        });
+        tippy(tippyJsSubElement, {
+            content: "This anime has English <b>Subtitle</b>",
+            allowHTML: true,
+            theme: "black",
+            maxWidth: 150,
+            placement: "top",
+            animateFill: true,
+            followCursor: "horizontal",
+            plugins: [followCursor, animateFill]
+        });
+        tippy(tippyJsDubElement, {
+            content: "This anime has English <b>Audio</b>",
+            allowHTML: true,
+            theme: "black",
+            maxWidth: 150,
+            placement: "top",
+            animateFill: true,
+            followCursor: "horizontal",
+            plugins: [followCursor, animateFill]
+        });
     });
     onDestroy(async () => {
-        tippyjsAnimeNumberElement?._tippy?.destroy();
+        tippyJsAnimeNumberElement?._tippy?.destroy();
+        tippyJsAnimeNameElement?._tippy?.destroy();
     });
 </script>
 
@@ -43,21 +80,26 @@
         >
             <div class="media">
                 <div class="media-content">
-                    <p class="is-size-4 has-text-white">{animeName}</p>
-                    <p class="subtitle is-size-6 has-text-white">
-                        Episodes : <span
-                            style="text-decoration:underline"
-                            bind:this={tippyjsAnimeNumberElement}>{animeNumber}</span
+                    <p class="is-size-5 has-text-white">
+                        <span class="anime_name" bind:this={tippyJsAnimeNameElement}
+                            >{animeName}</span
+                        >
+                    </p>
+                    <p class="subtitle has-text-white">
+                        <span class="is-size-7">Episodes :</span>
+                        <span class="is-size-5" bind:this={tippyJsAnimeNumberElement}
+                            >{animeNumber}</span
                         >
                         <br />
                     </p>
                     <div
-                        class="field is-grouped is-grouped-multiline is-flex is-justify-content-center"
+                        class="field is-grouped is-grouped-multiline is-flex is-justify-content-start"
                     >
                         <div class="control">
                             <div class="tags has-addons">
-                                <span class="tag is-dark heading">sub</span>
-                                <span class="tag is-info heading">dub</span>
+                                <span class="tag is-dark" bind:this={tippyJsSubElement}>Sub</span>
+                                <span class="tag is-success" bind:this={tippyJsDubElement}>Dub</span
+                                >
                             </div>
                         </div>
                     </div>
@@ -66,3 +108,12 @@
         </div>
     </a>
 </SwiperSlide>
+
+<style lang="scss">
+    .anime_name {
+        overflow: hidden !important;
+        width: 6em !important;
+        display: block ruby;
+        text-overflow: ellipsis;
+    }
+</style>
