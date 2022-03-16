51eade6e135e58cb9bbb081988ab3701347bef03
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-16 09:47:00 +0600

    [ AnimeCore ] Backend : Weird Refractor

--- AnimeCore/backend/apps/upload/views.py
+++ AnimeCore/backend/apps/upload/views.py
@@ -1,10 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-
 from django.http.request import HttpRequest
-from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
 
 from .models import AnimeInfoModel
 from .serializers import AnimeInfoSerializer
@@ -18,7 +15,6 @@ class AnimeInfoView(generics.ListCreateAPIView):
         AllowAny,
     ]
 
-    @method_decorator(cache_page(60 * 60 * 2))
     def get(self, request: HttpRequest, pk: int) -> Response:
         data = AnimeInfoModel.objects.get(id=pk)
         serializer = self.get_serializer(data)
