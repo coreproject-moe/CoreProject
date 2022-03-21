56f6e322c72433565a1833c0a3b6bef77d77d196
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-21 10:12:37 +0600

    [ AnimeCore ] Frontend : More Design changes
    Former-commit-id: e14f0e41d8bb190f171ec8e1174f5e2bcdf26fc0

--- AnimeCore/frontend/src/routes/anime/index.svelte
+++ AnimeCore/frontend/src/routes/anime/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import anime from "animejs";
+
     // Import Swiper Svelte components
     import { Swiper } from "swiper/svelte";
     import { EffectCoverflow, Pagination } from "swiper";
@@ -7,6 +9,10 @@
 
     import { responsiveMode } from "$store/responsive";
     import WrappedSwiperComponent from "$components/swiper/WrappedSwiperComponent.svelte";
+
+    let animeJSHeartIcon: HTMLElement;
+    let animeJSRocketIcon: HTMLElement;
+    let animeJSSparkleIcon: HTMLElement;
 
     let swiperSpacesBetween: number;
     let swiperSlidesPerView: number;
@@ -34,13 +40,8 @@
 </svelte:head>
 
 <div class="container">
-    <p class="title is-size-5 pt-6 has-text-white">
-        <span class="pr-3 pl-3"
-            ><ion-icon name="rocket-outline" style="transform: translateY(5px)" /></span
-        >
-        Recent Uploads :
-    </p>
-    <div class="box" style="background-color: black; border: 1px dotted white">
+    <p class="title is-size-5 pt-2 has-text-white" />
+    <div class="box has-background-black">
         <Swiper
             effect={"coverflow"}
             grabCursor={true}
@@ -63,4 +64,56 @@
             {/each}
         </Swiper>
     </div>
+</div>
+<div class="container">
+    <span
+        class="tag is-black is-rounded has-hover-gray is-size-6 is-clickable is-unselectable"
+        on:mouseenter|preventDefault={() => {
+            anime({
+                targets: [animeJSRocketIcon],
+                color: "#6495ed"
+            });
+        }}
+        on:mouseleave|preventDefault={() => {
+            anime({
+                targets: [animeJSRocketIcon],
+                color: "#FFFFFF"
+            });
+        }}
+    >
+        <ion-icon name="rocket-outline" class="pr-2" bind:this={animeJSRocketIcon} />Trending
+    </span>
+    <span
+        class="tag is-black is-rounded has-hover-gray is-size-6 is-clickable is-unselectable"
+        on:mouseenter|preventDefault={() => {
+            anime({
+                targets: [animeJSSparkleIcon],
+                color: "#ffff00"
+            });
+        }}
+        on:mouseleave|preventDefault={() => {
+            anime({
+                targets: [animeJSSparkleIcon],
+                color: "#ffffff"
+            });
+        }}
+    >
+        <ion-icon name="sparkles-outline" class="pr-2" bind:this={animeJSSparkleIcon} /> Popular
+    </span>
+    <span
+        class="tag is-black is-rounded has-hover-gray is-size-6 is-clickable is-unselectable"
+        on:mouseenter|preventDefault={() => {
+            anime({
+                targets: [animeJSHeartIcon],
+                color: "#ff0000"
+            });
+        }}
+        on:mouseleave|preventDefault={() => {
+            anime({
+                targets: [animeJSHeartIcon],
+                color: "#fffffff"
+            });
+        }}
+        ><ion-icon name="heart-outline" bind:this={animeJSHeartIcon} class="pr-2" /> Trending
+    </span>
 </div>
