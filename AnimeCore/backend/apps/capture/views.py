6439f5274a7a2590afc996170652dd385b315e98
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-21 15:39:40 +0600

    [ AnimeCore ] Backend : Refractors
    Former-commit-id: e75854ee239a115f67a766c97f00458e16bc39c9

--- AnimeCore/backend/apps/capture/views.py
+++ AnimeCore/backend/apps/capture/views.py
@@ -1,11 +1,9 @@
-from rest_framework import mixins
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
 
 from django.http.request import HttpRequest
 from django.shortcuts import get_object_or_404
