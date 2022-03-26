4663fe3d7d121b549e26dd8bedf4e0ebf7cf94fb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-26 13:34:19 +0600

    [ AnimeCore ] Backend + Frontend : Optimizations

--- AnimeCore/backend/apps/user/views.py
+++ AnimeCore/backend/apps/user/views.py
@@ -8,7 +8,6 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-
 from .serializers import UserSerializer
 
 # Create your views here.
