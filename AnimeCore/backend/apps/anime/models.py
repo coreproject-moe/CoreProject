1828bbed2e953dd47d17dec02ec4aca590b04deb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-03-28 00:34:16 +0600

    [ AnimeCore ] Backend : Got  Everything working

--- AnimeCore/backend/apps/anime/models.py
+++ AnimeCore/backend/apps/anime/models.py
@@ -73,8 +73,13 @@ class EpisodeModel(models.Model):
 
 class AnimeGenreModel(models.Model):
     mal_id = models.IntegerField(unique=True, blank=False, null=False, primary_key=True)
-    name = models.CharField(max_length=50, default="", null=False, blank=False)
+    name = models.CharField(
+        unique=True, max_length=50, default="", null=False, blank=False
+    )
     type = models.CharField(max_length=50, default="", null=False, blank=False)
+
+    def __str__(self) -> str:
+        return f"{self.mal_id}. {self.name} ({self.type})"
 
     class Meta:
         verbose_name = "Anime Genre"
