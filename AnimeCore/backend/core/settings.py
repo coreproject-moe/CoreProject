457098d99565735dba7c11966c748c711479e149
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-01 21:24:55 +0600

    [ AnimeCore ] Backend : Add `MAX_PAGINATE_BY`

--- AnimeCore/backend/core/settings.py
+++ AnimeCore/backend/core/settings.py
@@ -225,6 +225,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    "MAX_PAGINATE_BY": 20,
 }
 
 # Settings override
