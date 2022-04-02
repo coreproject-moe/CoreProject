68d6cfd505cbedc4df043b7f6087797c4f7882c0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-03 01:39:50 +0600

    [ AnimeCore ] Frontend : Add `purgecss`

--- AnimeCore/frontend/postcss.config.js
+++ AnimeCore/frontend/postcss.config.js
@@ -1,5 +1,29 @@
 module.exports = {
-    plugins: {
-        autoprefixer: {},
-    },
+    plugins: [
+        'postcss-flexbugs-fixes',
+        [
+            'postcss-preset-env',
+            {
+                autoprefixer: {
+                    flexbox: 'no-2009',
+                },
+                stage: 3,
+                features: {
+                    'custom-properties': false,
+                },
+            },
+        ],
+        [
+            '@fullhuman/postcss-purgecss',
+            {
+                content: [
+                    './pages/**/*.{js,jsx,ts,tsx}',
+                    './components/**/*.{js,jsx,ts,tsx}',
+                ],
+                defaultExtractor: (content) =>
+                    content.match(/[\w-/:]+(?<!:)/g) || [],
+                safelist: ['html', 'body'],
+            },
+        ],
+    ],
 };
