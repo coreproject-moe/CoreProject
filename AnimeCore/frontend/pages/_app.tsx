c6d12c4a5a0e2aaaf0b3aaafbab22c595a644604
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-03 01:13:43 +0600

    [ AnimeCore ] Frontend : Add Swiper
    Former-commit-id: 6bf4485f0bded4c0d347d894ee36b6b3208be527

--- AnimeCore/frontend/pages/_app.tsx
+++ AnimeCore/frontend/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.scss';
+import 'styles/globals.scss';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>AnimeCore</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
