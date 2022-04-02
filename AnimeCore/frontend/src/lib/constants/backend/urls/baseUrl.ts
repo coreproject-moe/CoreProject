1ee4cef56acd0dcd9c366088450c4261a5ec5400
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-02 06:01:12 +0600

    [ AnimeCore ] Architecture : Remove Frontend

--- AnimeCore/frontend/src/lib/constants/backend/urls/baseUrl.ts
+++
@@ -1,2 +0,0 @@
-// Main endpoint url | ( Django )
-export const baseUrl = "http://127.0.0.1:8000";
--- AnimeCore/frontend/src/routes/user/__layout.reset.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import FormBox from "$components/forms/FormBox.svelte";
-</script>
-
-<FormBox>
-    <div slot="form_box">
-        <slot />
-    </div>
-</FormBox>
