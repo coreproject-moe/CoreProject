ef728ec626466b388012a5d1c223b97dad5233ca
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-03 09:15:11 +0600

    [ AnimeCore ] Frontend : Unstable update
    Former-commit-id: bf90e96b5af34a85646aecde4192e78055c9d503

---
+++ AnimeCore/frontend/postcss.config.cjs
@@ -0,0 +1,32 @@
+const cssnano = require("cssnano");
+const postcss_preset_env = require("postcss-preset-env");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+const dev = process.env.NODE_ENV === "development";
+
+const config = {
+    plugins: [
+        postcss_preset_env({
+            stage: 0,
+            autoprefixer: {
+                grid: true
+            }
+        }),
+        cssnano({
+            autoprefixer: false,
+            preset: ["default"]
+        }),
+        // Run on buld
+        !dev &&
+            purgecss({
+                content: ["./src/**/**/*.{svelte,html,js,ts}"],
+                defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+                safelist: {
+                    deep: [],
+                    greedy: [/swiper/, /svelte/]
+                }
+            })
+    ]
+};
+
+module.exports = config;
