820736118ab9633f6ec3a13ac5640ff5ced64068
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-17 21:03:11 +0600

    [ AnimeCore ] Backend : Work on `admin`

--- AnimeCore/backend/apps/anime/admin.py
+++ AnimeCore/backend/apps/anime/admin.py
@@ -14,19 +14,39 @@ from .models import (
 )
 
 # Register your models here.
+
+
+@admin.register(AnimeRecommendationModel)
 class AnimeRecommendationAdmin(admin.ModelAdmin):
     raw_id_fields = ("entry",)
 
 
-admin.site.register(AnimeRecommendationModel, AnimeRecommendationAdmin)
+@admin.register(AnimeInfoModel)
+class AnimeInfoAdmin(admin.ModelAdmin):
+    filter_horizontal = [
+        "anime_genres",
+        "anime_themes",
+        "anime_studios",
+        "anime_producers",
+        "anime_name_synonyms",
+        "anime_episodes",
+    ]
+
+
+@admin.register(AnimeGenreModel)
+class AnimeGenreAdmin(admin.ModelAdmin):
+    """"""
+
+    # https://stackoverflow.com/questions/49293901/hide-model-from-main-admin-list-but-allow-creation-in-inline-editor
+    # def has_module_permission(self, request):
+    #     return False
+
 
 admin.site.register(
     [
-        AnimeInfoModel,
         EpisodeModel,
         EpisodeCommentModel,
         EpisodeTimestampModel,
-        AnimeGenreModel,
         AnimeThemeModel,
         AnimeStudioModel,
         AnimeProducerModel,
