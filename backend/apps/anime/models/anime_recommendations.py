1bf1f57568f88b9dd812224a11c7464406ac7c1e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-19 10:16:03 +0600

    [ AnimeCore ] Backend : Split `models.py`

---
+++ backend/apps/anime/models/anime_recommendations.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from .anime_info import AnimeInfoModel
+
+
+class AnimeRecommendationModel(models.Model):
+    entry = models.ForeignKey(
+        to=AnimeInfoModel, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    anime = models.IntegerField(db_index=True)
+    mal_url = models.URLField(unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.entry}"
+
+    class Meta:
+        verbose_name = "Anime Recommendations"
---
+++ backend/apps/anime/models/episode_comment.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class EpisodeCommentModel(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    comment_added = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.user}"
+
+    class Meta:
+        verbose_name = "Episode Comment"
+        # Sort by newest first
+        ordering = ("-comment_added",)
