3757f50d98149d194d022b4452f384e4d353affc
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-20 13:01:42 +0600

    [ AnimeCore ] Frontend : Fix swiper slider

--- frontend/AnimeCore/src/routes/index.svelte
+++ frontend/AnimeCore/src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Swiper, SwiperSlide } from "swiper/svelte";
 
-    import { EffectFade, Parallax } from "swiper";
+    import { EffectFade, Mousewheel } from "swiper";
     import MainHero from "$components/swiper/MainHero.svelte";
 
     let mainSlide = 0;
@@ -23,22 +23,33 @@
     };
 </script>
 
-<div>
-    <Swiper speed={600} parallax={true} direction="vertical" modules={[Parallax]} autoHeight={true}>
-        <SwiperSlide>
-            <Swiper modules={[EffectFade]} effect="fade" on:swiper={onSwiper}>
-                {#each Array(100) as f, i}
-                    <SwiperSlide>
-                        <MainHero
-                            backgroundImageUrl={"/images/Hyouka-poster.png"}
-                            animeName="Hyouka {i}"
-                            onBackClick={onSwiperBackward}
-                            onForwardClick={onSwiperForward}
-                        />
-                    </SwiperSlide>
-                {/each}
-            </Swiper>
-        </SwiperSlide>
-        <SwiperSlide>Hello world</SwiperSlide>
-    </Swiper>
-</div>
+<Swiper speed={600} direction="vertical" modules={[Mousewheel]} mousewheel={{ sensitivity: 0.001 }}>
+    <SwiperSlide>
+        <Swiper modules={[EffectFade]} effect="fade" on:swiper={onSwiper}>
+            {#each Array(100) as f, i}
+                <SwiperSlide>
+                    <MainHero
+                        backgroundImageUrl={"/images/Hyouka-poster.png"}
+                        animeName="Hyouka {i}"
+                        onBackClick={onSwiperBackward}
+                        onForwardClick={onSwiperForward}
+                    />
+                </SwiperSlide>
+            {/each}
+        </Swiper>
+    </SwiperSlide>
+    <SwiperSlide>Hello world</SwiperSlide>
+</Swiper>
+
+<style global>
+    html,
+    body {
+        position: relative;
+        height: 100%;
+    }
+
+    .swiper {
+        width: 100%;
+        height: 100%;
+    }
+</style>
