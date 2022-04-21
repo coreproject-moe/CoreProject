2c9d210536f3aa6c0294791d4c71bac167858e46
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-21 09:43:34 +0600

    [ AnimeCore ] Frontend : Got a working `TrendingHero`
    Former-commit-id: 06c423e245fa57f80be217799f713f63aa26a86a

---
+++ frontend/AnimeCore/src/lib/components/swiper/TrendingHero.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let backgroundImageUrl = "'https://bulma.io/images/placeholders/128x128.png'";
+</script>
+
+<section class="hero is-fullheight">
+    <div class="hero-body is-align-self-center">
+        <div>
+            <div class="title pb-5 is-size-2 has-text-white">
+                <div class="is-flex">
+                    <span class="pr-5 is-align-self-center"> TRENDING </span>
+                    <div
+                        class="is-align-self-center"
+                        style="display: inline-block;width: 26em;border-top: 10px solid;"
+                    />
+                </div>
+            </div>
+            <section
+                class="hero is-large"
+                style="
+                    border-radius:18px;
+                    border-color: #070519;
+                    background-image:url({backgroundImageUrl});
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    background-size: cover;
+                    box-shadow: 
+                        inset 5em 0 4em 1em #070519;
+                    
+                "
+            >
+                <div class="hero-body">
+                    <p class="title">Large hero</p>
+                    <p class="subtitle">Large subtitle</p>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>
