49bdb7a4065aa10cbc01964b67ebbd29e491c9b7
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-22 15:43:41 +0600

    [ AnimeCore ] Backend : Split `admin.py`

---
+++ backend/apps/anime/admin/anime_producer.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from ..models import AnimeProducerModel
+
+# Register your models here.
+
+
+@admin.register(AnimeProducerModel)
+class AnimeProducerAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
--- backend/apps/anime/serializers/__init__.py
+++ backend/apps/anime/serializers/__init__.py
@@ -5,5 +5,5 @@ from .anime_producer import AnimeProducerSerializer
 from .anime_recommendation import AnimeRecommendationSerializer
 
 from .episode import EpisodeSerializer
-from .episode_comments import EpisodeCommentSerializer
+from .episode_comment import EpisodeCommentSerializer
 from .episode_timestamp import EpisodeTimestampSerializer
