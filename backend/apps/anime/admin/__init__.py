ed4c101de80615117e3116cbab7c19531ebfd9e8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-04-25 17:45:33 +0600

    [ AnimeCore ] Backend : Minor Refractor
    Former-commit-id: 337d25db376e06964fb0c7052390284005c37b02

--- backend/apps/anime/admin/__init__.py
+++ backend/apps/anime/admin/__init__.py
@@ -2,12 +2,12 @@
 # def has_module_permission(self, request):
 #     return False
 
-from .anime_genre import AnimeGenreAdmin
 from .anime_info import AnimeInfoAdmin
-from .anime_producer import AnimeProducerAdmin
+from .anime_genre import AnimeGenreAdmin
 from .anime_studio import AnimeStudioAdmin
 from .anime_synonym import AnimeSynonymAdmin
-
+from .anime_producer import AnimeProducerAdmin
+from .anime_character import AnimeCharacterAdmin
 
 from .episode import EpisodeAdmin
 from .episode_comment import EpisodeCommentAdmin
