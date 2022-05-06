000ebc85c2e0c1c0ad851728600fa3d6364be1ad
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-06 18:04:57 +0600

    Update FromYourList.svelte

--- frontend/AnimeCore/src/lib/components/swiper/FromYourList.svelte
+++ frontend/AnimeCore/src/lib/components/swiper/FromYourList.svelte
@@ -17,7 +17,7 @@
                 background-size: cover;
                 background-image: url('{bannerImageUrl}');
                 box-shadow:
-                    inset 0px -65px 25px -20px rgba(7, 5, 25, 1),
+                    inset 0px -65px 25px -20px rgba(7, 5, 25, .9),
                     inset 0px -70px 75px -20px rgba(7, 5, 25, .8),
                     inset 0px -75px 75px -20px rgba(7, 5, 25, .7)
                 ;
