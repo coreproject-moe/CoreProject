bced5b592c16e4fdad97ea5cea7fe53fadb0c5c6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-07 22:26:35 +0600

    Add error pages (#203)
    * Add error pages
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * :|
    
    * fix font
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix `kokoro-Chan`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add `is-justify-content-center`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add `style="min-width: 100vw"`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Re usable component
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add title
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add Favicon
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update favicon.svg
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixes by @AKindWorld
    
    https://discord.com/channels/@me/958266195654639646/972354865068118066
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * More Fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add `alpine`
    
    Part #205
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Close #205
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update 400.html
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix as directed by @AKindWorld
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove Unused `KaoMoji`
    
    * Update happy_cry.svg
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update 403.html
    
    https://discord.com/channels/@me/958266195654639646/972532073967525988
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 from rest_framework_simplejwt.views import (
     TokenBlacklistView,
@@ -15,10 +16,21 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+handler400 = TemplateView.as_view(template_name="400.html")
+handler403 = TemplateView.as_view(template_name="403.html")
+handler404 = TemplateView.as_view(template_name="404.html")
+handler500 = TemplateView.as_view(template_name="500.html")
+
 urlpatterns = [
     #   Admin
     # ==========
     path("admin/", admin.site.urls),
+    #   Errors
+    # ===========
+    path("400/", TemplateView.as_view(template_name="400.html")),
+    path("403/", TemplateView.as_view(template_name="403.html")),
+    path("404/", TemplateView.as_view(template_name="404.html")),
+    path("500/", TemplateView.as_view(template_name="500.html")),
     #   OpenAPI
     # ============
     path(
