c9320544c598b25fb25cf0d19ac0f2b238808863
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-24 09:37:25 +0600

    fix

--- frontend/AnimeCore/src/routes/__layout.svelte
+++ frontend/AnimeCore/src/routes/__layout.svelte
@@ -3,25 +3,25 @@
 
     import Modal from "svelte-simple-modal";
 
-    import NProgress from 'nprogress';
-	import { navigating } from '$app/stores';
+    import NProgress from "nprogress";
+    import { navigating } from "$app/stores";
 
-	// NProgress css
-	import 'nprogress/nprogress.css';
+    // NProgress css
+    import "nprogress/nprogress.css";
 
-	NProgress.configure({
-		// Full list: https://github.com/rstacruz/nprogress#configuration
-		minimum: 0.16
-	});
+    NProgress.configure({
+        // Full list: https://github.com/rstacruz/nprogress#configuration
+        minimum: 0.16
+    });
 
-	$: {
-		if ($navigating) {
-			NProgress.start();
-		}
-		if (!$navigating) {
-			NProgress.done();
-		}
-	}
+    $: {
+        if ($navigating) {
+            NProgress.start();
+        }
+        if (!$navigating) {
+            NProgress.done();
+        }
+    }
 </script>
 
 <svelte:head>
