c8798d2da8ec1f9b8e6e144120781cf2edd7a7fb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-24 09:33:27 +0600

    add
    https://amalshaji.com/blog/sveltekit-nprogress

--- frontend/AnimeCore/src/routes/__layout.svelte
+++ frontend/AnimeCore/src/routes/__layout.svelte
@@ -2,6 +2,26 @@
     import "../app.scss";
 
     import Modal from "svelte-simple-modal";
+    
+    import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	// NProgress css
+	import 'nprogress/nprogress.css';
+
+	NProgress.configure({
+		// Full list: https://github.com/rstacruz/nprogress#configuration
+		minimum: 0.16
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <svelte:head>
