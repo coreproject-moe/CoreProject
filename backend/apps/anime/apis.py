f0ffaf939513fcbc338d324413eaa0d4692118ec
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-28 17:09:16 +0600

    Update apis.py

--- backend/apps/anime/apis.py
+++ backend/apps/anime/apis.py
@@ -15,19 +15,22 @@ from .views import (
 
 router = routers.SimpleRouter()
 router.register(r"anime", AnimeInfoView, basename="anime_info")
-base_router = routers.NestedSimpleRouter(router, r"anime", lookup="anime")
+anime_info_router = routers.NestedSimpleRouter(router, r"anime", lookup="anime")
 
-
-base_router.register(r"themes", AnimeThemeView, basename="themes")
-base_router.register(
+#   Register Anime Info router
+anime_info_router.register(r"themes", AnimeThemeView, basename="themes")
+anime_info_router.register(
     r"recommendations", AnimeRecommendationView, basename="recommendations"
 )
-base_router.register(r"episodes", EpisodeView, basename="episodes")
-base_router.register(r"studios", AnimeStudioView, basename="studios")
-base_router.register(r"producers", AnimeProducerView, basename="producers")
-base_router.register(r"characters", AnimeCharacterView, basename="characters")
+anime_info_router.register(r"episodes", EpisodeView, basename="episodes")
+anime_info_router.register(r"studios", AnimeStudioView, basename="studios")
+anime_info_router.register(r"producers", AnimeProducerView, basename="producers")
+anime_info_router.register(r"characters", AnimeCharacterView, basename="characters")
 
-episode_router = routers.NestedSimpleRouter(base_router, r"episodes", lookup="episodes")
+# Register Episode router ( subset of anime router )
+episode_router = routers.NestedSimpleRouter(
+    anime_info_router, r"episodes", lookup="episodes"
+)
 episode_router.register(r"comments", EpisodeCommentView, basename="episode_comments")
 episode_router.register(
     r"timestamps", EpisodeTimestampView, basename="episode_timestamp"
@@ -41,6 +44,6 @@ urlpatterns = [
         EpisodeTimestampView.as_view({"get": "total_watchtime"}),
     ),
     path("", include(router.urls)),
-    path("", include(base_router.urls)),
+    path("", include(anime_info_router.urls)),
     path("", include(episode_router.urls)),
 ]
