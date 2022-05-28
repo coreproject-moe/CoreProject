c28bf188ea9e1d6ccb4d3ac3d8497d5ef2a0f9a8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-28 09:55:19 +0600

    .

---
+++ frontend/AnimeCore/pages/_app.tsx
@@ -0,0 +1,32 @@
+import '../styles/globals.scss';
+
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+
+export default function App(props: AppProps) {
+    const { Component, pageProps } = props;
+
+    return (
+        <>
+            <Head>
+                <title>AnimeCore</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    /** Put your mantine theme override here */
+                    colorScheme: 'dark',
+                }}
+            >
+                <Component {...pageProps} />
+            </MantineProvider>
+        </>
+    );
+}
---
+++ frontend/AnimeCore/pages/index.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from 'next';
+import { MainHero } from '../components/swiper/MainHero';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade, Mousewheel, Navigation } from 'swiper';
+
+const Home: NextPage = () => {
+    return (
+        <Swiper
+            speed={600}
+            spaceBetween={0}
+            direction="vertical"
+            slidesPerView="auto"
+            modules={[Mousewheel]}
+            mousewheel={{ sensitivity: 0.001 }}
+            simulateTouch={false}
+        >
+            <SwiperSlide>
+                <Swiper
+                    modules={[EffectFade, Autoplay, Navigation]}
+                    navigation={{
+                        nextEl: '.mainhero__next__el',
+                        prevEl: '.mainhero__previous__el',
+                    }}
+                    effect="fade"
+                    direction="horizontal"
+                >
+                    {Array(10)
+                        .fill(1)
+                        .map((el, i) => {
+                            return (
+                                <SwiperSlide key={i}>
+                                    <MainHero />
+                                </SwiperSlide>
+                            );
+                        })}
+                </Swiper>
+            </SwiperSlide>
+        </Swiper>
+    );
+};
+
+export default Home;
