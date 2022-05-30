f9880f3d239577804bf4dceee1f41b915242eb4f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-30 09:54:31 +0600

    add

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -3,6 +3,9 @@ from typing import NoReturn
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.contrib.auth import get_user_model
+
+from django_unixdatetimefield import UnixDateTimeField
 
 from .mixins.resize import ResizeImageMixin
 
@@ -25,3 +28,27 @@ class User(AbstractUser, ResizeImageMixin):
             self.avatar.save(f"{self.username}.avif", file, save=False)
 
         super().save(*args, **kwargs)
+
+
+class MalModel(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=64, null=True, blank=True)
+    expires_in = models.IntegerField(null=True, blank=True)
+    refresh_token = models.CharField(max_length=64, null=True, blank=True)
+    created_at = UnixDateTimeField()
+
+
+class KitsuModel(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=64, null=True, blank=True)
+    expires_in = models.IntegerField(null=True, blank=True)
+    refresh_token = models.CharField(max_length=64, null=True, blank=True)
+    created_at = UnixDateTimeField()
+
+
+class AnilistModel(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=64, null=True, blank=True)
+    expires_in = models.IntegerField(null=True, blank=True)
+    refresh_token = models.CharField(max_length=64, null=True, blank=True)
+    created_at = UnixDateTimeField()
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -236,9 +236,6 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
-    "DEFAULT_PARSER_CLASSES": [
-        "rest_framework.parsers.JSONParser",
-    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
     "MAX_PAGINATE_BY": 100,
