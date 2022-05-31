5d9bc1bc4a73ca11a0b13962ec33c5795cfca5c5
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-05-31 12:28:10 +0600

    Update views.py

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,7 +1,7 @@
-from apps.user.tasks import count_beans
 from django.contrib.auth import get_user_model
 from django.http.request import HttpRequest
 from django.shortcuts import get_object_or_404
+
 from rest_framework import generics, mixins, status
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.parsers import FormParser, MultiPartParser
