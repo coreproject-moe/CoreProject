c2d3e3b4a3b9d05b8d805c5e654436c536ba6540
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-06-24 11:53:03 +0600

    add

---
+++ frontend/AnimeCore/components/swiper/MainHero.tsx
@@ -0,0 +1,415 @@
+import React, { useEffect, useState } from 'react';
+import {
+    createStyles,
+    Container,
+    Title,
+    Text,
+    Space,
+    Badge,
+    Button,
+    Skeleton,
+} from '@mantine/core';
+import { Navbar } from '../common/Navbar';
+import { useMediaQuery } from '@mantine/hooks';
+import * as voca from 'voca';
+
+const useStyles = createStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'black',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        paddingTop: theme.spacing.xl * 2,
+        paddingBottom: theme.spacing.xl * 2,
+        minHeight: '90vh',
+        boxShadow: `
+            inset 0 4px 1800px rgb(7, 5, 25),
+            inset 0 -40vh 140px 2px rgba(7, 5, 25, 0.9),
+            inset 0 -15vh 140px 2px rgba(7, 5, 25, 0.7),
+            inset 0 -5vh 140px 2px rgba(7, 5, 25, 0.4),
+            inset 0 -2vh 140px 2px rgba(7, 5, 25, 0.2)`,
+
+        [theme.fn.smallerThan('md')]: {
+            minHeight: '30vh',
+            maxHeight: '60vh',
+            boxShadow: `
+                inset 0px -30px 12px -2px rgba(7, 5, 25, 0.95),
+                inset 0 -40vh 140px 2px rgba(7, 5, 25, 0.8),
+                inset 0 -2vh 140px 2px rgba(7, 5, 25, 0.2)`,
+        },
+    },
+
+    inner: {
+        display: 'flex',
+
+        [theme.fn.smallerThan('md')]: {
+            flexDirection: 'column',
+        },
+    },
+
+    image: {
+        [theme.fn.smallerThan('md')]: {
+            display: 'none',
+        },
+    },
+    container: {
+        marginTop: 'auto',
+        marginLeft: 0,
+        paddingLeft: theme.spacing.xl * 4,
+
+        [theme.fn.smallerThan('md')]: {
+            paddingLeft: theme.spacing.sm * 2,
+            paddingRight: theme.spacing.sm * 2,
+        },
+    },
+    content: {
+        paddingTop: theme.spacing.xl * 2,
+        paddingBottom: theme.spacing.xl * 2,
+        marginRight: theme.spacing.xl * 3,
+
+        [theme.fn.smallerThan('md')]: {
+            marginRight: 0,
+        },
+    },
+
+    title: {
+        fontWeight: 900,
+        lineHeight: 1.05,
+        maxWidth: 500,
+        fontSize: 48,
+        display: 'flex',
+        alignItems: 'center',
+
+        [theme.fn.smallerThan('md')]: {
+            maxWidth: '100%',
+            fontSize: 34,
+            lineHeight: 1.15,
+        },
+    },
+
+    description: {
+        color: theme.white,
+        opacity: 0.75,
+        maxWidth: 500,
+
+        [theme.fn.smallerThan('md')]: {
+            maxWidth: '100%',
+        },
+    },
+
+    control: {
+        paddingLeft: 50,
+        paddingRight: 50,
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        fontSize: 22,
+
+        [theme.fn.smallerThan('md')]: {
+            width: '100%',
+        },
+    },
+
+    line: {
+        color: theme.colors.yellow[6],
+    },
+
+    info: {
+        ':after': {
+            content: '" ▪ "',
+        },
+    },
+    buttonContainer: {
+        display: 'flex',
+    },
+    tagContainer: {
+        [theme.fn.smallerThan('md')]: {
+            display: 'none',
+        },
+    },
+    infoContainer: {
+        [theme.fn.smallerThan('md')]: {
+            display: 'none',
+        },
+    },
+}));
+
+export const MainHero = ({ backgroundImage = '', backgroundBanner = '' }) => {
+    const { classes } = useStyles();
+    const [wordCount, setWordCount] = useState(0);
+    const [heroBackgroundImage, setHeroBackgroundImage] = useState('');
+
+    const mobile = useMediaQuery('(min-width: 0px) and (max-width: 576px)');
+    const tablet = useMediaQuery('(min-width: 577px) and (max-width: 768px)');
+    const fullhd = useMediaQuery('(min-width: 769px) and (max-width: 992px)');
+
+    useEffect(() => {
+        if (fullhd) {
+            setWordCount(500);
+            setHeroBackgroundImage(backgroundImage);
+        } else if (tablet) {
+            setWordCount(350);
+            setHeroBackgroundImage(backgroundImage);
+        } else if (mobile) {
+            setWordCount(200);
+            setHeroBackgroundImage(backgroundBanner);
+        } else {
+            setWordCount(500); // This is the normal one
+            setHeroBackgroundImage(backgroundImage); // This is the normal one
+        }
+    }, [fullhd, tablet, mobile, backgroundBanner, backgroundImage]);
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    setTimeout(() => {
+        if (isLoading) {
+            // setIsLoading(false);
+        }
+    }, 10000);
+
+    return (
+        <div
+            className={classes.root}
+            style={{
+                backgroundImage: `url('${heroBackgroundImage}')`,
+            }}
+        >
+            <Navbar />
+
+            <Container size="lg" className={classes.container}>
+                <div className={classes.inner}>
+                    <div className={classes.content}>
+                        <Title className={classes.title}>
+                            {isLoading ? (
+                                <>
+                                    <Skeleton
+                                        height={20}
+                                        width={120}
+                                    ></Skeleton>
+                                </>
+                            ) : (
+                                <>
+                                    <Text
+                                        component="span"
+                                        size="xl"
+                                        weight="bold"
+                                        color="yellow"
+                                        sx={(theme) => ({
+                                            [theme.fn.smallerThan('sm')]: {
+                                                fontSize: theme.fontSizes.sm,
+                                            },
+                                        })}
+                                    >
+                                        Featured
+                                    </Text>
+                                </>
+                            )}
+
+                            <Space w="sm" />
+                            {isLoading ? (
+                                <>
+                                    <Skeleton height={20} width={60}></Skeleton>
+                                </>
+                            ) : (
+                                <>
+                                    <div
+                                        className={classes.line}
+                                        style={{
+                                            display: 'inline-block',
+                                            width: '60px',
+                                            borderTop: '4px solid',
+                                            borderRadius: 10,
+                                        }}
+                                    />
+                                </>
+                            )}
+                        </Title>
+                        <Title order={1}>
+                            {isLoading ? (
+                                <>
+                                    <Skeleton mt="lg" width={180} height={50} />
+                                </>
+                            ) : (
+                                <>
+                                    <Text
+                                        size="lg"
+                                        color="white"
+                                        inherit
+                                        sx={(theme) => ({
+                                            [theme.fn.smallerThan('sm')]: {
+                                                fontSize: 30, // Fix Me
+                                            },
+                                        })}
+                                    >
+                                        Hyouka
+                                    </Text>
+                                </>
+                            )}
+                        </Title>
+                        <Title className={classes.infoContainer}>
+                            {isLoading ? (
+                                <>
+                                    <Skeleton mt="lg" width={270} height={20} />
+                                </>
+                            ) : (
+                                <>
+                                    <Text
+                                        className={classes.info}
+                                        component="span"
+                                        color="white"
+                                    >
+                                        TV
+                                    </Text>
+                                    <Text
+                                        className={classes.info}
+                                        component="span"
+                                        color="white"
+                                    >
+                                        22 eps
+                                    </Text>
+                                    <Text
+                                        className={classes.info}
+                                        component="span"
+                                        color="white"
+                                    >
+                                        Completed
+                                    </Text>
+                                    <Text
+                                        className={classes.info}
+                                        component="span"
+                                        color="white"
+                                    >
+                                        Spring 2012
+                                    </Text>
+                                    <Text component="span" color="white">
+                                        Kyoto Animations
+                                    </Text>
+                                </>
+                            )}
+                        </Title>
+                        <Space h="md" />
+                        <>
+                            {isLoading ? (
+                                <>
+                                    <Skeleton
+                                        mt="sm"
+                                        width="60vw"
+                                        height={60}
+                                    />
+                                </>
+                            ) : (
+                                <>
+                                    <Text color="gray">
+                                        {voca.truncate(
+                                            `Energy-conservative high school student Houtarou Oreki ends up with more than he bargained for when he signs up for the Classic Literature Club at his sister's behest—especially when he realizes how deep-rooted the club's history really is. Begrudgingly, Oreki is dragged into an...`,
+                                            wordCount
+                                        )}
+                                    </Text>
+                                </>
+                            )}
+                        </>
+                        <Space h="xl" />
+                        <div className={classes.tagContainer}>
+                            {isLoading ? (
+                                <>
+                                    <Skeleton height={24} width="50vw" />
+                                </>
+                            ) : (
+                                <>
+                                    <Badge
+                                        component="span"
+                                        size="lg"
+                                        radius="sm"
+                                        variant="filled"
+                                        mr="md"
+                                        sx={(theme) => ({
+                                            backgroundColor:
+                                                theme.colors.blue[9],
+                                        })}
+                                    >
+                                        Mystery
+                                    </Badge>
+                                    <Badge
+                                        component="span"
+                                        size="lg"
+                                        radius="sm"
+                                        variant="filled"
+                                        mr="md"
+                                        sx={(theme) => ({
+                                            backgroundColor:
+                                                theme.colors.blue[9],
+                                        })}
+                                    >
+                                        Slice of Life
+                                    </Badge>
+                                </>
+                            )}
+                        </div>
+                        <Space h="xl" />
+                        <div className={classes.buttonContainer}>
+                            {isLoading ? (
+                                <>
+                                    <Skeleton width="60px" />
+                                </>
+                            ) : (
+                                <>
+                                    <Button
+                                        color="yellow"
+                                        sx={(theme) => ({
+                                            backgroundColor:
+                                                theme.colors.yellow[9],
+                                            height: 60,
+
+                                            [theme.fn.largerThan('sm')]: {
+                                                width: 60,
+                                            },
+                                        })}
+                                        radius="lg"
+                                    >
+                                        <img
+                                            src="/icons/play.svg"
+                                            width={24}
+                                            height={24}
+                                        />
+                                        <Text color="dark" weight={700}>
+                                            Watch
+                                        </Text>
+                                    </Button>
+                                </>
+                            )}
+                            {isLoading ? (
+                                <>
+                                    <Skeleton ml="xl" height={60} width={120} />
+                                </>
+                            ) : (
+                                <>
+                                    <Button
+                                        ml="xl"
+                                        color="yellow"
+                                        variant="outline"
+                                        sx={(theme) => ({
+                                            borderWidth: 4,
+                                            height: 60,
+                                            borderColor: theme.colors.yellow[9],
+                                        })}
+                                        radius="lg"
+                                        rightIcon={
+                                            <img src="/icons/chevrons-right.svg" />
+                                        }
+                                    >
+                                        <Text
+                                            weight={700}
+                                            size="lg"
+                                            color="yellow"
+                                        >
+                                            Details
+                                        </Text>
+                                    </Button>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
