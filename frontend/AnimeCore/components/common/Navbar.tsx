3b9fb3d5a329da3f63255a631beffb21e4c3de7d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-09 22:43:32 +0600

    add

---
+++ frontend/AnimeCore/components/common/Navbar.tsx
@@ -0,0 +1,73 @@
+import { createStyles, Grid } from '@mantine/core';
+import { memo } from 'react';
+
+const useStyles = createStyles((theme) => ({
+    grid: {
+        paddingLeft: theme.spacing.xl * 3,
+        paddingRight: theme.spacing.xl * 2,
+
+        [theme.fn.smallerThan('md')]: {
+            paddingLeft: theme.spacing.sm * 2,
+            paddingRight: theme.spacing.sm * 2,
+        },
+    },
+    profile: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    avatar: {
+        cursor: 'pointer',
+    },
+    logo: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+
+    search: {
+        display: 'flex',
+        alignItems: 'center',
+
+        [theme.fn.smallerThan('md')]: {
+            display: 'none',
+        },
+    },
+}));
+
+const Navbar = () => {
+    const { classes } = useStyles();
+    return (
+        <Grid justify="space-between" className={classes.grid} grow>
+            <Grid.Col span={4} className={classes.search}>
+                <img alt="" src="/icons/search.svg" width={30} height={30} />
+            </Grid.Col>
+            <Grid.Col span={4} className={classes.logo}>
+                <img
+                  alt=""
+                  src="/logos/animecore_logo.svg"
+                  width={158}
+                  height={22}
+                />
+                <img
+                  style={{
+                        transform: 'translateY(5px)',
+                        color: '#AFAFAF',
+                    }}
+                  alt=""
+                  src="/icons/chevron-down.svg"
+                  width={20}
+                  height={20}
+                />
+            </Grid.Col>
+            <Grid.Col span={4} className={classes.profile}>
+                <img
+                  className={classes.avatar}
+                  alt=""
+                  src="/images/(Avatar)-placeholder.png"
+                  width={50}
+                  height={50}
+                />
+            </Grid.Col>
+        </Grid>
+    );
+};
+export default memo(Navbar);
