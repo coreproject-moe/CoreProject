974234b4d01c40ce685522838d7287894877b110
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-15 13:00:34 +0600

    fix?

--- frontend/AnimeCore/pages/_document.tsx
+++ frontend/AnimeCore/pages/_document.tsx
@@ -3,7 +3,12 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class _Document extends Document {
     render() {
         return (
-            <Html lang="en">
+            <Html
+                lang="en"
+                style={{
+                    backgroundColor: 'black',
+                }}
+            >
                 <Head>
                     <link rel="icon" href="/logos/favicon.svg" />
                 </Head>
