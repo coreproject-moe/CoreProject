70123fccbb3845c8314c3b79038ae202352ed520
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-17 12:48:06 +0600

    Update svelte.config.js

--- frontend/AnimeCore/svelte.config.js
+++ frontend/AnimeCore/svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
-import vercel from "@sveltejs/adapter-static";
+import vercel from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
