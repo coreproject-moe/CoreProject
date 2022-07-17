1b4b784279c295448c4f14f6e0f57568bdc03d99
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-17 08:47:28 +0600

    add

--- frontend/AnimeCore/svelte.config.js
+++ frontend/AnimeCore/svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,20 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			// if true, will deploy the app using edge functions
+			// (https://vercel.com/docs/concepts/functions/edge-functions)
+			// rather than serverless functions
+			edge: false,
+
+			// an array of dependencies that esbuild should treat
+			// as external when bundling functions
+			external: [],
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
 	}
 };
 
