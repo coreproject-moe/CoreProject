d00592f1eb644d91e2849f1e3a6e69c18c4f52a8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-17 12:46:18 +0600

    Update svelte.config.js

--- frontend/AnimeCore/svelte.config.js
+++ frontend/AnimeCore/svelte.config.js
@@ -14,7 +14,20 @@ const config = {
     ],
     kit: {
         // adapter: adapter({ fallback: "app.html" })
-        adapter: vercel({})
+        adapter: vercel({
+            // if true, will deploy the app using edge functions
+            // (https://vercel.com/docs/concepts/functions/edge-functions)
+            // rather than serverless functions
+            edge: false,
+
+            // an array of dependencies that esbuild should treat
+            // as external when bundling functions
+            external: [],
+
+            // if true, will split your app into multiple functions
+            // instead of creating a single one for the entire app
+            split: false
+        })
     }
 };
 
