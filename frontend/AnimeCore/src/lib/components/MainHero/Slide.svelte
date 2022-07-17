df5c8e41b1a0e3a69f5c2a916ca229193bc5a5e2
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-17 19:45:25 +0600

    Update Slide.svelte

--- frontend/AnimeCore/src/lib/components/MainHero/Slide.svelte
+++ frontend/AnimeCore/src/lib/components/MainHero/Slide.svelte
@@ -36,47 +36,47 @@
         </div>
     </div>
     {#if rootSwiper?.activeIndex === 0}
-    <div class="pl-24 hero-content text-neutral-content text-white justify-self-start">
-        <div class="max-w-prose">
-            <div class="text-secondary text-lg font-bold pb-3 flex gap-2">
-                Featured
-                <span class='flex items-center' >
-                    <span
-                    style="display: inline-block; width: 60px; border-top: 4px solid; border-radius: 10px;"
-                />
-                </span>
-            </div>
-            <h1 class="mb-4 text-6xl font-bold max-h-16 h-20 overflow-hidden hover:overflow-y-auto ">{animeTitle}</h1>
-            <h1 class='font-bold py-8 hidden md:flex'>
-                <span class="items pr-2">TV</span><span
-                    class="items pr-2">{animeEpisodeCount} eps</span
-                ><span class="items pr-2">Completed</span><span
-                    class="items pr-2">{animeAirTime}</span
-                ><span>{animeStudio}</span>
+        <div class="pl-10 md:pl-24 hero-content text-neutral-content text-white justify-self-start">
+            <div class="max-w-prose">
+                <div class="text-secondary text-lg font-bold pb-3 flex gap-2">
+                    Featured
+                    <span class='flex items-center' >
+                        <span
+                        style="display: inline-block; width: 60px; border-top: 4px solid; border-radius: 10px;"
+                    />
+                    </span>
+                </div>
+                <h1 class="mb-4 text-6xl font-bold max-h-16 h-20 overflow-hidden hover:overflow-y-auto ">{animeTitle}</h1>
+                <h1 class='font-bold py-8 hidden md:flex'>
+                    <span class="items pr-2">TV</span><span
+                        class="items pr-2">{animeEpisodeCount} eps</span
+                    ><span class="items pr-2">Completed</span><span
+                        class="items pr-2">{animeAirTime}</span
+                    ><span>{animeStudio}</span>
 
-            </h1>
-            <div class="mb-5 max-h-24 text-ellipsis overflow-hidden hover:overflow-y-auto" on:mouseenter={disableScroll} on:mouseleave={enableScroll}  >
-                <div class="pr-3 font-normal text-gray-400">{animeSummary}</div>
-            </div>
-            <div class='flex gap-4 pt-3'>
-                {#each tags as tag}
-                    <span class="badge bg-base-100 badge-lg rounded-md text-white">{tag}</span>
-                {/each}
-            </div>
-            <div class='mt-6 flex gap-4'>
-                <button class="btn btn-lg btn-secondary rounded-[16px] px-5 "><img alt="" src="/icons/play.svg" width="24" height="24"></button>
-                <button class="btn btn-secondary btn-lg btn-outline border-4 rounded-[16px]">
-                    <div class="text-lg font-bold flex gap-2">
-                        Details
-                        <span class='flex items-center' >
-                            <img alt="" src="/icons/chevrons-right.svg" width="24" height="24"/>
-                        </span>
-                    </div>
+                </h1>
+                <div class="mb-5 max-h-24 text-ellipsis overflow-hidden hover:overflow-y-auto" on:mouseenter={disableScroll} on:mouseleave={enableScroll}  >
+                    <div class="pr-3 font-normal text-gray-400">{animeSummary}</div>
+                </div>
+                <div class='flex gap-4 pt-3'>
+                    {#each tags as tag}
+                        <span class="badge bg-base-100 badge-lg rounded-md text-white">{tag}</span>
+                    {/each}
+                </div>
+                <div class='mt-6 flex gap-4'>
+                    <button class="btn btn-lg btn-secondary rounded-[16px] px-5 "><img alt="" src="/icons/play.svg" width="24" height="24"></button>
+                    <button class="btn btn-secondary btn-lg btn-outline border-4 rounded-[16px]">
+                        <div class="text-lg font-bold flex gap-2">
+                            Details
+                            <span class='flex items-center' >
+                                <img alt="" src="/icons/chevrons-right.svg" width="24" height="24"/>
+                            </span>
+                        </div>
 
-                </button>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
     {:else}
         <div></div>
     {/if}
