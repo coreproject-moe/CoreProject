e685b56b8cefc27892338a9946233d0168bc18a1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-17 19:41:09 +0600

    Update postcss.config.cjs

--- frontend/AnimeCore/postcss.config.cjs
+++ frontend/AnimeCore/postcss.config.cjs
@@ -1,6 +1,5 @@
 const cssnano = require("cssnano");
 const postcss_preset_env = require("postcss-preset-env");
-const purgecss = require("@fullhuman/postcss-purgecss");
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 
@@ -18,16 +17,6 @@ const config = {
             autoprefixer: false,
             preset: ["default"]
         }),
-        // Run on buld
-        !dev &&
-            purgecss({
-                content: ["./src/**/**/*.{svelte,html,js,ts}"],
-                defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-                safelist: {
-                    deep: [],
-                    greedy: [/swiper/, /svelte/, /data-theme$/]
-                }
-            }),
         tailwindcss({}),
         autoprefixer({})
     ]
