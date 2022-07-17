f37fcedbec889ce9bbd03b70c67a1ab71e71af93
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-17 09:40:45 +0600

    add

--- frontend/AnimeCore/tailwind.config.cjs
+++ frontend/AnimeCore/tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
+    content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
         fontFamily: {
-            sans: [`Kokoro`, 'sans-serif']
+            sans: [`Kokoro`, "sans-serif"]
         },
         extend: {}
     },
-    plugins: [require('@tailwindcss/typography'), require('daisyui')]
+    plugins: [require("@tailwindcss/typography"), require("daisyui")]
 };
