125113ea08be39a93412d9c78ad3f92eaaa05248
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-19 11:28:26 +0600

    add

--- frontend/AnimeCore/src/lib/components/common/ScrollArea.svelte
+++ frontend/AnimeCore/src/lib/components/common/ScrollArea.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     let klass = "";
     export { klass as class };
-
-
 </script>
 
 <div
@@ -14,40 +12,37 @@
 </div>
 
 <style lang="scss">
-.scrollbar {
-    scrollbar-width: thin;
-    scrollbar-color: rgba(255, 255, 255, 0.12);
+    .scrollbar {
+        scrollbar-width: thin;
+        scrollbar-color: rgba(255, 255, 255, 0.12);
 
-    /* fill parent */
-    display: block;
-    width: 100%;
-    height: 100%;
+        /* fill parent */
+        display: block;
+        width: 100%;
+        height: 100%;
 
-    /* set to some transparent color */
-    border-color: rgba(255, 255, 255, 0.0);
-    /* here we make the color transition */
-    transition: border-color 0.2s linear;
+        /* set to some transparent color */
+        border-color: rgba(255, 255, 255, 0.0);
+        /* here we make the color transition */
+        transition: border-color 0.2s linear;
 
-    &:hover{
-        /* the color we want the scrollbar on hover */
-        border-color: rgba(255, 255, 255, 0.12);
+        &:hover{
+            /* the color we want the scrollbar on hover */
+            border-color: rgba(255, 255, 255, 0.12);
+        }
+
+        &::-webkit-scrollbar,
+        &::-webkit-scrollbar-corner,
+        &::-webkit-scrollbar-thumb {
+            width: 5px;
+            border-radius: 16px;
+            /* add border to act as background-color */
+            border-right-style: inset;
+            /* sum viewport dimensions to guarantee border will fill scrollbar */
+            border-right-width: calc(100vw + 100vh);
+            /* inherit border-color to inherit transitions */
+            border-color: inherit;
+
+        }
     }
-
-    &::-webkit-scrollbar,
-    &::-webkit-scrollbar-corner,
-    &::-webkit-scrollbar-thumb {
-        width: 5px;
-        border-radius: 16px;
-        /* add border to act as background-color */
-        border-right-style: inset;
-        /* sum viewport dimensions to guarantee border will fill scrollbar */
-        border-right-width: calc(100vw + 100vh);
-        /* inherit border-color to inherit transitions */
-        border-color: inherit;
-
-  }
-}
-
-
-
 </style>
