7f5e2437fc6af38c7aeafc57313eb8489a76f7e4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-21 19:45:48 +0600

    add

--- frontend/AnimeCore/postcss.config.cjs
+++ frontend/AnimeCore/postcss.config.cjs
@@ -1,34 +1,27 @@
 const cssnano = require("cssnano");
 const postcss_preset_env = require("postcss-preset-env");
-const purgecss = require("@fullhuman/postcss-purgecss");
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const tailwindNesting = require("@tailwindcss/nesting/index");
+
 const dev = process.env.NODE_ENV === "development";
 
 const config = {
     plugins: [
+        tailwindcss({}),
+        tailwindNesting({}),
+        autoprefixer({}),
         postcss_preset_env({
             stage: 0,
             autoprefixer: {
                 grid: true
-            }
+            },
+            features: { "nesting-rules": false }
         }),
         cssnano({
             autoprefixer: false,
             preset: ["default"]
-        }),
-        // Run on buld
-        !dev &&
-        purgecss({
-            content: ["./src/**/**/*.{svelte,html,js,ts}"],
-            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-            safelist: {
-                deep: [],
-                greedy: [/swiper/, /svelte/, /data-theme$/]
-            }
-        }),
-        tailwindcss({}),
-        autoprefixer({})
+        })
     ]
 };
 
