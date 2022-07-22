722e6299ac4ade17e915971e3eb201f654d8b103
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-22 22:29:05 +0600

    Update Slide.svelte

--- frontend/AnimeCore/src/lib/components/Library/Slide.svelte
+++ frontend/AnimeCore/src/lib/components/Library/Slide.svelte
@@ -1,9 +1,59 @@
-<div class="hero min-h-[20vh] md:min-h-screen bg-base-200">
+<script lang='ts'>
+    import ChevronDown from "$icons/Chevron-Down.svelte";
+import Play from "$icons/Play.svelte";
+</script>
+
+<div class="hero min-h-[20vh] md:min-h-screen bg-base-100">
     <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
+        <div class="flex flex-col gap-3">
+            <p class='text-xl font-bold flex'>Latest Episode</p>
+            <p class='flex gap-2'>show from my list only <ChevronDown height={25} width={25} /></p>
+            {#each Array(5) as item}
+                <div class="w-80 h-24 rounded-xl bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-around">
+                    <div class='flex flex-col items-start'>
+                        <p class='font-bold'>Spy x Family</p>
+                        <p>Ep 06</p>
+                    </div>
+
+                    <button class="btn btn-circle btn-md btn-warning">
+                        <Play width={20} height={20} />
+                    </button>
+                </div>
+            {/each} 
+            <div class="btn-group justify-center">
+                <button class="btn">1</button>
+                <button class="btn">2</button>
+                <button class="btn btn-disabled">...</button>
+                <button class="btn">99</button>
+                <button class="btn">100</button>
+              </div>
+        </div>
+        <div class="divider lg:divider-horizontal"></div> 
+        <div class="flex flex-col">
+            <p class='font-bold text-3xl'>Continue Watching</p>
+            <div class="carousel carousel-center rounded-box">
+                <div class="carousel-item">
+                  <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+                </div> 
+                <div class="carousel-item">
+                  <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+                </div> 
+                <div class="carousel-item">
+                  <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+                </div> 
+                <div class="carousel-item">
+                  <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+                </div> 
+                <div class="carousel-item">
+                  <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+                </div> 
+                <div class="carousel-item">
+                  <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+                </div> 
+                <div class="carousel-item">
+                  <img src="https://placeimg.com/400/300/arch" alt="Pizza" />
+                </div>
+              </div>
+        </div>
     </div>
-  </div>
+</div>
