628337758b37e4fe6fc58ade3c5b2027b11dae9c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-22 16:56:54 +0600

    add

--- frontend/AnimeCore/src/routes/__layout.svelte
+++ frontend/AnimeCore/src/routes/__layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+import { onMount } from "svelte";
+
     import "../app.scss";
+
+    onMount(()=>{
+        document?.querySelectorAll("#loader").forEach(e=>e.remove())
+    })
 </script>
 
 <slot />
