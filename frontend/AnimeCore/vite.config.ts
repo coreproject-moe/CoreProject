1375931b4b956dc177f46ebd961bfa66f2d143a8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-07-26 18:59:56 +0600

    Update vite.config.ts

--- frontend/AnimeCore/vite.config.ts
+++ frontend/AnimeCore/vite.config.ts
@@ -4,18 +4,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [sveltekit()],
-    // legacy: { buildSsrCjsExternalHeuristics: true }, // https://github.com/sveltejs/kit/issues/5549
+    legacy: { buildSsrCjsExternalHeuristics: true }, // https://github.com/sveltejs/kit/issues/5549
     resolve: {
         alias: {
-            // these are the aliases and paths to them
             $store: path.resolve("./src/lib/store"),
             $hooks: path.resolve("./src/hooks"),
             $components: path.resolve("./src/lib/components"),
             $icons: path.resolve("./src/lib/icons"),
             $functions: path.resolve("./src/lib/functions")
         }
-    },
-    optimizeDeps: {
-        include: ["swiper"]
     }
 });
