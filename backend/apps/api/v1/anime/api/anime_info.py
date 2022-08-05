57753b3fd18da4d08b9cc3af6c53a5ec855009e7
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-05 21:23:10 +0600

    add

--- backend/apps/api/v1/anime/api/anime_info.py
+++ backend/apps/api/v1/anime/api/anime_info.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchQuery
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from ninja import Query, Router
@@ -15,43 +15,43 @@ router = Router()
 @paginate
 def get_anime_info(request, filters: AnimeInfoFilters = Query(...)):
     query_dict = filters.dict(exclude_none=True)
-    query_object = Q()
+    query_object = SearchQuery()
 
     # Django filters anyone?
     anime_name = query_dict.get("anime_name", None)
     if anime_name:
         query_object &= (
-            Q(anime_name__icontains=anime_name)
-            | Q(anime_name_japanese__icontains=anime_name)
-            | Q(anime_name_synonyms__name__icontains=anime_name)
+            SearchQuery(anime_name__icontains=anime_name)
+            | SearchQuery(anime_name_japanese__icontains=anime_name)
+            | SearchQuery(anime_name_synonyms__name__icontains=anime_name)
         )
 
     anime_genres = query_dict.get("anime_genres", None)
     if anime_genres:
-        query = Q()
+        query = SearchQuery()
         for position in anime_genres.split(","):
-            query |= Q(anime_genres__name__icontains=position)
+            query |= SearchQuery(anime_genres__name__icontains=position)
         query_object &= query
 
     anime_themes = query_dict.get("anime_themes", None)
     if anime_themes:
-        query = Q()
+        query = SearchQuery()
         for position in anime_themes.split(","):
-            query |= Q(anime_themes__name__icontains=position)
+            query |= SearchQuery(anime_themes__name__icontains=position)
         query_object &= query
 
     anime_studios = query_dict.get("anime_studios", None)
     if anime_studios:
-        query = Q()
+        query = SearchQuery()
         for position in anime_themes.split(","):
-            query |= Q(anime_studios__name__icontains=position)
+            query |= SearchQuery(anime_studios__name__icontains=position)
         query_object &= query
 
     anime_producer = query_dict.get("anime_producer", None)
     if anime_producer:
-        query = Q()
+        query = SearchQuery()
         for position in anime_themes.split(","):
-            query |= Q(anime_producers__name__icontains=position)
+            query |= SearchQuery(anime_producers__name__icontains=position)
         query_object &= query
 
     # 2 Step get query
