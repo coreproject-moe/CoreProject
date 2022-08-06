626fb9d6f93730fa5e3fb35cecf41f083af1d332
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-06 23:03:45 +0600

    Update anime_character.py

--- backend/apps/api/v1/anime/api/anime_character.py
+++ backend/apps/api/v1/anime/api/anime_character.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import get_list_or_404, get_object_or_404
 from ninja import Router
 
-from ..models import AnimeInfoModel
+from ..models import AnimeInfoModel, AnimeCharacterModel
 from ..schemas import AnimeCharacterSchema
 
 router = Router()
@@ -15,3 +15,19 @@ def get_individual_anime_character_info(request: HttpRequest, anime_id: int):
     )
 
     return query
+
+
+@router.post("/info/{int:anime_id}/character", response=list[AnimeCharacterSchema])
+def post_individual_anime_character_info(
+    request: HttpRequest, anime_id: int, payload: AnimeCharacterSchema
+):
+    # Set this at top
+    # Because if there is no anime_info_model with corresponding query theres no point in  continuing
+    anime_info_model = get_object_or_404(AnimeInfoModel, pk=anime_id)
+
+    instance, created = AnimeCharacterModel.objects.get_or_create(
+        **payload.dict(),
+    )
+    anime_info_model.anime_genres.add(instance)
+
+    return instance
