19f84b6ebfd0bcfcb5ec2b4c10d3384e50d2022a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-07 12:36:30 +0600

    add

---
+++ backend/apps/api/v1/characters/api.py
@@ -0,0 +1,21 @@
+from ninja import Router
+from ninja.pagination import paginate
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
+from .models import CharacterModel
+from .schemas import CharacterSchema
+
+router = Router(tags=["characters"])
+
+
+@router.get("", response=list[CharacterSchema])
+@paginate
+def get_character_info(request: HttpRequest):
+    query = CharacterModel.objects.all()
+    return query
+
+
+@router.get("/{str:character_id}/", response=CharacterSchema)
+def get_individual_character_info(request: HttpRequest, character_id: str):
+    queryset = get_object_or_404(CharacterModel, id=character_id)
+    return queryset
