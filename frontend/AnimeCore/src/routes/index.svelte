d0466aaddceca2df1b38842e60205496a1b6ee7e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-12 01:50:22 +0600

    Revert "add"
    This reverts commit 4be1a2dbaf928da3bf4cd2106439a443eae2a553.

--- frontend/AnimeCore/src/routes/index.svelte
+++ frontend/AnimeCore/src/routes/index.svelte
@@ -15,6 +15,7 @@
 
 <div
     class="h-screen w-screen carousel carousel-vertical text-white"
+    style="overscroll-behavior-block: contain"
 >
     <div class="carousel-item h-auto w-auto inline-grid">
         {#each data as item, index}
