4aeff362a4f52c0556ce4372769f15cdefeeb5ba
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-12 01:34:31 +0600

    Update index.svelte

--- frontend/AnimeCore/src/routes/index.svelte
+++ frontend/AnimeCore/src/routes/index.svelte
@@ -13,7 +13,10 @@
     <title>AnimeCore</title>
 </svelte:head>
 
-<div class="h-screen w-screen carousel carousel-vertical text-white overscroll-y-contain">
+<div
+    class="h-screen w-screen carousel carousel-vertical text-white"
+    style="overscroll-behavior-block: contain"
+>
     <div class="carousel-item h-auto w-auto inline-grid">
         {#each data as item, index}
             {#if index === mainHeroSlideActiveIndex}
