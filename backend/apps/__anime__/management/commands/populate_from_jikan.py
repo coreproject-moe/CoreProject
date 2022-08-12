2be759a267a1c9868bdf151dc3bf41d91ac040c0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-12 22:16:19 +0600

    Update populate_from_jikan.py

--- backend/apps/__anime__/management/commands/populate_from_jikan.py
+++ backend/apps/__anime__/management/commands/populate_from_jikan.py
@@ -1,4 +1,6 @@
+from email.mime import image
 from io import BytesIO
+from typing import Dict
 
 from apps.api.v1.anime.models import CharacterModel
 from core.requests import CachedLimiterSession
@@ -8,6 +10,7 @@ from requests.adapters import HTTPAdapter
 from requests_cache import RedisCache
 from requests_ratelimiter import RedisBucket
 from urllib3.util import Retry
+from django.db import IntegrityError
 
 retry_strategy = Retry(
     total=3,
@@ -35,51 +38,6 @@ class Command(BaseCommand):
         self.session.mount("https://", adapter)
         self.session.mount("http://", adapter)
 
-    def populate_anime_characters(self) -> None:
-        res = self.session.get(
-            f"https://api.jikan.moe/v4/characters/{self.character_number}"
-        )
-        data = res.json()
-
-        if (
-            res.status_code == 200
-            and data.get("status", None) != 404
-            and data.get("status", None) != "429"
-        ):
-            print(f"Got Character Info for {self.character_number}")
-            DATA = data["data"]
-
-            try:
-                image_url = DATA["images"]["webp"]["image_url"]
-            except KeyError:
-                image_url = DATA["images"]["jpg"]["image_url"]
-            finally:
-                character_image = self.session.get(image_url)
-
-            CharacterModel.objects.create(
-                mal_id=self.character_number,
-                name=DATA["name"],
-                name_kanji=DATA.get("name_kanji", None),
-                character_image=ContentFile(
-                    BytesIO(character_image.content).read(),
-                    f"{self.character_number}.{image_url.split('.')[-1]}",
-                ),
-                about=DATA["about"],
-            )
-        else:
-            print(f"Missed info for {self.character_number}")
-
-        self.character_number += 1
-
-        # We are taking 60,000 characters
-        if self.character_number > self.character_number_end:
-            import sys
-
-            sys.exit(0)
-
-        # Restart Function
-        self.populate_anime_characters()
-
     def add_arguments(self, parser):
         parser.add_argument(
             "character-number-start",
@@ -94,6 +52,70 @@ class Command(BaseCommand):
             default=100_000,
             help="Specifies the stopping number for character.",
         )
+
+    def get_character_data_from_jikan(self) -> Dict[str, str]:
+        res = self.session.get(
+            f"https://api.jikan.moe/v4/characters/{self.character_number}"
+        )
+        data = res.json()
+
+        if (
+            res.status_code == 200
+            and data.get("status", None) != 404
+            and data.get("status", None) != "429"
+        ):
+            print(f"Got Character Info for {self.character_number}")
+
+            data = data["data"]
+
+        else:
+            print(f"Missed info for {self.character_number}")
+            data = None
+
+        return data
+
+    def populate_anime_characters(self) -> None:
+        DATA = self.get_character_data_from_jikan()
+        if not DATA:
+            self.character_number += 1
+            if self.character_number > self.character_number_end:
+                import sys
+
+                sys.exit(0)
+
+            # Restart Function
+            self.populate_anime_characters()
+    
+        try:
+            image_url = DATA["images"]["webp"]["image_url"]
+        except KeyError:
+            image_url = DATA["images"]["jpg"]["image_url"]
+        finally:
+            character_image = self.session.get(image_url)
+
+        try:
+            CharacterModel.objects.create(
+                mal_id=self.character_number,
+                name=DATA["name"],
+                name_kanji=DATA.get("name_kanji", None),
+                character_image=ContentFile(
+                    BytesIO(character_image.content).read(),
+                    f"{self.character_number}.{image_url.split('.')[-1]}",
+                ),
+                about=DATA["about"],
+            )
+        except IntegrityError:
+            print(f"Entry exists : {self.character_number}")
+
+        finally:
+            self.character_number += 1
+            if self.character_number > self.character_number_end:
+                import sys
+
+                sys.exit(0)
+
+            # Restart Function
+            self.populate_anime_characters()
 
     def handle(self, *args, **options):
         self.character_number = options["character-number-start"]
