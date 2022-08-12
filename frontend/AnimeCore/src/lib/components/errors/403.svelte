95526333c4634da173cadd901bdd9aad5248c9fe
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-12 12:20:30 +0600

    add

--- frontend/AnimeCore/src/lib/components/errors/403.svelte
+++ frontend/AnimeCore/src/lib/components/errors/403.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-    export let status: string;
-    export let errorMessage: string;
-    export let errorText: string;
-
-    <script lang="ts">
+    /* eslint-disable */
     export let status: string;
     export let errorMessage: string;
     export let errorText: string;
@@ -12,7 +8,7 @@
 </script>
 
 <div class="flex items-center justify-center h-screen flex-col text-center gap-12">
-    <Anole width="321" height="118" />
+    <Alone width="321" height="118" />
     <h1 class="text-indigo-700 text-3xl">
         <b>{status}</b>
         -
