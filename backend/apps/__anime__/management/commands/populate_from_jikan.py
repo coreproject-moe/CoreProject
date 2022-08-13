248b884895eaab5a7972471acbfa619558af2b07
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-13 15:33:18 +0600

    Rewrite some codes

--- backend/apps/__anime__/management/commands/populate_from_jikan.py
+++ backend/apps/__anime__/management/commands/populate_from_jikan.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import os
 
 from apps.api.v1.anime.models import CharacterModel
 from core.requests import CachedLimiterSession
@@ -12,7 +13,7 @@ from urllib3.util import Retry
 
 retry_strategy = Retry(
     total=3,
-    status_forcelist=[429, 500, 502, 503, 504],
+    status_forcelist=[408, 429, 500, 502, 503, 504],
 )
 adapter = HTTPAdapter(max_retries=retry_strategy)
 
@@ -22,10 +23,11 @@ class Command(BaseCommand):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+
         self.session = CachedLimiterSession(
             bucket_class=RedisBucket,
             # Undocumented
-            bucket_kwargs={"bucket_name": "jikan_api"},
+            bucket_kwargs={"bucket_name": "_populate_"},
             backend=RedisCache(),
             # https://docs.api.jikan.moe/#section/Information/Rate-Limiting
             per_minute=60,
@@ -33,6 +35,7 @@ class Command(BaseCommand):
             # https://requests-cache.readthedocs.io/en/stable/user_guide/expiration.html
             expire_after=360,
         )
+
         self.session.mount("https://", adapter)
         self.session.mount("http://", adapter)
 
@@ -60,57 +63,138 @@ class Command(BaseCommand):
         if (
             res.status_code == 200
             and data.get("status", None) != 404
+            and data.get("status", None) != 408
             and data.get("status", None) != "429"
         ):
-            print(f"Got Character Info for {self.character_number}")
-
+            print(f"Got Character Info for {self.character_number} | Jikan")
             data = data["data"]
+            self.character_name = data["name"]
+            self.character_name_kanji = data.get("name_kanji", None)
+            self.character_about = data.get("about", None)
 
+            try:
+                self.image_url = data["images"]["webp"]["image_url"]
+            except KeyError:
+                self.image_url = data["images"]["jpg"]["image_url"]
+            finally:
+                self.character_image = self.session.get(self.image_url)
+
+        elif data.get("status", None) == 408:
+            print(f"Mal is Rate-Limiting us | {self.character_number}")
+
+            # Write the number to a file so that we can deal with it later
+            f = open("skipped.txt", "a")
+            f.write(f"{str(self.character_number)}\n")
+            f.close()
+
+            # We need to return None as data
+            data = None
         else:
-            print(f"Missed info for {self.character_number}")
+            print(f"Missed info for {self.character_number} | Jikan")
             data = None
 
         return data
 
+    def get_data_from_kitsu(self) -> None:
+        if not self.character_name:
+            print(f"Skipping | Kitsu")
+            return
+
+        res = self.session.get(
+            f"https://kitsu.io/api/edge/characters?filter[name]={self.character_name}"
+        )
+
+        data = res.json()
+        if res.status_code == 200:
+            print(f"Got Character Info for {self.character_number} | Kitsu")
+
+            data = data["data"][0]
+            self.kitsu_id = data["id"]
+            if not self.character_name_kanji:
+                self.character_name_kanji = data["attributes"]["names"]["ja_jp"]
+
+        else:
+            print(f"Missed info for {self.character_number} | Kitsu")
+
+    def get_data_from_anilist(self) -> None:
+        if not self.character_name:
+            print(f"Skipping | Anilist")
+            return
+
+        query = {
+            "query": "query($page:Int = 1 $id:Int $search:String $isBirthday:Boolean $sort:[CharacterSort]=[FAVOURITES_DESC]){Page(page:$page,perPage:20){pageInfo{total perPage currentPage lastPage hasNextPage}characters(id:$id search:$search isBirthday:$isBirthday sort:$sort){id name{userPreferred}image{large}}}}",
+            "variables": {
+                "page": 1,
+                "type": "CHARACTERS",
+                "search": self.character_name,
+                "sort": "SEARCH_MATCH",
+            },
+        }
+        res = self.session.post(url="https://graphql.anilist.co/", json=query)
+        data = res.json()["data"]
+
+        if data and res.status_code == 200:
+            try:
+                print(f"Got Character Info for {self.character_number} | Anilist")
+                self.anilist_id = data["Page"]["characters"][0]["id"]
+
+            except IndexError:
+                print(f"Entry for {self.character_name} doesn't exist | Anilist")
+
+                # Write the number to a file so that we can deal with it later
+                f = open("anilist.txt", "a")
+                f.write(f"{str(self.character_name)}\n")
+                f.close()
+
+        else:
+            print(f"Missed info for {self.character_number} | Anilist")
+
     def populate_anime_characters(self) -> None:
-        DATA = self.get_character_data_from_jikan()
-        if not DATA:
-            self.after_populate_anime_characters()
+        if self.character_number == 1:
+            # Remove files which will be necessary for logging failed request
+            os.path.exists("skipped.txt") and os.remove("skipped.txt")
+            os.path.exists("anilist.txt") and os.remove("anilist.txt")
 
-        try:
-            image_url = DATA["images"]["webp"]["image_url"]
-        except KeyError:
-            image_url = DATA["images"]["jpg"]["image_url"]
-        finally:
-            character_image = self.session.get(image_url)
+        while self.character_number < self.character_number_end:
+            DATA = self.get_character_data_from_jikan()
+            if not DATA:
+                self.after_populate_anime_characters()
+                continue
 
-        try:
-            CharacterModel.objects.create(
-                mal_id=self.character_number,
-                name=DATA["name"],
-                name_kanji=DATA.get("name_kanji", None),
-                character_image=ContentFile(
-                    BytesIO(character_image.content).read(),
-                    f"{self.character_number}.{image_url.split('.')[-1]}",
-                ),
-                about=DATA["about"],
-            )
+            try:
+                database = CharacterModel.objects.create(
+                    mal_id=self.character_number,
+                    name=self.character_name,
+                    name_kanji=self.character_name_kanji,
+                    character_image=ContentFile(
+                        BytesIO(self.character_image.content).read(),
+                        f"{self.character_number}.{self.image_url.split('.')[-1]}",
+                    ),
+                    about=self.character_about,
+                )
+                # Lazy query
+                self.get_data_from_kitsu()
+                self.get_data_from_anilist()
 
-        except IntegrityError:
-            print(f"Entry exists : {self.character_number}")
+                database.kitsu_id = self.kitsu_id
+                database.anilist_id = self.anilist_id
 
-        finally:
+                database.save()
+            except IntegrityError:
+                print(f"Entry exists : {self.character_number}")
+
             self.after_populate_anime_characters()
 
     def after_populate_anime_characters(self) -> None:
+        self.kitsu_id = None
+        self.anilist_id = None
+
+        self.character_name = None
+        self.character_name_kanji = None
+        self.chatacter_about = None
+        self.character_image = None
+
         self.character_number += 1
-        if self.character_number > self.character_number_end:
-            import sys
-
-            sys.exit(0)
-
-        # Restart Function
-        self.populate_anime_characters()
 
     def handle(self, *args, **options):
         self.character_number = options["character-number-start"]
