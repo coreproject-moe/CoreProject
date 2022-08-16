70209b7969d4d7b0e63e287db590c15445426198
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-16 21:55:17 +0600

    remove fingerprinting

--- backend/core/utilities/CachedLimiterSession.py
+++ backend/core/utilities/CachedLimiterSession.py
@@ -1,6 +1,8 @@
 from requests import Session
 from requests_cache import CacheMixin
 from requests_ratelimiter import LimiterMixin
+from requests.utils import default_headers
+from requests.structures import CaseInsensitiveDict
 
 
 class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
@@ -8,3 +10,13 @@ class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
     Session class with caching and rate-limiting behavior. Accepts arguments for both
     LimiterSession and CachedSession.
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.headers = self.default_headers()
+
+    def default_headers(self) -> CaseInsensitiveDict[str]:
+        """Remove user agent | Why does requests do this?"""
+        previous_dict = default_headers()
+        del previous_dict["User-Agent"]
+        return previous_dict
