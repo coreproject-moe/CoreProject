942a1ebab6ecbf27ff31769d97dfd64464c800fb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-17 10:51:24 +0600

    add

--- backend/apps/__user__/models.py
+++ backend/apps/__user__/models.py
@@ -26,7 +26,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin, ResizeImageMixin):
     )
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), blank=True, unique=True)
+    email = models.EmailField(
+        _("email address"),
+        blank=True,
+        unique=True,
+        help_text=_("Required. A valid email with a valid domain"),
+    )
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
