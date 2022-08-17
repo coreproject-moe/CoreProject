1864eecc203a3c23e46d595f8d1fe0debf22417f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-08-17 13:15:24 +0600

    Update populate_people.py

--- backend/apps/__anime__/management/commands/populate_people.py
+++ backend/apps/__anime__/management/commands/populate_people.py
@@ -25,11 +25,11 @@ class Command(BaseCommand):
     """
     Populates the character database
         How does it achieve this?
-        First we are taking `self.character_number` as a command line argument.
+        First we are taking `self.staff_number` as a command line argument.
             Then using a while loop :
                 1. Requesting the data from `https://api.jikan.moe/v4`
-                2. Using the `character_name` from data(jikan) we are getting `kitsu_id` from kitsu
-                3. Using the `character_name` from data(jikan) we are getting `anilist_id` from anilist
+                2. Using the `staff_name` from data(jikan) we are getting `kitsu_id` from kitsu
+                3. Using the `staff_name` from data(jikan) we are getting `anilist_id` from anilist
                 4. Saving everything to `CharacterModel`
     """
 
@@ -42,14 +42,14 @@ class Command(BaseCommand):
         self.kitsu_not_found_file_name = "kitsu_not_found.txt"
         self.anilist_not_found_file_name = "anilist_not_found.txt"
 
-        self.character_number: int
-        self.character_number_end: int
+        self.staff_number: int
+        self.staff_number_end: int
 
-        self.character_name: str
-        self.character_name_kanji: str
-        self.character_image: BytesIO = BytesIO()
+        self.staff_name: str
+        self.staff_name_kanji: str
+        self.staff_image: BytesIO = BytesIO()
 
-        self.character_about: str
+        self.staff_about: str
         self.image_url: str
 
         # Tried to implement Facade pattern
@@ -70,108 +70,24 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "character-number-start",
+            "staff-number-start",
             nargs="?",
             type=int,
             default=1,
-            help="Specifies the starting number for character.",
+            help="Specifies the starting number for staff.",
         )
         parser.add_argument(
-            "--character-number-end",
+            "--staff-number-end",
             type=int,
             default=100_000,
-            help="Specifies the stopping number for character.",
+            help="Specifies the stopping number for staff.",
         )
 
     def handle(self, *args, **options) -> None:
         """Starting point for our application"""
-        self.character_number = options["character-number-start"]
-        self.character_number_end = options["character_number_end"]
-        self.populate_anime_characters()
-
-    def get_character_data_from_jikan(
-        self,
-        character_number: int,
-        session: CachedLimiterSession,
-    ) -> dict[str, str] | None:
-        """
-        :param character_number: The id of character
-        :param session: Requests instance to get data
-        """
-        res = session.get(f"https://api.jikan.moe/v4/characters/{character_number}")
-        data = res.json()
-        return_data = None
-
-        if res.status_code == 200 and data.get("status", None) not in [404, 408, "429"]:
-            self.stdout.write(f"Got Character Info for {character_number} | Jikan")
-            data = data["data"]
-            self.character_name = data["name"]
-            self.character_name_kanji = data.get("name_kanji", None)
-            self.character_about = data.get("about", None)
-
-            try:
-                self.image_url = data["images"]["webp"]["image_url"]
-            except KeyError:
-                self.image_url = data["images"]["jpg"]["image_url"]
-            finally:
-                image = self.session.get(self.image_url)
-                self.character_image = BytesIO(image.content)
-                return_data = data
-
-        elif data.get("status", None) == 408:
-            self.stdout.write(f"Mal is Rate-Limiting us | {character_number}")
-
-            # Write the number to a file so that we can deal with it later
-            file = open(self.mal_rate_limit_file_name, "a", encoding="utf-8")
-            file.write(f"{str(character_number)}\n")
-            file.close()
-
-        else:
-            self.stdout.write(f"Missed info for {character_number} | Jikan")
-
-        return return_data
-
-    def get_data_from_kitsu(
-        self,
-        character_name: str,
-        character_number: int,
-        session: CachedLimiterSession,
-    ) -> str | None:
-        """
-        :param character_name: The name of the character
-        :param character_number: The id of character
-        :param session: Requests instance to get data
-        """
-
-        res = session.get(
-            f"https://kitsu.io/api/edge/people?filter[name]={character_name}"
-        )
-        data = res.json()
-        kitsu_id = None
-
-        if res.status_code == 200:
-            try:
-                data = data["data"][0]
-
-                # Side Effect
-                if not self.character_name_kanji:
-                    self.character_name_kanji = data["attributes"]["names"]["ja_jp"]
-
-                kitsu_id = data["id"]
-                self.stdout.write(f"Got Character Info for {character_number} | Kitsu")
-
-            except KeyError:
-                self.stdout.write(f"Entry for {character_name} doesn't exist | Kitsu")
-
-                # Write the number to a file so that we can deal with it later
-                file = open(self.kitsu_not_found_file_name, "a", encoding="utf-8")
-                file.write(f"{str(character_name)}\n")
-                file.close()
-
-        else:
-            self.stdout.write(f"Missed info for {character_number} | Kitsu")
-
-        return kitsu_id
+        self.staff_number = options["staff-number-start"]
+        self.staff_number_end = options["staff_number_end"]
+        self.populate_anime_staff()
 
     # Finished
     def get_data_from_anilist(
@@ -182,7 +98,7 @@ class Command(BaseCommand):
     ) -> str | None:
         """
         :param staff_name: The name of the character
-        :param character_number: The id of character
+        :param staff_number: The id of character
         :param session: Requests instance to get data
         """
 
@@ -246,16 +162,16 @@ class Command(BaseCommand):
 
         return anilist_id
 
-    def populate_anime_characters(self) -> None:
+    def populate_anime_staff(self) -> None:
         """
         Function's purpose :
             1. Request the data from `https://api.jikan.moe/v4`
-            2. Using the `character_name` from data(jikan) get `kitsu_id` from kitsu
-            3. Using the `character_name` from data(jikan) get `anilist_id` from anilist
+            2. Using the `staff_name` from data(jikan) get `kitsu_id` from kitsu
+            3. Using the `staff_name` from data(jikan) get `anilist_id` from anilist
             4. Save everything to `CharacterModel`
         """
 
-        if self.character_number == 1:
+        if self.staff_number == 1:
             # If user starts from 0 remove files which are necessary for logging failed request
             files_to_remove = [
                 self.mal_rate_limit_file_name,
@@ -266,47 +182,26 @@ class Command(BaseCommand):
                 if os.path.exists(file):
                     os.remove(file)
 
-        while self.character_number < self.character_number_end:
+        while self.staff_number < self.staff_number_end:
             data = self.get_character_data_from_jikan(
-                self.character_number,
+                self.staff_number,
                 session=self.session,
             )
 
             if data:
                 try:
-                    CharacterModel.objects.update_or_create(
-                        mal_id=self.character_number,
-                        defaults={
-                            "kitsu_id": self.get_data_from_kitsu(
-                                self.character_name,
-                                self.character_number,
-                                self.session,
-                            ),
-                            "anilist_id": self.get_data_from_anilist(
-                                self.character_name,
-                                self.character_number,
-                                self.session,
-                            ),
-                            "name": self.character_name,
-                            "name_kanji": self.character_name_kanji,
-                            "character_image": ContentFile(
-                                self.character_image.read(),
-                                f"{self.character_number}.{self.image_url.split('.')[-1]}",
-                            ),
-                            "about": self.character_about,
-                        },
-                    )
-
+                    pass
+                # Add
                 except IntegrityError as e:
                     print(e)
-                    self.stdout.write(f"Entry exists : {self.character_number}")
+                    self.stdout.write(f"Entry exists : {self.staff_number}")
 
-            self.after_populate_anime_characters()
+            self.after_populate_anime_staff()
 
-    def after_populate_anime_characters(self) -> None:
-        self.character_name = ""
-        self.character_name_kanji = ""
-        self.character_about = ""
-        self.character_image.truncate(0)
+    def after_populate_anime_staff(self) -> None:
+        self.staff_name = ""
+        self.staff_name_kanji = ""
+        self.staff_about = ""
+        self.staff_image.truncate(0)
 
-        self.character_number += 1
+        self.staff_number += 1
