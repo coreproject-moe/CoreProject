611fd850a399c5e92029fbfafaaeeaa216b1b46e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-09-05 10:54:25 +0600

    Update populate_staff.py

--- backend/apps/staff/management/commands/populate_staff.py
+++ backend/apps/staff/management/commands/populate_staff.py
@@ -11,10 +11,9 @@ from requests_cache import RedisCache  # type: ignore
 from requests_ratelimiter import RedisBucket
 from urllib3.util import Retry
 
+# pylint: disable=import-error
 from apps.staff.models import StaffAlternateNameModel, StaffModel
-from core.utilities.CachedLimiterSession import (  # pylint: disable=import-error
-    CachedLimiterSession,
-)
+from core.utilities.CachedLimiterSession import CachedLimiterSession
 
 retry_strategy = Retry(
     total=3,
