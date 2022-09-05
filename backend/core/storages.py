cbc616c162c40b32fce963885aa6782212feb355
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-09-06 00:05:02 +0600

    Update storages.py

--- backend/core/storages.py
+++ backend/core/storages.py
@@ -1,10 +1,8 @@
-from typing import Any
-
 from django.core.files.storage import FileSystemStorage
 
 
 class OverwriteStorage(FileSystemStorage):
     # https://stackoverflow.com/questions/9522759/imagefield-overwrite-image-file-with-same-name
-    def get_available_name(self, *args: Any, **kwargs: Any) -> str:
-        self.delete(args[0])
-        return super().get_available_name(*args, **kwargs)
+    def get_available_name(self, name: str, max_length: int | None = None) -> str:
+        self.delete(name)
+        return super().get_available_name(name, max_length)
