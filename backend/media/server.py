346f814b1fa0f7a14e3b5acab7a6e58f708f6fd6
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-09-13 14:38:55 +0600

    add files

--- backend/media/server.py
+++ backend/media/server.py
@@ -24,6 +24,11 @@ async def hello(
     return response
 
 
-app = web.Application()
-app.add_routes(routes)
-web.run_app(app)
+async def aiohttp_app() -> web.Application:
+    app = web.Application()
+    app.add_routes(routes)
+    return app
+
+
+if __name__ == "__main__":
+    web.run_app(aiohttp_app())
