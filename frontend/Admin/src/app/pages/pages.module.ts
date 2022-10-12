6d676093358344ed7cd4cb5e1f5147622bd17a02
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-10-12 12:06:48 +0600

    add

---
+++ frontend/Admin/src/app/pages/pages.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { NbMenuModule } from '@nebular/theme';
+
+import { ThemeModule } from '../@theme/theme.module';
+import { PagesComponent } from './pages.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ECommerceModule } from './e-commerce/e-commerce.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    NbMenuModule,
+    DashboardModule,
+    ECommerceModule,
+    MiscellaneousModule,
+  ],
+  declarations: [
+    PagesComponent,
+  ],
+})
+export class PagesModule {
+}
