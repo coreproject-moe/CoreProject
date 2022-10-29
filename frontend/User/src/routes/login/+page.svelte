c6eb624e1298db6d1046f3c48b956398fb8bec36
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-10-29 20:06:38 +0600

    Get shits working

--- frontend/User/src/routes/login/+page.svelte
+++ frontend/User/src/routes/login/+page.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
-	let background_image = 'https://placeimg.com/1000/800/arch';
-
 	let CHOICES = [
-		'https://images.unsplash.com/photo-1666688090267-4858c2075629?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60',
+		'https://images.unsplash.com/photo-1666688090267-4858c2075629',
 		'https://placeimg.com/1000/800/arch'
 	];
 	var index = Math.floor(Math.random() * CHOICES.length);
 
+	let CHOICE_NUMBER = 1;
+
 	setInterval(() => {
-		background_image = CHOICES[index];
-	}, 5000);
+		CHOICE_NUMBER = index;
+	}, 1000);
 </script>
 
-<div class="grid h-screen justify-center content-center">
+<div class="grid h-screen w-screen">
+	<!-- Background Image Container -->
 	<div style="grid-area: 1 / 1 / 2 / 2">
-		<div>Hello</div>
+		{#each CHOICES as item}
+			{#if CHOICES.indexOf(item) == CHOICE_NUMBER}
+				<div
+					class="h-screen w-screen bg-no-repeat bg-center bg-cover"
+					style="background-image:url('{item}')"
+				/>
+			{/if}
+		{/each}
 	</div>
-	<div style="grid-area: 1 / 1 / 2 / 2">
+
+	<!-- Login card  -->
+	<div style="grid-area: 1 / 1 / 2 / 2" class="inline-grid justify-center content-center">
 		<div class="card w-96 bg-base-100 shadow-xl">
 			<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
 			<div class="card-body">
