9890a9c1a6d26b153c34262a103c5adbf7f9539a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-10-31 07:43:11 +0600

    Revert "Rework"
    This reverts commit 2dd0194bfb74ce56afb901577b18cf68a4f23dbd.

---
+++ android/AnimeCore/Views/Login/login.py
@@ -0,0 +1,5 @@
+from kivymd.uix.screen import MDScreen
+
+
+class LoginScreen(MDScreen):
+    pass
--- backend/django_core/apps/user/forms.py
+++ backend/django_core/apps/user/forms.py
@@ -2,6 +2,11 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import CustomUser
 
+from django import forms
+from crispy_bootstrap5.bootstrap5 import FloatingField
+from crispy_forms.layout import Layout
+from crispy_forms.helper import FormHelper
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -13,3 +18,20 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email",)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            FloatingField("first_name"),
+            FloatingField("last_name"),
+            FloatingField("email"),
+            FloatingField("username"),
+            FloatingField("username_discriminator"),
+        )
+
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
