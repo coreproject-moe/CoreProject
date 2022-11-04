fc537bfbefcb2c267f7fab568afedffd91841e1d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-04 07:05:08 +0600

    Remove all

--- frontend/AnimeCore/src/lib/icons/Chevron-Down.svelte
+++
@@ -1,9 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" {...$$props} fill="none" viewBox="0 0 24 24">
-    <path
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="3.5"
-        d="M6 9L12 15L18 9"
-    />
-</svg>
--- frontend/AnimeCore/src/routes/mal/[id=integer]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import type { PageData } from "./$types";
-
-    export let data: PageData;
-</script>
-
-<svelte:head>
-    <title>{data.animeData.title_english}</title>
-</svelte:head>
