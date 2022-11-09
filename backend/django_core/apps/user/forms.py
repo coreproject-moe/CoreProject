02006236d2fbcf7049b74906c617eb00d30f07ec
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-09 09:28:43 +0600

    Add items

--- backend/django_core/apps/user/forms.py
+++ backend/django_core/apps/user/forms.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-
+from django.urls import reverse
 from .models import CustomUser
 
 from django import forms
+from crispy_forms.bootstrap import FormActions
 from crispy_bootstrap5.bootstrap5 import FloatingField
-from crispy_forms.layout import Layout, Submit
+from crispy_forms.layout import Layout, Submit, HTML, ButtonHolder
 from crispy_forms.helper import FormHelper
 
 
@@ -35,7 +36,18 @@ class UserRegistrationForm(forms.ModelForm):
             FloatingField("email", required=True),
             FloatingField("username", required=True),
             FloatingField("username_discriminator", required=True),
-            Submit("submit", "Submit", css_class="btn-primary"),
+            FormActions(
+                ButtonHolder(
+                    Submit("submit", "Submit", css_class="btn-warning"),
+                    HTML(
+                        f"""
+                            or
+                            <a class='text-white' href={reverse('login_view')}>Login</a>
+                        """
+                    ),
+                    css_class="d-flex justify-content-center gap-2 align-items-center",
+                )
+            ),
         )
 
     class Meta:
