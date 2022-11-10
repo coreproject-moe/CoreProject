f1a32cfde777efe41a0692449824bec62ed861f3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-10 14:46:56 +0600

    Final add

--- backend/django_core/apps/frontend/views.py
+++ backend/django_core/apps/frontend/views.py
@@ -2,23 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from user_agents import parse
 from ..anime.models import AnimeModel
-from pregex import Pregex
-from pregex.core import cl, gr, op
+
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
-
-MAL_PATH: Pregex = (
-    "/animecore/" + op.Either("mal", "myanimelist") + "/" + gr.Capture(cl.AnyDigit())
-)
-ANILIST_PATH: Pregex = "/animecore/anilist/" + gr.Capture(cl.AnyDigit())
-KISTU_PATH: Pregex = "/animecore/kitsu/" + gr.Capture(cl.AnyDigit())
 
 
 def animecore(request: HttpRequest) -> HttpResponse:
     user_agent_parsed = parse(request.META["HTTP_USER_AGENT"])
 
     if user_agent_parsed.is_bot:
+        """Locally import everything else load time is slow"""
+
+        from pregex import Pregex
+        from pregex.core import cl, gr, op
+
+        MAL_PATH: Pregex = (
+            "/animecore/"
+            + op.Either("mal", "myanimelist")
+            + "/"
+            + gr.Capture(cl.AnyDigit())
+        )
+        ANILIST_PATH: Pregex = "/animecore/anilist/" + gr.Capture(cl.AnyDigit())
+        KISTU_PATH: Pregex = "/animecore/kitsu/" + gr.Capture(cl.AnyDigit())
+
         # /animecore/mal/1
         if myanimelist_path := MAL_PATH.get_captures(request.get_full_path()):
             myanimelist_number = myanimelist_path[0][0]
@@ -52,7 +59,11 @@ def animecore(request: HttpRequest) -> HttpResponse:
                 "opengraph_url": request.build_absolute_uri(),
             }
 
-        return render(request, "frontend/bot_response.html", context=context)
+        return render(
+            request,
+            "frontend/bot_response.html",
+            context=context,
+        )
 
     else:
         return render(request, "frontend/animecore.html")
