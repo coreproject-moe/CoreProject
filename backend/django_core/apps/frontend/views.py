67ac6ae4f4f93f7f0e4be9746ae636818e3223df
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-10 14:40:24 +0600

    Update views.py
    Regex based codes

--- backend/django_core/apps/frontend/views.py
+++ backend/django_core/apps/frontend/views.py
@@ -1,16 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from user_agents import parse
+from ..anime.models import AnimeModel
+from pregex import Pregex
+from pregex.core import cl, gr, op
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+MAL_PATH: Pregex = (
+    "/animecore/" + op.Either("mal", "myanimelist") + "/" + gr.Capture(cl.AnyDigit())
+)
+ANILIST_PATH: Pregex = "/animecore/anilist/" + gr.Capture(cl.AnyDigit())
+KISTU_PATH: Pregex = "/animecore/kitsu/" + gr.Capture(cl.AnyDigit())
 
 
 def animecore(request: HttpRequest) -> HttpResponse:
     user_agent_parsed = parse(request.META["HTTP_USER_AGENT"])
 
-    # user_agent_parsed.is_bot
-    if True:
-        return render(request, "frontend/bot_response.html")
+    if user_agent_parsed.is_bot:
+        # /animecore/mal/1
+        if myanimelist_path := MAL_PATH.get_captures(request.get_full_path()):
+            myanimelist_number = myanimelist_path[0][0]
+            anime_model = get_object_or_404(AnimeModel, mal_id=myanimelist_number)
+            context = {
+                "opengraph_title": anime_model.anime_name,
+                "opengraph_type": "webpage",
+                "opengraph_image": anime_model.anime_banner,
+                "opengraph_url": request.build_absolute_uri(),
+            }
+
+        # /animecore/anilist/1
+        elif anilist_path := ANILIST_PATH.get_captures(request.get_full_path()):
+            anilist_number = anilist_path[0][0]
+            anime_model = get_object_or_404(AnimeModel, anilist_id=anilist_number)
+            context = {
+                "opengraph_title": anime_model.anime_name,
+                "opengraph_type": "webpage",
+                "opengraph_image": anime_model.anime_banner,
+                "opengraph_url": request.build_absolute_uri(),
+            }
+
+        # /animecore/kitsu/1
+        elif kitsu_path := KISTU_PATH.get_captures(request.get_full_path()):
+            kitsu_number = kitsu_path[0][0]
+            anime_model = get_object_or_404(AnimeModel, kitsu_id=kitsu_number)
+            context = {
+                "opengraph_title": anime_model.anime_name,
+                "opengraph_type": "webpage",
+                "opengraph_image": anime_model.anime_banner,
+                "opengraph_url": request.build_absolute_uri(),
+            }
+
+        return render(request, "frontend/bot_response.html", context=context)
 
     else:
         return render(request, "frontend/animecore.html")
