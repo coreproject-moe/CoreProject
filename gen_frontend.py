4c39732fee5dd2c475baf2b15260682ed8609711
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-12 08:21:03 +0600

    Update gen_frontend.py

--- gen_frontend.py
+++ gen_frontend.py
@@ -1,16 +1,33 @@
 import subprocess
 import os
 import shutil
+from pathlib import Path
+from distutils.dir_util import copy_tree
+
+BASE_DIR = Path(__file__).resolve().parent
 
 
 def build_animecore():
-    os.chdir(f'{os.getcwd()}/frontend/AnimeCore/')
-    
-    # NPM commands
-    subprocess.check_call('npm install',shell=True)
-    subprocess.check_call('npm run build',shell=True)
+    os.chdir(f"{BASE_DIR}/frontend/AnimeCore/")
 
-    shutil.copy2(f'{os.getcwd()}/frontend/AnimeCore/build',f'{os.getcwd()}/backend/django_core/static/')
+    # NPM commands
+    subprocess.check_call("npm install", shell=True)
+    subprocess.check_call("npm run build", shell=True)
+
+    os.makedirs(
+        os.path.dirname(
+            f"{BASE_DIR}/backend/django_core/apps/frontend/templates/frontend/"
+        ),
+        exist_ok=True,
+    )
+    shutil.copy(
+        f"{BASE_DIR}/frontend/AnimeCore/build/app.html",
+        f"{BASE_DIR}/backend/django_core/apps/frontend/templates/frontend/animecore.html",
+    )
+    copy_tree(
+        f"{BASE_DIR}/frontend/AnimeCore/build/",
+        f"{BASE_DIR}/backend/django_core/static/",
+    )
 
 
 build_animecore()
