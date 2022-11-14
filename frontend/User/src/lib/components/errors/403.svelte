9841cfb2a1e7fb59ba6a9f0a9d3325a03a4193b9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-14 12:19:48 +0600

    Fix

---
+++ frontend/User/src/lib/components/errors/403.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    /* eslint-disable */
+    export let status: string;
+    export let errorMessage: string;
+    export let errorText: string;
+
+    import Alone from "$kaomoji/Alone.svelte";
+</script>
+
+<div class="flex items-center justify-center h-screen flex-col text-center gap-12">
+    <Alone width="321" height="118" />
+    <h1 class="text-indigo-700 text-3xl">
+        <b>{status}</b>
+        -
+        <b>{errorMessage}</b>
+    </h1>
+    <p class="w-[70vw] font-bold">
+        {@html errorText}
+    </p>
+</div>
--- frontend/User/svelte.config.js
+++ frontend/User/svelte.config.js
@@ -1,20 +1,29 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: [
-        preprocess({
-            postcss: true
-        })
-    ],
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
-    kit: {
-        appDir: 'svelte__user',
-        adapter: adapter({ fallback: 'app.html' })
-    }
+	kit: {
+		appDir: 'svelte__user',
+		adapter: adapter({ fallback: 'app.html' }),
+		trailingSlash: 'always',
+		alias: {
+			$hooks: path.resolve('./src/hooks'),
+			$components: path.resolve('./src/lib/components'),
+			$icons: path.resolve('./src/lib/icons'),
+			$kaomoji: path.resolve('./src/lib/kaomoji'),
+			$error: path.resolve('./src/lib/components/errors')
+		}
+	}
 };
 
 export default config;
