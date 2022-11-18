e75ccf42558df36ab38224b8b0e1fcd5885d1e8b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-18 07:36:01 +0600

    Update _app.tsx

--- frontend/AnimeCore/pages/_app.tsx
+++ frontend/AnimeCore/pages/_app.tsx
@@ -6,16 +6,6 @@ import Head from 'next/head';
 import NextNProgress from 'nextjs-progressbar';
 import { SpotlightProvider } from '@mantine/spotlight';
 import localFont from '@next/font/local';
-import type { SpotlightAction } from '@mantine/spotlight';
-
-const actions: SpotlightAction[] = [
-    {
-        title: 'Home',
-        description: 'Get to home page',
-        onTrigger: () => console.log('Home'),
-        icon: <></>,
-    },
-];
 
 const kokoroFont = localFont({
     src: [
@@ -31,7 +21,6 @@ const kokoroFont = localFont({
         {
             path: '../fonts/Kokoro/Kokoro-Italic.woff2',
         },
-
         {
             path: '../fonts/Kokoro/Kokoro-Bold.woff2',
         },
@@ -57,56 +46,56 @@ const App = (props: AppProps) => {
             </Head>
 
             <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={{
-                    /** Put your mantine theme override here */
-                    colorScheme: 'dark',
-                    /** Font Family */
-                    fontFamily: kokoroFont.style.fontFamily,
-                    /** Heading */
-                    headings: {
-                        sizes: {
-                            h1: { fontSize: 60 },
-                            h2: { fontSize: 40 },
-                            h3: { fontSize: 32 },
-                            h4: { fontSize: 24 },
-                            h5: { fontSize: 20 },
-                            h6: { fontSize: 16 },
-                        },
-                    },
+    withGlobalStyles
+    withNormalizeCSS
+    theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'dark',
+        /** Font Family */
+        fontFamily: kokoroFont.style.fontFamily,
+        /** Heading */
+        headings: {
+            sizes: {
+                h1: { fontSize: 60 },
+                h2: { fontSize: 40 },
+                h3: { fontSize: 32 },
+                h4: { fontSize: 24 },
+                h5: { fontSize: 20 },
+                h6: { fontSize: 16 },
+            },
+        },
 
-                    //** Color */
-                    colors: {
-                        yellow: [
-                            '#FFF2CA',
-                            '#F5E6B7',
-                            '#F8E5AB',
-                            '#EEDBA1',
-                            '#EDD68D',
-                            '#E9CC75',
-                            '#F3D36F',
-                            '#EEC959',
-                            '#FAD050',
-                            '#E3BD49',
-                        ],
-                        blue: [
-                            '#F0EEFF',
-                            '#DCD9F7',
-                            '#AAA4E2',
-                            '#7569E1',
-                            '#4133BA',
-                            '#3325BO',
-                            '#2D209B',
-                            '#23197D',
-                            '#100D33',
-                            '#070519',
-                        ],
-                    },
-                }}
+        //** Color */
+        colors: {
+            yellow: [
+                '#FFF2CA',
+                '#F5E6B7',
+                '#F8E5AB',
+                '#EEDBA1',
+                '#EDD68D',
+                '#E9CC75',
+                '#F3D36F',
+                '#EEC959',
+                '#FAD050',
+                '#E3BD49',
+            ],
+            blue: [
+                '#F0EEFF',
+                '#DCD9F7',
+                '#AAA4E2',
+                '#7569E1',
+                '#4133BA',
+                '#3325BO',
+                '#2D209B',
+                '#23197D',
+                '#100D33',
+                '#070519',
+            ],
+        },
+    }}
             >
                 <ModalsProvider>
-                    <SpotlightProvider actions={actions}>
+                    <SpotlightProvider actions={[]}>
                         <NotificationsProvider position="top-right">
                             <NextNProgress />
                             <Component {...pageProps} />
