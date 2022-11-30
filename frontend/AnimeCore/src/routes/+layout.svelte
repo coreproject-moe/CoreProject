700772b3d4d9454e56a9d99eb8659c22177bc6e0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-11-30 09:34:36 +0600

    Move files away from JS

--- frontend/AnimeCore/src/routes/+layout.svelte
+++ frontend/AnimeCore/src/routes/+layout.svelte
@@ -32,24 +32,7 @@
             document.querySelector<HTMLElement>(".root")?.style.removeProperty("display");
         }, 1000);
     });
-
-   
 </script>
 
-<svelte:head>
-    <meta
-        name="robots"
-        content="index,follow"
-    />
-    <meta
-        name="googlebot"
-        content="index,follow"
-    />
-    <meta
-        name="description"
-        content="Bridging the gap between streaming & torrenting sevices, with a modern and clean interface"
-    />
-    <!-- <meta name="author" content="baseplate-admin,akindworld" /> -->
-</svelte:head>
 
 <slot />
