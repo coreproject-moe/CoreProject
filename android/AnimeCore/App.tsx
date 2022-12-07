9ed9c26c22d46e948e33674d17bd1a7125bf21cf
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-12-07 09:56:26 +0600

    Add

--- android/AnimeCore/App.tsx
+++ android/AnimeCore/App.tsx
@@ -1,15 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -18,3 +9,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello world</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
