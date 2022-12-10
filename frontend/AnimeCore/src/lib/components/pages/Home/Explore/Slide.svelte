ed872901c88b007cee5fddda92b8663ab9fe5c5d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-12-10 20:18:51 +0600

    [`Frontend`] : Fix Search Input

--- frontend/AnimeCore/src/lib/components/pages/Home/Explore/Slide.svelte
+++ frontend/AnimeCore/src/lib/components/pages/Home/Explore/Slide.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import FourBoxSquares from "$icons/FourBoxSquares.svelte";
+    import Search from "$icons/Search.svelte";
 
     let selectionChoice = "popular";
 </script>
@@ -8,8 +9,9 @@
     <div class="flex gap-3 md:gap-32 pt-16">
         {#each ["popular", "trending", "top rated", "upcoming"] as item}
             <button
-                class="btn {selectionChoice.toLowerCase() === item.toLowerCase() &&
-                    'btn-active'} btn-ghost text-white capitalize font-bold text-lg"
+                class="btn {selectionChoice.toLowerCase() === item.toLowerCase()
+                    ? 'btn-active'
+                    : ''} btn-ghost text-white capitalize font-bold text-lg"
                 on:click={() => {
                     selectionChoice = item.toLowerCase();
                 }}
@@ -58,9 +60,21 @@
         </div>
         <div class="flex flex-col w-[135px]">
             Search
-            <select class="mt-2 select w-full max-w-[135px] bg-neutral">
-                <option selected>Any</option>
-            </select>
+            <div class="relative mt-2">
+                <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                    <Search
+                        class="pl-1"
+                        color="white"
+                        height="18"
+                        width="18"
+                    />
+                </span>
+                <input
+                    type="text"
+                    class="input bg-neutral w-[135px] pl-9"
+                    autocomplete="off"
+                />
+            </div>
         </div>
         <div class="flex flex-col w-[135px]">
             Sort by
