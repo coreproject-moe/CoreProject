4167c05c4efe55e2c109617bc7462c36e368d011
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-12-11 21:15:35 +0600

    maybe readd

--- backend/django_core/core/auth.py
+++ backend/django_core/core/auth.py
@@ -1,11 +1,23 @@
-from ninja.security import APIKeyHeader
+from apps.user.models import CustomUser
+from ninja.security import HttpBasicAuth
 
-from django.http import HttpRequest
+from django.contrib.auth.hashers import check_password
+from django.db.models import Q
 
 
-class AuthBearer(APIKeyHeader):
-    param_name = "X-Token"
+class AuthBearer(HttpBasicAuth):
+    def authenticate(self, request, username: str, password: str) -> str:
+        try:
+            user_model: CustomUser = (
+                CustomUser.objects.get_username_with_discriminator().get(
+                    Q(username_with_discriminator=username) | Q(email__iexact=username)
+                )
+            )
+            if check_password(password, user_model.password):
+                query = user_model
 
-    def authenticate(self, request: HttpRequest, key: str) -> str:
-        if key == "supersecret":
-            return key
+        except CustomUser.DoesNotExist:
+            query = None
+
+        finally:
+            return query
