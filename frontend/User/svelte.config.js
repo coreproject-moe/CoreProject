d9eb9b1538acb1495bca83e7116b865818228fce
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-12-11 20:29:53 +0600

    add

--- frontend/User/svelte.config.js
+++ frontend/User/svelte.config.js
@@ -4,20 +4,20 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: [vitePreprocess()],
-    kit: {
-        appDir: 'svelte__user',
-        adapter: adapter({ fallback: 'app.html' }),
-        alias: {
-            $hooks: path.resolve('./src/hooks'),
-            $components: path.resolve('./src/lib/components'),
-            $icons: path.resolve('./src/lib/icons'),
-            $kaomoji: path.resolve('./src/lib/kaomoji'),
-            $error: path.resolve('./src/lib/components/errors')
-        }
-    }
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [vitePreprocess()],
+	kit: {
+		appDir: 'svelte__user',
+		adapter: adapter({ fallback: 'app.html' }),
+		alias: {
+			$hooks: path.resolve('./src/hooks'),
+			$components: path.resolve('./src/lib/components'),
+			$icons: path.resolve('./src/lib/icons'),
+			$kaomoji: path.resolve('./src/lib/kaomoji'),
+			$error: path.resolve('./src/lib/components/errors')
+		}
+	}
 };
 
 export default config;
