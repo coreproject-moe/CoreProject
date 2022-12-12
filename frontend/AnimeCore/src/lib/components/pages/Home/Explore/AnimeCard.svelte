940e58046d78e82e1938980fc987491baeaa9bab
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-12-12 12:12:48 +0600

    [`Frontend`] : Bugfix (#958)
    * Update Slide.svelte
    
    * Modify yes
    
    * Fix weird Height thingy not working
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update .pre-commit-config.yaml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ frontend/AnimeCore/src/lib/components/pages/Home/Explore/AnimeCard.svelte
@@ -0,0 +1,8 @@
+<div class="card h-52 w-[450px] bg-base-100 shadow-xl">
+    <div class="card-body">
+        <div class="flex flex-row">
+            <div class="object-cover w-[135px] h-[200px]">Hello</div>
+            <p>helo</p>
+        </div>
+    </div>
+</div>
