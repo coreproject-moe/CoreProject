27d100259fd6d726e541c076b12a2f5db4715203
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-12-31 17:11:36 +0600

    [`Backend`] : Better `urls.py` (#1052)
    * Update views.py
    
    * add
    
    * Fix
    
    * Add Files
    
    * Add
    
    * Add
    
    * remove
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Better codes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- backend/django_core/apps/api/urls.py
+++ backend/django_core/apps/api/urls.py
@@ -1,6 +1,7 @@
 from ninja import NinjaAPI
 
 from django.urls import path
+from django.utils.module_loading import import_string
 
 api = NinjaAPI(title="CoreProjectAPI")
 
@@ -10,36 +11,41 @@ api = NinjaAPI(title="CoreProjectAPI")
 # ___ ANIME ROUTER _____
 
 from .views.anime import router as anime_router
-from .views.anime.anime_character import router as anime_router_anime_character_router
-from .views.anime.anime_genre import router as anime_router_anime_genre_router
-from .views.anime.anime_producer import router as anime_router_anime_producer_router
-from .views.anime.anime_studio import router as anime_router_anime_studio_router
-from .views.anime.anime_theme import router as anime_router_anime_theme_router
 
-anime_router.add_router("", anime_router_anime_genre_router, tags=["anime_info"])
-anime_router.add_router("", anime_router_anime_character_router, tags=["anime_info"])
-anime_router.add_router("", anime_router_anime_producer_router, tags=["anime_info"])
-anime_router.add_router("", anime_router_anime_studio_router, tags=["anime_info"])
-anime_router.add_router("", anime_router_anime_theme_router, tags=["anime_info"])
-
-from .views.anime.episode import router as anime_router_anime_episode_router
-from .views.anime.episode_comment import (
-    router as anime_router_anime_episode_comment_router,
-)
-from .views.anime.episode_timestamp import (
-    router as anime_router_anime_episode_timestamp_router,
-)
-
-anime_router.add_router("", anime_router_anime_episode_router, tags=["anime_episodes"])
-anime_router.add_router(
-    "", anime_router_anime_episode_comment_router, tags=["anime_episodes"]
-)
-anime_router.add_router(
-    "", anime_router_anime_episode_timestamp_router, tags=["anime_episodes"]
-)
-
-# Final route add
 api.add_router("/anime", anime_router, tags=["anime_info"])
+
+anime_router.add_router(
+    "", import_string("apps.api.views.anime.anime_genre.router"), tags=["anime_info"]
+)
+anime_router.add_router(
+    "", import_string("apps.api.views.anime.anime_character.router"), tags=["anime_info"]
+)
+anime_router.add_router(
+    "", import_string("apps.api.views.anime.anime_producer.router"), tags=["anime_info"]
+)
+anime_router.add_router(
+    "", import_string("apps.api.views.anime.anime_studio.router"), tags=["anime_info"]
+)
+anime_router.add_router(
+    "", import_string("apps.api.views.anime.anime_theme.router"), tags=["anime_info"]
+)
+
+# Episodes
+
+anime_router.add_router(
+    "", import_string("apps.api.views.anime.episode.router"), tags=["anime_episodes"]
+)
+anime_router.add_router(
+    "",
+    import_string("apps.api.views.anime.episode_comment.router"),
+    tags=["anime_episodes"],
+)
+anime_router.add_router(
+    "",
+    import_string("apps.api.views.anime.episode_timestamp.router"),
+    tags=["anime_episodes"],
+)
+
 
 # __ CHARACTER ROUTER ___
 
@@ -72,24 +78,26 @@ from .views.trackers import router as tracker_router
 
 api.add_router("/trackers", tracker_router, tags=["trackers"])
 
-from .views.trackers.anilist import router as tracker_router_anilist_router
-from .views.trackers.kitsu import router as tracker_router_kitsu_router
-from .views.trackers.mal import router as tracker_router_myanimelist_router
-
-tracker_router.add_router("", tracker_router_anilist_router, tags=["trackers"])
-tracker_router.add_router("", tracker_router_kitsu_router, tags=["trackers"])
-tracker_router.add_router("", tracker_router_myanimelist_router, tags=["trackers"])
+tracker_router.add_router(
+    "", import_string("apps.api.views.trackers.anilist.router"), tags=["trackers"]
+)
+tracker_router.add_router(
+    "", import_string("apps.api.views.trackers.kitsu.router"), tags=["trackers"]
+)
+tracker_router.add_router(
+    "", import_string("apps.api.views.trackers.mal.router"), tags=["trackers"]
+)
 
 # __ USER ROUTER __
 
 from .views.user import router as user_router
-from .views.user.login import router as user_router_login_router
-from .views.user.logout import router as user_router_logout_router
-
-user_router.add_router("", user_router_login_router, tags=["user"])
-user_router.add_router("", user_router_logout_router, tags=["user"])
 
 api.add_router("/user", user_router, tags=["user"])
+
+user_router.add_router("", import_string("apps.api.views.user.login.router"), tags=["user"])
+user_router.add_router(
+    "", import_string("apps.api.views.user.logout.router"), tags=["user"]
+)
 
 
 urlpatterns = [
