3cd2437ee755d32f9fa791cf4203b9b3d93a415d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-03 14:58:08 +0600

    add

--- android/AnimeCore/App.tsx
+++ android/AnimeCore/App.tsx
@@ -4,18 +4,9 @@ import { Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={styles.container}>
+        <View className="flex-1 items-center justify-center bg-white">
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
