d3bec1b86b8e7549721605e94d10abb9f90bf005
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-03 14:52:09 +0600

    use React cli

--- android/AnimeCore/App.tsx
+++ android/AnimeCore/App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
+        <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
