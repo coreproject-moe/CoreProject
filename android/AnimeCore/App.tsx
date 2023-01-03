7e33743db44c38a8c7ba44e77eacf4ef316249f0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-03 15:44:12 +0600

    add

--- android/AnimeCore/App.tsx
+++ android/AnimeCore/App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+    const [loaded] = useFonts({
+        Montserrat: require('@/assets/fonts/Kokoro/Kokoro-Regular.woff2'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
     return (
-        <View className="flex-1 items-center justify-center bg-white">
+        <View className="h-full flex items-center justify-center bg-white">
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
