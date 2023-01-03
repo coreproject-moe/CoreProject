79fe5f312ee95f39e21204861b4ebb244261cfd9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-03 15:51:53 +0600

    try

--- android/AnimeCore/babel.config.js
+++ android/AnimeCore/babel.config.js
@@ -2,6 +2,38 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: ['nativewind/babel'],
+        plugins: [
+            ['nativewind/babel'],
+            [
+                'babel-plugin-tsconfig-paths',
+                {
+                    relative: true,
+                    extensions: [
+                        '.js',
+                        '.jsx',
+                        '.ts',
+                        '.tsx',
+                        '.es',
+                        '.es6',
+                        '.mjs',
+                    ],
+                    rootDir: '.',
+                    tsconfig: 'tsconfig.json',
+                    transformFunctions: [
+                        'require',
+                        'require.resolve',
+                        'System.import',
+                        'jest.genMockFromModule',
+                        'jest.mock',
+                        'jest.unmock',
+                        'jest.doMock',
+                        'jest.dontMock',
+                        'jest.setMock',
+                        'require.requireActual',
+                        'require.requireMock',
+                    ],
+                },
+            ],
+        ],
     };
 };
