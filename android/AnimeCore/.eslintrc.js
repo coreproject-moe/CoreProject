f8d06f76016a7aee8d4f52a6b8618eb1e4694f25
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-13 20:54:58 +0600

    Fix

---
+++ android/AnimeCore/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
