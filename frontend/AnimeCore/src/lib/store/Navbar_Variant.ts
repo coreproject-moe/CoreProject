95018867d195c1a3fa3a1441d1a5d70e8e654dd8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-15 10:41:19 +0600

    Update Navbar_Variant.ts

--- frontend/AnimeCore/src/lib/store/Navbar_Variant.ts
+++ frontend/AnimeCore/src/lib/store/Navbar_Variant.ts
@@ -1,7 +1,3 @@
 import { writable } from "svelte/store";
 
 export const navbar_variant = writable<"black" | "white" | "">("");
-
-navbar_variant.subscribe((value) => {
-    console.log(value);
-});
