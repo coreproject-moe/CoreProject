62fc8ed3cb79e0705b3dad7008e2a911ca319662
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-18 20:08:54 +0600

    Add

--- backend/django_core/apps/api/filters/producers.py
+++ backend/django_core/apps/api/filters/producers.py
@@ -2,8 +2,8 @@ from ninja import Schema
 
 
 class ProducerFilter(Schema):
-    mal_id: str = None
-
+    mal_id: int | None = None
+    
     # icontains based search
     #   Allowed :
     #       A1 Pictures, Studio Ghibli
