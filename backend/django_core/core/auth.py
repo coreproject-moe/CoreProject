2490477b01efa45c3840de2af56e4d1d9792de2f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-21 10:38:53 +0600

    Update auth.py

--- backend/django_core/core/auth.py
+++ backend/django_core/core/auth.py
@@ -1,4 +1,4 @@
-from typing import Self
+from typing import Type
 
 from apps.user.models import CustomUser, Token
 from ninja.security import HttpBearer
@@ -9,10 +9,10 @@ from django.http import HttpRequest
 
 class AuthBearer(HttpBearer):
     def authenticate(
-        self: Self,
+        self,
         request: HttpRequest,
         token: str,
-    ) -> CustomUser | AnonymousUser:
+    ) -> Type[CustomUser | AnonymousUser]:
         try:
             token_data = Token.objects.get(token=token)
             return token_data.user
