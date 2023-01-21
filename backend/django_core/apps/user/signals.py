98f06860473559494f87d87ea6073c626fd7fedc
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-21 10:36:07 +0600

    add

--- backend/django_core/apps/user/signals.py
+++ backend/django_core/apps/user/signals.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import NoReturn
+from typing import NoReturn, Unpack, TypedDict
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.http import HttpRequest
@@ -8,9 +8,13 @@ from .models import CustomUser
 from .utilities import get_client_ip, get_random_username_discriminator
 
 
+class DjangoInstance(TypedDict):
+    instance: CustomUser
+
+
 @receiver(pre_save, sender=CustomUser)
-def user_discriminator_handler(**kwargs) -> NoReturn:
-    instance: CustomUser = kwargs["instance"]
+def user_discriminator_handler(**kwargs: Unpack[DjangoInstance]) -> NoReturn:
+    instance = kwargs["instance"]
     if not instance.username_discriminator:
         instance.username_discriminator = get_random_username_discriminator(
             username=instance.username
@@ -18,7 +22,7 @@ def user_discriminator_handler(**kwargs) -> NoReturn:
 
 
 @receiver(pre_save, sender=CustomUser)
-def user_ip_handler(**kwargs) -> NoReturn:
+def user_ip_handler(**kwargs: Unpack[DjangoInstance]) -> NoReturn:
     # https://stackoverflow.com/questions/4721771/get-current-user-log-in-signal-in-django
     request: HttpRequest | None = None
     # Reversed is better than not doing reverse
@@ -35,4 +39,5 @@ def user_ip_handler(**kwargs) -> NoReturn:
         ip = get_client_ip(request)
     except AttributeError:
         ip = "0.0.0.0"
+
     instance.ip = ip
