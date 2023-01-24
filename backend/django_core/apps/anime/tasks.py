e295f0cc80df31d6b2ca75d2e3d7684a3ead4d1f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-24 09:00:07 +0600

    Update tasks.py

--- backend/django_core/apps/anime/tasks.py
+++ backend/django_core/apps/anime/tasks.py
@@ -3,9 +3,9 @@ from .models import AnimeModel
 from core.utils.rgb_to_hex import rgb_to_hex
 from core.utils.k_means_color_analyzer import KMeansColorAnalyzer
 from numpy.typing import NDArray
+from django.conf import settings
 
-
-SHOW_PLOT = False
+SHOW_PLOT = True
 
 
 @db_task()
@@ -14,7 +14,9 @@ def set_field_brightness(
     field_name: str,
     image: NDArray,
 ) -> None:
-    rgb_tuple = KMeansColorAnalyzer(image).best_color(plot=SHOW_PLOT)
+    rgb_tuple = KMeansColorAnalyzer(image).best_color(
+        plot=all([SHOW_PLOT, settings.DEBUG]),
+    )
     instance = AnimeModel.objects.get(pk=pk)
     setattr(
         instance,
