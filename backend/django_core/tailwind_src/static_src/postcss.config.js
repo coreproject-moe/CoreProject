057011133e1549e28a6f2fe70c9ef0b00eff5e74
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-31 19:20:16 +0600

    [`Backend`] : Add some more things to tailwind

--- backend/django_core/tailwind_src/static_src/postcss.config.js
+++ backend/django_core/tailwind_src/static_src/postcss.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     plugins: {
-        "postcss-import": {},
-        "postcss-simple-vars": {},
-        "postcss-nested": {}
+        cssnano: {
+            autoprefixer: false,
+            preset: ['default'],
+        },
+        autoprefixer: {},
+        'postcss-import': {},
+        'postcss-simple-vars': {},
+        'postcss-nested': {},
     },
-}
+};
