94dbed0226cd0325bd4871f6c22c8f8557e71177
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-01-31 19:14:15 +0600

    Update tailwind.config.js

--- backend/django_core/tailwind_src/static_src/tailwind.config.js
+++ backend/django_core/tailwind_src/static_src/tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
          */
 
         /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-        '../templates/**/*.html',
+        // '../templates/**/*.html',
 
         /*
          * Main templates directory of the project (BASE_DIR/templates).
@@ -24,7 +24,7 @@ module.exports = {
          * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
          * Adjust the following line to match your project structure.
          */
-        '../../**/templates/**/*.html',
+        // '../../**/templates/**/*.html',
 
         /**
          * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
@@ -33,7 +33,7 @@ module.exports = {
         /* JS 1: Ignore any JavaScript in node_modules folder. */
         // '!../../**/node_modules',
         /* JS 2: Process all JavaScript files in the project. */
-        '../../**/*.js',
+        // '../../**/*.js',
 
         /**
          * Python: If you use Tailwind CSS classes in Python, uncomment the following line
