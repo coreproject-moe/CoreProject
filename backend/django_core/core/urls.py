ab6585bb2152eb80ac9d33f01151f6b5ef3233bd
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-02-02 21:57:34 +0600

    [`Backend`] :  Add Magical Hover effect (#1343)
    * Add Backend Files
    
    * add
    
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- backend/django_core/core/urls.py
+++ backend/django_core/core/urls.py
@@ -13,93 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from . import views
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.views import debug
-from django.views.generic import TemplateView
-from .utilities.format import format_kokoro_color
-from django.templatetags.static import static
 
 
-handler400 = TemplateView.as_view(
-    template_name="errors/base.html",
-    extra_context={
-        "svg": {
-            "file": static("images/kaomoji/400.svg"),
-            "width": 414,
-            "height": 123,
-        },
-        "error_status_code": 400,
-        "error_status": "bad request",
-        "error_text": format_kokoro_color(
-            "kokoro-chan faced some issues in trying to decipher that request."
-            "In the meantime, you can try checking the URL and refreshing the page, "
-            "or go back home, "
-            "browse the forums or come say hi!"
-        ),
-    },
-)
-handler403 = TemplateView.as_view(
-    template_name="errors/base.html",
-    extra_context={
-        "svg": {
-            "file": static("images/kaomoji/403.svg"),
-            "width": 321,
-            "height": 118,
-        },
-        "error_status_code": 403,
-        "error_status": "Forbidden",
-        "error_text": format_kokoro_color(
-            "Even to her precious user-kun, "
-            "kokoro-chan has some secrets that she wants to hide. "
-            "Well, since there's nothing you can do about that, "
-            "you can go back home, browse the forums or come say hi! "
-        ),
-    },
-)
-handler404 = TemplateView.as_view(
-    template_name="errors/base.html",
-    extra_context={
-        "svg": {
-            "file": static("images/kaomoji/404.svg"),
-            "width": 414,
-            "height": 123,
-        },
-        "error_status_code": 404,
-        "error_status": "Page Not Found",
-        "error_text": format_kokoro_color(
-            "Our hardworking kokoro-chan was unable to find that page. "
-            "While she collects more data on it, "
-            "why don't you go back home, "
-            "explore some random anime, "
-            "browse the forums or come say hi!"
-        ),
-    },
-)
-handler500 = TemplateView.as_view(
-    template_name="errors/base.html",
-    extra_context={
-        "svg": {
-            "file": static("images/kaomoji/500.svg"),
-            "width": 387,
-            "height": 114,
-        },
-        "error_status_code": 500,
-        "error_status": "Internal Server Error",
-        "error_text": format_kokoro_color(
-            "Uh-oh, looks like our cute kokoro-chan worked really hard"
-            "for the past few days and has now fallen asleep. "
-            "You can wait for her to wake up by looking at the status page, "
-            "or come say hi to other fellow kokoro-chan worshippers! "
-            "ah- also let's wish her sweet dreams! "
-        ),
-    },
-)
+handler400 = views.four_zero_zero_view
+handler403 = views.four_zero_three_view
+handler404 = views.four_zero_four_view
+handler500 = views.five_zero_zero_view
 
 urlpatterns = [
-    path("", debug.default_urlconf),
+    # Default django welcome page
+    # path("", debug.default_urlconf),
+    path("", views.home_view, name="home_view"),
     #   Admin Site
     # ================
     path("admin/", admin.site.urls),
---
+++ backend/django_core/core/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import render
+from django.templatetags.static import static
+from .utilities.format import format_kokoro_color
+from django.http import HttpResponse, HttpRequest
+
+ERROR_DICT = {
+    400: {
+        "svg": {
+            "file": static("images/kaomoji/400.svg"),
+            "width": 414,
+            "height": 123,
+        },
+        "error_status": "bad request",
+        "error_text": format_kokoro_color(
+            "kokoro-chan faced some issues in trying to decipher that request."
+            "In the meantime, you can try checking the URL and refreshing the page, "
+            "or go back home, browse the forums or come say hi!"
+        ),
+    },
+    403: {
+        "svg": {
+            "file": static("images/kaomoji/403.svg"),
+            "width": 321,
+            "height": 118,
+        },
+        "error_status_code": 403,
+        "error_status": "Forbidden",
+        "error_text": format_kokoro_color(
+            "Even to her precious user-kun, "
+            "kokoro-chan has some secrets that she wants to hide. "
+            "Well, since there's nothing you can do about that, "
+            "you can go back home, browse the forums or come say hi! "
+        ),
+    },
+    404: {
+        "svg": {
+            "file": static("images/kaomoji/404.svg"),
+            "width": 414,
+            "height": 123,
+        },
+        "error_status_code": 404,
+        "error_status": "Page Not Found",
+        "error_text": format_kokoro_color(
+            "Our hardworking kokoro-chan was unable to find that page. "
+            "While she collects more data on it, "
+            "why don't you go back home, "
+            "explore some random anime, "
+            "browse the forums or come say hi!"
+        ),
+    },
+    500: {
+        "svg": {
+            "file": static("images/kaomoji/500.svg"),
+            "width": 387,
+            "height": 114,
+        },
+        "error_status_code": 500,
+        "error_status": "Internal Server Error",
+        "error_text": format_kokoro_color(
+            "Uh-oh, looks like our cute kokoro-chan worked really hard"
+            "for the past few days and has now fallen asleep. "
+            "You can wait for her to wake up by looking at the status page, "
+            "or come say hi to other fellow kokoro-chan worshippers! "
+            "ah- also let's wish her sweet dreams! "
+        ),
+    },
+}
+
+
+async def home_view(request: HttpRequest) -> HttpResponse:
+    return render(
+        request,
+        template_name="home/index.html",
+    )
+
+
+async def four_zero_zero_view(request: HttpRequest, exception: None = None) -> HttpResponse:
+    context = ERROR_DICT.get(400)
+    context.setdefault("error_status_code", 400)
+    return render(
+        request,
+        "errors/base.html",
+        context,
+    )
+
+
+async def four_zero_three_view(
+    request: HttpRequest, exception: None = None
+) -> HttpResponse:
+    context = ERROR_DICT.get(403)
+    context.setdefault("error_status_code", 403)
+    return render(
+        request,
+        "errors/base.html",
+        context,
+    )
+
+
+async def four_zero_four_view(request: HttpRequest, exception: None = None) -> HttpResponse:
+    context = ERROR_DICT.get(404)
+    context.setdefault("error_status_code", 404)
+    return render(
+        request,
+        "errors/base.html",
+        context,
+    )
+
+
+async def five_zero_zero_view(request: HttpRequest) -> HttpResponse:
+    context = ERROR_DICT.get(500)
+    context.setdefault("error_status_code", 500)
+
+    return render(
+        request,
+        "errors/base.html",
+        context,
+    )
