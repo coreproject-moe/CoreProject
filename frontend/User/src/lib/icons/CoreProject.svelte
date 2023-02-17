df1f801765304d1725a717c40ffad694f9b5455f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-02-17 17:18:49 +0600

    [`Frontend`] : More Code Reduction

---
+++ frontend/User/src/lib/icons/CoreProject.svelte
@@ -0,0 +1,14 @@
+<div class="font-bold text-4xl select-none flex">
+	<span class="inline-flex text-white">c</span>
+	<span class="inline-flex text-warning">o</span>
+	<span class="inline-flex text-white">r</span>
+	<span class="inline-flex text-white">e</span>
+	&nbsp;
+	<span class="inline-flex text-info">p</span>
+	<span class="inline-flex text-info">r</span>
+	<span class="inline-flex text-info">o</span>
+	<span class="inline-flex text-info">j</span>
+	<span class="inline-flex text-info">e</span>
+	<span class="inline-flex text-info">c</span>
+	<span class="inline-flex text-info">t</span>
+</div>
