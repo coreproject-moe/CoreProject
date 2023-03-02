7b3d94cdf7b9337140590ddd7141ac0c3c32a510
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-02 10:38:59 +0600

    [`Backend`] : Fix debugs

--- backend/django_core/core/settings.py
+++ backend/django_core/core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = False if os.environ.get("DEBUG") else True
 
 # Increase this in future
 # If you increase this make sure to run `migrations`
