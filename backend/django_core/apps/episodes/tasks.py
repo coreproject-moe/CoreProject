04bf5a29db0af22996d4e9d12ffc484c09ebc7be
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-05 21:24:03 +0600

    Update tasks.py

--- backend/django_core/apps/episodes/tasks.py
+++ backend/django_core/apps/episodes/tasks.py
@@ -1,13 +1,7 @@
-import asyncio
-import os
-
-from asgiref.sync import sync_to_async
 from celery import shared_task
 import httpx
 
 from django.conf import settings
-from django.core.files import File
-from django.core.files.storage import FileSystemStorage
 
 from .models import EpisodeModel
 
@@ -15,11 +9,10 @@ from .models import EpisodeModel
 # Uploader functions
 
 
-async def post_files_to_streamsb(file):
+def post_files_to_streamsb(file):
     STREAMDB_SERVER_URL = "https://api.streamsb.com/api/upload/server"
-    CLIENT = httpx.AsyncClient()
-
-    server_url_res = await CLIENT.get(
+    CLIENT = httpx.Client(timeout=None, http2=True)
+    server_url_res = CLIENT.get(
         STREAMDB_SERVER_URL,
         params={
             "key": settings.STREAMSB_KEY,
@@ -28,7 +21,7 @@ async def post_files_to_streamsb(file):
     server_url_res_json = server_url_res.json()
     server_url = server_url_res_json["result"]
 
-    file_res = await CLIENT.post(
+    file_res = CLIENT.post(
         server_url,
         files={
             "file": file.open("rb"),
@@ -38,20 +31,13 @@ async def post_files_to_streamsb(file):
             "json": "1",
         },
     )
-    file_code = file_res.json()["result"][0]["code"]
+    file_res_json = file_res.json()
+    file_code = file_res_json["result"][0]["code"]
+
     return file_code
 
 
 # Create your tasks here
-
-
-async def main(episode_model_instance: EpisodeModel, file):
-    tasks = {
-        "streamsb": post_files_to_streamsb(file=file),
-    }
-    results = await asyncio.gather(*tasks.values())
-    episode_model_instance.providers = dict(zip(tasks, results))
-    await sync_to_async(episode_model_instance.save)()
 
 
 @shared_task()
@@ -59,8 +45,12 @@ def upload_file_to_providers_and_set_thumbnail(
     pk: int,
 ) -> None:
     instance = EpisodeModel.objects.get(pk=pk)
-    episode_file = instance.episode_file
 
-    asyncio.run(main(instance, episode_file))
+    if episode_file := getattr(instance, "episode_file"):
+        data = {
+            "streamsb": post_files_to_streamsb(episode_file),
+        }
 
-    os.unlink(episode_file.path)
+        instance.providers = data
+        instance.episode_file.delete()
+        instance.save()
