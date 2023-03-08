1fb3eb5b0b2d2ede939b8ca37ea8e9dcf09273e7
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-08 20:23:56 +0600

    add

--- backend/django_core/apps/anime/models/anime_theme.py
+++ backend/django_core/apps/anime/models/anime_theme.py
@@ -2,13 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-TYPE_CHOICES = ()
+TYPE_CHOICES = [
+    ("Anime", "anime"),
+    ("Manga", "manga"),
+]
 
 
 class AnimeThemeModel(models.Model):
     mal_id = models.IntegerField(unique=True, blank=False, null=False)
     name = models.CharField(unique=True, max_length=50, default="", null=False, blank=False)
-    type = models.CharField(max_length=50, default="", null=False, blank=False)
+    type = models.CharField(
+        max_length=50,
+        choices=TYPE_CHOICES,
+        default="",
+        null=False,
+        blank=False,
+    )
 
     def __str__(self) -> str:
         return f"{self.pk}. {self.name} ({self.type})"
