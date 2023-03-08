d837ad28d826165058a96ae498ba81fc260cddf9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-08 18:18:24 +0600

    [`Backend`] : Patch Varnish (#1614)
    * Update anime_theme.py
    
    * add
    
    * add
    
    * Create 0002_alter_animethememodel_type.py

--- backend/django_core/apps/anime/models/anime_theme.py
+++ backend/django_core/apps/anime/models/anime_theme.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+TYPE_CHOICES = ()
+
 
 class AnimeThemeModel(models.Model):
     mal_id = models.IntegerField(unique=True, blank=False, null=False)
     name = models.CharField(unique=True, max_length=50, default="", null=False, blank=False)
-    type = models.CharField(max_length=50, default="", unique=True, null=False, blank=False)
+    type = models.CharField(max_length=50, default="", null=False, blank=False)
 
     def __str__(self) -> str:
         return f"{self.pk}. {self.name} ({self.type})"
