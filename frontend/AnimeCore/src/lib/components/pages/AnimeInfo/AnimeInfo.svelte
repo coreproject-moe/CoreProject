021bc98ffbadad96c60c9f3005d7f63839c71bdc
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-12 14:14:25 +0600

    [`Frontend`] : Fix

--- frontend/AnimeCore/src/lib/components/pages/AnimeInfo/AnimeInfo.svelte
+++ frontend/AnimeCore/src/lib/components/pages/AnimeInfo/AnimeInfo.svelte
@@ -19,7 +19,7 @@
     $: mobile = $responsiveMode === "mobile";
 </script>
 
-<anime-info-base class="flex flex-col gap-2 w-72">
+<anime-info-base class="flex flex-col gap-2 w-3/5 md:w-72">
     <h1 class="text-white text-2xl md:text-4xl font-bold">{title_english}</h1>
     <p class="text-neutral-400 text-sm">
         <!-- Todo modify this to have anime synonyms  -->
