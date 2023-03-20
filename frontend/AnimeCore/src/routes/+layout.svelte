3f8a717ffcf20f1927d3324d132a2b3c016c1f94
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-20 10:42:59 +0600

    Update +layout.svelte

--- frontend/AnimeCore/src/routes/+layout.svelte
+++ frontend/AnimeCore/src/routes/+layout.svelte
@@ -13,6 +13,11 @@
             ?.forEach((e) => e.remove());
         document?.querySelector<HTMLElement>(".root")?.style.removeProperty("display");
     });
+    
+    import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:window
