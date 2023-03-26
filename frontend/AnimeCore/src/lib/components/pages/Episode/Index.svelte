2e9b78948dbb51d65540c5da0afa3a870aae99d1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-26 09:11:29 +0600

    add

--- frontend/AnimeCore/src/lib/components/pages/Episode/Index.svelte
+++ frontend/AnimeCore/src/lib/components/pages/Episode/Index.svelte
@@ -12,35 +12,53 @@
         episode_timestamps: Array<string>;
     };
 
-    let htmlIFrameElement: HTMLIFrameElement | undefined = undefined;
-    $: {
-        if (htmlIFrameElement) {
-            // streamsb
-            let document = htmlIFrameElement.contentWindow?.document;
-            let htmlVidoeElement = document?.querySelector(".jw-video") as HTMLVideoElement;
-            htmlVidoeElement.currentTime;
+    // let htmlIFrameElement: HTMLIFrameElement | undefined = undefined;
+    // $: {
+    //     if (htmlIFrameElement) {
+    //         // streamsb
+    //         let document = htmlIFrameElement.contentWindow?.document;
+    //         let htmlVidoeElement = document?.querySelector(".jw-video") as HTMLVideoElement;
+    //         htmlVidoeElement.currentTime;
+    //     }
+    // }
+
+    const widthMapping = {
+        wide: {
+            height: 800,
+            width: 1280
+        },
+        normal: {
+            height: 500,
+            width: 800
         }
-    }
+    };
+    const videoPlayerWidth: keyof typeof widthMapping = "normal";
 </script>
 
-<div class="relative grid h-screen">
+<div class="relative grid h-screen ">
     <div class="absolute inset-0 grid h-screen w-screen">
         <div class="hero">
             <div class="grid h-full w-full">
                 <div class="pt-8 pl-6 pb-0 md:pr-[72px] md:pl-20">
                     <Navbar />
                 </div>
-                <div class="mx-6 mt-0 flex flex-col items-center md:mx-20">
+                <div
+                    class="mx-6 mt-0 flex items-start md:mx-20 {videoPlayerWidth === 'normal'
+                        ? 'flex-row'
+                        : 'flex-col'}"
+                >
                     {#each Object.entries(data?.providers) as item}
                         {@const key = item[0]}
                         {@const value = item[1]}
-                        {@const url = `https://sbbrisk.com/e/${value}.html`}
+                        {@const url = "https://google.com" ?? `https://sbbrisk.com/e/${value}.html`}
+                        {@const player_height = widthMapping[videoPlayerWidth]["height"]}
+                        {@const player_width = widthMapping[videoPlayerWidth]["width"]}
                         {#if key === "streamsb"}
                             <iframe
-                                bind:this={htmlIFrameElement}
-                                class="h-[200px] w-[80vw] md:h-[80vh]"
                                 title={data?.episode_name}
                                 src={url}
+                                class="bg-black"
+                                style="height:{player_height}px; width:{player_width}px;"
                                 frameborder="0"
                                 marginwidth="0"
                                 marginheight="0"
@@ -51,6 +69,7 @@
                             pass
                         {/if}
                     {/each}
+                    <div class="divider divider-horizontal before:bg-white after:bg-white" />
                 </div>
             </div>
         </div>
