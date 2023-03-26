c7ae97952b0e2e118fdb3e1323b7f5683136da5f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-26 08:54:46 +0600

    add

--- android/AnimeCore/tailwind.config.js
+++ android/AnimeCore/tailwind.config.js
@@ -11,22 +11,5 @@ module.exports = {
     },
 
     // eslint-disable-next-line import/no-extraneous-dependencies
-    plugins: [require('@tailwindcss/typography'), require('daisyui')],
-    daisyui: {
-        themes: [
-            {
-                kokoro: {
-                    primary: '#7569E1',
-                    secondary: '#E3BD49',
-                    accent: '#FFF7F8',
-                    neutral: '#1E2036',
-                    'base-100': '#070519',
-                    info: '#DCD9F7',
-                    success: '#6FCF97',
-                    warning: '#EDD68D',
-                    error: '#EB5757',
-                },
-            },
-        ],
-    },
+    plugins: [require('@tailwindcss/typography')],
 };
