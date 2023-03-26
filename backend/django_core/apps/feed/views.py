56320e40444d50f9964e28328118587f820a1554
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-26 21:36:50 +0600

    add

--- backend/django_core/apps/feed/views.py
+++ backend/django_core/apps/feed/views.py
@@ -1,10 +1,29 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 
+# Import our models here
 from apps.anime.models import AnimeModel
+from apps.characters.models import CharacterModel
+from apps.producers.models import ProducerModel
+from apps.staffs.models import StaffModel
 
 # Create your views here.
 
 
-def all_anime_ids(request):
+def all_anime_ids(request: HttpRequest) -> JsonResponse:
     query = AnimeModel.objects.all().values_list("pk", flat=True)
+    return JsonResponse({"data": list(query)})
+
+
+def all_character_ids(request: HttpRequest) -> JsonResponse:
+    query = CharacterModel.objects.all().values_list("pk", flat=True)
+    return JsonResponse({"data": list(query)})
+
+
+def all_producer_ids(request: HttpRequest) -> JsonResponse:
+    query = ProducerModel.objects.all().values_list("pk", flat=True)
+    return JsonResponse({"data": list(query)})
+
+
+def all_staff_ids(request: HttpRequest) -> JsonResponse:
+    query = StaffModel.objects.all().values_list("pk", flat=True)
     return JsonResponse({"data": list(query)})
