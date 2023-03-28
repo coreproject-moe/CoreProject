ad2e5698e0b687786d9e9b52bea2ad5cc1a0d663
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-28 21:54:58 +0600

    Update Search.svelte

--- frontend/AnimeCore/src/lib/components/modals/Search.svelte
+++ frontend/AnimeCore/src/lib/components/modals/Search.svelte
@@ -11,27 +11,60 @@
             $timerStore = "start";
             break;
     }
+
+    import Search from "$icons/Search.svelte";
+    import { responsiveMode } from "$store/Responsive";
+
+    let mobile: boolean;
+    $: mobile = $responsiveMode === "mobile";
+
+    let dropDownElement: Partial<HTMLDivElement> | undefined = undefined;
+    let data = [];
+
+    const onFocus = () => {
+        dropDownElement?.classList?.add("dropdown-open");
+    };
+
+    const onBlur = () => {
+        dropDownElement?.classList?.remove("dropdown-open");
+    };
 </script>
 
 <input
+    bind:checked={value}
     type="checkbox"
     id={$modals.search}
     class="modal-toggle"
 />
-<div class="modal">
-    <div class="modal-box">
-        <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-        <p class="py-4">
-            You've been selected for a chance to get one year of subscription to use Wikipedia for
-            free!
-        </p>
-        <div class="modal-action">
-            <label
-                for={$modals.search}
-                class="btn"
-            >
-                Yay!
-            </label>
+<div class="modal flex flex-col gap-20">
+    <div class="modal-box p-1">
+        <div class="relative text-gray-600 focus-within:text-gray-400">
+            <span class="absolute flex h-full w-16 items-center justify-center">
+                <Search
+                    color="white"
+                    height={15}
+                    width={15}
+                />
+            </span>
+            <input
+                on:focus={onFocus}
+                on:blur={onBlur}
+                type="Search"
+                class="input w-full pl-16"
+                placeholder="Search for anything"
+                autocomplete="off"
+            />
         </div>
     </div>
+    <label
+        for=""
+        class="flex gap-20"
+    >
+        <div>
+            <p class="pl-3 font-bold">Anime</p>
+            <div class="modal-box relative w-72">pass</div>
+        </div>
+        <div class="modal-box w-72" />
+        <div class="modal-box w-72" />
+    </label>
 </div>
