12e677a92f8f493c69fa6dd29b211cc79a1e93a1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-03-28 21:09:17 +0600

    Update middleware.py

--- backend/django_core/apps/api/middleware.py
+++ backend/django_core/apps/api/middleware.py
@@ -1,43 +1,42 @@
-from django.utils.decorators import sync_and_async_middleware
-from collections.abc import Callable
-from django.http import HttpRequest, HttpResponse
-from asgiref.sync import sync_to_async
-
 import asyncio
+from collections.abc import Callable
+
+from typing import Any
+from asgiref.sync import sync_to_async
+from django.http import HttpRequest, HttpResponse
+from django.utils.decorators import sync_and_async_middleware
 
 
 @sync_and_async_middleware
 def process_put_patch(get_response: Callable) -> Callable:
-    if asyncio.iscoroutinefunction(get_response):
+    async def async_middleware(request: HttpRequest) -> Any:
+        if (
+            request.method in ("PUT", "PATCH")
+            and request.content_type != "application/json"
+        ):
+            initial_method = request.method
+            request.method = "POST"
+            request.META["REQUEST_METHOD"] = "POST"
+            await sync_to_async(request._load_post_and_files)()
+            request.META["REQUEST_METHOD"] = initial_method
+            request.method = initial_method
 
-        async def middleware(request: HttpRequest) -> HttpResponse:
-            if (
-                request.method in ("PUT", "PATCH")
-                and request.content_type != "application/json"
-            ):
-                initial_method = request.method
-                request.method = "POST"
-                request.META["REQUEST_METHOD"] = "POST"
-                await sync_to_async(request._load_post_and_files)()
-                request.META["REQUEST_METHOD"] = initial_method
-                request.method = initial_method
+        return await get_response(request)
 
-            return await get_response(request)
+    def sync_middleware(request: HttpRequest) -> Any:
+        if (
+            request.method in ("PUT", "PATCH")
+            and request.content_type != "application/json"
+        ):
+            initial_method = request.method
+            request.method = "POST"
+            request.META["REQUEST_METHOD"] = "POST"
+            request._load_post_and_files()
+            request.META["REQUEST_METHOD"] = initial_method
+            request.method = initial_method
 
-    else:
+        return get_response(request)
 
-        def middleware(request: HttpRequest) -> HttpResponse:
-            if (
-                request.method in ("PUT", "PATCH")
-                and request.content_type != "application/json"
-            ):
-                initial_method = request.method
-                request.method = "POST"
-                request.META["REQUEST_METHOD"] = "POST"
-                request._load_post_and_files()
-                request.META["REQUEST_METHOD"] = initial_method
-                request.method = initial_method
-
-            return get_response(request)
-
-    return middleware
+    return (
+        async_middleware if asyncio.iscoroutinefunction(get_response) else sync_middleware
+    )
