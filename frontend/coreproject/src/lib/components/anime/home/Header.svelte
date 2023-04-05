a3ece3f083846bc437af2ebc5d62c211779994ed
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-05 21:13:58 +0600

    [`Frontend`] : patches

--- frontend/coreproject/src/lib/components/anime/home/Header.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import AnimeCore from '$icons/AnimeCore.svelte';
-	import Logo from '$icons/Logo.svelte';
-
-	import { Avatar } from '@skeletonlabs/skeleton';
-</script>
-
--- frontend/coreproject/src/routes/+layout.svelte
+++ frontend/coreproject/src/routes/+layout.svelte
@@ -10,12 +10,13 @@
 
 	import AnimeCore from '$icons/AnimeCore.svelte';
 	import Logo from '$icons/Logo.svelte';
+	import Search from '$icons/Search.svelte';
 </script>
 
 <div class="h-screen overflow-hidden">
 	<AppShell>
-		<svelte:fragment slot="header"
-			><div class="h-24 flex justify-between">
+		<svelte:fragment slot="header">
+			<div class="h-24 flex justify-between">
 				<Logo height="48" width="33.6" class="mt-12 mx-12" />
 
 				<AnimeCore width="168" height="33.6" class="self-center" />
@@ -25,12 +26,17 @@
 				</div>
 			</div>
 		</svelte:fragment>
-		<svelte:fragment slot="sidebarLeft"
-			><div class="w-32 flex justify-center">
-				<button type="button" class="btn-icon variant-filled">(icon)</button>
+		<svelte:fragment slot="sidebarLeft">
+			<div class="w-32 flex justify-center mt-14 flex-col items-center">
+				<button type="button" class="btn-icon bg-warning-400 rounded-lg p-0">
+					<Search color="black" width="20" height="20" />
+				</button>
+
+				<button type="button" class="btn-icon bg-initial rounded-lg p-0 mt-14"> Home </button>
 			</div>
 		</svelte:fragment>
 
+		<!-- Page contents go here  -->
 		<slot />
 	</AppShell>
 </div>
