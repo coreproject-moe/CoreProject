a4113682d514d024784abbf90fa8abacad17d80e
sssuneeth <suneeth.since2005@gmail.com>
2023-04-09 23:45:52 +0530

    [`Frontend`] : Fix some info page responsive layout

--- frontend/AnimeCore/src/lib/components/pages/anime_info/AnimeMainDetails.svelte
+++ frontend/AnimeCore/src/lib/components/pages/anime_info/AnimeMainDetails.svelte
@@ -13,122 +13,177 @@
 	import Share from '$icons/Share.svelte';
 	import SettingsOutline from '$icons/SettingsOutline.svelte';
 	import Edit from '$icons/Edit.svelte';
+
+	const icon_mapping = {
+		anime_options: {
+			read: {
+				icon: {
+					component: Read,
+					width: 22,
+					height: 22,
+					color: "bg-surface-500"
+				}
+			},
+			listen: {
+				icon: {
+					component: Listen,
+					width: 22,
+					height: 22,
+					color: "bg-surface-500"
+				}
+			},
+		},
+		user_options_icons : {
+			video: {
+				icon: {
+					component: Video,
+					variant: false,
+					width: 20,
+					height: 20
+				}
+			},
+			edit: {
+				icon: {
+					component: Edit,
+					variant: "with_underline_around_pencil",
+					width: 20,
+					height: 20
+				}
+			},
+			download: {
+				icon: {
+					component: Download,
+					variant: false,
+					width: 20,
+					height: 20
+				}
+			},
+			share: {
+				icon: {
+					component: Share,
+					variant: false,
+					width: 20,
+					height: 20
+				}
+			}
+		}
+	}
 </script>
 
 <div class="grid grid-cols-12 items-start">
-	<div class="col-span-10 grid grid-cols-12 items-end xl:gap-10">
+	<div class="col-span-10 grid grid-cols-12 items-end md:gap-10">
 		<div class="col-span-7 flex items-end gap-10">
-			<img class="xl:w-60 2xl:w-80 xl:rounded-xl 2xl:rounded-3xl" src={anime.cardBackgroundImage} alt={anime.titles.eng} />
+			<img class="md:w-[15vw] md:rounded-xl" src={anime.cardBackgroundImage} alt={anime.titles.eng} />
 			<div>
-				<h2 class="font-bold 2xl:!text-5xl 3xl:!text-6xl">{anime.titles.eng}</h2>
+				<span class="font-bold md:text-[3vw]">{anime.titles.eng}</span>
 
-				<p class="pt-3 3xl:mt-5 flex items-center gap-2 text-surface-100">
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] font-medium after:content-['▪'] after:ml-1">{anime.titles.japanese}</span>
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] font-medium after:content-['▪'] after:ml-1">{anime.titles.eng}</span>
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] font-medium">{anime.titles.others}</span>
+				<p class="flex items-center gap-2 text-surface-100">
+					<span class="md:text-[0.8vw] font-medium after:content-['▪'] after:ml-1">{anime.titles.japanese}</span>
+					<span class="md:text-[0.8vw] font-medium after:content-['▪'] after:ml-1">{anime.titles.eng}</span>
+					<span class="md:text-[0.8vw] font-medium">{anime.titles.others}</span>
 				</p>
 
-				<p class="pt-4 flex flex-wrap items-center gap-2 pr-5">
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] 2xl:!leading-none font-semibold after:content-['▪'] after:ml-1">
+				<p class="pt-2 flex flex-wrap items-center gap-2 pr-5">
+					<span class="md:text-[0.9vw] md:leading-none font-semibold after:content-['▪'] after:ml-1">
 						{anime.type}
 					</span>
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] 2xl:!leading-none font-semibold after:content-['▪'] after:ml-1">
+					<span class="md:text-[0.9vw] md:leading-none font-semibold after:content-['▪'] after:ml-1">
 						{anime.episodes} eps
 					</span>
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] 2xl:!leading-none font-semibold after:content-['▪'] after:ml-1">
+					<span class="md:text-[0.9vw] md:leading-none font-semibold after:content-['▪'] after:ml-1">
 						{anime.status}</span>
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] 2xl:!leading-none font-semibold after:content-['▪'] after:ml-1">
+					<span class="md:text-[0.9vw] md:leading-none font-semibold after:content-['▪'] after:ml-1">
 						{anime.premiered}
 					</span>
-					<span class="xl:text-xs 2xl:text-sm 3xl:!text-[16px] 2xl:!leading-none font-semibold">{anime.studio}</span>
+					<span class="md:text-[0.9vw] md:leading-none font-semibold">{anime.studio}</span>
 				</p>
 
-				<div class="xl:mt-5 2xl:mt-7 3xl:mt-12 flex items-center xl:gap-3 2xl:gap-5">
+				<div class="md:mt-10 flex items-center md:gap-4">
 					<button
 						type="button"
-						class="btn 2xl:btn-md 3xl:btn-lg xl:h-16 2xl:h-20 justify-start xl:rounded-lg 2xl:rounded-xl bg-primary-500 xl:text-xs 2xl:text-xl font-bold text-white"
+						class="btn md:h-[8vh] md:w-[8vw] justify-center md:rounded-lg bg-primary-500 md:text-[1vw] font-bold text-white"
 					>
 						<PlayCircle width="30" height="30" color="white" />
 						<div class="flex flex-col items-start">
-							<span class="leading-5">Watch</span>
-							<span class="xl:text-[0.6rem] 2xl:text-lg font-normal leading-none opacity-70">Ep 01</span>
+							<span class="md:leading-5">Watch</span>
+							<span class="md:text-[0.8vw] font-normal leading-none text-surface-100">Ep 01</span>
 						</div>
 					</button>
 
-					<button
-						type="button"
-						class="btn xl:h-16 2xl:h-20 2xl:w-20 3xl:w-24 flex-col gap-1 xl:rounded-lg 2xl:rounded-xl bg-secondary-100 xl:text-xs 2xl:text-base 3xl:text-xl xl:font-bold 2xl:font-semibold text-surface-500"
-					>
-						<Read width="22" height="22" color="bg-surface-500" />
-						Read
-					</button>
+					{#each Object.entries(icon_mapping.anime_options) as item}
+						{@const item_name = item[0]}
+						{@const item_icon = item[1].icon}
 
-					<button
+						{@const component = item_icon.component}
+						{@const component_width = item_icon.width}
+						{@const component_height = item_icon.height}
+						{@const component_color = item_icon.color}
+
+						<button
 						type="button"
-						class="btn xl:h-16 2xl:h-20 2xl:w-20 3xl:w-24 flex-col gap-1 xl:rounded-lg 2xl:rounded-xl bg-secondary-100 xl:text-xs 2xl:text-base 3xl:text-xl xl:font-bold 2xl:font-semibold text-surface-500"
-					>
-						<Listen width="22" height="22" color="bg-surface-500" />
-						Listen
-					</button>
+						class="btn md:h-[8vh] md:w-[5vw] flex-col gap-1 md:rounded-lg bg-secondary-100 md:text-[1vw] md:font-semibold text-surface-500 capitalize"
+						>
+							<svelte:component 
+								this={component}
+								width={component_width}
+								height={component_height}
+								color={component_color}
+							/>
+							{item_name}
+						</button>
+					{/each}
 				</div>
 
-				<div class="user_options mt-5 flex xl:gap-2 2xl:gap-3">
-					<button
-						type="button"
-						class="btn-icon xl:btn-icon-sm 3xl:btn-icon-lg 2xl:w-12 3xl:w-10 2xl:h-12 3xl:h-10 xl:rounded 2xl:rounded-lg bg-warning-400 p-0 text-surface-500"
-					>
-						<Video width="20" />
-					</button>
+				<div class="mt-5 flex md:gap-2">
+					{#each Object.entries(icon_mapping.user_options_icons) as item}
+						{@const item_icon = item[1].icon}
 
-					<button
-						type="button"
-						class="btn-icon xl:btn-icon-sm 2xl:btn-icon-lg 2xl:w-12 3xl:w-10 2xl:h-12 3xl:h-10 xl:rounded 2xl:rounded-lg bg-warning-400 p-0 text-surface-500"
-					>
-						<Edit width="20" variant="with_underline_around_pencil" />
-					</button>
+						{@const component = item_icon.component}
+						{@const component_width = item_icon.width}
+						{@const component_height = item_icon.height}
+						{@const component_variant = item_icon.variant}
 
-					<button
-						type="button"
-						class="btn-icon xl:btn-icon-sm 2xl:btn-icon-lg 2xl:w-12 3xl:w-10 2xl:h-12 3xl:h-10 xl:rounded 2xl:rounded-lg bg-warning-400 p-0 text-surface-500"
-					>
-						<Download width="20" />
-					</button>
-
-					<button
-						type="button"
-						class="btn-icon xl:btn-icon-sm 2xl:btn-icon-lg 2xl:w-12 3xl:w-10 2xl:h-12 3xl:h-10 xl:rounded 2xl:rounded-lg bg-warning-400 p-0 text-surface-500"
-					>
-						<Share width="20" />
-					</button>
+						<button
+							type="button"
+							class="btn-icon md:w-[2.5vw] md:rounded-lg bg-warning-400 p-0 text-surface-500"
+						>
+							<svelte:component
+								this = {component}
+								width = {component_width}
+								height = {component_height}
+								variant = {component_variant}
+							 />
+						</button>
+					{/each}
 				</div>
 			</div>
 		</div>
 
 		<div class="col-span-5 pr-8">
 			<div class="flex items-center gap-4">
-				<h4 class="font-semibold 2xl:!text-2xl 3xl:!text-3xl">
+				<span class="font-semibold md:text-[1.6vw]">
 					Synopsis
-				</h4>
+				</span>
 				<SettingsOutline width="15" height="15" class="opacity-75" />
 			</div>
 
 			<ScrollArea
 				offsetScrollbar
-				parentClass="mt-4 2xl:mt-6 3xl:mt-8"
-				class="xl:max-h-40 3xl:max-h-48 text-justify xl:!text-xs 2xl:!text-base 2xl:!leading-normal text-surface-50"
+				parentClass="mt-5"
+				class="md:max-h-36 text-justify md:text-[1vw] md:leading-tight text-surface-50"
 			>
 				{anime.description}
 			</ScrollArea>
 
-			<div class="xl:mt-3 2xl:mt-5 3xl:mt-10 flex gap-2">
+			<div class="md:mt-3 flex gap-2">
 				{#each anime.generes as genere}
-					<div class="rounded bg-surface-500 p-1 px-3 xl:text-xs 2xl:!text-base 3xl:!text-lg text-white">
+					<div class="rounded bg-surface-500 p-1 px-3 md:text-[0.8vw] text-white">
 						{genere}
 					</div>
 				{/each}
 			</div>
 
-			<div class="xl:mt-3 3xl:mt-6 flex w-max items-center gap-2 rounded bg-white/20 p-1 px-3 3xl:p-2 3xl:px-5 xl:text-[10px] 2xl:text-xs 3xl:!text-[15px]">
+			<div class="md:mt-3 flex w-max items-center gap-2 rounded bg-white/20 p-1 px-3 md:text-[0.7vw]">
 				<div class="flex items-center gap-1">
 					Score:
 					<span class="text-warning-400">{anime.score}</span>
@@ -149,7 +204,7 @@
 		</div>
 	</div>
 
-	<div class="col-span-2 xl:mt-10 2xl:mt-3 3xl:mt-10">
+	<div class="col-span-2 md:mt-7">
 		<SidebarDetails {anime} />
 	</div>
 </div>
--- frontend/AnimeCore/src/lib/components/pages/anime_info/SidebarDetails.svelte
+++ frontend/AnimeCore/src/lib/components/pages/anime_info/SidebarDetails.svelte
@@ -11,38 +11,38 @@
 
 <div>
 	<div class="flex items-center gap-4">
-		<h4 class="font-semibold 2xl:!text-2xl 3xl:!text-3xl">Ratings</h4>
+		<span class="font-semibold md:text-[1.6vw]">Ratings</span>
 		<SettingsOutline width="15" height="15" class="opacity-75" />
 	</div>
 
-	<div class="xl:mt-4 2xl:mt-8">
+	<div class="md:mt-3">
 		<div class="divide-x flex items-end gap-3">
-			<span class="xl:text-3xl 2xl:text-4xl 3xl:text-5xl font-bold underline decoration-white/25 underline-offset-[15px]"
+			<span class="md:text-[2.5vw] font-bold underline decoration-white/25 underline-offset-[2vh]"
 				>{Math.ceil((anime.rating / 5) * 100)}%</span
 			>
-			<span class="xl:text-xs 2xl:text-base 3xl:text-lg pl-2">{anime.totalResponse} Ratings</span>
+			<span class="md:text-[0.8vw] pl-2">{anime.totalResponse} Ratings</span>
 		</div>
 
-		<div class="xl:mt-3 2xl:mt-5 3xl:mt-7">
+		<div class="md:mt-3">
 			{#each anime.milestones as milestone}
-				<div class="flex items-center xl:gap-2 2xl:gap-4 2xl:mt-1">
-					<span class="2xl:text-lg 3xl:text-xl">#{milestone.value}</span>
-					<span class="xl:text-xs 2xl:text-sm 3xl:text-lg text-surface-50">{milestone.title}</span>
+				<div class="flex items-center md:gap-2">
+					<span class="md:text-[1.3vw]">#{milestone.value}</span>
+					<span class="md:text-[1vw] text-surface-50">{milestone.title}</span>
 				</div>
 			{/each}
 		</div>
 
 		<button
-			class="btn xl:btn-sm 3xl:btn-lg xl:mt-3 gap-2 rounded bg-secondary-100 px-2 xl:py-1 2xl:px-3 3xl:px-5 text-xs text-surface-500"
+			class="btn md:mt-3 gap-2 rounded bg-secondary-100 md:w-[12vw] md:h-[3vh] p-0 md:text-[1vw] text-surface-500"
 		>
 			<TrendingUp width="15" height="15" />
 			Detailed Distribution
 		</button>
 
-		<div class="rating xl:mt-2 2xl:mt-3 3xl:mt-10">
-			<span class="xl:text-xs 2xl:text-lg">Your rating</span>
+		<div class="md:mt-3">
+			<span class="md:text-[1vw]">Your rating</span>
 
-			<div class="ratings flex items-center gap-3">
+			<div class="flex items-center gap-3">
 				<div class="flex gap-2">
 					{#each Array(Math.floor(anime.rating)) as _}
 						<Star variation="solid" size="18" class="text-white" />
@@ -51,8 +51,8 @@
 						<Star variation="outline" size="18" class="text-white" />
 					{/each}
 				</div>
-				<span class="xl:text-xs 2xl:text-lg font-bold">{Math.ceil((anime.rating / 5) * 100)}%</span>
-				<button class="btn-icon xl:btn-icon-sm rounded bg-secondary-100 p-1 text-surface-500">
+				<span class="md:text-[1.3vw] font-bold">{Math.ceil((anime.rating / 5) * 100)}%</span>
+				<button class="btn-icon rounded bg-secondary-100 p-1 text-surface-500">
 					<Edit
 						width="13"
 						height="13"
@@ -63,7 +63,7 @@
 			</div>
 		</div>
 
-		<button class="review btn btn-sm mt-2 flex items-center gap-2 p-0 xl:text-xs 2xl:text-lg">
+		<button class="btn btn-sm mt-2 flex items-center gap-2 p-0 md:text-[1vw]">
 			Add a review
 			<ExternalLink width="15" height="15" />
 		</button>
