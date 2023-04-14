fbac12a0cf1b45c7d526bbd38206194e20ea49f2
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-14 07:00:06 +0600

    Fix signals

--- backend/django_core/apps/images/apps.py
+++ backend/django_core/apps/images/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.images"
+
+    def ready(self) -> None:
+        from . import signals as signals
