4548be42dcf6c2b0739032454d9d550a07ceb517
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-21 12:47:55 +0600

    Update anime.py

--- backend/django_core/builder/anime.py
+++ backend/django_core/builder/anime.py
@@ -3,9 +3,13 @@ from typing import Literal
 
 
 class AnimeBuilder:
+
     @staticmethod
-    def build_list(name: Literal["anime"] | Literal["character"]):
+    def build_list():
         alphabet_list = list(string.ascii_uppercase + ".")
         return [
             f"https://myanimelist.net/anime.php?letter={letter}" for letter in alphabet_list
         ]
+
+    def build_urls():
+        pass
