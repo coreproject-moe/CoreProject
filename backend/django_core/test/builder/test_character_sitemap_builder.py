c2a5948e8c4515ac76591015ad0f905d8fc63b3a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-21 10:43:10 +0600

    add

--- backend/django_core/test/builder/test_character_sitemap_builder.py
+++
@@ -1,9 +0,0 @@
-from builder.sitemap import SitemapBuilder
-
-
-def test_character_sitemap_builder():
-    builder = SitemapBuilder("https://myanimelist.net/sitemap/index.xml")
-    data = builder.build(filter="character")
-    assert "https://myanimelist.net/character/1/Spike_Spiegel" in data
-    assert "https://myanimelist.net/character/5/Ichigo_Kurosaki" in data
-    assert "https://myanimelist.net/character/144103/Kokona_no_Obaachan" in data
