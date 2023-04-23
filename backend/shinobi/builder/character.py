b7653768359f31c9616862fd3283b4982f21a0d8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-23 09:31:06 +0600

    add

---
+++ backend/shinobi/builder/character.py
@@ -0,0 +1,101 @@
+import string
+
+import httpx
+from selectolax.parser import HTMLParser
+
+from shinobi.decorators.return_error_decorator import return_on_error
+from shinobi.utilities.regex import RegexHelper
+
+
+class CharacterBuilder:
+    def __init__(self) -> None:
+        self.anchors: list[str] = []
+        self.visited_urls: set[str] = set()
+
+        # Reusuable clients
+        self.client = httpx.Client()
+
+        # Facades
+        self.regex_helper = RegexHelper()
+
+    @staticmethod
+    def get_parser(html: str) -> HTMLParser:
+        return HTMLParser(html)
+
+    @return_on_error("")
+    def has_next_page(self, html: str) -> bool:
+        parser = self.get_parser(html)
+        node = parser.css_first("div.normal_header > div.fl-r > div > span.bgColor1")
+
+        select_node_list = node.text().split(" ")
+        for item in select_node_list:
+            if self.regex_helper.check_if_string_contains_bracket(item):
+                bracketed_element_position = select_node_list.index(item)
+                break
+
+        if bracketed_element_position == len(select_node_list) - 1:
+            return False
+
+        return True
+
+    def get_all_pages_in_span_tag(self, html):
+        parser = self.get_parser(html)
+        node = (
+            parser.css_first("div.normal_header > div.fl-r > div > span.bgColor1")
+            .select("a")
+            .matches
+        )
+        anchors = [anchor.attributes["href"] for anchor in node]
+        return anchors
+
+    def _build_word_list(self) -> list[str]:
+        alphabet_list = list(string.ascii_uppercase)
+        return [
+            f"https://myanimelist.net/character.php?letter={letter}"
+            for letter in alphabet_list
+        ]
+
+    def _build_urls(self, url: str) -> None:
+        print(url)
+        self.visited_urls.add(url)
+
+        res = self.client.get(url)
+        html = res.content
+
+        anime_nodes = self.get_parser(html).css("a[href*='/anime/']")
+        for anime_node in anime_nodes:
+            anime_href = anime_node.attributes["href"]
+            if (
+                anime_href not in self.anchors
+                and self.regex_helper.check_if_string_contains_integer(anime_href)
+            ):
+                self.anchors.append(anime_href)
+
+        if self.has_next_page(html):
+            all_pages = self.get_all_pages_in_span_tag(html)
+            for item in all_pages:
+                myanimelist_formated_url = "https://myanimelist.net" + item
+                if myanimelist_formated_url not in self.visited_urls:
+                    next_url = myanimelist_formated_url
+                    break
+
+            self._build_urls(next_url)
+
+    def _build_ids(self) -> list[int]:
+        return [self.regex_helper.get_first_integer_from_url(item) for item in self.anchors]
+
+    def build_dictionary(
+        self, excluded_ids: list[int] | None = None, sort=False
+    ) -> dict[int, str]:
+        for url in self._build_word_list():
+            self._build_urls(url)
+
+        dictionary = dict(zip(self._build_ids(), self.anchors))
+
+        if sort:
+            dictionary = dict(sorted(dictionary.items()))
+
+        if excluded_ids:
+            dictionary = {k: v for k, v in dictionary.items() if k not in excluded_ids}
+
+        return dictionary
---
+++ backend/shinobi/test/builder/test_character_builder.py
@@ -0,0 +1,14 @@
+from shinobi.builder.character import CharacterBuilder
+
+
+def test_anime_dictionary_builder():
+    builder = CharacterBuilder()
+    dictionary = builder.build_dictionary(excluded_ids=[54915])
+
+    dictionary_keys = list(dictionary.keys())
+    dictionary_values = list(dictionary.values())
+
+    assert 54915 not in list(dictionary_keys)
+
+    assert "https://myanimelist.net/character/117223/Aqua" in dictionary_values
+    assert "https://myanimelist.net/character/206276/Hitori_Gotou" in dictionary_values
