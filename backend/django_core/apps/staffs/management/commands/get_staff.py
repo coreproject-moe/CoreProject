5c0cddfc0083f54849926306d43a8d196183be08
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-04-24 19:04:23 +0600

    add

--- backend/django_core/apps/staffs/management/commands/get_staff.py
+++ backend/django_core/apps/staffs/management/commands/get_staff.py
@@ -19,22 +19,22 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "staff_number",
+            "staff_id",
             type=int,
             help="The staff number to get information for",
         )
 
     def handle(self, *args, **options) -> NoReturn:
-        staff_number: int = options["staff_number"]
-        res = self.client.get(f"https://myanimelist.net/people/{staff_number}")
+        staff_id: int = options["staff_id"]
+        res = self.client.get(f"https://myanimelist.net/people/{staff_id}")
 
         parser = StaffParser(res.content)
         data_dictionary = parser.build_dictionary()
 
         try:
-            staff_instance = StaffModel.objects.get(mal_id=staff_number)
+            staff_instance = StaffModel.objects.get(mal_id=staff_id)
         except StaffModel.DoesNotExist:
-            self.stdout.write(f"No StaffModel found for {self.style.ERROR(staff_number)}")
+            self.stdout.write(f"No StaffModel found for {self.style.ERROR(staff_id)}")
             sys.exit(1)
 
         for attr, value in data_dictionary.items():
@@ -42,4 +42,4 @@ class Command(BaseCommand):
                 setattr(staff_instance, attr, value)
 
         staff_instance.save()
-        self.stdout.write(f"Successfully got info for {self.style.SUCCESS(staff_number)}")
+        self.stdout.write(f"Successfully got info for {self.style.SUCCESS(staff_id)}")
--- backend/django_core/apps/staffs/tasks.py
+++ backend/django_core/apps/staffs/tasks.py
@@ -1,21 +1,20 @@
 from celery import shared_task
-from shinobi.builder.character import CharacterBuilder
 
 from django.core.management import call_command
 from django.db.models import Q
 from django.utils import timezone
-
-from .models import CharacterModel
+from shinobi.builder.staff import StaffBuilder
+from .models import StaffModel
 
 
 @shared_task()
-def get_perodic_character():
-    builder = CharacterBuilder()
+def get_perodic_staff():
+    builder = StaffBuilder()
 
-    instances = CharacterModel.objects.filter(
+    instances = StaffModel.objects.filter(
         Q(updated_at__gte=timezone.now() - timezone.timedelta(days=7)) & Q(is_locked=False)
     )
     dictionary = builder.build_dictionary(excluded_ids=instances.values_list(flat=True))
 
-    for character in list(dictionary.keys()):
-        call_command("get_character", character_id=character)
+    for staff in list(dictionary.keys()):
+        call_command("get_staff", staff_id=staff)
