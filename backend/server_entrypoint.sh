221fa67dc8533861953f8304b651576610018a4b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-05-03 14:38:38 +0600

    add

--- backend/server_entrypoint.sh
+++ backend/server_entrypoint.sh
@@ -17,7 +17,7 @@ done
 python manage.py collectstatic --clear --link --no-input
 
 # python manage.py createsuperuser --noinput
-gunicorn core.asgi:application -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 --access-logfile /code/logs/gunicorn.log --error-logfile - --user nobody
+gunicorn core.asgi:application -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 --access-logfile /code/logs/gunicorn.log --error-logfile - --user nobody --group nogroup
 
 # for debug
 #python manage.py runserver 0.0.0.0:8000
