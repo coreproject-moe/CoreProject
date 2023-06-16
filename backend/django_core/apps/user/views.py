e88091d992d47fa82a368b7a03e513fe7e1046ee
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-16 16:14:34 +0600

    [`Backend`] : Remove django views for `username_validaty` (#2219)
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- backend/django_core/apps/user/views.py
+++ backend/django_core/apps/user/views.py
@@ -1,15 +1,12 @@
 import hashlib
 import textwrap
-from http import HTTPStatus
 
 import requests
-from django.conf import settings
 from django.core.management.utils import get_random_secret_key
 from django.core.validators import URLValidator
 from django.http import FileResponse, HttpRequest, HttpResponse, StreamingHttpResponse
 from django.shortcuts import render
 
-from .forms import UsernameWithDiscriminatorForm
 from .models import CustomUser
 
 # Create your views here
@@ -18,7 +15,7 @@ from .models import CustomUser
 async def avatar_view(
     request: HttpRequest,
     user_id: int,
-) -> StreamingHttpResponse | HttpResponse:
+) -> StreamingHttpResponse | HttpResponse | FileResponse:
     try:
         user = await CustomUser.objects.aget(pk=user_id)
     except CustomUser.DoesNotExist:
@@ -55,7 +52,7 @@ async def avatar_view(
         )
 
     except Exception as e:
-        response = HttpResponse(
+        return HttpResponse(
             textwrap.dedent(
                 f"""
                     Please Check your <b>email</b> string.
@@ -67,52 +64,3 @@ async def avatar_view(
                 """
             )
         )
-
-    return response
-
-
-def username_and_discriminator_validity_checker_view(
-    request: HttpRequest,
-) -> HttpRequest:
-    form = UsernameWithDiscriminatorForm(request.POST or None)
-
-    if request.method == "POST" and form.is_valid():
-        if CustomUser.objects.filter(username=form.cleaned_data.get("username")).distinct(
-            "discriminator"
-        ).count() > int("9" * settings.DISCRIMINATOR_LENGTH):
-            return HttpResponse("Your username is too popular. Please pick another one")
-
-        if (
-            CustomUser.objects.get_username_with_discriminator()
-            .filter(
-                username_with_discriminator=f"""{
-                form
-                .cleaned_data
-                .get('username')
-            }#{
-                str(
-                    form
-                    .cleaned_data
-                    .get('discriminator')
-                ).zfill(
-                    settings
-                    .DISCRIMINATOR_LENGTH
-                )
-            }"""
-            )
-            .exists()
-        ):
-            # Return status code 406 if username with discriminator exists
-            return HttpResponse(
-                "An username with the said discriminator is found",
-                status=HTTPStatus.FOUND,
-            )
-
-        else:
-            # Return status code 200 if username with discriminator is available
-            return HttpResponse(
-                "Username found",
-                status=HTTPStatus.NOT_FOUND,
-            )
-    else:
-        return form.errors
