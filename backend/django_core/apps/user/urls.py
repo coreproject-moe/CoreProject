4965e3a89226a8ab90b15d538a1384a174be3782
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-16 15:42:26 +0600

    add

--- backend/django_core/apps/user/urls.py
+++ backend/django_core/apps/user/urls.py
@@ -4,9 +4,4 @@ from . import views
 
 urlpatterns = [
     path("avatar/<int:user_id>", views.avatar_view, name="avatar_view"),
-    path(
-        "__username_validity_endpoint__/",
-        views.username_and_discriminator_validity_checker_view,
-        name="username_and_discriminator_validity_checker_view",
-    ),
 ]
