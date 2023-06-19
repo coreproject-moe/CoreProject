827d12308da9eb4ea3e1d20ebe7e3cf2f453d85d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-19 21:27:01 +0600

    Update histogram.py

--- backend/django_core/apps/api/views/stats/histogram.py
+++ backend/django_core/apps/api/views/stats/histogram.py
@@ -11,9 +11,14 @@ from ninja import Router
 router = Router()
 
 
-@router.get("/anime", response=dict[int, dict[int, dict[Literal["count"], int]]])
-def anime_histogram(request) -> dict[int, dict[int, dict[Literal["count"], int]]]:
-    date_objects = AnimeModel.objects.all().values_list("created_at")
+# Types
+RETURN_TYPE = dict[int, dict[int, dict[Literal["count"], int]]]
+
+
+def get_json_data_given_model(
+    model: AnimeModel | CharacterModel | ProducerModel | StaffModel,
+) -> RETURN_TYPE:
+    date_objects = model.objects.all().values_list("created_at")
     pd_dataframe = pd.DataFrame(date_objects, columns=["created_at"])
     pd_dataframe["created_at"] = pd.to_datetime(pd_dataframe["created_at"])
 
@@ -28,7 +33,7 @@ def anime_histogram(request) -> dict[int, dict[int, dict[Literal["count"], int]]
         orient="records"
     )  # [{"month": 6, "year": 2023, "count": 1}]
 
-    returnable_dictionary: dict[int, dict[int, dict[Literal["count"], int]]] = {}
+    returnable_dictionary: dict[int, RETURN_TYPE] = {}
     for item in histogram_json:
         year = item["year"]
         month = item["month"]
@@ -37,95 +42,22 @@ def anime_histogram(request) -> dict[int, dict[int, dict[Literal["count"], int]]
         returnable_dictionary.setdefault(year, {})
         returnable_dictionary[year].setdefault(month, {"count": count})
 
-    return returnable_dictionary
+
+@router.get("/anime", response=RETURN_TYPE)
+def anime_histogram(request: HttpRequest) -> RETURN_TYPE:
+    return get_json_data_given_model(AnimeModel)
 
 
-@router.get("/character", response=dict[int, dict[int, dict[Literal["count"], int]]])
-def character_histogram(
-    request: HttpRequest,
-) -> dict[int, dict[int, dict[Literal["count"], int]]]:
-    date_objects = CharacterModel.objects.all().values_list("created_at")
-    pd_dataframe = pd.DataFrame(date_objects, columns=["created_at"])
-    pd_dataframe["created_at"] = pd.to_datetime(pd_dataframe["created_at"])
-
-    # Year and month
-    pd_dataframe["month"] = pd_dataframe["created_at"].dt.month
-    pd_dataframe["year"] = pd_dataframe["created_at"].dt.year
-
-    histogram_data = (
-        pd_dataframe.groupby(["month", "year"]).size().reset_index(name="count")
-    )
-    histogram_json = histogram_data.to_dict(
-        orient="records"
-    )  # [{"month": 6, "year": 2023, "count": 1}]
-
-    returnable_dictionary: dict[int, dict[int, dict[Literal["count"], int]]] = {}
-    for item in histogram_json:
-        year = item["year"]
-        month = item["month"]
-        count = item["count"]
-
-        returnable_dictionary.setdefault(year, {})
-        returnable_dictionary[year].setdefault(month, {"count": count})
-
-    return returnable_dictionary
+@router.get("/character", response=RETURN_TYPE)
+def character_histogram(request: HttpRequest) -> RETURN_TYPE:
+    return get_json_data_given_model(CharacterModel)
 
 
-@router.get("/staffs", response=dict[int, dict[int, dict[Literal["count"], int]]])
-def character_histogram(
-    request: HttpRequest,
-) -> dict[int, dict[int, dict[Literal["count"], int]]]:
-    date_objects = StaffModel.objects.all().values_list("created_at")
-    pd_dataframe = pd.DataFrame(date_objects, columns=["created_at"])
-    pd_dataframe["created_at"] = pd.to_datetime(pd_dataframe["created_at"])
-
-    # Year and month
-    pd_dataframe["month"] = pd_dataframe["created_at"].dt.month
-    pd_dataframe["year"] = pd_dataframe["created_at"].dt.year
-
-    histogram_data = (
-        pd_dataframe.groupby(["month", "year"]).size().reset_index(name="count")
-    )
-    histogram_json = histogram_data.to_dict(
-        orient="records"
-    )  # [{"month": 6, "year": 2023, "count": 1}]
-
-    returnable_dictionary: dict[int, dict[int, dict[Literal["count"], int]]] = {}
-    for item in histogram_json:
-        year = item["year"]
-        month = item["month"]
-        count = item["count"]
-
-        returnable_dictionary.setdefault(year, {})
-        returnable_dictionary[year].setdefault(month, {"count": count})
-
-    return returnable_dictionary
+@router.get("/staff", response=dict[int, RETURN_TYPE])
+def staff_histogram(request: HttpRequest) -> dict[int, RETURN_TYPE]:
+    return get_json_data_given_model(StaffModel)
 
 
-@router.get("/producers", response=dict[int, dict[int, dict[Literal["count"], int]]])
-def anime_histogram(request) -> dict[int, dict[int, dict[Literal["count"], int]]]:
-    date_objects = ProducerModel.objects.all().values_list("created_at")
-    pd_dataframe = pd.DataFrame(date_objects, columns=["created_at"])
-    pd_dataframe["created_at"] = pd.to_datetime(pd_dataframe["created_at"])
-
-    # Year and month
-    pd_dataframe["month"] = pd_dataframe["created_at"].dt.month
-    pd_dataframe["year"] = pd_dataframe["created_at"].dt.year
-
-    histogram_data = (
-        pd_dataframe.groupby(["month", "year"]).size().reset_index(name="count")
-    )
-    histogram_json = histogram_data.to_dict(
-        orient="records"
-    )  # [{"month": 6, "year": 2023, "count": 1}]
-
-    returnable_dictionary: dict[int, dict[int, dict[Literal["count"], int]]] = {}
-    for item in histogram_json:
-        year = item["year"]
-        month = item["month"]
-        count = item["count"]
-
-        returnable_dictionary.setdefault(year, {})
-        returnable_dictionary[year].setdefault(month, {"count": count})
-
-    return returnable_dictionary
+@router.get("/producer", response=dict[int, RETURN_TYPE])
+def producer_histogram(request: HttpRequest) -> dict[int, RETURN_TYPE]:
+    return get_json_data_given_model(ProducerModel)
