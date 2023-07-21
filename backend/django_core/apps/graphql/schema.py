ca939062ee9c5ec2abcefb45f6a40aed8b03ce75
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-21 13:15:04 +0600

    Update schema.py

--- backend/django_core/apps/graphql/schema.py
+++ backend/django_core/apps/graphql/schema.py
@@ -2,17 +2,18 @@ import strawberry
 import strawberry_django
 from strawberry_django.permissions import IsSuperuser
 
+from gqlauth.core.types_ import GQLAuthError, GQLAuthErrors
+from gqlauth.core.utils import get_user
 from gqlauth.user import arg_mutations as mutations
 from strawberry.schema.config import StrawberryConfig
 from strawberry_django.optimizer import DjangoOptimizerExtension
-
+from strawberry.types import Info
 from .mutations.anime import AnimeInput
 
 from .types.anime import Anime
 from .types.character import Character
 from .types.producer import Producer
 from .types.staff import Staff
-
 
 
 @strawberry.type
@@ -29,14 +30,12 @@ class Mutations:
     verify_account = mutations.VerifyAccount.field
     token_auth = mutations.ObtainJSONWebToken.field
 
-    # Anime related things
-    create_anime: Anime = strawberry_django.mutations.create(
-        AnimeInput,
-        # We allow only superuser to create data
-        extensions=[
-            IsSuperuser(),
-        ],
-    )
+    @strawberry.mutation
+    def create_anime(self, data: AnimeInput) -> Anime:
+        user = get_user(data)
+        if not user.is_superuser:
+            raise GQLAuthError(code=GQLAuthErrors.UNAUTHENTICATED)
+        print(data)
 
 
 schema = strawberry.Schema(
