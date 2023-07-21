79d68aa83565f222cf19f0426f70b6a22067896f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-21 19:47:44 +0600

    Update login.py

--- backend/django_core/apps/api/views/user/login.py
+++ backend/django_core/apps/api/views/user/login.py
@@ -3,13 +3,14 @@ from apps.user.backends import EmailOrUsernameModelBackend
 from django.http import Http404, HttpRequest
 from ninja import Form, Router
 from pydantic import EmailStr
-
+from django_ratelimit.decorators import ratelimit
 from ...schemas.user.login import LoginSchema
 
 router = Router()
 
 
 @router.post("/login", response=LoginSchema)
+@ratelimit(key="ip", rate="1/s")
 def post_user_login_info(
     request: HttpRequest,
     username: str | EmailStr = Form(...),
