1e738df59622b6d359e6d2557e4854443a17d21d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-21 13:47:27 +0600

    Update schema.py

--- backend/django_core/apps/graphql/schema.py
+++ backend/django_core/apps/graphql/schema.py
@@ -1,15 +1,16 @@
 import strawberry
 import strawberry_django
-from strawberry_django.permissions import IsSuperuser
 
-from gqlauth.core.types_ import GQLAuthError, GQLAuthErrors
-from gqlauth.core.utils import get_user
+from strawberry.types import Info
+
 from gqlauth.user import arg_mutations as mutations
 from strawberry.schema.config import StrawberryConfig
 from strawberry_django.optimizer import DjangoOptimizerExtension
-from strawberry.types import Info
 from .mutations.anime import AnimeInput
+from strawberry_django.permissions import IsSuperuser
+from gqlauth.core.utils import get_user
 
+from gqlauth.user.queries import UserQueries
 from .types.anime import Anime
 from .types.character import Character
 from .types.producer import Producer
@@ -23,6 +24,8 @@ class Query:
     staffs: list[Staff] = strawberry_django.field(pagination=True)
     producers: list[Producer] = strawberry_django.field(pagination=True)
 
+    me: UserQueries
+
 
 @strawberry.type
 class Mutations:
@@ -30,12 +33,10 @@ class Mutations:
     verify_account = mutations.VerifyAccount.field
     token_auth = mutations.ObtainJSONWebToken.field
 
-    @strawberry.mutation
-    def create_anime(self, data: AnimeInput) -> Anime:
-        user = get_user(data)
-        if not user.is_superuser:
-            raise GQLAuthError(code=GQLAuthErrors.UNAUTHENTICATED)
-        print(data)
+    @strawberry.mutation()
+    def create_anime(self, info: Info, data: AnimeInput) -> Anime:
+        user = get_user(info)
+        print(user)
 
 
 schema = strawberry.Schema(
