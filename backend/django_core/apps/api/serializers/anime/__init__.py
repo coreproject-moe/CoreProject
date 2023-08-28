b6be62ffa8a2a8722cd925accfe6f41d989a8d64
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-28 16:19:16 +0600

    add

--- backend/django_core/apps/api/serializers/anime/__init__.py
+++ backend/django_core/apps/api/serializers/anime/__init__.py
@@ -1,19 +1,16 @@
 from apps.anime.models import AnimeModel, AnimeNameSynonymModel
 from apps.anime.models.anime_genre import AnimeGenreModel
 from apps.anime.models.anime_theme import AnimeThemeModel
+from apps.characters.models import CharacterModel
 from rest_framework import serializers
 
 from ...bases.serializer import GetOrCreateSlugRelatedField
 from ..character import CharacterSerializer
+from .genre import AnimeGenreSerializer
 
 
 class AnimeSerializer(serializers.ModelSerializer):
-    genres = serializers.SlugRelatedField(
-        many=True,
-        required=False,
-        slug_field="name",
-        queryset=AnimeGenreModel.objects.all(),
-    )
+    genres = AnimeGenreSerializer(many=True, required=False)
     themes = serializers.SlugRelatedField(
         many=True,
         required=False,
@@ -26,9 +23,49 @@ class AnimeSerializer(serializers.ModelSerializer):
         required=False,
         queryset=AnimeNameSynonymModel.objects.all(),
     )
-    characters = CharacterSerializer(many=True)
+    characters = CharacterSerializer(
+        many=True,
+        required=False,
+    )
 
     class Meta:
         model = AnimeModel
         fields = "__all__"
         read_only_fields = ["is_locked"]
+
+    def create(self, validated_data):
+        # Anime specific data
+        genres_data = validated_data.pop("genres")
+        character_data = validated_data.pop("characters")
+
+        instance = AnimeModel.objects.create(**validated_data)
+
+        if character_data:
+            for character in character_data:
+                character_instance = CharacterModel.objects.get(mal_id=character["mal_id"])
+                instance.characters.add(character_instance)
+
+        if genres_data:
+            for genre in genres_data:
+                genre_instance = AnimeGenreModel.objects.get(mal_id=genre["mal_id"])
+                instance.genres.add(genre_instance)
+
+        instance.save()
+        return instance
+
+    def update(self, instance: AnimeModel, validated_data):
+        if characters := validated_data.pop("characters"):
+            for character in characters:
+                character_instance = CharacterModel.objects.get(mal_id=character["mal_id"])
+                instance.characters.add(character_instance)
+
+        if genres := validated_data.pop("genres"):
+            for genre in genres:
+                genre_instance = AnimeGenreModel.objects.get(mal_id=genre["mal_id"])
+                instance.genres.add(genre_instance)
+
+        for attr, val in validated_data:
+            setattr(instance, attr, val)
+
+        instance.save()
+        return instance
