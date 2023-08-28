106f307d849ce442ed6bb5191745d8e35313aa56
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-28 17:28:16 +0600

    add

--- backend/django_core/apps/api/serializers/anime/__init__.py
+++ backend/django_core/apps/api/serializers/anime/__init__.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import empty
 from apps.anime.models import AnimeModel, AnimeNameSynonymModel
 from apps.anime.models.anime_genre import AnimeGenreModel
 from apps.anime.models.anime_theme import AnimeThemeModel
@@ -5,27 +6,17 @@ from apps.characters.models import CharacterModel
 from rest_framework import serializers
 
 from ...bases.serializer import GetOrCreateSlugRelatedField
-from ..character import CharacterSerializer
 from .genre import AnimeGenreSerializer
+from .theme import AnimeThemeSerializer
+from ..character import CharacterSerializer
 
 
-class AnimeSerializer(serializers.ModelSerializer):
-    genres = AnimeGenreSerializer(many=True, required=False)
-    themes = serializers.SlugRelatedField(
-        many=True,
-        required=False,
-        slug_field="name",
-        queryset=AnimeThemeModel.objects.all(),
-    )
+class AbstractBaseAnimeSerializer(serializers.ModelSerializer):
     name_synonyms = GetOrCreateSlugRelatedField(
         many=True,
         slug_field="name",
         required=False,
         queryset=AnimeNameSynonymModel.objects.all(),
-    )
-    characters = CharacterSerializer(
-        many=True,
-        required=False,
     )
 
     class Meta:
@@ -33,39 +24,30 @@ class AnimeSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["is_locked"]
 
-    def create(self, validated_data):
-        # Anime specific data
-        genres_data = validated_data.pop("genres")
-        character_data = validated_data.pop("characters")
 
-        instance = AnimeModel.objects.create(**validated_data)
+class AnimeGETSerializer(AbstractBaseAnimeSerializer):
+    genres = AnimeGenreSerializer(many=True, read_only=True)
+    themes = AnimeThemeSerializer(many=True, read_only=True)
+    characters = CharacterSerializer(many=True, read_only=True)
 
-        if character_data:
-            for character in character_data:
-                character_instance = CharacterModel.objects.get(mal_id=character["mal_id"])
-                instance.characters.add(character_instance)
 
-        if genres_data:
-            for genre in genres_data:
-                genre_instance = AnimeGenreModel.objects.get(mal_id=genre["mal_id"])
-                instance.genres.add(genre_instance)
+class AnimePOSTSerializer(AbstractBaseAnimeSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        slug_field="mal_id",
+        queryset=AnimeGenreModel.objects.all(),
+    )
+    themes = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        slug_field="mal_id",
+        queryset=AnimeThemeModel.objects.all(),
+    )
 
-        instance.save()
-        return instance
-
-    def update(self, instance: AnimeModel, validated_data):
-        if characters := validated_data.pop("characters"):
-            for character in characters:
-                character_instance = CharacterModel.objects.get(mal_id=character["mal_id"])
-                instance.characters.add(character_instance)
-
-        if genres := validated_data.pop("genres"):
-            for genre in genres:
-                genre_instance = AnimeGenreModel.objects.get(mal_id=genre["mal_id"])
-                instance.genres.add(genre_instance)
-
-        for attr, val in validated_data:
-            setattr(instance, attr, val)
-
-        instance.save()
-        return instance
+    characters = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        slug_field="mal_id",
+        queryset=CharacterModel.objects.all(),
+    )
