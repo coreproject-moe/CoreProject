e0fb3ed5d737bf7fe839a9686e1c64b0140c4935
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-28 15:27:52 +0600

    add

--- backend/django_core/apps/api/urls.py
+++ backend/django_core/apps/api/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .views.anime import (
     AnimeAPIView,
     AnimeSpecificAPIView,
-    AnimeSpecificGenreAPIView,
-    AnimeSpecificThemeAPIView,
 )
 from .views.anime.genre import AnimeGenresAPIView, AnimeGenresSpecificAPIView
 from .views.anime.theme import AnimeThemesAPIView, AnimeThemesSpecificAPIView
@@ -12,8 +10,6 @@ from .views.anime.theme import AnimeThemesAPIView, AnimeThemesSpecificAPIView
 urlpatterns = [
     path("anime/", AnimeAPIView.as_view()),
     path("anime/<int:pk>/", AnimeSpecificAPIView.as_view()),
-    path("anime/<int:pk>/genres/", AnimeSpecificGenreAPIView.as_view()),
-    path("anime/<int:pk>/themes/", AnimeSpecificThemeAPIView.as_view()),
     path("anime/genres/", AnimeGenresAPIView.as_view()),
     path("anime/genres/<int:pk>/", AnimeGenresSpecificAPIView.as_view()),
     path("anime/themes/", AnimeThemesAPIView.as_view()),
--- backend/django_core/apps/api/views/anime/__init__.py
+++ backend/django_core/apps/api/views/anime/__init__.py
@@ -34,54 +34,28 @@ class AnimeSpecificAPIView(
     serializer_class = AnimeSerializer
 
 
-class AnimeSpecificGenreAPIView(
-    SuperUserUpdateProtectedAPIView,
-    mixins.ListModelMixin,
-    generics.GenericAPIView,
-):
-    serializer_class = AnimeGenreSerializer
+# class AnimeSpecificThemeAPIView(
+#     SuperUserUpdateProtectedAPIView,
+#     mixins.ListModelMixin,
+#     generics.GenericAPIView,
+# ):
+#     serializer_class = AnimeThemeSerializer
 
-    def get_queryset(self):
-        return (
-            AnimeModel.objects.prefetch_related("genres").get(pk=self.kwargs["pk"]).genres
-        )
+#     def get_queryset(self) -> AnimeModel:
+#         return (
+#             AnimeModel.objects.prefetch_related("themes").get(pk=self.kwargs["pk"]).themes
+#         )
 
-    def get(self, *args, **kwargs) -> Response:
-        return self.list(*args, **kwargs)
+#     def get(self, *args, **kwargs) -> Response:
+#         return self.list(*args, **kwargs)
 
-    def post(self, request: HttpRequest, *args, **kwargs) -> Response:
-        anime_model_instance = AnimeModel.objects.get(pk=self.kwargs["pk"])
-        serializer: AnimeGenreSerializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            instance, _ = AnimeGenreModel.objects.get_or_create(**serializer.data)
-            anime_model_instance.genres.add(instance)
+#     def post(self, request: HttpRequest, *args, **kwargs) -> Response:
+#         anime_model_instance = AnimeModel.objects.get(pk=self.kwargs["pk"])
 
-        anime_model_instance.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         serializer: AnimeGenreSerializer = self.get_serializer(data=request.data)
+#         if serializer.is_valid():
+#             instance, _ = AnimeThemeModel.objects.get_or_create(**serializer.data)
+#             anime_model_instance.themes.add(instance)
 
-
-class AnimeSpecificThemeAPIView(
-    SuperUserUpdateProtectedAPIView,
-    mixins.ListModelMixin,
-    generics.GenericAPIView,
-):
-    serializer_class = AnimeThemeSerializer
-
-    def get_queryset(self) -> AnimeModel:
-        return (
-            AnimeModel.objects.prefetch_related("themes").get(pk=self.kwargs["pk"]).themes
-        )
-
-    def get(self, *args, **kwargs) -> Response:
-        return self.list(*args, **kwargs)
-
-    def post(self, request: HttpRequest, *args, **kwargs) -> Response:
-        anime_model_instance = AnimeModel.objects.get(pk=self.kwargs["pk"])
-
-        serializer: AnimeGenreSerializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            instance, _ = AnimeThemeModel.objects.get_or_create(**serializer.data)
-            anime_model_instance.themes.add(instance)
-
-        anime_model_instance.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         anime_model_instance.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
