2edba1ccb4338aaa6b6f7b900e15a4ad27bafea3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-29 11:36:51 +0600

    add

--- backend/django_core/apps/api/urls.py
+++ backend/django_core/apps/api/urls.py
@@ -5,16 +5,18 @@ from .views.anime import (
     AnimeSpecificAPIView,
     AnimeViewSet,
 )
+from .views.characters import CharacterViewSet, CharacterSpecificAPIView
 from .views.anime.genre import AnimeGenresAPIView, AnimeGenresSpecificAPIView
 from .views.anime.theme import AnimeThemesAPIView, AnimeThemesSpecificAPIView
 from .views.user.login import LoginAPIView
 from .views.user.logout import LogoutAPIView
 
-anime_router = DefaultRouter()
-anime_router.register(r"anime", AnimeViewSet, basename="anime")
+base_router = DefaultRouter()
+base_router.register(r"anime", AnimeViewSet, basename="anime")
+base_router.register(r"character", CharacterViewSet, basename="character")
 
 urlpatterns = [
-    path("", include(anime_router.urls)),
+    path("", include(base_router.urls)),
     # Anime specific routes
     path("anime/<int:pk>/", AnimeSpecificAPIView.as_view()),
     path("anime/genres/", AnimeGenresAPIView.as_view()),
@@ -24,4 +26,6 @@ urlpatterns = [
     # User routes
     path("user/login/", LoginAPIView.as_view()),
     path("user/logout/", LogoutAPIView.as_view()),
+    # Character specific routes
+    path("character/<int:pk>/", CharacterSpecificAPIView.as_view()),
 ]
---
+++ backend/django_core/apps/api/views/characters/__init__.py
@@ -0,0 +1,31 @@
+from apps.characters.models import CharacterModel
+from apps.api.permissions import IsSuperUserOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, mixins, viewsets
+
+from ...filters.character import CharacterFilter
+from ...serializers.character import CharacterSerializer
+
+
+class CharacterViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = CharacterModel.objects.all()
+    serializer_class = CharacterSerializer
+
+    # Filters
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = CharacterFilter
+
+    # Permissions
+    permission_classes = (IsSuperUserOrReadOnly,)
+
+
+class CharacterSpecificAPIView(
+    generics.RetrieveUpdateDestroyAPIView,
+):
+    queryset = CharacterModel.objects.all()
+    serializer_class = CharacterSerializer
+    permission_classes = (IsSuperUserOrReadOnly,)
