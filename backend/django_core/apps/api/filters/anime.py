2228da9607babd8342ef65ead0d8ec43ebd014f1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-29 09:38:30 +0600

    Update anime.py

--- backend/django_core/apps/api/filters/anime.py
+++ backend/django_core/apps/api/filters/anime.py
@@ -3,6 +3,7 @@ from apps.anime.models import AnimeModel
 
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models.functions import Greatest
+from django.db.models.query import QuerySet
 
 
 class AnimeFilter(filters.FilterSet):
@@ -11,20 +12,13 @@ class AnimeFilter(filters.FilterSet):
     mal_id = filters.CharFilter(method="mal_id_filter", label="MyAnimeList ID Filter")
     kitsu_id = filters.CharFilter(method="kitsu_id_filter", label="Kitsu ID Filter")
     anilist_id = filters.CharFilter(method="anilist_id_filter", label="Anilist ID Filter")
+    # many to many fields
+    genre = filters.CharFilter(method="genre_filter", label="Genre Filter")
+    themes = filters.CharFilter(method="themes_filter", label="Themes Filter")
 
-    def mal_id_filter(self, queryset: AnimeModel, name: str, value: str) -> AnimeModel:
-        query = queryset.filter(mal_id__in=value.split(","))
-        return query
-
-    def kitsu_id_filter(self, queryset: AnimeModel, name: str, value: str) -> AnimeModel:
-        query = queryset.filter(kitsu_id__in=value.split(","))
-        return query
-
-    def anilist_id_filter(self, queryset: AnimeModel, name: str, value: str) -> AnimeModel:
-        query = queryset.filter(anilist_id__in=value.split(","))
-        return query
-
-    def name_filter(self, queryset: AnimeModel, name, value: str) -> AnimeModel:
+    def name_filter(
+        self, queryset: QuerySet[AnimeModel], name, value: str
+    ) -> QuerySet[AnimeModel]:
         query = (
             queryset.annotate(
                 similiarity=Greatest(
@@ -38,5 +32,41 @@ class AnimeFilter(filters.FilterSet):
             )
             .order_by("-similiarity")
         )
-        print(query.query)
+        return query
+
+    def mal_id_filter(
+        self, queryset: QuerySet[AnimeModel], name: str, value: str
+    ) -> QuerySet[AnimeModel]:
+        query = queryset.filter(mal_id__in=value.split(","))
+
+        return query
+
+    def kitsu_id_filter(
+        self, queryset: QuerySet[AnimeModel], name: str, value: str
+    ) -> QuerySet[AnimeModel]:
+        query = queryset.filter(kitsu_id__in=value.split(","))
+        return query
+
+    def anilist_id_filter(
+        self, queryset: QuerySet[AnimeModel], name: str, value: str
+    ) -> QuerySet[AnimeModel]:
+        query = queryset.filter(anilist_id__in=value.split(","))
+        return query
+
+    def genre_filter(
+        self, queryset: QuerySet[AnimeModel], name, value: str
+    ) -> QuerySet[AnimeModel]:
+        query = queryset.filter(genres__name__in=value.split(","))
+        return query
+
+    def theme_filter(
+        self, queryset: QuerySet[AnimeModel], name: str, value: str
+    ) -> QuerySet[AnimeModel]:
+        query = queryset.filter(themes__name__in=value.split(","))
+        return query
+
+    def character_filter(
+        self, queryset: QuerySet[AnimeModel], name: str, value: str
+    ) -> QuerySet[AnimeModel]:
+        query = queryset.filter(characters__name__in=value.split(","))
         return query
