37062b6645f29e92c4abc2ba1564f9c01f972b9c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-31 19:44:37 +0600

    add

---
+++ backend/django_core/apps/api/serializers/episode/__init__.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from apps.episodes.models import EpisodeModel
+
+
+class EpisodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EpisodeModel
+        fields = [
+            "episode_number",
+            "episode_name",
+            "episode_thumbnail",
+            "episode_summary",
+            "episode_length",
+            "episode_type",
+            "providers",
+        ]
+        extra_kwargs = {
+            "created_at": {
+                "read_only": True,
+            },
+            "updated_at": {
+                "read_only": True,
+            },
+        }
---
+++ backend/django_core/apps/api/views/anime/episode/__init__.py
@@ -0,0 +1,35 @@
+from apps.anime.models import AnimeModel
+from apps.episodes.models import EpisodeModel
+from django.http import HttpRequest
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+from ....serializers.episode import EpisodeSerializer
+
+# If we need to revisit, take a look at our ChadGPT conversation
+# https://chat.openai.com/share/06777217-e9a9-4fb4-bcfc-28e9f59be6f8
+
+
+class EpisodeAPIView(generics.ListAPIView):
+    serializer_class = EpisodeSerializer
+    filter_backends = (DjangoFilterBackend,)
+    # Permissions
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    # Pagination
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = AnimeModel.objects.get(pk=self.kwargs["pk"]).episodes.all()
+        return queryset
+
+    def post(self, request: HttpRequest, pk: int) -> Response:
+        serializer: EpisodeSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        episode_instance = EpisodeModel.objects.create(**serializer.validated_data)
+
+        AnimeModel.objects.get(pk=pk).episodes.add(episode_instance)
+
+        return Response(status=200, data=serializer.data)
