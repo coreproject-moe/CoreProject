060a2ba913b14119fa964e8910d8ca4a98c88ac0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-31 19:59:50 +0600

    Update __init__.py

--- backend/django_core/apps/api/views/anime/__init__.py
+++ backend/django_core/apps/api/views/anime/__init__.py
@@ -9,13 +9,10 @@ from ...serializers.anime import AnimeGETSerializer, AnimePOSTSerializer
 
 
 class AnimeViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet,
+    viewsets.ModelViewSet,
 ):
     queryset = AnimeModel.objects.all()
-    serializer_class = AnimeGETSerializer
+
     # Filters
     filter_backends = (DjangoFilterBackend,)
     filterset_class = AnimeFilter
@@ -26,40 +23,8 @@ class AnimeViewSet(
     # Pagination
     pagination_class = LimitOffsetPagination
 
-
-class AnimeSpecificAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = AnimeModel.objects.all()
-    permission_classes = (IsSuperUserOrReadOnly,)
-
     def get_serializer_class(self):
         if self.request.method == "GET":
             return AnimeGETSerializer
         else:
             return AnimePOSTSerializer
-
-
-# class AnimeSpecificThemeAPIView(
-#     SuperUserUpdateProtectedAPIView,
-#     mixins.ListModelMixin,
-#     generics.GenericAPIView,
-# ):
-#     serializer_class = AnimeThemeSerializer
-
-#     def get_queryset(self) -> AnimeModel:
-#         return (
-#             AnimeModel.objects.prefetch_related("themes").get(pk=self.kwargs["pk"]).themes
-#         )
-
-#     def get(self, *args, **kwargs) -> Response:
-#         return self.list(*args, **kwargs)
-
-#     def post(self, request: HttpRequest, *args, **kwargs) -> Response:
-#         anime_model_instance = AnimeModel.objects.get(pk=self.kwargs["pk"])
-
-#         serializer: AnimeGenreSerializer = self.get_serializer(data=request.data)
-#         if serializer.is_valid():
-#             instance, _ = AnimeThemeModel.objects.get_or_create(**serializer.data)
-#             anime_model_instance.themes.add(instance)
-
-#         anime_model_instance.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
