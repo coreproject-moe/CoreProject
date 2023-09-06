8d9ee1d465370317af07691c0b0024311ef7c67c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-05 19:25:12 -0700

    feat: automate updating ui (#2638)
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Revert "add"
    
    This reverts commit dbf4b8b576b94b6f69ec902f957bf42d9063d36f.
    
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- frontend/AnimeCore/src/lib/functions/format_time.test.ts
+++
@@ -1,8 +0,0 @@
-import { FormatTime } from "$functions/format_time";
-import { expect, test } from "vitest";
-
-test("format date function", () => {
-    const formated_time = new FormatTime(1600);
-    expect(formated_time.format_seconds_to_minutes).toBe("26");
-    expect(formated_time.format_seconds_to_time_stamp_duration).toBe("26:40");
-});
