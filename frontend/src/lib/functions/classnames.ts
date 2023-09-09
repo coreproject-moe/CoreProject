174ad718fbb6891d9c5103ba04cd344b749858e3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-09-09 10:24:50 +0600

    add

--- frontend/src/lib/functions/classnames.ts
+++
@@ -1,5 +0,0 @@
-import clsx from "clsx";
-import type { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
