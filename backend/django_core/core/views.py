e74295c85447b067680606ec9255eb30e073c77d
Tokito <tokitousan2005@gmail.com>
2023-09-29 17:12:10 +0530

    fix: implicit concatanation  (#2711)
    * fix: syntax issue
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- backend/django_core/core/views.py
+++ backend/django_core/core/views.py
@@ -13,8 +13,8 @@ ERROR_DICT = {
         "error_status": "bad request",
         "error_text": format_kokoro_color(
             "kokoro-chan faced some issues in trying to decipher that request."
-            "In the meantime, you can try checking the URL and refreshing the page, "
-            "or go back home, browse the forums or come say hi!"
+            + "In the meantime, you can try checking the URL and refreshing the page, "
+            + "or go back home, browse the forums or come say hi!"
         ),
     },
     403: {
@@ -27,9 +27,9 @@ ERROR_DICT = {
         "error_status": "Forbidden",
         "error_text": format_kokoro_color(
             "Even to her precious user-kun, "
-            "kokoro-chan has some secrets that she wants to hide. "
-            "Well, since there's nothing you can do about that, "
-            "you can go back home, browse the forums or come say hi! "
+            + "kokoro-chan has some secrets that she wants to hide. "
+            + "Well, since there's nothing you can do about that, "
+            + "you can go back home, browse the forums or come say hi! "
         ),
     },
     404: {
@@ -42,10 +42,10 @@ ERROR_DICT = {
         "error_status": "Page Not Found",
         "error_text": format_kokoro_color(
             "Our hardworking kokoro-chan was unable to find that page. "
-            "While she collects more data on it, "
-            "why don't you go back home, "
-            "explore some random anime, "
-            "browse the forums or come say hi!"
+            + "While she collects more data on it, "
+            + "why don't you go back home, "
+            + "explore some random anime, "
+            + "browse the forums or come say hi!"
         ),
     },
     500: {
@@ -57,11 +57,11 @@ ERROR_DICT = {
         "error_status_code": 500,
         "error_status": "Internal Server Error",
         "error_text": format_kokoro_color(
-            "Uh-oh, looks like our cute kokoro-chan worked really hard"
-            "for the past few days and has now fallen asleep. "
-            "You can wait for her to wake up by looking at the status page, "
-            "or come say hi to other fellow kokoro-chan worshippers! "
-            "ah- also let's wish her sweet dreams! "
+            "Uh-oh, looks like our cute kokoro-chan worked really hard "
+            + "for the past few days and has now fallen asleep. "
+            + "You can wait for her to wake up by looking at the status page, "
+            + "or come say hi to other fellow kokoro-chan worshippers! "
+            + "ah- also let's wish her sweet dreams!"
         ),
     },
 }
@@ -96,7 +96,7 @@ def four_zero_zero_view(
     request: HttpRequest,
     exception: None = None,
 ) -> HttpResponse:
-    context = ERROR_DICT.get(400)
+    context = ERROR_DICT[400]
     context.setdefault("error_status_code", 400)
     return render(
         request,
@@ -106,7 +106,7 @@ def four_zero_zero_view(
 
 
 def four_zero_three_view(request: HttpRequest, exception: None = None) -> HttpResponse:
-    context = ERROR_DICT.get(403)
+    context = ERROR_DICT[403]
     context.setdefault("error_status_code", 403)
     return render(
         request,
@@ -119,7 +119,7 @@ def four_zero_four_view(
     request: HttpRequest,
     exception: None = None,
 ) -> HttpResponse:
-    context = ERROR_DICT.get(404)
+    context = ERROR_DICT[404]
     context.setdefault("error_status_code", 404)
     return render(
         request,
@@ -129,7 +129,7 @@ def four_zero_four_view(
 
 
 def five_zero_zero_view(request: HttpRequest) -> HttpResponse:
-    context = ERROR_DICT.get(500)
+    context = ERROR_DICT[500]
     context.setdefault("error_status_code", 500)
 
     return render(
