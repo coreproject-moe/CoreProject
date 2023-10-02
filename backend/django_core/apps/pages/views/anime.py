2407d04561ef975b51fb10074a20db9f637494b3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-02 20:23:39 +0600

    add

--- backend/django_core/apps/pages/views/anime.py
+++ backend/django_core/apps/pages/views/anime.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 
 def anime_home_view(request):
-    return render(request, "anime/home.html")
+    icons = [
+        {
+            "html": "icons/compass.html",
+            "klass": "hello",
+            "label": "",
+            "href": "",
+        },
+        {
+            "html": "icons/compass.html",
+            "klass": "world",
+            "label": "",
+            "href": "",
+        },
+    ]
+    return render(request, "anime/home.html", context={"icons": icons})
