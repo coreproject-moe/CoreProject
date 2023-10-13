6a16e88ae576650dbb6a4d37e33a7f9981632cad
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-13 08:57:02 +0600

    add

--- backend/django_core/apps/pages/urls.py
+++ backend/django_core/apps/pages/urls.py
@@ -5,5 +5,6 @@ from .views.stack import stack_view
 
 urlpatterns = [
     path("anime/", anime_home_view, name="anime_home_view"),
+    path("anime/explore/", anime_home_view, name="anime_home_view"),
     path("stack/", stack_view, name="stack_view"),
 ]
