2ebb3278b02bfe300cafc0e96ea020d798b6d751
tokitou-san <tokitousan2005@gmail.com>
2023-10-14 16:07:46 +0530

    Fix: add scroll_area component

---
+++ backend/components/scroll_area/scroll_area.py
@@ -0,0 +1,25 @@
+from django_components import component
+
+
+@component.register("scroll_area")
+class ScollArea(component.Component):
+	template_name = "scroll_area/scroll_area.html"
+
+	# get params
+	def get_context_data(
+		self,
+		scroll_area_class: str,
+		parent_class: str,
+		offset_scrollbar: bool = False,
+		gradient_mask: bool = False
+	):
+		return {
+			"class": scroll_area_class,
+			"parent_class": parent_class,
+			"offset_scrollbar": offset_scrollbar,
+			"gradient_mask": gradient_mask
+		}
+
+	class Media:
+		css = ""
+		js = ""
