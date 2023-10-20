9aaa8ad28ad9dc7d6380bf1da32577e17abb1697
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-20 18:12:39 +0600

    add

--- backend/django_core/apps/user/forms.py
+++ backend/django_core/apps/user/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import CustomUser
@@ -16,25 +15,4 @@ class CustomUserChangeForm(UserChangeForm["CustomUser"]):
         fields = ("email",)
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        label="Email / Username:",
-        widget=forms.TextInput(
-            attrs={
-                "autofocus": True,
-                "placeholder": "sora_amamiya@coreproject.moe / soraamamiya#0001",
-                "class": "h-12 w-full rounded-xl border-[0.4vw] border-primary-500 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-all placeholder:text-white/50 focus:border-primary-400 md:h-[3.125vw] md:rounded-[0.75vw] md:border-[0.2vw] md:px-[1vw] md:text-[1.1vw]",
-            }
-        ),
-        help_text="we’ll send you a verification email, so please ensure it’s active",
-    )
 
-    password = forms.CharField(
-        label="Password:",
-        widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "enter your existing password",
-                "class": "h-12 w-full rounded-xl border-[0.4vw] border-primary-500 bg-transparent pl-5 text-base font-medium outline-none !ring-0 transition-all placeholder:text-white/50 focus:border-primary-400 md:h-[3.125vw] md:rounded-[0.75vw] md:border-[0.2vw] md:pl-[1vw] md:text-[1.1vw]",
-            }
-        ),
-    )
