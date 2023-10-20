3f24a815926e23a0e51faeec5cf1457faacb499b
tokitou-san <tokitousan2005@gmail.com>
2023-10-20 21:31:54 +0530

    add mock

--- backend/django_core/apps/pages/views/user.py
+++ backend/django_core/apps/pages/views/user.py
@@ -7,4 +7,18 @@ from ..forms.user import LoginForm
 def login_view(request: HttpRequest):
     form = LoginForm(request.POST or None)
 
-    return render(request, "user/login.html", context={"form": form})
+    animes = [
+        {"name": "Demon Slayer", "cover": "/images/mock/DemonSlayer-cover.avif"},
+        {"name": "Hyouka", "cover": "/images/mock/Hyouka-bg.avif"},
+        {"name": "You Lie in April", "cover": "/images/mock/YourLieInApril-bg.avif"},
+        {"name": "Attack on Titan", "cover": "/images/mock/AttackOnTitan-bg.avif"},
+        {"name": "Jujutsu Kaisen", "cover": "/images/mock/JujutsuKaisen.avif"},
+        {"name": "Death Note", "cover": "/images/mock/DeathNote-bg.avif"}
+    ]
+
+    context = {
+        "form": form,
+        "animes": animes,
+    }
+
+    return render(request, "user/login.html", context)
