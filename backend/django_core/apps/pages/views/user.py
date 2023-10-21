982dd40ed603a60fcff7f7737af38c2d018c7d4e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-22 05:47:19 +0600

    add

--- backend/django_core/apps/pages/views/user.py
+++ backend/django_core/apps/pages/views/user.py
@@ -3,9 +3,9 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.templatetags.static import static
-from utilities.toast import toast
 
 from ..forms.user import LoginForm
+from django_htmx.http import retarget
 
 
 def login_view(request: HttpRequest):
@@ -23,12 +23,6 @@ def login_view(request: HttpRequest):
         {"name": "Death Note", "cover": static("/images/mock/DeathNote-bg.avif")},
     ]
 
-    context = {
-        "form": form,
-        "animes": animes,
-    }
-    login_unsuccessful = False
-
     if form.is_valid():
         username = form.cleaned_data["username"]
         user = authenticate(
@@ -41,22 +35,41 @@ def login_view(request: HttpRequest):
                 f"You have attempted to login {config.FAILURE_LIMIT} times, with no success."
                 f"Your account is locked for {utils.get_lockout_cooloff_time(username=username)} seconds"
             )
-            return toast(request, message=detail)
+            response = render(request, "components/toast.html", {"message": detail})
+            return retarget(response, "#toast")
 
         if user is not None:
             login(request, user)
+            utils.check_request(
+                request,
+                login_unsuccessful=False,
+                username=username,
+            )
+            response = render(
+                request,
+                "components/toast.html",
+                {"message": (f"Welcome Back {username}")},
+            )
+            return retarget(response, "#toast")
+
         else:
-            login_unsuccessful = True
-            utils.add_login_attempt_to_db(request, login_valid=False, username=username)
-            return toast(request, message="User not found!")
+            utils.add_login_attempt_to_db(
+                request,
+                login_valid=False,
+                username=username,
+            )
+            response = render(
+                request,
+                "components/toast.html",
+                {"message": "User not found!"},
+            )
+            return retarget(response, "#toast")
 
-        utils.check_request(
-            request,
-            login_unsuccessful=login_unsuccessful,
-            username=username,
-        )
-
-        if login_unsuccessful:
-            return toast(request, message="Login not allowed")
-
-    return render(request, "user/login.html", context)
+    return render(
+        request,
+        "user/login.html",
+        context={
+            "form": form,
+            "animes": animes,
+        },
+    )
--- backend/django_core/utilities/toast.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpRequest
-from django.shortcuts import render
-from django_htmx.http import retarget
-
-
-def toast(request: HttpRequest, message: str):
-    response = render(request, "components/toast.html", {"message": message})
-    return retarget(response, "#toast")
