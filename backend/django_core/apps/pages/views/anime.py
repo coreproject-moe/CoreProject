7017c1f5ae7fc957ca16cc458485902c3dce126b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-21 12:03:06 +0600

    add

--- backend/django_core/apps/pages/views/anime.py
+++ backend/django_core/apps/pages/views/anime.py
@@ -8,31 +8,31 @@ def anime_home_view(request):
     icons = [
         {
             "html": "icons/home.html",
-            "class": "w-[1.25vw] text-white",
+            "class": "w-[1.25vw]",
             "label": "home",
             "href": "/anime/",
         },
         {
             "html": "icons/compass.html",
-            "class": "w-[1.25vw] text-white",
+            "class": "w-[1.25vw]",
             "label": "explore",
             "href": "/anime/explore/",
         },
         {
             "html": "icons/list.html",
-            "class": "w-[1.75vw] text-white",
+            "class": "w-[1.75vw]",
             "label": "list",
             "href": "/anime/list/",
         },
         {
             "html": "icons/calender.html",
-            "class": "w-[1.25vw] text-white",
+            "class": "w-[1.25vw]",
             "href": "/anime/shedule",
             "label": "schedule/",
         },
         {
             "html": "icons/chat.html",
-            "class": "w-[1.25vw] text-white",
+            "class": "w-[1.25vw]",
             "href": "/anime/forum",
             "label": "forum/",
         },
