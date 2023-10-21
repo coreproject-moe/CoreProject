9349fbb010c0f74426924ce15f716472aef26a77
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-21 18:23:19 +0600

    add login logics

--- backend/django_core/apps/pages/views/user.py
+++ backend/django_core/apps/pages/views/user.py
@@ -1,8 +1,10 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseForbidden
 from django.shortcuts import render
 from django.templatetags.static import static
-
+from django.contrib.auth import authenticate, login
 from ..forms.user import LoginForm
+from defender import utils
+from defender import config
 
 
 def login_view(request: HttpRequest):
@@ -24,5 +26,32 @@ def login_view(request: HttpRequest):
         "form": form,
         "animes": animes,
     }
+    login_unsuccessful = False
+
+    if form.is_valid():
+        username = form.cleaned_data["username"]
+        user = authenticate(
+            username=username,
+            password=form.cleaned_data["password"],
+        )
+        if utils.is_already_locked(request, username=username):
+            detail = (
+                f"You have attempted to login {config.FAILURE_LIMIT} times, with no success."
+                f"Your account is locked for {utils.get_lockout_cooloff_time(username=username)} seconds"
+            )
+            return HttpResponseForbidden(detail)
+        if user is not None:
+            login(request, user)
+        else:
+            login_unsuccessful = True
+            utils.add_login_attempt_to_db(request, login_valid=False, username=username)
+            return HttpResponseForbidden(f'Attempts = {utils.get_user_attempts(username)}')
+        utils.check_request(
+            request,
+            login_unsuccessful=login_unsuccessful,
+            username=username,
+        )
+        if login_unsuccessful:
+            return HttpResponseForbidden("Login not allowed")
 
     return render(request, "user/login.html", context)
