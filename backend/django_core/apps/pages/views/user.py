8533f0c9e64d43cbf56a3a556a2089c2d031129e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-22 12:37:58 +0600

    Update user.py

--- backend/django_core/apps/pages/views/user.py
+++ backend/django_core/apps/pages/views/user.py
@@ -23,6 +23,8 @@ def login_view(request: HttpRequest):
         {"name": "Death Note", "cover": static("/images/mock/DeathNote-bg.avif")},
     ]
 
+    login_unsuccessful = False
+
     if form.is_valid():
         username = form.cleaned_data["username"]
         user = authenticate(
@@ -31,39 +33,46 @@ def login_view(request: HttpRequest):
         )
 
         if utils.is_already_locked(request, username=username):
-            detail = (
+            message = (
                 f"You have attempted to login {config.FAILURE_LIMIT} times, with no success."
                 f"Your account is locked for {utils.get_lockout_cooloff_time(username=username)} seconds"
             )
-            response = render(request, "components/toast.html", {"message": detail})
-            return retarget(response, "#toast")
 
         if user is not None:
             login(request, user)
-            utils.check_request(
-                request,
-                login_unsuccessful=False,
-                username=username,
-            )
-            response = render(
-                request,
-                "components/toast.html",
-                {"message": (f"Welcome Back {username}")},
-            )
-            return retarget(response, "#toast")
+            message = f"Welcome Back {username}"
 
         else:
+            login_unsuccessful = True
             utils.add_login_attempt_to_db(
                 request,
                 login_valid=False,
                 username=username,
             )
-            response = render(
-                request,
-                "components/toast.html",
-                {"message": "User not found!"},
+            
+            login_attempts = int(config.FAILURE_LIMIT) - int(
+                utils.get_user_attempts(request, username=username)
             )
-            return retarget(response, "#toast")
+
+            if login_attempts == 0:
+                message = f"Your account is locked for {utils.get_lockout_cooloff_time(username=username)} seconds"
+            else:
+                message = (
+                    "User not found!\n"
+                    f"You have {login_attempts} attempts left for {username}"
+                )
+
+        utils.check_request(
+            request,
+            login_unsuccessful=login_unsuccessful,
+            username=username,
+        )
+        response = render(
+            request,
+            "components/toast.html",
+            {"message": message},
+        )
+        return retarget(response, "#toast")
 
     return render(
         request,
