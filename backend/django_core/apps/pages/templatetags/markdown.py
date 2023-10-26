4bd33ae53103bce3dcd1a9b24e5d51393c8c86f4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-26 10:57:29 +0600

    add

--- backend/django_core/apps/pages/templatetags/markdown.py
+++ backend/django_core/apps/pages/templatetags/markdown.py
@@ -1,4 +1,5 @@
 import markdown as python_markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
 from django import template
 
 register = template.Library()
@@ -7,4 +8,16 @@ register = template.Library()
 @register.filter(name="markdown")
 def markdown(text: str):
     """Returns the value turned into a list."""
-    return python_markdown.markdown(text)
+    x = python_markdown.markdown(
+        text,
+        extensions=[
+            "fenced_code",
+            CodeHiliteExtension(
+                noclasses=True,
+                pygments_style="github-dark",
+                use_pygments=True,
+            ),
+        ],
+    )
+    print(x)
+    return x
