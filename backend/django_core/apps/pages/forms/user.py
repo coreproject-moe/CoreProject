5d640cedff94c7b7060873f97d3f60728de78f41
tokitou-san <tokitousan2005@gmail.com>
2023-10-27 16:58:00 +0530

    add

--- backend/django_core/apps/pages/forms/user.py
+++ backend/django_core/apps/pages/forms/user.py
@@ -134,5 +134,4 @@ class SecondRegisterForm(forms.Form):
 
         if username := cleaned_data.get("username"):
             if CustomUser.objects.filter(username=username).exists():
-                self.fields["username"].widget.attrs["class"] += " focus:border-error"
                 self.add_error("username", error="`username` already taken! try another one")
