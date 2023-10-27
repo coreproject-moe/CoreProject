75fa15c5fff1e5be0cb04f465727be0df13a5bd9
tokitou-san <tokitousan2005@gmail.com>
2023-10-27 13:08:09 +0530

    add

--- backend/django_core/apps/pages/views/user.py
+++ backend/django_core/apps/pages/views/user.py
@@ -145,7 +145,16 @@ def register_view(request: HttpRequest, _internal_state_: int | None = 2) -> Htt
         if _internal_state_ == 2:
             form = SecondRegisterForm(request.POST or None)
 
-            return render(request, "user/register/_2.html", context={"form": form})
+            if form.is_valid():
+                username = form.cleaned_data["username"]
+                print(username)
+
+            return render(
+                request,
+                "user/register/_2.html",
+                context={
+                    "form": form
+                })
 
     return render(
         request,
