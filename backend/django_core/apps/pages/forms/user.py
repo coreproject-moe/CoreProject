24c2159590283bfbe60fb733fed6e17d5636d831
tokitouq <tokitousan2005@gmail.com>
2023-10-29 00:37:52 +0530

    fix markdown

--- backend/django_core/apps/pages/forms/user.py
+++ backend/django_core/apps/pages/forms/user.py
@@ -156,4 +156,4 @@ class ResetPasswordForm(forms.Form):
         email = self.cleaned_data.get("email")
 
         if not CustomUser.objects.filter(email=email).exists():
-            self.add_error("email", error="**Email** not registered! enter registered email address")
+            self.add_error("email", error="**Email** not registered! please enter a registered email address")
