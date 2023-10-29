0044ba92e39fad87c1ff42ac3532614668e43c73
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-29 13:26:04 +0600

    [`Backend`] : Merge commits

---
+++ backend/django_core/apps/pages/request.py
@@ -0,0 +1,10 @@
+from django_htmx.middleware import HtmxDetails
+from django.http import HttpRequest
+
+# https://github.com/adamchainz/django-htmx/blob/a08750cb67943b8d49322e3226e6c7f62eff07bc/example/example/views.py#L19-L22
+
+
+# Typing pattern recommended by django-stubs:
+# https://github.com/typeddjango/django-stubs#how-can-i-create-a-httprequest-thats-guaranteed-to-have-an-authenticated-user
+class HtmxHttpRequest(HttpRequest):
+    htmx: HtmxDetails
--- backend/django_core/apps/pages/views/stack.py
+++ backend/django_core/apps/pages/views/stack.py
@@ -1,10 +1,17 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from typing import TYPE_CHECKING
+from ..data.anime import icons
+
+if TYPE_CHECKING:
+    from ..request import HtmxHttpRequest
 
 from ..data.stack import context
 
 # Create your views here.
 
 
-def stack_view(request: HttpRequest) -> HttpResponse:
+def stack_view(request: "HtmxHttpRequest") -> HttpResponse:
     return render(request, "stack/index.html", context)
