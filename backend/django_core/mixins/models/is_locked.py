1a2de47e465112480f11dc097ff4611538833d45
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-10-29 15:54:24 +0600

    add

--- backend/django_core/mixins/models/is_locked.py
+++ backend/django_core/mixins/models/is_locked.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class IsLockedMixin(models.Model):
-    is_locked = models.BooleanField(default=False)
+    is_locked = models.BooleanField(db_default=False)
 
     class Meta:
         abstract = True
