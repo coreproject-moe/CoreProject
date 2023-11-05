4eb3f8a2cdfe128a2839e30f5e89aa57f58db97d
tokitouq <tokitousan2005@gmail.com>
2023-11-05 22:03:19 +0530

    Update anime.py

--- backend/django_core/apps/pages/views/anime.py
+++ backend/django_core/apps/pages/views/anime.py
@@ -23,15 +23,6 @@ async def anime_home_view(request: "HtmxHttpRequest") -> HttpResponse:
         context={"icons": icons, "latest_animes": latest_animes},
     )
 
-
-async def anime_info_view(request: "HtmxHttpRequest", pk: int) -> HttpResponse:
-    return render(
-        request,
-        "anime/_layout.html",
-        context={"icons": icons},
-    )
-
-
 async def anime_explore_view(request: "HtmxHttpRequest") -> HttpResponse:
     if request.htmx:
         return render(request, "anime/_partial_explore.html")
@@ -39,5 +30,12 @@ async def anime_explore_view(request: "HtmxHttpRequest") -> HttpResponse:
     return render(
         request,
         "anime/_layout.html",
-        context={"icons": icons},
+        context={"icons": icons}
+    )
+
+async def anime_info_view(request: "HtmxHttpRequest", pk: int) -> HttpResponse:
+    return render(
+        request,
+        "anime/_layout.html",
+        context={"icons": icons}
     )
