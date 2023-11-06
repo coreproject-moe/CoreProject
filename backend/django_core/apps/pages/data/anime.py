e509e73779e6c374b17a8fee163f43b2ea30c3f6
tokitouq <tokitousan2005@gmail.com>
2023-11-06 23:06:22 +0530

    add anime data

--- backend/django_core/apps/pages/data/anime.py
+++ backend/django_core/apps/pages/data/anime.py
@@ -71,3 +71,37 @@ latest_animes = [
         "image": "https://static1.cbrimages.com/wordpress/wp-content/uploads/2021/03/demon-slayer-banner.jpg",
     },
 ]
+
+anime = {
+    id: 1,
+    "mal_id": 23273,
+    "anilist_id": None,
+    "kitsu_id": None,
+    "name": "Your Lie in April",
+    "japanese_name": "四月は君の嘘",
+    "source": None,
+    "aired_from": None,
+    "aired_to": None,
+    "banner": "/images/YourLieInApril-bg.avif",
+    "cover": "/images/Hyouka-bg.avif",
+    "banner_background_color": "#2A1710",
+    "cover_background_color": "#D8E4D8",
+    "synopsis":
+        "Kousei Arima is a child prodigy known as the \"Human Metronome\" for playing the piano with precision and perfection. Guided by a strict mother and rigorous training, Kousei dominates every competition he enters, earning the admiration of his musical peers and praise from audiences. When his mother suddenly passes away, the subsequent trauma makes him unable to hear the sound of a piano, and he never takes the stage thereafter.\r\n\r\nNowadays, Kousei lives a quiet and unassuming life as a junior high school student alongside his friends Tsubaki Sawabe and Ryouta Watari. While struggling to get over his mother's death, he continues to cling to music. His monochrome life turns upside down the day he encounters the eccentric violinist Kaori Miyazono, who thrusts him back into the spotlight as her accompanist. Through a little lie, these two young musicians grow closer together as Kaori tries to fill Kousei's world with color.",
+    "background": "Winner in the anime division of the 2016 Sugoi Japan® Awards.",
+    "rating": "",
+    "updated": "2023-03-11T02:37:40.790Z",
+    "name_synonyms": [],
+    "genres": "/api/v1/anime/1/genres",
+    "themes": "/api/v1/anime/1/themes",
+    "characters": "/api/v1/anime/1/character",
+    "studios": "/api/v1/anime/1/studios",
+    "producers": "/api/v1/anime/1/producers",
+    "staffs": "/api/v1/anime/1/staffs",
+    "recommendations": [],
+    "episodes": "/api/v1/anime/1/episodes",
+    "openings": [],
+    "endings": [],
+    "episodes_count": 9,
+    "average_episode_length": 1611
+}
