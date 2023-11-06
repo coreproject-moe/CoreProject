6628158ff645c3e160a682444b812669073b423e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-06 16:53:42 +0600

    addd

--- backend/components/scroll_area/scroll_area.js
+++ backend/components/scroll_area/scroll_area.js
@@ -1,4 +1,4 @@
-htmx.onLoad(function (content) {
+const scroll_area = function ({ content }) {
     const scroll_area = content.querySelector('scroll-area') ?? null;
     if (scroll_area == null) {
         console.log('elements are null | skipping | `scroll-area.js`');
@@ -7,7 +7,7 @@ htmx.onLoad(function (content) {
 
     let add_mask_bottom = scroll_area.scrollHeight > scroll_area.clientHeight,
         mask_class =
-        '[mask-image:_linear-gradient(180deg,_rgba(7,5,25,0.95)_80%,_rgba(0,0,0,0)_100%)]';
+            '[mask-image:_linear-gradient(180deg,_rgba(7,5,25,0.95)_80%,_rgba(0,0,0,0)_100%)]';
 
     if (add_mask_bottom) scroll_area.classList.add(mask_class);
 
@@ -18,4 +18,6 @@ htmx.onLoad(function (content) {
             scroll_area.classList.remove(mask_class);
         else scroll_area.classList.add(mask_class);
     });
-});
+};
+
+htmx.onLoad((content) => scroll_area({ content }));
