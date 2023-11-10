316b727f7fad4e9b23957f9934f8abb4d8024f44
tokitouq <tokitousan2005@gmail.com>
2023-11-10 19:00:37 +0530

    fix

--- backend/components/vercel_hover/vercel_hover.py
+++ backend/components/vercel_hover/vercel_hover.py
@@ -17,19 +17,9 @@ class VercelHover(component.Component):
         glider_container_class: str | None = None,
         active_element_class: str | None = None,
     ):
-        while True:
-            random_integer = random.randint(0, sys.maxsize)
-
-            if cache.get("vercel_hover") == random_integer:
-                continue
-            else:
-                cache.set("vercel_hover", random_integer, 1)
-                break
-
         return {
             "duration": duration,
             "direction": direction,
             "glider_container_class": glider_container_class,
             "active_element_class": active_element_class,
-            "random_integer": random_integer,
         }
