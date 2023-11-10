5074bef978c39553348e71dd51be65cf071c41fd
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-10 15:10:06 +0600

    [`Backend`] : Move from recursion to while loop (#2895)
    Move from recursion to while loop

--- backend/components/scroll_area/scroll_area.py
+++ backend/components/scroll_area/scroll_area.py
@@ -5,16 +5,6 @@ from django.core.cache import cache
 from django_components import component
 
 
-def get_random_integer():
-    random_integer = random.randint(0, sys.maxsize)
-
-    if cache.get("scroll_area") == random_integer:
-        get_random_integer()
-    else:
-        cache.set("scroll_area", random_integer, 1)
-        return random_integer
-
-
 @component.register("scroll_area")
 class ScollArea(component.Component):
     template_name = "scroll_area/scroll_area.html"
@@ -25,11 +15,20 @@ class ScollArea(component.Component):
         parent_class: str,
         offset_scrollbar: str | None = None,
     ):
+        while True:
+            random_integer = random.randint(0, sys.maxsize)
+
+            if cache.get("scroll_area") == random_integer:
+                continue
+            else:
+                cache.set("scroll_area", random_integer, 1)
+                break
+
         return {
             "class": scroll_area_class,
             "parent_class": parent_class,
             "offset_scrollbar": offset_scrollbar,
-            "random_integer": get_random_integer(),
+            "random_integer": random_integer,
         }
 
     # class Media:
--- backend/components/vercel_hover/vercel_hover.py
+++ backend/components/vercel_hover/vercel_hover.py
@@ -5,16 +5,6 @@ from django.core.cache import cache
 from django_components import component
 
 
-def get_random_integer():
-    random_integer = random.randint(0, sys.maxsize)
-
-    if cache.get("vercel_hover") == random_integer:
-        get_random_integer()
-    else:
-        cache.set("vercel_hover", random_integer, 1)
-        return random_integer
-
-
 @component.register("vercel_hover")
 class VercelHover(component.Component):
     template_name = "vercel_hover/vercel_hover.html"
@@ -27,10 +17,19 @@ class VercelHover(component.Component):
         glider_container_class: str | None = None,
         active_element_class: str | None = None,
     ):
+        while True:
+            random_integer = random.randint(0, sys.maxsize)
+
+            if cache.get("vercel_hover") == random_integer:
+                continue
+            else:
+                cache.set("vercel_hover", random_integer, 1)
+                break
+
         return {
             "duration": duration,
             "direction": direction,
             "glider_container_class": glider_container_class,
             "active_element_class": active_element_class,
-            "random_integer": get_random_integer(),
+            "random_integer": random_integer,
         }
