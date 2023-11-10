94589ca5d5308aa7b49bb3b661cce26ada860b65
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-10 09:49:00 +0600

    fix: `scroll-area` logics
    Update scroll_area.py

--- backend/components/scroll_area/scroll_area.py
+++ backend/components/scroll_area/scroll_area.py
@@ -1,23 +1,34 @@
 from django_components import component
 import random
+from django.core.cache import cache
+import sys
+
+
+def get_random_integer():
+    random_integer = random.randint(0, sys.float_info.max)
+
+    if cache.get("scroll_area") == random_integer:
+        get_random_integer()
+    else:
+        cache.set("scroll_area", random_integer, 1)
+        return random_integer
+
 
 @component.register("scroll_area")
 class ScollArea(component.Component):
     template_name = "scroll_area/scroll_area.html"
 
-    # get params
     def get_context_data(
         self,
         scroll_area_class: str,
         parent_class: str,
         offset_scrollbar: str | None = None,
     ):
-        random_integer = random.randint(0,1_00_00_00_00_000)
         return {
             "class": scroll_area_class,
             "parent_class": parent_class,
             "offset_scrollbar": offset_scrollbar,
-            "random_integer":random_integer
+            "random_integer": get_random_integer(),
         }
 
     # class Media:
