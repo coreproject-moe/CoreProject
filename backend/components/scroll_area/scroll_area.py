c3aae8bbaf28dcda6e8f4043883249ed606bb0f1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-10 09:34:18 +0600

    fix: `scroll-area`

--- backend/components/scroll_area/scroll_area.py
+++ backend/components/scroll_area/scroll_area.py
@@ -1,5 +1,5 @@
 from django_components import component
-
+import random
 
 @component.register("scroll_area")
 class ScollArea(component.Component):
@@ -12,11 +12,13 @@ class ScollArea(component.Component):
         parent_class: str,
         offset_scrollbar: str | None = None,
     ):
+        random_integer = random.randint(0,1_00_00_00_00_000)
         return {
             "class": scroll_area_class,
             "parent_class": parent_class,
             "offset_scrollbar": offset_scrollbar,
+            "random_integer":random_integer
         }
 
-    class Media:
-        js = "scroll_area/scroll_area.js"
+    # class Media:
+    #     js = "scroll_area/scroll_area.js"
