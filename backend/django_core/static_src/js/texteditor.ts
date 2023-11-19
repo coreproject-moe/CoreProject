af519e3efed43706945b863194198955abbf46ca
tokitouq <tokitousan2005@gmail.com>
2023-11-19 08:41:12 +0530

    move to functions and rename

--- backend/django_core/static_src/js/texteditor.ts
+++ backend/django_core/static_src/js/texteditor.ts
@@ -88,45 +88,9 @@ async function handle_input(event: Event) {
         }
         
         // show emoji popover
-        emoji_popover();
+        initialize_emoji_popover();
     }
 }
-
-async function emoji_popover() {
-    // emoji popover logics
-    let custom_emoji_popover: HTMLElement | null = document.querySelector("custom-emoji-popover");
-
-    if (!custom_emoji_popover) {
-        custom_emoji_popover = document.createElement("custom-emoji-popover");
-        textarea_element?.parentElement?.appendChild(custom_emoji_popover)
-    }
-
-    custom_emoji_popover.className = "absolute min-w-[12vw] flex-col divide-y divide-accent/10 overflow-hidden rounded-[0.5vw] bg-neutral text-[1vw]";
-    custom_emoji_popover.style.position = "absolute"; // make sure its absolute
-    custom_emoji_popover.style.top = caret_offset_top!;
-    custom_emoji_popover.style.left = caret_offset_left!;
-
-    // remove children for inserting new ones
-    custom_emoji_popover.replaceChildren();
-
-    emoji_matches.slice(0, 5).forEach((emoji, index) => {
-        let child_el = document.createElement("div");
-        child_el.className = 'flex cursor-pointer items-center gap-[0.5vw] px-[0.75vw] py-[0.25vw] leading-[1.75vw] hover:bg-primary-500 hover:text-white';
-        child_el.innerHTML = `
-            <img
-                class='md:w-[1vw]'
-                src=${emoji.emoji}
-            >
-            <span>${emoji.keyword}</span>
-        `;
-
-        // check active
-        if (index === active_emoji_index) {
-            child_el.classList.add("bg-primary", "text-white");
-        };
-        custom_emoji_popover?.appendChild(child_el);
-    });
-};
 
 async function handle_keydown(event: KeyboardEvent) {
     /** Emoji specific codes */
@@ -137,14 +101,14 @@ async function handle_keydown(event: KeyboardEvent) {
                 active_emoji_index =
                     (active_emoji_index - 1 + SHOWN_EMOJI_LIMIT) %
                     SHOWN_EMOJI_LIMIT;
-                emoji_popover();
+                initialize_emoji_popover();
                 break;
             }
             case 'arrowdown': {
                 event.preventDefault();
                 active_emoji_index =
                     (active_emoji_index + 1) % SHOWN_EMOJI_LIMIT;
-                emoji_popover();
+                initialize_emoji_popover();
                 break;
             }
             case 'enter': {
@@ -519,6 +483,42 @@ function is_valid_url(url_string: string): boolean {
         return false;
     }
 }
+
+async function initialize_emoji_popover() {
+    // emoji popover logics
+    let custom_emoji_popover: HTMLElement | null = document.querySelector("custom-emoji-popover");
+
+    if (!custom_emoji_popover) {
+        custom_emoji_popover = document.createElement("custom-emoji-popover");
+        textarea_element?.parentElement?.appendChild(custom_emoji_popover)
+    }
+
+    custom_emoji_popover.className = "absolute min-w-[12vw] flex-col divide-y divide-accent/10 overflow-hidden rounded-[0.5vw] bg-neutral text-[1vw]";
+    custom_emoji_popover.style.position = "absolute"; // make sure its absolute
+    custom_emoji_popover.style.top = caret_offset_top!;
+    custom_emoji_popover.style.left = caret_offset_left!;
+
+    // remove children for inserting new ones
+    custom_emoji_popover.replaceChildren();
+
+    emoji_matches.slice(0, 5).forEach((emoji, index) => {
+        let child_el = document.createElement("div");
+        child_el.className = 'flex cursor-pointer items-center gap-[0.5vw] px-[0.75vw] py-[0.25vw] leading-[1.75vw] hover:bg-primary-500 hover:text-white';
+        child_el.innerHTML = `
+            <img
+                class='md:w-[1vw]'
+                src=${emoji.emoji}
+            >
+            <span>${emoji.keyword}</span>
+        `;
+
+        // check active
+        if (index === active_emoji_index) {
+            child_el.classList.add("bg-primary", "text-white");
+        };
+        custom_emoji_popover?.appendChild(child_el);
+    });
+};
 
 // Add events
 if (textarea_element) {
