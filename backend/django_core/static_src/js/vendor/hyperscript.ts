a567e56d787c208c0db2de7aa90e3159083998d0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-21 07:22:40 +0600

    add

--- backend/django_core/static_src/js/vendor/hyperscript.ts
+++ backend/django_core/static_src/js/vendor/hyperscript.ts
@@ -1,11 +1,5 @@
 import _hyperscript from 'hyperscript.org';
 
-declare global {
-    interface Window {
-        _hyperscript: typeof _hyperscript;
-    }
-}
-
 _hyperscript.browserInit();
 _hyperscript.processNode(document.body);
 window._hyperscript = _hyperscript;
