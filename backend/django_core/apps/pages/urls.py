8c0e1d765b488fd8c059bf71b9e6c1fe1b9f8c7f
Tokito <tokitousan2005@gmail.com>
2023-11-24 20:55:53 +0530

    [`Backend`]: add django urls prefix pattern (#2984)
    * fix: django urls prefix pattern
    
    * fix: fmt

--- backend/django_core/apps/pages/urls.py
+++ backend/django_core/apps/pages/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from .views.anime import (
     anime_episode_view,
@@ -12,32 +12,40 @@ from .views.stack import stack_view
 from .views.upload import upload_view
 from .views.user import login_view, logout_view, register_view, reset_password_view
 
+# fmt: off
 urlpatterns = [
     # Anime pages
-    path("anime/", anime_home_view, name="anime_home_view"),
-    path(
-        "anime/_slider/<int:pk>/",
-        anime_home_view_partial_slider_view,
-        name="anime_home_view_partial_slider_view",
-    ),
-    path("anime/explore/", anime_explore_view, name="anime_explore_view"),
-    # Anime info page
-    path("anime/mal/<int:pk>/", anime_info_view, name="anime_info_view"),
-    # Anime episode page
-    path(
-        "anime/mal/<int:mal_id>/episode/<int:pk>/",
-        anime_episode_view,
-        name="anime_episode_view",
-    ),
+    path("anime/", include([
+        path("", anime_home_view, name="anime_home_view"),
+        path(
+            "_slider/<int:pk>/",
+            anime_home_view_partial_slider_view,
+            name="anime_home_view_partial_slider_view",
+        ),
+        path("explore/", anime_explore_view, name="anime_explore_view"),
+        # Anime info page
+        path("anime/mal/<int:pk>/", anime_info_view, name="anime_info_view"),
+        # Anime episode page
+        path(
+            "mal/<int:mal_id>/episode/<int:pk>/",
+            anime_episode_view,
+            name="anime_episode_view",
+        ),
+    ])),
     # Stack page
     path("stack/", stack_view, name="stack_view"),
     # User pages
-    path("user/login/", login_view, name="login_view"),
-    path("user/logout/", logout_view, name="logout_view"),
-    path("user/register/", register_view, name="register_view"),
-    path("user/reset-password/", reset_password_view, name="reset_password_view"),
+    path("user/", include([
+        path("login/", login_view, name="login_view"),
+        path("logout/", logout_view, name="logout_view"),
+        path("register/", register_view, name="register_view"),
+        path("reset-password/", reset_password_view, name="reset_password_view"),
+    ])),
     # Upload page
     path("upload/", upload_view, name="upload_view"),
     # Partials
-    path("partials/markdown", markdown_endpoint, name="partial_markdown_endpoint"),
+    path("partials/", include([
+        path("markdown", markdown_endpoint, name="partial_markdown_endpoint"),
+    ])),
 ]
+# fmt: off
