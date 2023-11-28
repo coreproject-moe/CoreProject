f3ee7cd31797e1d7dcc9e14b75b5359048e479f9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-28 09:32:08 +0600

    Update index.ts

--- backend/django_core/static_src/components/index.ts
+++ backend/django_core/static_src/components/index.ts
@@ -7,6 +7,6 @@ svelteRetag({
     component: ScrollArea,
     tagname: 'scroll-area',
     attributes: ['parent_class', 'offset_scrollbar', 'gradient_mask', 'class'], // Changes to these attributes will be reactively forwarded to your component
-    shadow: false, // Use the light DOM]
+    shadow: false, // Use the light DOM
     hydratable: true,
 });
