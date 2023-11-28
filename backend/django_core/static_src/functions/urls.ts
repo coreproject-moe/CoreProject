5fadce30b380998fd630a808b68f2cafee948789
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-28 18:05:27 +0600

    [`Epic`] : Add djagno resolving capability to the frontned (#3001)
    * add
    
    * Update context_processors.py
    
    * add
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add
    
    * finish
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update urls.ts
    
    * Update context_processors.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ backend/django_core/static_src/functions/urls.ts
@@ -0,0 +1,33 @@
+const urls = window.urls;
+
+export function reverse(view: string, ...args: Array<string | number>) {
+    const url = urls.get(view);
+    if (!url) {
+        throw new Error(`No Match found for ${view}`);
+    }
+
+    // Matches the following patterns
+    // <str:pk> | <int:pk>
+    const match_pattern = /\<(int|str):(\w+)>\/?/g;
+    const matches = url?.match(match_pattern) ?? [];
+
+    if (matches?.length === 0) {
+        return url;
+    } else if (matches?.length !== args.length) {
+        throw new Error('`args` doesnot match with `urlpattern`');
+    }
+
+    const replacements: Record<string, string> = matches.reduce(
+        (obj, k, i) => ({ ...obj, [k]: args[i] }),
+        {}
+    );
+
+    // Create a regular expression pattern to match all occurrences of the keys in replacements
+    const pattern = new RegExp(Object.keys(replacements).join('|'), 'g');
+
+    const final_url = url.replace(pattern, function (match: string) {
+        return replacements[match];
+    });
+
+    return final_url;
+}
