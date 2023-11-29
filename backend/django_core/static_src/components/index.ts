a90520f3be93036bf17c72c2c07e41feccef4e00
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-11-29 18:05:50 +0600

    add

--- backend/django_core/static_src/components/index.ts
+++ backend/django_core/static_src/components/index.ts
@@ -2,21 +2,29 @@
 import svelteRetag from "svelte-retag";
 
 import CommentBox from "$components/specific/CommentBox.svelte";
-import ScrollArea from "$components//minor/ScrollArea.svelte";
+import ScrollArea from "$components/minor/ScrollArea.svelte";
+import MyAnime from "$components/specific/MyAnime.svelte";
 
 import "../css/tippy.scss";
 
 svelteRetag({
     component: ScrollArea,
-    tagname: "scroll-area",
-    attributes: ["parent_class", "offset_scrollbar", "gradient_mask", "class"], // Changes to these attributes will be reactively forwarded to your component
+    tagname: `scroll-area`,
+    attributes: [`parent_class`, `offset_scrollbar`, `gradient_mask`, `class`], // Changes to these attributes will be reactively forwarded to your component
     shadow: false, // Use the light DOM
     hydratable: true
 });
 svelteRetag({
     component: CommentBox,
-    tagname: "coreproject-commentbox",
-    attributes: ["submit_url"],
+    tagname: `coreproject-commentbox`,
+    attributes: [`submit_url`],
+    shadow: false,
+    hydratable: true
+});
+svelteRetag({
+    component: MyAnime,
+    tagname: `coreproject-my-anime-list`,
+    attributes: [`anime_name`, `anime_status`, `anime_image`, `anime_current_episodes`, `anime_total_episodes`],
     shadow: false,
     hydratable: true
 });
