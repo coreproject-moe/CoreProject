5df34d7dcfd994e134ebfd97eeb4e2fa6c03931d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-02 08:56:04 +0600

    add

--- backend/django_core/static_src/components/minor/index.ts
+++ backend/django_core/static_src/components/minor/index.ts
@@ -1,25 +1,22 @@
 // @ts-ignore
 import svelteRetag from "svelte-retag";
-import ScrollArea from "$components/minor/ScrollArea/Index.svelte";
-import Comment from "$components/minor/Comment/Index.svelte";
-import Markdown from "$components/minor/Markdown/Index.svelte";
 
 svelteRetag({
-    component: Markdown,
+    component: await import("$components/minor/Markdown/Index.svelte"),
     tagname: `coreproject-markdown`,
     attributes: [`markdown`, `class`], // Changes to these attributes will be reactively forwarded to your component
     shadow: false, // Use the light DOM
     hydratable: true
 });
 svelteRetag({
-    component: ScrollArea,
+    component: await import("$components/minor/ScrollArea/Index.svelte"),
     tagname: `coreproject-scroll-area`,
     attributes: [`parent_class`, `offset_scrollbar`, `gradient_mask`, `class`], // Changes to these attributes will be reactively forwarded to your component
     shadow: false, // Use the light DOM
     hydratable: true
 });
 svelteRetag({
-    component: Comment,
+    component: await import("$components/minor/Comment/Index.svelte"),
     tagname: `coreproject-comment`,
     attributes: [`api_url`],
     shadow: false, // Use the light DOM
