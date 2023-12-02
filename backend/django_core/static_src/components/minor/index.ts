cb19ed695f0504577fb63d0dd4119a81806bd799
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-02 09:17:24 +0600

    [`Frontend`] : remove `await import`
    Update index.ts
    
    Update index.ts

--- backend/django_core/static_src/components/minor/index.ts
+++ backend/django_core/static_src/components/minor/index.ts
@@ -1,22 +1,25 @@
 // @ts-ignore
 import svelteRetag from "svelte-retag";
+import ScrollArea from "$components/minor/ScrollArea/Index.svelte";
+import Comment from "$components/minor/Comment/Index.svelte";
+import Markdown from "$components/minor/Markdown/Index.svelte";
 
 svelteRetag({
-    component: await import("$components/minor/Markdown/Index.svelte"),
+    component: Markdown,
     tagname: `coreproject-markdown`,
     attributes: [`markdown`, `class`], // Changes to these attributes will be reactively forwarded to your component
     shadow: false, // Use the light DOM
     hydratable: true
 });
 svelteRetag({
-    component: await import("$components/minor/ScrollArea/Index.svelte"),
+    component: ScrollArea,
     tagname: `coreproject-scroll-area`,
     attributes: [`parent_class`, `offset_scrollbar`, `gradient_mask`, `class`], // Changes to these attributes will be reactively forwarded to your component
     shadow: false, // Use the light DOM
     hydratable: true
 });
 svelteRetag({
-    component: await import("$components/minor/Comment/Index.svelte"),
+    component: Comment,
     tagname: `coreproject-comment`,
     attributes: [`api_url`],
     shadow: false, // Use the light DOM
--- backend/django_core/static_src/components/specific/index.ts
+++ backend/django_core/static_src/components/specific/index.ts
@@ -1,16 +1,19 @@
 // @ts-ignore
 import svelteRetag from "svelte-retag";
 
+import CommentBox from "$components/specific/CommentBox/Index.svelte";
+import MyAnime from "$components/specific/MyAnime/Index.svelte";
+
 // Specific Components
 svelteRetag({
-    component: await import("$components/specific/CommentBox/Index.svelte"),
+    component: CommentBox,
     tagname: `coreproject-specific-commentbox`,
     attributes: [`submit_url`],
     shadow: false,
     hydratable: true
 });
 svelteRetag({
-    component: await import("$components/specific/MyAnime/Index.svelte"),
+    component: MyAnime,
     tagname: `coreproject-specific-my-anime-list`,
     attributes: [`anime_name`, `anime_status`, `anime_image`, `anime_current_episodes`, `anime_total_episodes`, `dropdown_class`, `anime_description`, `anime_studio`, `anime_genres`],
     shadow: false,
