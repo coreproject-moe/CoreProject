8f3c5cbf5ad913146241cc80a41ecde19d88590c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-06 00:32:10 +0600

    Update CommetBlock.svelte

--- backend/django_core/static_src/components/minor/Comment/CommetBlock.svelte
+++ backend/django_core/static_src/components/minor/Comment/CommetBlock.svelte
@@ -16,7 +16,12 @@
         children: number;
     };
 
-    const comment_level = item.path.split(".").length; // this might help later
+    // icon import
+    import Arrow from "$icons/Arrow/Index.svelte";
+    import Dot from "$icons/Dot/Index.svelte";
+    import Share from "$icons/Share/Index.svelte";
+    import Chat from "$icons/Chat/Index.svelte";
+    import Cross from "$icons/Cross/Index.svelte";
 
     // Object.entries(item).forEach(([key, obj]) => {
     //     if (typeof obj === "object" && key !== "user") {
@@ -37,8 +42,8 @@
                 class="h-full w-full shrink-0 rounded-full object-cover"
             />
         </a>
-        <button class="h-full md:w-full flex justify-center group cursor-pointer active:scale-95 transition-transform">
-            <div class="bg-neutral rounded-full md:w-[0.15vw] h-full group-hover:md:w-[0.2vw] group-hover:bg-warning transition-colors" />
+        <button class="group flex h-full cursor-pointer justify-center transition-transform active:scale-95 md:w-full">
+            <div class="h-full rounded-full bg-neutral transition-colors group-hover:bg-warning md:w-[0.15vw] group-hover:md:w-[0.2vw]" />
         </button>
     </div>
     <div class="flex flex-col items-start gap-1 md:gap-[0.25vw]">
@@ -57,36 +62,40 @@
         </div>
         <div class="flex items-center gap-3 md:gap-[0.75vw]">
             <div class="flex items-center md:gap-[0.35vw]">
-                <button class="btn md:h-max min-h-full btn-secondary p-0">
-                    <coreproject-icon-arrow
+                <button class="btn btn-secondary min-h-full p-0 md:h-max">
+                    <Arrow
                         class="md:w-[1.25vw]"
                         variant="outline"
-                    ></coreproject-icon-arrow>
+                    />
                 </button>
-                <span class="md:text-[0.9vw] font-semibold text-accent">106</span>
-                <button class="btn md:h-max min-h-full btn-secondary p-0">
-                    <coreproject-icon-arrow
-                        class="md:w-[1.25vw] rotate-90"
+                <span class="font-semibold text-accent md:text-[0.9vw]">106</span>
+                <button class="btn btn-secondary min-h-full p-0 md:h-max">
+                    <Arrow
+                        class="rotate-90 md:w-[1.25vw]"
                         variant="outline"
-                    ></coreproject-icon-arrow>
+                    />
                 </button>
             </div>
-            <button class="btn min-h-full md:h-max !bg-transparent p-0 text-xs md:text-[0.9vw] md:gap-[0.35vw]">
-                <coreproject-icon-chat class="md:w-[1vw]"></coreproject-icon-chat>
+            <button class="btn min-h-full !bg-transparent p-0 text-xs md:h-max md:gap-[0.35vw] md:text-[0.9vw]">
+                <Chat class="md:w-[1vw]" />
                 <span>Replay</span>
             </button>
-            <button class="btn min-h-full md:h-max !bg-transparent p-0 text-xs md:text-[0.9vw] md:gap-[0.35vw]">
-                <coreproject-icon-share class="md:w-[1vw]"></coreproject-icon-share>
+            <button class="btn min-h-full !bg-transparent p-0 text-xs md:h-max md:gap-[0.35vw] md:text-[0.9vw]">
+                <Share class="md:w-[1vw]"></Share>
                 <span>Share</span>
             </button>
             <div class="dropdown">
-                <div tabindex="0" role="button" class="btn btn-secondary p-0 h-max min-h-max md:gap-[0.15vw]">
-                    <coreproject-icon-dot class="md:w-[0.2vw]"></coreproject-icon-dot>
-                    <coreproject-icon-dot class="md:w-[0.2vw]"></coreproject-icon-dot>
-                    <coreproject-icon-dot class="md:w-[0.2vw]"></coreproject-icon-dot>
+                <div
+                    tabindex="0"
+                    role="button"
+                    class="btn btn-secondary h-max min-h-max p-0 md:gap-[0.15vw]"
+                >
+                    <Dot class="md:w-[0.2vw]" />
+                    <Dot class="md:w-[0.2vw]" />
+                    <Dot class="md:w-[0.2vw]" />
                 </div>
-                <ul class="dropdown-content z-10 bg-neutral md:rounded-[0.25vw] overflow-hidden">
-                    <li class="md:py-[0.5vw] md:px-[1vw] md:text-[1vw] hover:bg-primary hover:text-accent transition-colors cursor-pointer">
+                <ul class="dropdown-content z-10 overflow-hidden bg-neutral md:rounded-[0.25vw]">
+                    <li class="cursor-pointer transition-colors hover:bg-primary hover:text-accent md:px-[1vw] md:py-[0.5vw] md:text-[1vw]">
                         <span>Report</span>
                     </li>
                 </ul>
@@ -95,7 +104,7 @@
 
         <!-- Render replies here -->
         {#if item.children !== 0}
-            <div class="flex flex-col md:gap-[1.5vw] md:mt-[1.5vw]">
+            <div class="flex flex-col md:mt-[1.5vw] md:gap-[1.5vw]">
                 {#each Object.entries(item) as [key, obj]}
                     <!-- Avoid user object -->
                     {#if typeof obj === "object" && key !== "user"}
@@ -108,12 +117,23 @@
 </div>
 
 {#if item.children > 1}
-    <div class="flex items-end md:gap-[0.5vw] md:ml-[0.55vw]">
-        <svg class="md:w-[2vw] text-neutral" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15"><path fill="currentColor" fill-rule="evenodd" d="M9.877 12H11.5a.5.5 0 0 0 0-1H9.9c-1.128 0-1.945 0-2.586-.053c-.637-.052-1.057-.152-1.403-.328a3.5 3.5 0 0 1-1.53-1.53c-.176-.346-.276-.766-.328-1.403C4 7.045 4 6.228 4 5.1V3.5a.5.5 0 0 0-1 0v1.623c0 1.1 0 1.958.056 2.645c.057.698.175 1.265.434 1.775a4.5 4.5 0 0 0 1.967 1.967c.51.26 1.077.377 1.775.434C7.92 12 8.776 12 9.877 12Z" clip-rule="evenodd"/></svg>
+    <div class="flex items-end md:ml-[0.55vw] md:gap-[0.5vw]">
+        <svg
+            class="text-neutral md:w-[2vw]"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 15 15"
+        >
+            <path
+                fill="currentColor"
+                fill-rule="evenodd"
+                d="M9.877 12H11.5a.5.5 0 0 0 0-1H9.9c-1.128 0-1.945 0-2.586-.053c-.637-.052-1.057-.152-1.403-.328a3.5 3.5 0 0 1-1.53-1.53c-.176-.346-.276-.766-.328-1.403C4 7.045 4 6.228 4 5.1V3.5a.5.5 0 0 0-1 0v1.623c0 1.1 0 1.958.056 2.645c.057.698.175 1.265.434 1.775a4.5 4.5 0 0 0 1.967 1.967c.51.26 1.077.377 1.775.434C7.92 12 8.776 12 9.877 12Z"
+                clip-rule="evenodd"
+            />
+        </svg>
 
-        <button class="btn btn-secondary p-0 h-max min-h-max flex items-center md:gap-[0.75vw]">
-            <div class="rotate-45 bg-neutral md:w-[1.5vw] md:h-[1.5vw] rounded-full grid place-items-center">
-                <coreproject-icon-cross class="md:w-[1vw] text-accent p-0"></coreproject-icon-cross>
+        <button class="btn btn-secondary flex h-max min-h-max items-center p-0 md:gap-[0.75vw]">
+            <div class="grid rotate-45 place-items-center rounded-full bg-neutral md:h-[1.5vw] md:w-[1.5vw]">
+                <Cross class="p-0 text-accent md:w-[1vw]" />
             </div>
             <span class="md:text-[1vw]">{item.children - 1} More</span>
         </button>
