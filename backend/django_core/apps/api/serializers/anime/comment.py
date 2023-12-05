e2f46d03aaf40b94cec1412c6e4bcf1a6ecc60c8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-05 10:05:43 +0600

    [`Backend`] : Refractor to get children length

--- backend/django_core/apps/api/serializers/anime/comment.py
+++ backend/django_core/apps/api/serializers/anime/comment.py
@@ -1,6 +1,8 @@
 from apps.user.models import CustomUser
 from rest_framework import serializers
 
+from apps.anime.models.anime_comment import AnimeCommentModel
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +22,7 @@ class AnimeCommentSerializer(serializers.Serializer):
     user = UserSerializer(read_only=True)
     text = serializers.CharField()
     path = serializers.CharField(required=False)
+    children = serializers.SerializerMethodField()
+
+    def get_children(self, obj: AnimeCommentModel) -> int:
+        return obj.children().count()
