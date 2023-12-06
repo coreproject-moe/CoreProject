32a7cd79e04b9bee154fd21eb1fec886d940ce9a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-06 08:51:01 +0600

    add

--- backend/django_core/static_src/components/specific/index.ts
+++ backend/django_core/static_src/components/specific/index.ts
@@ -1,18 +1,17 @@
 // @ts-ignore
 import svelteRetag from "svelte-retag";
 
+const mapping = [
+    { tagname: "coreproject-specific-commentbox", component: await import("$components/specific/CommentBox/Index.svelte"), attributes: [`submit_url`] },
+    { tagname: "coreproject-specific-my-anime-list", component: await import("$components/specific/MyAnime/Index.svelte"), attributes: [`anime_name`, `anime_status`, `anime_image`, `anime_current_episodes`, `anime_total_episodes`, `dropdown_class`, `anime_description`, `anime_studio`, `anime_genres`] }
+];
 // Specific Components
-svelteRetag({
-    component: (await import("$components/specific/CommentBox/Index.svelte")).default,
-    tagname: `coreproject-specific-commentbox`,
-    attributes: [`submit_url`],
-    shadow: false,
-    hydratable: true
-});
-svelteRetag({
-    component: (await import("$components/specific/MyAnime/Index.svelte")).default,
-    tagname: `coreproject-specific-my-anime-list`,
-    attributes: [`anime_name`, `anime_status`, `anime_image`, `anime_current_episodes`, `anime_total_episodes`, `dropdown_class`, `anime_description`, `anime_studio`, `anime_genres`],
-    shadow: false,
-    hydratable: true
+mapping.forEach((item) => {
+    svelteRetag({
+        component: item.component.default,
+        tagname: item.tagname,
+        attributes: item.attributes,
+        shadow: false,
+        hydratable: true
+    });
 });
