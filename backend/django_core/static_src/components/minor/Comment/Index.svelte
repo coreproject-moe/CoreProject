fa6c61569662144901f33274ca86f1f83978518d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-07 23:27:53 +0600

    [`Frontend`] : Work on tree (#3089)
    add

--- backend/django_core/static_src/components/minor/Comment/Index.svelte
+++ backend/django_core/static_src/components/minor/Comment/Index.svelte
@@ -2,6 +2,7 @@
     import { JSONToTree } from "./json_to_tree";
     import CommetBlock from "./CommetBlock.svelte";
     import type { Comment } from "../../../types/comment";
+    import { comment_needs_update } from "../../../stores/comment";
 
     export let api_url: string;
 
@@ -11,6 +12,8 @@
         previous: null | number;
         results: Comment[];
     }
+
+    let tree = get_comments();
 
     async function get_comments() {
         const res = await fetch(api_url, {
@@ -27,16 +30,25 @@
             throw new Error(await res.text());
         }
     }
+
+    comment_needs_update.subscribe((val) => {
+        if (val === true) {
+            tree = get_comments();
+            comment_needs_update.set(false);
+        }
+    });
 </script>
 
-{#await get_comments()}
-    Loading...
-{:then tree_branch}
-    <div class="flex flex-col md:gap-[1.5vw]">
-        {#each tree_branch as branch}
-            <CommetBlock item={branch} />
-        {/each}
-    </div>
-{:catch e}
-    Something is wrong {e}
-{/await}
+{#key tree}
+    {#await tree}
+        Loading...
+    {:then tree_branch}
+        <div class="flex flex-col md:gap-[1.5vw]">
+            {#each tree_branch as branch}
+                <CommetBlock item={branch} />
+            {/each}
+        </div>
+    {:catch e}
+        Something is wrong {e}
+    {/await}
+{/key}
