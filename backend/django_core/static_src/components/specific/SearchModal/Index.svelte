68ef08446e936cd551cd475583eb582819c9f5df
tokitouq <tokitousan2005@gmail.com>
2023-12-08 19:23:22 +0530

    move to web components

---
+++ backend/django_core/static_src/components/specific/SearchModal/Index.svelte
@@ -0,0 +1,15 @@
+<dialog
+    id="search_model"
+    class="modal"
+>
+    <div class="modal-box">
+        <h3 class="text-lg font-bold">Hello!</h3>
+        <p class="py-4">Press ESC key or click outside to close</p>
+    </div>
+    <form
+        method="dialog"
+        class="modal-backdrop"
+    >
+        <button>close</button>
+    </form>
+</dialog>
