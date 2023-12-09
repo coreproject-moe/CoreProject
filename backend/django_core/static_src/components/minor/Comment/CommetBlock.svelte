a14a3fc55698e1f41488018bca1365622cb93bc7
tokitouq <tokitousan2005@gmail.com>
2023-12-09 09:14:25 +0530

    feat: add functionality

--- backend/django_core/static_src/components/minor/Comment/CommetBlock.svelte
+++ backend/django_core/static_src/components/minor/Comment/CommetBlock.svelte
@@ -16,9 +16,18 @@
     // Bindings
     let user_reaction: typeof item.user_reaction,
         ratio: typeof item.ratio,
-        reply_shown = false;
+        reply_shown = false,
+        is_comment_highlighted = false;
 
     onMount(() => {
+        const url_params = new URLSearchParams(window.location.search);
+        if (url_params.has("comment")) {
+            const comment_id = Number(url_params.get("comment")!);
+            if (comment_id === item.pk) {
+                is_comment_highlighted = true;
+            }
+        }
+        // user actions
         user_reaction = item.user_reaction;
         ratio = item.ratio;
     });
@@ -76,80 +85,88 @@
         </button>
     </div>
     <div class="flex flex-col items-start gap-1 md:gap-[0.25vw]">
-        <a
-            href="/user/"
-            class="flex flex-col text-xs leading-none md:text-[1vw]"
+        <div
+            id={`comment-${item.pk}`}
+            class="relative flex flex-col items-start gap-1 md:gap-[0.25vw]"
         >
-            <div class="flex items-center md:gap-[0.5vw]">
-                <div class="text-white">{`${item.user.first_name} ${item.user.last_name}`}</div>
-                <div class="md:text-[0.75vw]">{item.user.username}</div>
-            </div>
-            <div class="text-surface-300 md:text-[0.75vw] md:leading-[1.5vw]">{new FormatDate(item.created_at).format_to_time_from_now}</div>
-        </a>
-        <div class="text-sm leading-snug text-accent md:text-[1vw] md:leading-[1.5vw]">
-            <Markdown markdown={item.text} />
-        </div>
-        <div class="flex items-center gap-3 md:gap-[0.75vw]">
-            <div class="flex items-center md:gap-[0.35vw]">
-                <button
-                    on:click|preventDefault={async () => {
-                        if (user_reaction === "upvoted") {
-                            await delete_to_reaction_endpoint();
-                        } else {
-                            await post_to_reaction_endpoint("upvote");
-                        }
-                    }}
-                    class="btn btn-secondary min-h-full p-0 md:h-max"
-                >
-                    {#if user_reaction === "upvoted"}
-                        <Arrow
-                            variant="fill"
-                            class="text-warning md:w-[1.25vw]"
-                        />
-                    {:else}
-                        <Arrow
-                            class="md:w-[1.25vw]"
-                            variant="outline"
-                        />
-                    {/if}
-                </button>
-                <span class="font-semibold text-accent md:text-[0.9vw]">{ratio}</span>
-                <button
-                    class="btn btn-secondary min-h-full p-0 md:h-max"
-                    on:click={async () => {
-                        if (user_reaction === "downvoted") {
-                            await delete_to_reaction_endpoint();
-                        } else {
-                            await post_to_reaction_endpoint("downvote");
-                        }
-                    }}
-                >
-                    {#if user_reaction === "downvoted"}
-                        <Arrow
-                            class="rotate-180 text-warning md:w-[1.25vw]"
-                            variant="fill"
-                        />
-                    {:else}
-                        <Arrow
-                            class="rotate-180 md:w-[1.25vw]"
-                            variant="outline"
-                        />
-                    {/if}
-                </button>
-            </div>
-            <button
-                class="btn min-h-full !bg-transparent p-0 text-xs md:h-max md:gap-[0.35vw] md:text-[0.9vw]"
-                on:click|preventDefault={() => (reply_shown = !reply_shown)}
+            <div
+                hidden={!is_comment_highlighted}
+                class="-z-1 pointer-events-none absolute inset-0 bg-neutral/25 md:-m-[1vw] md:-mb-[0.75vw] md:rounded-[0.5vw]"
+            ></div>
+            <a
+                href="/user/"
+                class="flex flex-col text-xs leading-none md:text-[1vw]"
             >
-                <Chat class="md:w-[1vw]" />
-                <span>Replay</span>
-            </button>
-            <button class="btn min-h-full !bg-transparent p-0 text-xs md:h-max md:gap-[0.35vw] md:text-[0.9vw]">
-                <Share class="md:w-[1vw]" />
-                <span>Share</span>
-            </button>
+                <div class="flex items-center md:gap-[0.5vw]">
+                    <div class="text-white">{`${item.user.first_name} ${item.user.last_name}`}</div>
+                    <div class="md:text-[0.75vw]">{item.user.username}</div>
+                </div>
+                <div class="text-surface-300 md:text-[0.75vw] md:leading-[1.5vw]">{new FormatDate(item.created_at).format_to_time_from_now}</div>
+            </a>
+            <div class="text-sm leading-snug text-accent md:text-[1vw] md:leading-[1.5vw]">
+                <Markdown markdown={item.text} />
+            </div>
+            <div class="flex items-center gap-3 md:gap-[0.75vw]">
+                <div class="flex items-center md:gap-[0.35vw]">
+                    <button
+                        on:click|preventDefault={async () => {
+                            if (user_reaction === "upvoted") {
+                                await delete_to_reaction_endpoint();
+                            } else {
+                                await post_to_reaction_endpoint("upvote");
+                            }
+                        }}
+                        class="btn btn-secondary min-h-full p-0 md:h-max"
+                    >
+                        {#if user_reaction === "upvoted"}
+                            <Arrow
+                                variant="fill"
+                                class="text-warning md:w-[1.25vw]"
+                            />
+                        {:else}
+                            <Arrow
+                                class="md:w-[1.25vw]"
+                                variant="outline"
+                            />
+                        {/if}
+                    </button>
+                    <span class="font-semibold text-accent md:text-[0.9vw]">{ratio}</span>
+                    <button
+                        class="btn btn-secondary min-h-full p-0 md:h-max"
+                        on:click={async () => {
+                            if (user_reaction === "downvoted") {
+                                await delete_to_reaction_endpoint();
+                            } else {
+                                await post_to_reaction_endpoint("downvote");
+                            }
+                        }}
+                    >
+                        {#if user_reaction === "downvoted"}
+                            <Arrow
+                                class="rotate-180 text-warning md:w-[1.25vw]"
+                                variant="fill"
+                            />
+                        {:else}
+                            <Arrow
+                                class="rotate-180 md:w-[1.25vw]"
+                                variant="outline"
+                            />
+                        {/if}
+                    </button>
+                </div>
+                <button
+                    class="btn min-h-full !bg-transparent p-0 text-xs md:h-max md:gap-[0.35vw] md:text-[0.9vw]"
+                    on:click|preventDefault={() => (reply_shown = !reply_shown)}
+                >
+                    <Chat class="md:w-[1vw]" />
+                    <span>Replay</span>
+                </button>
+                <button class="btn min-h-full !bg-transparent p-0 text-xs md:h-max md:gap-[0.35vw] md:text-[0.9vw]">
+                    <Share class="md:w-[1vw]" />
+                    <span>Share</span>
+                </button>
 
-            <!-- <div class="dropdown">
+                <!-- <div class="dropdown">
                 <div
                     tabindex="0"
                     role="button"
@@ -165,6 +182,7 @@
                     </li>
                 </ul>
             </div> -->
+            </div>
         </div>
         {#if reply_shown}
             <div class="md:mt-[1vw]">
