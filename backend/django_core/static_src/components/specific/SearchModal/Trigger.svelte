b1af67520651dfaa50ac9f78f97468722096dd48
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-11 00:15:50 +0600

    [`Frontend`] : better codes (#3104)
    * add
    
    * add

---
+++ backend/django_core/static_src/components/specific/SearchModal/Trigger.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { search_modal_state } from "../../../stores/modal";
+
+    let klass = "";
+    export { klass as class };
+    export let style = "";
+
+    const handle_button_click = () => {
+        search_modal_state.set(true);
+    };
+</script>
+
+<button
+    class={klass}
+    {style}
+    on:click|preventDefault={handle_button_click}
+>
+    <slot />
+</button>
