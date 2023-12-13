eda6d620f0ce2ad6bb5a3b607c9a6f5bd4e3d165
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-13 19:48:02 +0600

    add

--- backend/django_core/static_src/components/minor/Comment/Index.svelte
+++ backend/django_core/static_src/components/minor/Comment/Index.svelte
@@ -3,6 +3,7 @@
     import CommetBlock from "./CommetBlock.svelte";
     import type { Comment } from "../../../types/comment";
     import { comment_needs_update } from "../../../stores/comment";
+    import { onMount } from "svelte";
 
     export let api_url: string;
 
@@ -13,7 +14,20 @@
         results: Comment[];
     }
 
-    let tree = get_comments();
+    let loading_state: "loading" | "error" | "loaded";
+    let tree_branch: Comment[] = new Array<Comment>();
+
+    onMount(() => {
+        loading_state = "loading";
+        get_comments()
+            .then((res) => {
+                tree_branch = res;
+                loading_state = "loaded";
+            })
+            .catch((err) => {
+                loading_state = "error";
+            });
+    });
 
     async function get_comments() {
         const res = await fetch(api_url, {
@@ -31,24 +45,26 @@
         }
     }
 
-    comment_needs_update.subscribe((val) => {
+    comment_needs_update.subscribe(async (val) => {
         if (val === true) {
-            tree = get_comments();
+            tree_branch = await get_comments();
             comment_needs_update.set(false);
         }
     });
 </script>
 
-{#key tree}
-    {#await tree}
-        Loading...
-    {:then tree_branch}
+{#if loading_state === "loading"}
+    Loading..
+{:else if loading_state === "error"}
+    Something is wrong
+{:else if loading_state === "loaded"}
+    {#if tree_branch}
         <div class="flex flex-col md:gap-[1.5vw]">
             {#each tree_branch as branch}
                 <CommetBlock item={branch} />
             {/each}
         </div>
-    {:catch e}
-        Something is wrong {e}
-    {/await}
-{/key}
+    {:else}
+        No comments
+    {/if}
+{/if}
