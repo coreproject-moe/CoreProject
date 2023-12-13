1833c0c56beb2d6b2afb5f533e22405f643748dc
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-13 20:19:38 +0600

    [`Frontend`] : Rename variabels (#3123)
    * add
    
    * add

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -2,7 +2,6 @@ import { join, resolve } from "path";
 import process from "process";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import topLevelAwait from "vite-plugin-top-level-await";
 const STATIC_SRC = resolve("./django_core/static_src");
 
 const COMPONENT_DIRECTORY = join(STATIC_SRC, "components");
@@ -20,12 +19,6 @@ export default defineConfig({
         }
     },
     plugins: [
-        topLevelAwait({
-            // The export name of top-level await promise for each chunk module
-            promiseExportName: "__coreproject__",
-            // The function to generate import names of top-level await promise in each chunk module
-            promiseImportName: (i) => `__coreproject__${i}`
-        }),
         svelte({
             compilerOptions: {
                 customElement: true
@@ -46,7 +39,7 @@ export default defineConfig({
         manifest: true,
         chunkSizeWarningLimit: 2048,
         emptyOutDir: true,
-        target: "es2015",
+        target: "es2022",
         cssTarget: "esnext",
         minify: "terser",
         // sourcemap: true,
