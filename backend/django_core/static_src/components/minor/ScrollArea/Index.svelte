9497c758228a2b2e4088d9d939ed5009cdccec85
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-13 21:27:17 +0600

    [`Frontend`] : Better patch (#3126)
    * add
    
    * Update Index.svelte
    
    * Update Index.svelte

--- backend/django_core/static_src/components/minor/ScrollArea/Index.svelte
+++ backend/django_core/static_src/components/minor/ScrollArea/Index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import IntersectionObserver from "svelte-intersection-observer";
     import { cn } from "$functions/classname";
-    import { get_transition_duration_as_ms } from "$functions/get_transition_durtion_as_ms";
 
     let klass = "";
     export { klass as class };
@@ -12,7 +11,7 @@
     let scroll_area: HTMLElement;
     let add_mask_bottom: boolean;
 
-    let first_element_intersecting: boolean, second_element_intersecting: boolean, first_element: HTMLElement, second_element: HTMLElement;
+    let first_element_intersecting: boolean, end_element_intersecting: boolean, first_element: HTMLElement, end_element: HTMLElement;
 
     $: add_mask_bottom = scroll_area ? scroll_area.scrollHeight > scroll_area.clientHeight : false;
 
@@ -22,34 +21,28 @@
             add_mask_bottom = clientHeight + scrollTop === scrollHeight ? false : true;
         },
         handle_mouseenter = () => {
-            const duration = get_transition_duration_as_ms({ element: scroll_area, ms: true });
-            setTimeout(
-                () => {
-                    if (first_element_intersecting && second_element_intersecting) {
-                        add_mask_bottom = false;
-                    }
-                },
-                duration ? duration / 4 : 0
-            );
+            scroll_area.addEventListener("transitionend", () => {
+                if (first_element_intersecting && end_element_intersecting) {
+                    add_mask_bottom = false;
+                }
+            });
         },
         handle_mouseleave = () => {
-            setTimeout(
-                () => {
-                    if (first_element_intersecting && second_element_intersecting) {
-                        add_mask_bottom = true;
-                    }
-                },
-                get_transition_duration_as_ms({ element: scroll_area, ms: true }) ?? 0
-            );
+            scroll_area.addEventListener("transitionend", () => {
+                if (first_element_intersecting && end_element_intersecting) {
+                    add_mask_bottom = true;
+                }
+            });
         };
+    // TODO: Do AOT calculations on `transitionrun` to  prevent flicker
 </script>
 
 <div
     role="contentinfo"
     bind:this={scroll_area}
-    on:scroll={handle_scroll}
-    on:mouseenter={handle_mouseenter}
-    on:mouseleave={handle_mouseleave}
+    on:scroll|preventDefault={handle_scroll}
+    on:mouseenter|preventDefault={handle_mouseenter}
+    on:mouseleave|preventDefault={handle_mouseleave}
     class={cn(parent_class, offset_scrollbar && "pr-3 md:pr-[0.75vw]", "flex h-full w-full overflow-y-scroll overscroll-y-contain scrollbar-thin [scrollbar-color:rgba(255,255,255,0.12)transparent]")}
     class:[mask-image:linear-gradient(180deg,rgba(7,5,25,0.95)80%,rgba(0,0,0,0)100%)]={gradient_mask && add_mask_bottom}
 >
@@ -65,11 +58,11 @@
         <slot />
 
         <IntersectionObserver
-            element={second_element}
-            bind:intersecting={second_element_intersecting}
+            element={end_element}
+            bind:intersecting={end_element_intersecting}
             threshold={1}
         >
-            <div bind:this={second_element} />
+            <div bind:this={end_element} />
         </IntersectionObserver>
     </div>
 </div>
