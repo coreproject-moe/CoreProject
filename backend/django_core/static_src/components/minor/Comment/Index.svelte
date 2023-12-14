643b40e21ff58d9a5575bada9400123afaf1fe1e
tokitouq <tokitousan2005@gmail.com>
2023-12-14 23:13:39 +0530

    revert: comment test state change

--- backend/django_core/static_src/components/minor/Comment/Index.svelte
+++ backend/django_core/static_src/components/minor/Comment/Index.svelte
@@ -75,11 +75,11 @@
     });
 </script>
 
-{#if loading_state === "loaded"}
+{#if loading_state === "loading"}
     <CommentBlockSkeleton skeleton_count={1} />
 {:else if loading_state === "error"}
     Something is wrong Error : {@html error}
-{:else if loading_state === "loading"}
+{:else if loading_state === "loaded"}
     {#if tree_branch}
         <div class="flex flex-col md:gap-[1.5vw]">
             {#each tree_branch as branch}
