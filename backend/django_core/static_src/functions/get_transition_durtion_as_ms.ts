aa938fcb13cbd7de32a28553f8bccdd4540cca4e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-14 15:59:50 +0600

    [`Frontned`] : Add `old_json` capability (#3130)
    add

--- backend/django_core/static_src/functions/get_transition_durtion_as_ms.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @author yorris
- *  @see https://gist.github.com/yoriiis/3bfd3833d5aaaa454c682f782b1e3a23
- */
-export function get_transition_duration_as_ms({ element, ms = false }: { element: HTMLElement; ms?: boolean }) {
-    return parseFloat(window.getComputedStyle(element).transitionDuration) * (ms ? 1000 : 1);
-}
