c564e75949ff8be41e317bdd5bd70f5eef3f18b4
Tokito <tokitousan2005@gmail.com>
2023-12-15 21:05:00 +0530

    [`Frontend`]: Revert state change (#3154)
    fix

---
+++ backend/django_core/static_src/components/minor/Comment/Empty.svelte
@@ -0,0 +1,11 @@
+<div class="flex w-full items-center justify-center md:gap-[1vw]">
+    <img
+        src="/static/public/images/gifs/fish.gif"
+        alt=""
+        class="md:h-[15vw]"
+    />
+    <div class="flex flex-col md:gap-[0.1vw]">
+        <span class="font-semibold md:text-[2vw]">No comments found!</span>
+        <span class="italic md:text-[1.25vw]">"Be the first one to comment"</span>
+    </div>
+</div>
