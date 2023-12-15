ec73b10bee6c9d8fb973858adf461e558c686ad4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-15 18:06:53 +0600

    add

--- backend/django_core/static_src/functions/string_to_bool.ts
+++ backend/django_core/static_src/functions/string_to_bool.ts
@@ -1,7 +1,14 @@
 export function string_to_boolean(string: string) {
     const _string = string.toLowerCase();
-
-    if (_string === "true") return true;
-    else if (_string === "false") return false;
-    else throw Error("String is not convertable to boolean");
+    switch (_string) {
+        case "true": {
+            return true;
+        }
+        case "false": {
+            return false;
+        }
+        default: {
+            throw Error("String is not convertable to boolean");
+        }
+    }
 }
