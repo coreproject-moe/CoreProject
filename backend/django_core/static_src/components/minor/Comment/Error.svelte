32817122c9327140a6391bd0c945787eb7d19c8d
Tokito <tokitousan2005@gmail.com>
2023-12-15 21:37:35 +0530

    [`Frontend`]: Fix comment error style (#3155)
    fix: add error

---
+++ backend/django_core/static_src/components/minor/Comment/Error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let error: string;
+</script>
+
+<div class="flex w-full items-center justify-center md:gap-[1vw]">
+    <img
+        src="/static/public/images/gifs/sad.gif"
+        alt=""
+        class="md:h-[10vw]"
+    />
+    <div class="flex flex-col md:gap-[0.1vw]">
+        <span class="font-semibold md:text-[1.5vw]">Something went wrong!</span>
+        <span class="italic md:text-[1.15vw]">Error: {error}</span>
+    </div>
+</div>
