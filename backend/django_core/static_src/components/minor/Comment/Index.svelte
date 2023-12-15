4743c0371b4b32d7c1ae1855bb13c8b71eb45a7e
tokitouq <tokitousan2005@gmail.com>
2023-12-15 20:49:33 +0530

    fix: add fish gif

--- backend/django_core/static_src/components/minor/Comment/Index.svelte
+++ backend/django_core/static_src/components/minor/Comment/Index.svelte
@@ -94,10 +94,14 @@
         </div>
     {:else}
         <div class="flex w-full items-center justify-center md:gap-[1vw]">
-            <Empty class="stroke-accent md:w-[10vw]" />
-            <div class="flex flex-col md:gap-[0.15vw]">
-                <span class="font-semibold md:text-[1.25vw]">No comments found!</span>
-                <span class="italic md:text-[1.1vw]">"Be the first one to comment"</span>
+            <img
+                src="/static/public/images/gifs/fish.gif"
+                alt=""
+                class="md:h-[15vw]"
+            />
+            <div class="flex flex-col md:gap-[0.1vw]">
+                <span class="font-semibold md:text-[2vw]">No comments found!</span>
+                <span class="italic md:text-[1.25vw]">"Be the first one to comment"</span>
             </div>
         </div>
     {/if}
