d70aed43d3717217a41887a2bf6fdeb34570e38b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-16 20:22:45 +0600

    add

--- backend/django_core/apps/api/serializers/episode/comment.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class EpisodeCommentSerializer(serializers.Serializer):
-    user = serializers.CharField(read_only=True)
-    text = serializers.CharField()
-    path = serializers.CharField(required=False)
--- backend/django_core/apps/api/views/anime/episode/comment.py
+++ backend/django_core/apps/api/views/anime/episode/comment.py
@@ -5,8 +5,8 @@ from django.http import HttpRequest
 from rest_framework import generics, permissions
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
-
-from ....serializers.episode.comment import EpisodeCommentSerializer
+from django.shortcuts import get_object_or_404
+from ....serializers.comments import CommentSerializer
 
 # If we need to revisit, take a look at our ChadGPT conversation
 # https://chat.openai.com/share/06777217-e9a9-4fb4-bcfc-28e9f59be6f8
@@ -16,18 +16,17 @@ class EpisodeCommentAPIView(generics.ListAPIView):
     # this is due to drf-spectacular
     queryset = CommentModel.objects.none()
     # Searilizers
-    serializer_class = EpisodeCommentSerializer
+    serializer_class = CommentSerializer
     # Permissions
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     # Pagination
     pagination_class = LimitOffsetPagination
 
-    def get_queryset(self, *args, **kwargs):
-        queryset = (
-            AnimeModel.objects.get(pk=self.kwargs["pk"])
-            .episodes.get(episode_number=self.kwargs["episode_number"])
-            .episode_comments
-        )
+    def get_queryset(self):
+        queryset = get_object_or_404(
+            get_object_or_404(AnimeModel, pk=self.kwargs["pk"]).episodes,
+            episode_number=self.kwargs["episode_number"],
+        ).episode_comments.all()
         return queryset
 
     def post(self, request: HttpRequest, pk: int, episode_number: int) -> Response:
