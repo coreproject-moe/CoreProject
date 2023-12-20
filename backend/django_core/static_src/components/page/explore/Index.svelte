f088cd3f0bffb7adbe65a3593014a0b5be89d30a
tokitouq <tokitousan2005@gmail.com>
2023-12-20 21:39:56 +0530

    add more svelte codes

--- backend/django_core/static_src/components/page/explore/Index.svelte
+++ backend/django_core/static_src/components/page/explore/Index.svelte
@@ -1,3 +1,102 @@
+<script lang="ts">
+    import { cn } from "$functions/classname";
+
+    // Mapping
+    let filter_options_mapping: {
+        [key: string]: {
+            title: string;
+            class: string;
+            value: string;
+            items?: Record<string, string> | undefined;
+            selected_items: Array<[string, string]>;
+        };
+    } = {
+        time_range: {
+            title: "Time Range",
+            class: "hidden flex-col md:gap-[0.35vw]",
+            value: "",
+            selected_items: []
+        },
+        genres: {
+            title: "Genres",
+            class: "md:flex flex-col md:gap-[0.35vw]",
+            value: "",
+            items: {
+                action: "Action",
+                adventure: "Adventure",
+                hentai: "Hentai",
+                romance: "Romance"
+            },
+            selected_items: []
+        },
+        year: {
+            title: "Year",
+            class: "md:flex flex-col md:gap-[0.35vw]",
+            value: "",
+            items: {
+                2023: "2023",
+                2022: "2022",
+                2021: "2021",
+                2020: "2020"
+            },
+            selected_items: []
+        },
+        season: {
+            title: "Season",
+            class: "md:flex flex-col md:gap-[0.35vw]",
+            value: "",
+            items: {
+                winter: "Winter",
+                spring: "Spring",
+                summer: "Summer",
+                fall: "Fall"
+            },
+            selected_items: []
+        },
+        format: {
+            title: "Format",
+            class: "hidden md:flex flex-col md:gap-[0.35vw]",
+            value: "",
+            items: {
+                tv_show: "TV Show",
+                movie: "Movie"
+            },
+            selected_items: []
+        },
+        airing_status: {
+            title: "Airing Status",
+            class: "hidden flex-col md:gap-[0.35vw]",
+            value: "",
+            selected_items: []
+        },
+        sort_by: {
+            title: "Sort by",
+            class: "hidden flex-col md:gap-[0.35vw]",
+            value: "",
+            selected_items: []
+        }
+    };
+
+    // Functions
+    const update_selected_items = (key: string, selected_item: [string, string]) => {
+        let filter_option = filter_options_mapping[key];
+        let is_selected = filter_option.selected_items?.some((item) => item[0] === selected_item[0]);
+
+        if (is_selected) {
+            filter_option.selected_items = filter_option.selected_items?.filter((item) => item[0] !== selected_item[0]);
+        } else {
+            filter_option.selected_items = [...filter_option.selected_items, selected_item];
+        }
+
+        // update filer_options_mapping
+        filter_options_mapping[key] = filter_option;
+    },
+    clear_selected_items = (key: string) => {
+        // update filter_options_mapping
+        filter_options_mapping[key].selected_items = [];
+    };
+</script>
+
 <section class="mt-20 flex flex-col p-5 md:mt-0 md:gap-[1.5vw] md:pb-[2.5vw] md:pl-[1.5vw] md:pr-[3.75vw] md:pt-0">
     <div class="flex flex-col gap-2 md:gap-[0.5vw]">
         <span class="text-2xl font-bold leading-none md:text-[2vw]">
@@ -31,66 +130,52 @@
                     />
                 </div>
             </div>
-            <div class="md:flex flex-col md:gap-[0.35vw] group">
-                <span class="font-semibold leading-none md:text-[1vw]">Genres</span>
-                <div class="relative flex items-center">
-                    <span class="absolute cursor-pointer opacity-100 duration-300 group-focus-within:opacity-0">
-                        <span class="ml-3 text-base md:ml-[1vw] md:text-[0.9vw]">Any</span>
-                    </span>
-                    <input
-                        type="text"
-                        class="peer w-full rounded-lg border-none bg-neutral py-3 text-base leading-none placeholder focus:ring-0 md:w-[11vw] md:rounded-[0.5vw] md:bg-neutral text-neutral-content md:py-[0.8vw] md:pl-[1vw] md:text-[1vw] placeholder:font-medium font-semibold"
-                    />
-                    <button class="btn !bg-transparent border-none absolute right-0 mr-3 w-4 p-0 md:mr-[1vw] md:w-[1vw]">
-                        <coreproject-icon-chevron class="md:w-[1vw]"></coreproject-icon-chevron>
-                    </button>
+            {#each Object.entries(filter_options_mapping) as option}
+                {@const title = option[1].title}
+                {@const klass = option[1].class}
+                {@const selected_items = option[1].selected_items}
+                {@const filter_items = option[1].items}
+
+                <div class={cn(klass, "group")}>
+                    <span class="font-semibold leading-none md:text-[1vw]">{title}</span>
+                    <div class="relative flex items-center">
+                        <span class="absolute cursor-pointer opacity-100 duration-300 group-focus-within:opacity-0">
+                            {#if selected_items.length > 0}
+                                <span class="ml-3 rounded bg-primary p-1 text-sm font-semibold md:ml-[0.75vw] md:rounded-[0.25vw] md:p-[0.35vw] md:text-[0.85vw]">
+                                    <!-- show first item -->
+                                    {selected_items[0][1]}
+                                </span>
+                                <!-- show count of remaining items if exists -->
+                                {#if selected_items.length > 1}
+                                    <span class="ml-1 rounded bg-primary/50 p-1 text-sm font-semibold md:ml-[0.15vw] md:rounded-[0.25vw] md:p-[0.35vw] md:text-[0.85vw]">
+                                        +{selected_items.filter((item) => item !== selected_items[0]).length}
+                                    </span>
+                                {/if}
+                            {:else}
+                                <span class="ml-3 text-base md:ml-[1vw] md:text-[0.9vw]">Any</span>
+                            {/if}
+                        </span>
+                        <input
+                            bind:value={option[1].value}
+                            on:blur={() => (option[1].value = "")}
+                            type="text"
+                            class="peer w-full rounded-lg border-none bg-neutral py-3 text-base leading-none placeholder focus:ring-0 md:w-[11vw] md:rounded-[0.5vw] md:bg-neutral text-neutral-content md:py-[0.8vw] md:pl-[1vw] md:text-[1vw] placeholder:font-medium font-semibold"
+                        />
+                        {#if selected_items.length > 0}
+                            <button
+                                on:click={() => clear_selected_items(option[0])}
+                                class="btn !bg-transparent border-none absolute right-0 mr-3 w-4 p-0 md:mr-[1vw] md:w-[1vw]"
+                            >
+                                <coreproject-icon-cross class="md:w-[1vw]"></coreproject-icon-cross>
+                            </button>
+                        {:else}
+                            <button class="btn !bg-transparent border-none absolute right-0 mr-3 w-4 p-0 md:mr-[1vw] md:w-[1vw]">
+                                <coreproject-icon-chevron class="md:w-[1vw]"></coreproject-icon-chevron>
+                            </button>
+                        {/if}
+                    </div>
                 </div>
-            </div>
-            <div class="md:flex flex-col md:gap-[0.35vw] group">
-                <span class="font-semibold leading-none md:text-[1vw]">Year</span>
-                <div class="relative flex items-center">
-                    <span class="absolute cursor-pointer opacity-100 duration-300 group-focus-within:opacity-0">
-                        <span class="ml-3 text-base md:ml-[1vw] md:text-[0.9vw]">Any</span>
-                    </span>
-                    <input
-                        type="text"
-                        class="peer w-full rounded-lg border-none bg-neutral py-3 text-base leading-none placeholder focus:ring-0 md:w-[11vw] md:rounded-[0.5vw] md:bg-neutral text-neutral-content md:py-[0.8vw] md:pl-[1vw] md:text-[1vw] placeholder:font-medium font-semibold"
-                    />
-                    <button class="btn !bg-transparent border-none absolute right-0 mr-3 w-4 p-0 md:mr-[1vw] md:w-[1vw]">
-                        <coreproject-icon-chevron class="md:w-[1vw]"></coreproject-icon-chevron>
-                    </button>
-                </div>
-            </div>
-            <div class="md:flex flex-col md:gap-[0.35vw] group">
-                <span class="font-semibold leading-none md:text-[1vw]">Season</span>
-                <div class="relative flex items-center">
-                    <span class="absolute cursor-pointer opacity-100 duration-300 group-focus-within:opacity-0">
-                        <span class="ml-3 text-base md:ml-[1vw] md:text-[0.9vw]">Any</span>
-                    </span>
-                    <input
-                        type="text"
-                        class="peer w-full rounded-lg border-none bg-neutral py-3 text-base leading-none placeholder focus:ring-0 md:w-[11vw] md:rounded-[0.5vw] md:bg-neutral text-neutral-content md:py-[0.8vw] md:pl-[1vw] md:text-[1vw] placeholder:font-medium font-semibold"
-                    />
-                    <button class="btn !bg-transparent border-none absolute right-0 mr-3 w-4 p-0 md:mr-[1vw] md:w-[1vw]">
-                        <coreproject-icon-chevron class="md:w-[1vw]"></coreproject-icon-chevron>
-                    </button>
-                </div>
-            </div>
-            <div class="md:flex flex-col md:gap-[0.35vw] group">
-                <span class="font-semibold leading-none md:text-[1vw]">Format</span>
-                <div class="relative flex items-center">
-                    <span class="absolute cursor-pointer opacity-100 duration-300 group-focus-within:opacity-0">
-                        <span class="ml-3 text-base md:ml-[1vw] md:text-[0.9vw]">Any</span>
-                    </span>
-                    <input
-                        type="text"
-                        class="peer w-full rounded-lg border-none bg-neutral py-3 text-base leading-none placeholder focus:ring-0 md:w-[11vw] md:rounded-[0.5vw] md:bg-neutral text-neutral-content md:py-[0.8vw] md:pl-[1vw] md:text-[1vw] placeholder:font-medium font-semibold"
-                    />
-                    <button class="btn !bg-transparent border-none absolute right-0 mr-3 w-4 p-0 md:mr-[1vw] md:w-[1vw]">
-                        <coreproject-icon-chevron class="md:w-[1vw]"></coreproject-icon-chevron>
-                    </button>
-                </div>
-            </div>
+            {/each}
         </div>
 
         <button class="btn btn-neutral h-max min-h-max rounded-lg p-[0.85rem] md:rounded-[0.5vw] md:p-[0.9vw]">
