abc984740fdd7fe2d24d669d00d00943a58879db
tokitouq <tokitousan2005@gmail.com>
2023-12-20 17:49:18 +0530

    fix

---
+++ backend/django_core/static_src/components/page/index.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import svelteRetag from "svelte-retag";
+
+// Pages
+const mapping = [
+    { tagname: `explore`, component: await import("./explore/Index.svelte") },
+]
+
+mapping.forEach((item) => {
+    svelteRetag({
+        component: item.component.default,
+        tagname: `coreproject-page-${item.tagname}`,
+        attributes: true,
+        shadow: false,
+        hydratable: true
+    });
+});
