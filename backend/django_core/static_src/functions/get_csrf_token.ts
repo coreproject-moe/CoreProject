d9a3ae45038b30fa47f9cfba95f9b53f6dbf04c2
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-22 14:30:41 +0600

    Update get_csrf_token.ts

--- backend/django_core/static_src/functions/get_csrf_token.ts
+++ backend/django_core/static_src/functions/get_csrf_token.ts
@@ -1,11 +1,11 @@
 import * as _ from "lodash-es";
+import { string_to_boolean } from "./string_to_bool";
 
 export function get_csrf_token() {
     // Set by django
     let token: string | null = null;
     token = window.csrfmiddlewaretoken;
 
-    
     if (_.isNull(token)) {
         throw new Error("There is no CSRF Token set by `django`");
     }
