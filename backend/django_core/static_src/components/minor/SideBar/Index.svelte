d8dba7146049b22d8d9b14e12f87a665cfc12e85
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-22 22:53:02 +0600

    add

--- backend/django_core/static_src/components/minor/SideBar/Index.svelte
+++ backend/django_core/static_src/components/minor/SideBar/Index.svelte
@@ -11,17 +11,7 @@
     import List from "$icons/List/Index.svelte";
     import Calender from "$icons/Calender/Index.svelte";
     import Chat from "$icons/Chat/Index.svelte";
-
-    let hover_glider_element: HTMLElement | null = null,
-        glider_container_element: HTMLElement | null = null;
-
-    export let glider_container_class: string | null = null,
-        active_element_class: string | null = null;
-    export let direction: "horizontal" | "vertical" = "horizontal",
-        GLIDER_TRANSITION_DURATION = 200;
-
-    let mouse_leave_timeout: NodeJS.Timeout,
-        is_hovered_from_prev_el = false;
+    import VercelHover from "$components/svelte/VercelHover.svelte";
 
     const mappings = [
         {
@@ -66,62 +56,16 @@
             label: "forum"
         }
     ];
-
-    const handle_mouseenter = (event: Event) => {
-            const target = event.target as HTMLElement;
-            const target_computed_style = getComputedStyle(target);
-            if (_.isNull(glider_container_element) || _.isNull(hover_glider_element)) return;
-
-            glider_container_element.style.position = "relative";
-
-            hover_glider_element.style.height = target_computed_style.height;
-            hover_glider_element.style.width = target_computed_style.width;
-
-            const target_zindex = parseInt(target_computed_style.zIndex);
-            glider_container_element.style.zIndex = String(target_zindex ?? 0);
-            hover_glider_element.style.zIndex = String(target_zindex - 1 ?? -1);
-
-            switch (direction) {
-                case "vertical":
-                    hover_glider_element.style.transform = `translateY(${target.offsetTop}px)`;
-                    break;
-                case "horizontal":
-                    hover_glider_element.style.transform = `translateX(${target.offsetLeft}px)`;
-                    break;
-                default:
-                    throw Error("Method Not Implemented");
-            }
-
-            if (is_hovered_from_prev_el) {
-                GLIDER_TRANSITION_DURATION = 200;
-                hover_glider_element.style.transitionDuration = `${GLIDER_TRANSITION_DURATION}ms`;
-            } else {
-                GLIDER_TRANSITION_DURATION = 100;
-                hover_glider_element.style.transitionDuration = `${GLIDER_TRANSITION_DURATION}ms`;
-                setTimeout(() => {
-                    hover_glider_element!.style.opacity = "100";
-                }, GLIDER_TRANSITION_DURATION);
-                is_hovered_from_prev_el = true;
-            }
-
-            clearTimeout(mouse_leave_timeout);
-        },
-        handle_mouseleave = () => {
-            mouse_leave_timeout = setTimeout(() => {
-                hover_glider_element!.style.opacity = "0";
-                is_hovered_from_prev_el = false;
-            }, GLIDER_TRANSITION_DURATION);
-        };
 </script>
 
-<glider-container
-    bind:this={glider_container_element}
-    class={glider_container_class}
+<VercelHover
+    glider_container_class="mt-[2.8125vw] flex flex-col items-center gap-[0.75vw]"
+    active_element_class="rounded-[0.75vw] bg-white/10"
+    direction="vertical"
+    GLIDER_TRANSITION_DURATION={200}
+    let:handle_mouseenter
+    let:handle_mouseleave
 >
-    <active-glider
-        bind:this={hover_glider_element}
-        class={cn(active_element_class, "absolute opacity-0 duration-200 ease-in-out")}
-    />
     {#each mappings as item}
         {@const is_active = item.href === $url}
         <button
@@ -167,4 +111,4 @@
             </div>
         </button>
     {/each}
-</glider-container>
+</VercelHover>
