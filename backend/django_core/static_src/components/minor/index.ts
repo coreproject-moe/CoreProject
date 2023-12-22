67a871b5493391e978f0d9662c06639730f1181b
Tokito <tokitousan2005@gmail.com>
2023-12-22 22:45:09 +0530

    [`Frontend`]: fix patches (#3213)
    * fix: patches
    
    * fix: replace with expand hover
    
    * add
    
    ---------
    
    Signed-off-by: Tokito <tokitousan2005@gmail.com>

--- backend/django_core/static_src/components/minor/index.ts
+++ backend/django_core/static_src/components/minor/index.ts
@@ -4,7 +4,8 @@ const mappings = [
     { tagname: "markdown", component: await import("./Markdown/Index.svelte") },
     { tagname: "scroll-area", component: await import("./ScrollArea/Index.svelte") },
     { tagname: "comment", component: await import("./Comment/Index.svelte") },
-    { tagname: "sidebar", component: await import("./SideBar/Index.svelte") }
+    { tagname: "hover-expand", component: await import("./HoverExpand/Index.svelte") },
+    { tagname: "sidebar", component: await import("./SideBar/Index.svelte") },
 ];
 
 mappings.forEach((item) => {
