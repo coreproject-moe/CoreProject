7660335d769a74c4f79e9ca29b94d6f6d8b8a9b5
Tokito <tokitousan2005@gmail.com>
2023-12-27 13:58:35 +0530

    [`Frontend`]: Feat/rating component (#3227)
    * fix: image alt
    
    * fix: add basic
    
    * fxi: rendering
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ backend/django_core/static_src/components/minor/Rating/Index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { cn } from "$functions/classname";
+
+    const klass = "md:w-[0.75vw] md:h-[1.5vw]";
+    export {klass as class};
+
+    let RATING_COUNT = 5;
+</script>
+
+<div class="rating rating-half">
+    {#each Array(RATING_COUNT) as _}
+        {@const common_classes = "bg-accent checked:!bg-accent border-none mask mask-star-2"}
+
+        <input type="radio" name="rating-10" class={cn(klass, common_classes, "mask-half-1")} />
+        <input type="radio" name="rating-10" class={cn(klass, common_classes, "mask-half-2")} />
+    {/each}
+</div>
