b8e22a0364082ecebf410b09fd268d4d0bbcd0d2
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-31 16:12:39 +0600

    Update browser.ts

--- backend/django_core/static_src/constants/browser.ts
+++ backend/django_core/static_src/constants/browser.ts
@@ -1,13 +1,15 @@
 import { detect } from "detect-browser";
+import * as _ from "lodash-es";
 
 const browser = detect();
 
-const IS_CHROME: boolean = ["chrome", "chromium-webview", "android", "yandexbrowser"].includes(browser?.name!);
-const IS_SAFARI: boolean = browser?.name === "safari";
-const IS_EDGE: boolean = browser?.name === "edge-chromium";
-const IS_OPERA: boolean = browser?.name === "opera";
-const IS_INTERNET_EXPLORER: boolean = browser?.name === "ie" || browser?.name === "edge";
+const IS_CHROME: boolean = _.includes(["chrome", "chromium-webview", "android", "yandexbrowser"], browser?.name.toLowerCase());
+const IS_SAFARI: boolean = _.includes(["safari"], browser?.name.toLowerCase());
+const IS_EDGE: boolean = _.includes(["edge-chromium"], browser?.name.toLowerCase());
+const IS_OPERA: boolean = _.includes(["opera"], browser?.name.toLowerCase());
 
-export const IS_FIREFOX = browser?.name === "firefox";
+const IS_INTERNET_EXPLORER: boolean = _.includes(["ie", "edge"], browser?.name.toLowerCase());
+
+export const IS_FIREFOX = _.includes(['"firefox"'], browser?.name.toLowerCase());
 
 export const IS_CHROMIUM = (IS_CHROME || IS_EDGE || IS_OPERA) && !(IS_FIREFOX && IS_INTERNET_EXPLORER && IS_SAFARI);
