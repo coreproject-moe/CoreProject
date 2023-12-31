e9ae02ebce89879ee32938119f922c1fd881fda4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-12-31 16:00:31 +0600

    feat: add browser-detect (#3265)
    * add
    
    * Update browser.ts
    
    * add

--- backend/django_core/static_src/constants/browser.ts
+++ backend/django_core/static_src/constants/browser.ts
@@ -1,8 +1,13 @@
-const IS_CHROME = navigator.userAgent.includes("Chrome");
-const IS_SAFARI = navigator.userAgent.includes("Safari");
-const IS_EDGE = navigator.userAgent.includes("Edge");
-const IS_OPERA = navigator.userAgent.includes("Opera") || navigator.userAgent.includes("OPR");
-const IS_INTERNET_EXPLORER = navigator.userAgent.includes("Trident") || navigator.userAgent.includes("MSIE");
-export const IS_FIREFOX = navigator.userAgent.includes("Firefox");
+import { detect } from "detect-browser";
+
+const browser = detect();
+
+const IS_CHROME: boolean = ["chrome", "chromium-webview", "android", "yandexbrowser"].includes(browser?.name!);
+const IS_SAFARI: boolean = browser?.name === "safari";
+const IS_EDGE: boolean = browser?.name === "edge-chromium";
+const IS_OPERA: boolean = browser?.name === "opera";
+const IS_INTERNET_EXPLORER: boolean = browser?.name === "ie" || browser?.name === "edge";
+
+export const IS_FIREFOX = browser?.name === "firefox";
 
 export const IS_CHROMIUM = (IS_CHROME || IS_EDGE || IS_OPERA) && !(IS_FIREFOX && IS_INTERNET_EXPLORER && IS_SAFARI);
