43085ef1f6ae0d145a368b8592c661c78f414ec1
tokitouq <tokitousan2005@gmail.com>
2024-01-01 21:27:54 +0530

    fix: custom elements and svlete icons

--- backend/django_core/static_src/components/specific/UserForm/Register/Forms/3.svelte
+++ backend/django_core/static_src/components/specific/UserForm/Register/Forms/3.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+    import CoreText from "$icons/CoreText/Index.svelte";
+    import ArrowUpRight from "$icons/ArrowUpRight/Index.svelte";
+</script>
+
 <form
     on:submit|preventDefault
     class="flex flex-col justify-between h-full"
@@ -5,26 +10,27 @@
     <div class="flex flex-col gap-2 whitespace-nowrap font-bold uppercase leading-none tracking-widest text-white md:text-[1.2vw] md:gap-[0.5vw]">
         <span class="flex items-center text-base font-bold uppercase tracking-widest text-white md:text-[1.2vw]">
             welcome to&nbsp;
-            <coreproject-icon-core-text></coreproject-icon-core-text>project
+            <CoreText />
+            project
         </span>
         <span class="uppercase text-xs md:text-[1vw] font-medium text-white/90">Your Anime Sanctuary Awaits!</span>
     </div>
     <div class="flex flex-col gap-5 items-start md:gap-[3vw]">
-        <user-info class="flex flex-col md:gap-[1.5vw]">
+        <div class="flex flex-col md:gap-[1.5vw]">
             <span class="text-lg font-semibold text-primary-500 md:text-[1.5vw] leading-none">Account Summary</span>
-            <username class="flex flex-col md:gap-[0.5vw]">
+            <div class="flex flex-col md:gap-[0.5vw]">
                 <span class="text-lg font-medium md:text-[1.1vw] leading-none">Username:</span>
                 <span class="text-base font-medium text-accent md:text-[1.1vw] leading-none">
                     dummy username
                 </span>
-            </username>
-            <email class="flex flex-col md:gap-[0.5vw]">
+            </div>
+            <div class="flex flex-col md:gap-[0.5vw]">
                 <span class="text-lg font-medium md:text-[1.1vw] leading-none">Email:</span>
                 <span class="text-base font-medium text-accent md:text-[1.1vw] leading-none">
                     dummy email
                 </span>
-            </email>
-        </user-info>
+            </div>
+        </div>
         <div class="mt-3 flex flex-col gap-2 md:gap-[0.5vw] items-start md:mt-0">
             <button class="btn btn-secondary p-0 h-max min-h-max text-base font-semibold leading-none text-primary underline md:text-[1vw] flex flex-col items-start">
                 &lt; resend code &gt;
@@ -48,7 +54,7 @@
             class="btn btn-primary text-accent rounded-lg text-base font-semibold md:rounded-[0.5vw] md:text-[0.95vw] leading-none h-max min-h-max md:p-[1vw] p-4"
         >
             <span>Finish</span>
-            <coreproject-icon-arrow-up-right class="w-4 rotate-45 md:w-[1vw]"></coreproject-icon-arrow-up-right>
+            <ArrowUpRight class="w-4 rotate-45 md:w-[1vw]" />
         </button>
     </div>
 </form>
