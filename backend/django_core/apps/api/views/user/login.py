bb7227a4e3217f4f90da81092ae1684e63ab8e7f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-04 21:29:48 +0600

    Update login.py

--- backend/django_core/apps/api/views/user/login.py
+++ backend/django_core/apps/api/views/user/login.py
@@ -1,5 +1,16 @@
+from typing import Any
 from rest_framework.authtoken import views as rest_framework_authtoken_views
+from rest_framework.response import Response
+from django.contrib.auth import login
+from django.http import HttpRequest
 
 
 class LoginAPIView(rest_framework_authtoken_views.ObtainAuthToken):
-    pass
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data["user"]
+            if user is not None:
+                login(request, user)
+                print(1)
+        return super().post(request, *args, **kwargs)
