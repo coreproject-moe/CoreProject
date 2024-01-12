8d0f4311d436f2d5edf4983255e329169734e6d9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-12 16:34:51 +0600

    add

--- backend/django_core/static_src/stores/url.ts
+++ backend/django_core/static_src/stores/url.ts
@@ -8,8 +8,8 @@ export const url = writable(window.location.pathname);
 //     url.set(this.location.pathname);
 // });
 
-window.document.addEventListener("htmx:confirm", (event: any) => {
-    url.set(event.detail.path);
-    // https://htmx.org/events/#htmx:confirm
-    event.detail.issueRequest();
-});
+// window.document.addEventListener("htmx:confirm", (event: any) => {
+//     url.set(event.detail.path);
+//     // https://htmx.org/events/#htmx:confirm
+//     event.detail.issueRequest();
+// });
