18a5883002004fa661906c18f1d556b04a263eee
tokitouq <tokitousan2005@gmail.com>
2024-01-30 08:22:45 +0530

    feat: add landing page

--- backend/django_core/static_src/components/page/home/Index.svelte
+++ backend/django_core/static_src/components/page/home/Index.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import Sky from "../../../public/images/paralax/sky.svg";
+	import BackGround from "../../../public/images/paralax/back-ground.svg"
+	import MidGround from "../../../public/images/paralax/mid-ground.svg";
+	import Floating1 from "../../../public/images/paralax/floating-1.svg";
+	import Floating2 from "../../../public/images/paralax/floating-2.svg";
+	import { onMount } from "svelte";
 
     // @ts-ignore
     import Typewriter from "typewriter-effect/dist/core";
@@ -13,78 +18,83 @@
         });
 
         typewriter
-            .typeString('For watching <b class="text-primary">Animes.</b>')
+            .typeString('For watching <b>Animes.</b>')
             .pauseFor(2000)
             .deleteChars(16)
-            .typeString('reading <b class="text-warning">Mangas.</b>')
+            .typeString('reading <b>Mangas.</b>')
             .pauseFor(2000)
             .deleteChars(15)
-            .typeString('listening to <b class="text-accent">Musics.</b>')
+            .typeString('listening to <b>Musics.</b>')
             .pauseFor(2000)
             .deleteAll()
-            .typeString('Welcome to <b class="text-accent">CoreProject.</b>')
+            .typeString('Welcome to <b>CoreProject.</b>')
             .pauseFor(5000)
             .start();
     });
+
+	let sky_el: HTMLImageElement,
+		background_el: HTMLImageElement,
+		mid_ground_el: HTMLImageElement,
+		floating_1_el: HTMLImageElement,
+		floating_2_el: HTMLImageElement,
+		gradient_el: HTMLDivElement,
+		content_el: HTMLDivElement;
+
+	function handle_window_scroll(e: Event) {
+		let value = window.scrollY;
+		
+		sky_el.style.top = value * 0.75 + "px";
+		content_el.style.marginTop = value * 0.85 + "px";
+
+		background_el.style.bottom = value * 0.15 + "px";
+		mid_ground_el.style.bottom = value * 0 + "px";
+		floating_1_el.style.marginTop = value * 0.5 + "px";
+		floating_2_el.style.marginTop = value * 0.25 + "px";
+	};
 </script>
 
-<main class="w-screen h-screen bg-secondary pt-[5vw] flex justify-center">
-    <div class="flex flex-col text-center light-animate leading-none gap-[1.5vw]">
-        <h2 class="light-animate text-[5vw] font-bold">Imagine a new platform.</h2>
-        <p class="text-[2.75vw] text-neutral-content/75" id="typing_text"></p>
-    </div>
+<svelte:window on:scroll={handle_window_scroll} />
+
+<main class="bg-secondary">
+	<section class="h-screen relative overflow-hidden">
+		<div bind:this={gradient_el} class="bg-gradient-to-b from-[#2A1E80] to-[#EA76B3] absolute inset-0" />
+		<img bind:this={sky_el} class="sky absolute top-0 inset-x-0 w-4/5 mx-auto" src={Sky} alt="Sky" />
+		
+		<div bind:this={content_el} class="absolute inset-0 flex flex-col items-center top-[5vw] leading-none gap-[0.75vw]">
+			<h2 class="text-white text-[5vw] font-bold">Imagine a new platform.</h2>
+			<p class="text-[2.5vw] text-accent" id="typing_text"></p>
+			<button class="btn btn-neutral p-[1.1vw] text-[1.1vw] uppercase min-h-max h-max rounded-[1vw]">Get Started</button>
+		</div>
+
+		<img bind:this={floating_1_el} class="floating pointer-events-none floating-1 absolute top-1/4 left-[15vw] inset-x-0 w-[10vw]" src={Floating1} alt="Floating1" />
+		<img bind:this={background_el} class="pointer-events-none background absolute bottom-0 inset-x-0 w-full" src={BackGround} alt="Background" />
+		<img bind:this={floating_2_el} class="floating pointer-events-none floating-2 absolute top-2/4 left-2/4 inset-x-0 w-[7vw]" src={Floating2} alt="Floating2" />
+		<img bind:this={mid_ground_el} class="pointer-events-none mid-ground absolute bottom-0 inset-x-0 w-full" src={MidGround} alt="MidGround" />
+	</section>
+	<section class="h-screen"></section>
 </main>
 
-<style lang="scss">
-@keyframes lights {
-  0% {
-    color: hsl(230, 40%, 80%);
-    text-shadow:
-      0 0 1em hsla(320, 100%, 50%, 0.2),
-      0 0 0.125em hsla(320, 100%, 60%, 0.3),
-      -1em -0.125em 0.5em hsla(40, 100%, 60%, 0),
-      1em 0.125em 0.5em hsla(200, 100%, 60%, 0);
-  }
-  
-  30% { 
-    color: hsl(230, 80%, 90%);
-    text-shadow:
-      0 0 1em hsla(320, 100%, 50%, 0.5),
-      0 0 0.125em hsla(320, 100%, 60%, 0.5),
-      -0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2),
-      0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4);
-  }
-  
-  40% { 
-    color: hsl(230, 100%, 95%);
-    text-shadow:
-      0 0 1em hsla(320, 100%, 50%, 0.5),
-      0 0 0.125em hsla(320, 100%, 90%, 0.5),
-      -0.25em -0.125em 0.125em hsla(40, 100%, 60%, 0.2),
-      0.25em 0.125em 0.125em hsla(200, 100%, 60%, 0.4);
-  }
-  
-  70% {
-    color: hsl(230, 80%, 90%);
-    text-shadow:
-      0 0 1em hsla(320, 100%, 50%, 0.5),
-      0 0 0.125em hsla(320, 100%, 60%, 0.5),
-      0.5em -0.125em 0.25em hsla(40, 100%, 60%, 0.2),
-      -0.5em 0.125em 0.25em hsla(200, 100%, 60%, 0.4);
-  }
-  
-  100% {
-    color: hsl(230, 40%, 80%);
-    text-shadow:
-      0 0 1em hsla(320, 100%, 50%, 0.2),
-      0 0 0.125em hsla(320, 100%, 60%, 0.3),
-      1em -0.125em 0.5em hsla(40, 100%, 60%, 0),
-      -1em 0.125em 0.5em hsla(200, 100%, 60%, 0);
-  }
-  
+<style>
+.floating {  
+    animation-name: floating;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    margin-left: 30px;
+    margin-top: 5px;
 }
 
-.light-animate {
-  animation: lights 5s linear infinite;
+.floating-1 {
+    animation-delay: 0s;
+}
+
+.floating-2 {
+    animation-delay: 1s;
+}
+ 
+@keyframes floating {
+    0% { transform: translate(0,  0px); }
+    50%  { transform: translate(0, 15px); }
+    100%   { transform: translate(0, -0px); }    
 }
 </style>
