0afb97c8d9678b62a04b27a59b647398b9692d59
tokitouq <tokitousan2005@gmail.com>
2024-02-07 16:15:15 +0530

    feat: add more lottie files

--- backend/django_core/static_src/components/page/home/Index.svelte
+++ backend/django_core/static_src/components/page/home/Index.svelte
@@ -9,6 +9,8 @@
 
     import BgLottie from "./lotties/bg-lottie.json";
     import ScrollLottie from "./lotties/scroll.json";
+    import SquiggleLottie from "./lotties/squiggle.json";
+    import StarLottie from "./lotties/stars.json";
 </script>
 
 <div class="bg-secondary min-h-dvh w-screen overflow-x-hidden">
@@ -23,7 +25,7 @@
         />
     </div>
 
-    <navbar class="absolute inset-x-0 top-0 flex h-[4.5rem] items-center justify-between bg-secondary/95 p-3 backdrop-blur-3xl md:static md:h-[8.5vh] md:bg-secondary md:p-[1vw]">
+    <navbar class="absolute inset-x-0 top-0 flex h-[4.5rem] items-center justify-between bg-secondary/95 p-3 backdrop-blur-3xl md:h-[8.5vh] md:p-[1vw] z-[999]">
         <a href="/">
             <CoreProjectIcon class="w-9 md:w-[2.25vw] md:pt-[0.75vw]" />
         </a>
@@ -47,17 +49,41 @@
         <Navbar />
     </navbar>
 
-    <section class="w-full flex flex-col justify-center items-center relative">
+    <section class="w-full flex flex-col justify-center items-center relative md:mt-[10vh]">
         <div class="md:text-[8vw] leading-none font-bold text-center flex flex-col md:mt-[3vw]">
             <h2 class="bg-gradient-to-r from-rose-500 from-[35%] to-[65%] to-blue-500 inline-block text-transparent bg-clip-text">Imagine a new</h2>
-            <h2 class="bg-gradient-to-r from-blue-500 from-[35%] to-[65%] to-yellow-600 inline-block text-transparent bg-clip-text">Platform.</h2>
+            <h2 class="bg-gradient-to-r from-blue-500 from-[35%] to-[65%] to-yellow-600 inline-block text-transparent bg-clip-text relative">
+                Platform.
+                <div class="size-[5vw] -rotate-45 absolute right-[5vw] bottom-[2vw]">
+                    <LottiePlayer
+                        src={SquiggleLottie}
+                        autoplay="{true}"
+                        loop="{false}"
+                        mode={"bounce"}
+                        speed={0.75}
+                        renderer="svg"
+                        background="transparent"
+                    />
+                </div>
+            </h2>
         </div>
         <p class="md:text-[1.25vw] md:mt-[3vw] md:max-w-[45vw] text-center opacity-90">
             Bridging the gap between streaming and torrenting sites with a modern and clean interface.
         </p>
-        <button class="md:mt-[1vw] btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max md:p-[1.25vw] md:text-[1vw] md:rounded-[1vw] shadow-lg shadow-rose-500/25 leading-none">
-            ✨ Start Exploring Now!
-        </button>
+        <div class="relative">
+            <button class="md:mt-[1vw] btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max md:p-[1.25vw] md:text-[1vw] md:rounded-[1vw] shadow-lg shadow-rose-500/25 leading-none">
+                Start Exploring Now!
+            </button>
+            <div class="size-[4vw] absolute -right-[2.5vw] top-0">
+                <LottiePlayer
+                    src={StarLottie}
+                    autoplay="{true}"
+                    loop="{true}"
+                    renderer="svg"
+                    background="transparent"
+                />
+            </div>
+        </div>
         <div class="size-[2vw] md:mt-[3vw] opacity-75">
             <LottiePlayer
                 src={ScrollLottie}
