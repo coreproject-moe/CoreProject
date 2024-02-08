ce18ba1acb81dc2b83316b7134507ac10e063d64
tokitouq <tokitousan2005@gmail.com>
2024-02-08 17:31:38 +0530

    feat: add products section

---
+++ backend/django_core/static_src/components/icons/SoundCore/Index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { cn } from "$functions/classname";
+
+    let klass = "";
+    export { klass as class };
+</script>
+
+<div class={cn(klass, "mt-[1vw] items-center justify-center text-[1.5vw]")}>
+    <div class="select-none inline-flex gap-[0.25vw] font-bold leading-none">
+        <p class="inline-flex">
+            <span class="text-white">s</span>
+            <span class="text-white">o</span>
+            <span class="text-white">u</span>
+            <span class="text-white">n</span>
+            <span class="text-white">d</span>
+        </p>
+        <p class="inline-flex">
+            <span class="text-accent opacity-75">c</span>
+            <span class="text-warning">o</span>
+            <span class="text-accent opacity-75">r</span>
+            <span class="text-accent opacity-75">e</span>
+        </p>
+    </div>
+</div>
--- backend/django_core/static_src/components/page/home/Index.svelte
+++ backend/django_core/static_src/components/page/home/Index.svelte
@@ -6,6 +6,9 @@
     import CoreProjectIcon from "$icons/CoreProject/Index.svelte";
     import CoreProjectText from "$icons/CoreProjectText/Index.svelte";
     import Search from "$icons/Search/Index.svelte";
+    import AnimeCore from "$icons/AnimeCore/index.svelte";
+    import MangaCore from "$icons/MangaCore/Index.svelte";
+    import SoundCore from "$icons/SoundCore/Index.svelte";
 
     import BgLottie from "./lotties/bg-lottie.json";
     import ScrollLottie from "./lotties/scroll.json";
@@ -13,8 +16,8 @@
     import StarLottie from "./lotties/stars.json";
 </script>
 
-<div class="bg-secondary min-h-dvh w-screen overflow-x-hidden">
-    <div class="w-full h-[100vh] overflow-hidden absolute">
+<div class="bg-secondary overflow-x-hidden relative">
+    <div class="inset-0 overflow-hidden absolute">
         <LottiePlayer
             src={BgLottie}
             autoplay="{true}"
@@ -25,7 +28,7 @@
         />
     </div>
 
-    <navbar class="absolute inset-x-0 top-0 flex h-[4.5rem] items-center justify-between bg-secondary/95 p-3 backdrop-blur-3xl md:h-[8.5vh] md:p-[1vw] z-[999]">
+    <navbar class="fixed inset-x-0 top-0 flex h-[4.5rem] items-center justify-between bg-secondary/95 p-3 backdrop-blur-3xl md:h-[8.5vh] md:p-[1vw] z-[999]">
         <a href="/">
             <CoreProjectIcon class="w-9 md:w-[2.25vw] md:pt-[0.75vw]" />
         </a>
@@ -92,6 +95,50 @@
                 renderer="svg"
                 background="transparent"
             />
+        </div>
+    </section>
+
+    <section class="relative md:mt-[10vw] flex flex-col items-center justify-center bg-secondary/75s md:py-[3vw]">
+        <h2 class="md:text-[2.5vw] font-bold bg-gradient-to-r from-rose-500 from-[35%] to-[65%] to-blue-500 inline-block text-transparent bg-clip-text">Explore Our Trio.</h2>
+        <div class="flex items-end justify-center md:mt-[1vw] md:gap-[1.5vw]">
+            <div class="relative flex items-center justify-center flex-col bg-secondary md:rounded-[1vw] md:p-[1vw] md:w-[20vw] text-center md:gap-[1vw]">
+                <MangaCore class="!text-[2.5vw] !m-0" />
+                <p class="md:text-[1.1vw] md:leading-[1.5vw] md:line-clamp-4">
+                    Dive into the captivating world of manga with our streaming service. Access a diverse collection of manga titles, from timeless classics to the latest releases, and lose yourself in the art of storytelling.
+                </p>
+                <button class="relative w-full btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max md:p-[1.25vw] md:text-[1.25vw] md:rounded-[1vw] leading-none">
+                    Coming soon
+                </button>
+                <div class="absolute inset-0 bg-secondary/75" />
+            </div>
+            <div class="flex items-center justify-center flex-col bg-secondary border-[0.15vw] md:rounded-[1vw] border-error md:p-[1vw] border-dashed md:w-[25vw] text-center md:gap-[1vw] relative">
+                <AnimeCore class="md:w-[15vw]" />
+                <p class="md:text-[1.1vw] md:leading-[1.5vw]">
+                    Immerse yourself in a world of endless anime streaming. Explore a vast library of your favorite shows and discover new ones, all at your fingertips.
+                </p>
+                <button class="relative w-full btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max md:p-[1.25vw] md:text-[1.25vw] md:rounded-[1vw] shadow-lg shadow-rose-500/25 leading-none">
+                    Explore
+                </button>
+                <div class="size-[4vw] absolute -right-[1.5vw] -top-[1.75vw]">
+                    <LottiePlayer
+                        src={StarLottie}
+                        autoplay="{true}"
+                        loop="{false}"
+                        renderer="svg"
+                        background="transparent"
+                    />
+                </div>
+            </div>
+            <div class="relative flex items-center justify-center flex-col bg-secondary md:rounded-[1vw] md:p-[1vw] md:w-[20vw] text-center md:gap-[1vw]">
+                <SoundCore class="!text-[2.5vw] !m-0" />
+                <p class="md:text-[1.1vw] md:leading-[1.5vw] md:line-clamp-4">
+                    Elevate your auditory experience with our music streaming platform. Explore a rich tapestry of sounds, from chart-toppers to hidden gems, and let the music transport you to new dimensions of sonic bliss.
+                </p>
+                <button class="relative w-full btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max md:p-[1.25vw] md:text-[1.25vw] md:rounded-[1vw] leading-none">
+                    Coming soon
+                </button>
+                <div class="absolute inset-0 bg-secondary/75" />
+            </div>
         </div>
     </section>
 </div>
