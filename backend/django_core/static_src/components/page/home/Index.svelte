25eff5a2f7edf72ac3b547cb435e28f94840c292
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-02-09 17:29:41 +0600

    Import optimization

--- backend/django_core/static_src/components/page/home/Index.svelte
+++ backend/django_core/static_src/components/page/home/Index.svelte
@@ -1,121 +1,141 @@
 <script lang="ts">
     //@ts-ignore
     import { LottiePlayer } from "@lottiefiles/svelte-lottie-player";
-    
+
     import AnimeCore from "$icons/AnimeCore/index.svelte";
     import MangaCore from "$icons/MangaCore/Index.svelte";
     import SoundCore from "$icons/SoundCore/Index.svelte";
 
-    import BgLottie from "./lotties/bg-lottie.json";
-    import ScrollLottie from "./lotties/scroll.json";
-    import SquiggleLottie from "./lotties/squiggle.json";
-    import StarLottie from "./lotties/stars.json";
-
     const products_mapping = [
         {
             icon: MangaCore,
+            div_class: "order-2 md:order-1",
             icon_class: "!text-3xl md:!text-[2.5vw] !m-0",
-            description: "Dive into the captivating world of manga with our streaming service. Access a diverse collection of manga titles, from timeless classics to the latest releases, and lose yourself in the art of storytelling.",
+            description:
+                "Dive into the captivating world of manga with our streaming service. Access a diverse collection of manga titles, from timeless classics to the latest releases, and lose yourself in the art of storytelling.",
             href: "/manga",
-            available: false,
+            available: false
         },
         {
             icon: AnimeCore,
+            div_class: "order-1 md:order-2",
             icon_class: "w-48 md:w-[15vw]",
             description: "Immerse yourself in a world of endless anime streaming. Explore a vast library of your favorite shows and discover new ones, all at your fingertips.",
             href: "/anime",
-            available: true,
+            available: true
         },
         {
             icon: SoundCore,
+            div_class: "order-3 md:order-3",
             icon_class: "!text-3xl md:!text-[2.5vw] !m-0",
-            description: "Elevate your auditory experience with our music streaming platform. Explore a rich tapestry of sounds, from chart-toppers to hidden gems, and let the music transport you to new dimensions of sonic bliss.",
+            description:
+                "Elevate your auditory experience with our music streaming platform. Explore a rich tapestry of sounds, from chart-toppers to hidden gems, and let the music transport you to new dimensions of sonic bliss.",
             href: "/sound",
-            available: false,
-        },
+            available: false
+        }
     ];
 </script>
 
-<div class="bg-secondary overflow-x-hidden relative min-h-dvh">
-    <div class="inset-0 overflow-hidden absolute">
-        <LottiePlayer
-            src={BgLottie}
-            autoplay="{true}"
-            loop="{false}"
-            renderer="svg"
-            background="transparent"
-            style={{ height: "100%", width: "100%", "object-fit": "contain" }}
-        />
+<div class="relative min-h-dvh overflow-x-hidden bg-secondary">
+    <div class="absolute inset-0 overflow-hidden">
+        {#await import("./lotties/bg-lottie.json") then item}
+            <LottiePlayer
+                src={item.default}
+                autoplay={true}
+                loop={false}
+                renderer="svg"
+                background="transparent"
+                style={{ height: "100%", width: "100%", "object-fit": "contain" }}
+            />
+        {/await}
     </div>
 
-    <section class="w-full flex flex-col justify-center items-center relative mt-20 md:mt-[10vh]">
-        <div class="text-5xl md:text-[8vw] leading-none font-bold text-center flex flex-col md:mt-[3vw]">
-            <h2 class="bg-gradient-to-r from-rose-500 from-[35%] to-[65%] to-blue-500 inline-block text-transparent bg-clip-text">Imagine a new</h2>
-            <h2 class="bg-gradient-to-r from-blue-500 from-[35%] to-[65%] to-yellow-600 inline-block text-transparent bg-clip-text relative">
+    <section class="relative mt-20 flex w-full flex-col items-center justify-center md:mt-[10vh]">
+        <div class="flex flex-col text-center text-5xl font-bold leading-none md:mt-[3vw] md:text-[8vw]">
+            <h2 class="inline-block bg-gradient-to-r from-rose-500 from-[35%] to-blue-500 to-[65%] bg-clip-text text-transparent">Imagine a new</h2>
+            <h2 class="relative inline-block bg-gradient-to-r from-blue-500 from-[35%] to-yellow-600 to-[65%] bg-clip-text text-transparent">
                 Platform.
-                <div class="size-12 md:size-[5vw] -rotate-45 absolute right-3 md:right-[5vw] md:bottom-[2vw] bottom-2">
-                    <LottiePlayer
-                        src={SquiggleLottie}
-                        autoplay="{true}"
-                        loop="{false}"
-                        mode={"bounce"}
-                        speed={0.75}
-                        renderer="svg"
-                        background="transparent"
-                    />
+                <div class="absolute bottom-2 right-3 size-12 -rotate-45 md:bottom-[2vw] md:right-[5vw] md:size-[5vw]">
+                    {#await import("./lotties/squiggle.json") then item}
+                        <LottiePlayer
+                            src={item.default}
+                            autoplay={true}
+                            loop={false}
+                            mode={"bounce"}
+                            speed={0.75}
+                            renderer="svg"
+                            background="transparent"
+                        />
+                    {/await}
                 </div>
             </h2>
         </div>
-        <p class="text-base max-w-72 md:text-[1.25vw] mt-2 md:mt-[3vw] md:max-w-[45vw] text-center opacity-90">
+        <p class="mt-2 max-w-72 text-center text-base opacity-90 md:mt-[3vw] md:max-w-[45vw] md:text-[1.25vw]">
             Bridging the gap between streaming and torrenting sites with a modern and clean interface.
         </p>
         <div class="relative flex items-center">
-            <a href="#explore-our-trio" class="md:mt-[1vw] mt-4 btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max p-4 text-base rounded-xl md:p-[1.25vw] md:text-[1vw] md:rounded-[1vw] shadow-lg shadow-rose-500/25 leading-none">
+            <a
+                href="#explore-our-trio"
+                class="btn mt-4 h-max min-h-max rounded-xl border-none bg-gradient-to-r from-rose-500 to-blue-500 p-4 text-base leading-none text-accent shadow-lg shadow-rose-500/25 md:mt-[1vw] md:rounded-[1vw] md:p-[1.25vw] md:text-[1vw]"
+            >
                 Start Exploring Now!
             </a>
-            <div class="size-14 md:size-[4vw] absolute -right-14 md:-right-[4vw]">
+            <div class="absolute -right-14 size-14 md:-right-[4vw] md:size-[4vw]">
+                {#await import("./lotties/stars.json") then item}
+                    <LottiePlayer
+                        src={item.default}
+                        autoplay={true}
+                        loop={false}
+                        renderer="svg"
+                        background="transparent"
+                    />
+                {/await}
+            </div>
+        </div>
+        <div class="mt-5 size-6 opacity-75 md:mt-[3vw] md:size-[2vw]">
+            {#await import("./lotties/scroll.json") then item}
                 <LottiePlayer
-                    src={StarLottie}
-                    autoplay="{true}"
-                    loop="{false}"
+                    src={item.default}
+                    autoplay={true}
+                    loop={true}
                     renderer="svg"
                     background="transparent"
                 />
-            </div>
-        </div>
-        <div class="md:size-[2vw] size-6 mt-5 md:mt-[3vw] opacity-75">
-            <LottiePlayer
-                src={ScrollLottie}
-                autoplay="{true}"
-                loop="{true}"
-                renderer="svg"
-                background="transparent"
-            />
+            {/await}
         </div>
     </section>
 
     <section
         id="explore-our-trio"
-        class="relative mt-16 md:mt-[10vw] flex flex-col items-center justify-center md:py-[3vw]"
+        class="relative mt-16 flex flex-col items-center justify-center md:mt-[10vw] md:py-[3vw]"
     >
-        <h2 class="md:text-[2.5vw] text-2xl font-bold bg-gradient-to-r from-rose-500 from-[35%] to-[65%] to-blue-500 text-transparent bg-clip-text leading-none">Explore Our Trio.</h2>
-        <div class="flex flex-col md:flex-row items-end justify-center mt-2 p-5 md:mt-[1vw] md:gap-[1.5vw]">
+        <h2 class="bg-gradient-to-r from-rose-500 from-[35%] to-blue-500 to-[65%] bg-clip-text text-2xl font-bold leading-none text-transparent md:text-[2.5vw]">Explore Our Trio.</h2>
+        <div class="mt-2 flex flex-col items-end justify-center p-5 md:mt-[1vw] md:flex-row md:gap-[1.5vw]">
             {#each products_mapping as item}
                 <div
-                    class="relative flex items-center justify-center flex-col bg-secondary rounded-xl md:rounded-[1vw] md:p-[1vw] p-5 md:w-[20vw] gap-2 text-center md:gap-[1vw] border-error md:border-[0.15vw] border-2 border-dashed"
+                    class="relative flex flex-col items-center justify-center gap-2 rounded-xl border-2 border-dashed border-error bg-secondary p-5 text-center md:w-[20vw] md:gap-[1vw] md:rounded-[1vw] md:border-[0.15vw] md:p-[1vw]"
                     class:md:!w-[25vw]={item.available}
                     class:!border-none={!item.available}
                 >
-                    <svelte:component this={item.icon} class={item.icon_class} />
-                    <p class="text-sm line-clamp-3 md:text-[1.1vw] md:leading-[1.5vw] md:line-clamp-4">
+                    <svelte:component
+                        this={item.icon}
+                        class={item.icon_class}
+                    />
+                    <p class="line-clamp-3 text-sm md:line-clamp-4 md:text-[1.1vw] md:leading-[1.5vw]">
                         {item.description}
                     </p>
                     {#if item.available}
-                        <a href={item.href} class="relative w-full btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max md:p-[1.25vw] md:text-[1.25vw] md:rounded-[1vw] shadow-lg shadow-rose-500/25 leading-none p-4 text-lg rounded-xl">
+                        <a
+                            href={item.href}
+                            class="btn relative h-max min-h-max w-full rounded-xl border-none bg-gradient-to-r from-rose-500 to-blue-500 p-4 text-lg leading-none text-accent shadow-lg shadow-rose-500/25 md:rounded-[1vw] md:p-[1.25vw] md:text-[1.25vw]"
+                        >
                             Explore
                         </a>
                     {:else}
-                        <a href="/" class="relative w-full btn border-none bg-gradient-to-r from-rose-500 to-blue-500 text-accent h-max min-h-max md:p-[1.25vw] md:text-[1.25vw] md:rounded-[1vw] leading-none p-3 text-lg rounded-xl">
+                        <a
+                            href="/"
+                            class="btn relative h-max min-h-max w-full rounded-xl border-none bg-gradient-to-r from-rose-500 to-blue-500 p-3 text-lg leading-none text-accent md:rounded-[1vw] md:p-[1.25vw] md:text-[1.25vw]"
+                        >
                             Coming soon
                         </a>
                         <div class="absolute inset-0 bg-secondary/75" />
