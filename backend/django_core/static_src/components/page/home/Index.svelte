8f26285bdc23e4247538a1fa2a38541d57e31ea5
tokitouq <tokitousan2005@gmail.com>
2024-02-09 14:44:45 +0530

    remove: navbar as per request

--- backend/django_core/static_src/components/page/home/Index.svelte
+++ backend/django_core/static_src/components/page/home/Index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     //@ts-ignore
     import { LottiePlayer } from "@lottiefiles/svelte-lottie-player";
-    import Navbar from "$components/specific/Navbar/Index.svelte";
     
     import CoreProjectIcon from "$icons/CoreProject/Index.svelte";
     import CoreProjectText from "$icons/CoreProjectText/Index.svelte";
@@ -27,30 +26,6 @@
             style={{ height: "100%", width: "100%", "object-fit": "contain" }}
         />
     </div>
-
-    <navbar class="fixed inset-x-0 top-0 flex h-[4.5rem] items-center justify-between bg-secondary/95 p-3 backdrop-blur-3xl md:h-[8.5vh] md:p-[1vw] z-[999]">
-        <a href="/">
-            <CoreProjectIcon class="w-9 md:w-[2.25vw] md:pt-[0.75vw]" />
-        </a>
-        <div class="relative flex items-center md:static">
-            <div class="absolute left-1/2 -translate-x-1/2">
-                <a href="/anime" class="hidden md:flex">
-                    <CoreProjectText class="!text-[1.75vw] !m-0" />
-                </a>
-                <form class="relative flex h-12 w-[65vw] items-center md:hidden">
-                    <button class="btn absolute left-4 p-0 !bg-transparent border-none" aria-label="Search">
-                        <Search class="w-5 text-neutral-content/75" />
-                    </button>
-                    <input
-                        type="text"
-                        placeholder="Search for animes, mangas..."
-                        class="h-full w-full rounded-lg border-none bg-neutral/75 pl-12 text-base font-semibold text-accent shadow-lg !ring-0 placeholder:font-medium placeholder:text-surface-200"
-                    />
-                </form>
-            </div>
-        </div>
-        <Navbar />
-    </navbar>
 
     <section class="w-full flex flex-col justify-center items-center relative md:mt-[10vh]">
         <div class="md:text-[8vw] leading-none font-bold text-center flex flex-col md:mt-[3vw]">
