cf2e09f4324482a930c32062c2a294a79b7c58f4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-02-14 10:51:52 +0600

    Remove files around

--- backend/django_core/static_src/types/window.d.ts
+++ backend/django_core/static_src/types/window.d.ts
@@ -1,9 +1,7 @@
-import { Timer as EasyTimer } from "easytimer.js";
 import _hyperscript from "hyperscript.org";
 
 declare global {
     interface Window {
-        timer: EasyTimer;
         _hyperscript: typeof _hyperscript;
         csrfmiddlewaretoken: string;
         urls: Map<string, string>;
