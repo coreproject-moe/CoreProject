32788d2d1bed8e9b9ad7d599dda9c3b1047356ce
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-02-22 21:19:25 +0600

    Remove `django-ltree`
    add

--- backend/django_core/apps/comments/models.py
+++ backend/django_core/apps/comments/models.py
@@ -3,47 +3,12 @@ from django.contrib.postgres import indexes as idx
 from django.db import models
 from django_ltree.models import TreeModel
 from mixins.models.created_at import CreatedAtMixin
-from django.core.validators import RegexValidator
-from django_ltree.fields import PathField
-from django import forms
-from django.forms.widgets import TextInput
-
-path_label_validator = RegexValidator(
-    r"(?P<root>[a-zA-Z][a-zA-Z0-9_]*|\d+)(?:\.[a-zA-Z0-9_]+)*$",
-    "A label is a sequence of alphanumeric characters and underscores separated by dots.",
-    "invalid",
-)
-
-# Monkeypatch things
-
-
-class FixedPathFormField(forms.CharField):
-    default_validators = [path_label_validator]
-
-
-class FixedPathField(PathField):
-    default_validators = [path_label_validator]
-
-    def formfield(self, **kwargs):
-        kwargs["form_class"] = FixedPathFormField
-        kwargs["widget"] = TextInput(attrs={"class": "vTextField"})
-        return super(PathField, self).formfield(**kwargs)
-
-    class Meta:
-        abstract = True
-
-
-class FixedTreeModel(TreeModel):
-    path = FixedPathField()
-
-    class Meta:
-        abstract = True
 
 
 # Create your models here.
 
 
-class CommentModel(CreatedAtMixin, FixedTreeModel):
+class CommentModel(CreatedAtMixin, TreeModel):
     label_size = 4
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     text = models.TextField()
