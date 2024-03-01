f6b1d70fd0dd2da0787ec899b2921506db47c8cd
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-03-01 08:03:15 +0600

    Update Index.svelte

--- backend/django_core/static_src/components/specific/UserForm/ResetPassword/Index.svelte
+++ backend/django_core/static_src/components/specific/UserForm/ResetPassword/Index.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import { goto, reverse } from "$functions/urls";
+</script>
+
 <form class="bg-surface-900 flex h-max w-full flex-col gap-[15vw] p-10 md:h-full md:justify-between md:gap-0 md:rounded-none md:p-0">
     <content class="contents">
         <div class="flex flex-col gap-2 whitespace-nowrap font-bold uppercase leading-none tracking-widest text-white md:gap-[0.5vw] md:text-[1.2vw]">
@@ -6,30 +10,34 @@
         </div>
         <div class="flex flex-col gap-[0.3rem] md:gap-[0.5vw]">
             <label class="text-base font-semibold leading-none md:text-[1.1vw]">Email</label>
-            <!-- {{ form.email }} -->
+            <input
+                placeholder="sora_amamiya@coreproject.moe"
+                class="border-primary-500 focus:border-primary-400 h-12 w-full rounded-xl border-2 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-all placeholder:text-white/50 md:h-[3.125vw] md:rounded-[0.75vw] md:border-[0.2vw] md:px-[1vw] md:text-[1.1vw]"
+            />
             <div class="flex items-center gap-2 md:gap-[0.5vw]">
                 <coreproject-icon-info class="w-3 opacity-70 md:w-[0.9vw]"></coreproject-icon-info>
 
                 <span class="text-surface-300 text-[0.7rem] leading-none md:text-[0.8vw]">
                     <!-- {% if form.email.errors %}
-                    <coreproject-markdown markdown="{{form.email.errors|first}}"></coreproject-markdown>
-                {% else %}
-                    {{ form.email.help_text }}
-                {% endif %} -->
+                        <coreproject-markdown markdown="{{form.email.errors|first}}"></coreproject-markdown>
+                    {% else %}
+                        {{ form.email.help_text }}
+                    {% endif %} -->
+                    we’ll send you a reset password link on your registered email address
                 </span>
             </div>
         </div>
         <div class="flex items-center justify-between">
             <div class="flex flex-col gap-1 md:gap-[0.5vw]">
                 <span class="text-surface-100 text-xs leading-none md:text-[0.75vw]">Don't have a core account?</span>
-                <!-- <button
-                hx-get="{% url 'register_view' %}"
-                hx-push-url="{% url 'register_view' %}"
-                hx-target="<content/>"
-                class="text-start text-base leading-none md:text-[1.1vw] text-primary underline"
-            >
-                Register
-            </button> -->
+                <button
+                    on:click|preventDefault={() => {
+                        goto({ url: reverse("register_view"), verb: "GET", target: "body" });
+                    }}
+                    class="text-start text-base leading-none text-primary underline md:text-[1.1vw]"
+                >
+                    Register
+                </button>
             </div>
             <button
                 type="submit"
