06a864bdcb32d0fe3fb4d08254c42201b2fe9b06
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-03-01 08:03:29 +0600

    Remove hyperscript (#3492)
    * add
    
    * add
    
    * Update Index.svelte

--- backend/django_core/static_src/main.ts
+++ backend/django_core/static_src/main.ts
@@ -1,14 +1,6 @@
 // import css
 import "./css/index.postcss";
 
-// Configure hyperscript
-
-import _hyperscript from "hyperscript.org";
-
-_hyperscript.browserInit();
-_hyperscript.processNode(document.body);
-window._hyperscript = _hyperscript;
-
 // Import htmx
 import "htmx.org";
 
