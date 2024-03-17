ed98db32a91e6aa0acabc6878681a85bca634ba9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-03-17 17:10:36 +0600

    Get anime mutation working (#3545)
    add

--- backend/django_core/apps/gql/mutations/anime.py
+++ backend/django_core/apps/gql/mutations/anime.py
@@ -1,9 +1,34 @@
 import strawberry_django
 from strawberry import auto
 from apps.anime.models import AnimeModel
-import datetime
 
 
-@strawberry_django.input(AnimeModel, fields=["name"], partial=True)
+@strawberry_django.input(AnimeModel)
 class AnimeInput:
+    mal_id: auto
+    anilist_id: auto
+    kitsu_id: auto
+
     name: auto
+    name_japanese: auto
+    name_synonyms: list[str]
+
+    source: auto
+    aired_from: auto
+    aired_to: auto
+
+    synopsis: auto
+    background: auto
+    rating: auto
+
+    # Images
+    banner: auto
+    cover: auto
+
+    # We need pk for these
+    staffs: list[int] = None
+    genres: list[int] = None
+    themes: list[int] = None
+    studios: list[int] = None
+    producers: list[int] = None
+    characters: list[int] = None
