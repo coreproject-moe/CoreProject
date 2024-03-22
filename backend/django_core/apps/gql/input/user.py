b4de5eeb7bbb579e670517ff1fc14c60f9ed70c8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-03-23 02:37:29 +0600

    add

--- backend/django_core/apps/gql/input/user.py
+++
@@ -1,9 +0,0 @@
-from strawberry import auto
-import strawberry_django
-from apps.user.models import CustomUser
-
-
-@strawberry_django.input(CustomUser)
-class UserLoginInput:
-    username: auto
-    password: auto
--- backend/django_core/apps/gql/mutations/user.py
+++ backend/django_core/apps/gql/mutations/user.py
@@ -2,12 +2,14 @@ import strawberry
 from strawberry.types import Info
 import strawberry_django
 
-from ..input.user import UserLoginInput
-
 from ..types.token import TokenType
 from ..models import Token
-
-from django.contrib.auth import authenticate as django_authenticate, login as django_login
+from ..permissions import IsAuthenticated
+from django.contrib.auth import (
+    authenticate as django_authenticate,
+    login as django_login,
+    logout as django_logout,
+)
 from django.http.request import HttpRequest
 from typing import cast
 
@@ -15,14 +17,22 @@ from typing import cast
 @strawberry.type
 class UserMutation:
     @strawberry_django.mutation()
-    def login(self, info: Info, data: UserLoginInput) -> TokenType | None:
+    def login(self, info: Info, username: str, password: str) -> TokenType | None:
         request: HttpRequest = cast(HttpRequest, info.context["request"])
-        user = django_authenticate(request, username=data.username, password=data.password)
+        user = django_authenticate(request, username=username, password=password)
         if user is not None:
             django_login(request, user)
             token, _ = Token.objects.get_or_create(user=user.pk)
             return token
 
-    @strawberry_django.mutation()
-    def logout(self, info: Info, data: TokenType) -> bool:
-        pass
+    @strawberry_django.mutation(permission_classes=[IsAuthenticated])
+    def logout(self, info: Info, token: str) -> bool:
+        request: HttpRequest = cast(HttpRequest, info.context["request"])
+        try:
+            token_instance = Token.objects.get(token=token)
+            if token_instance.user == request.user:
+                token_instance.delete()
+                django_logout(request)
+                return True
+        except Token.DoesNotExist:
+            return False
