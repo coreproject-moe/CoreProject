10d62461a28caac2a000691fafaa2ed82b3f0d4c
tokitouq <tokitousan2005@gmail.com>
2024-04-05 15:57:21 +0530

    fix

--- backend/django_core/static_src/components/minor/Comment/ReplyButton.svelte
+++ backend/django_core/static_src/components/minor/Comment/ReplyButton.svelte
@@ -1,36 +1,39 @@
 <script lang="ts">
     import { cn } from "$functions/classname";
-    import Chat from "$icons/Chat/Index.svelte";
     import { DeviceEnum } from "$types/device";
     import { createEventDispatcher } from "svelte";
+    
+    // Icons
+    import Chat from "$icons/Chat/Index.svelte";
 
     const dispatch = createEventDispatcher();
+
+    const button_mapping = [
+        {
+            device: DeviceEnum.Mobile,
+            class: "sm:hidden"
+        },
+        {
+            device: DeviceEnum.Tablet,
+            class: "hidden sm:flex md:hidden"
+        },
+        {
+            device: DeviceEnum.Desktop,
+            class: "hidden md:flex"
+        },
+    ]
 
     const handle_click = (device: DeviceEnum) => {
         dispatch("reply", device);
     };
 </script>
 
-<button
-    on:click={() => handle_click(DeviceEnum.Mobile)}
-    class={cn(`btn h-max min-h-full !bg-transparent p-0 text-xs md:gap-[0.35vw] md:text-[0.9vw] sm:hidden`)}
->
-    <Chat class="w-4 md:w-[1vw]" />
-    <span>Replay</span>
-</button>
-
-<button
-    on:click={() => handle_click(DeviceEnum.Tablet)}
-    class={cn(`btn h-max min-h-full !bg-transparent p-0 text-xs md:gap-[0.35vw] md:text-[0.9vw] hidden sm:flex md:hidden`)}
->
-    <Chat class="w-4 md:w-[1vw]" />
-    <span>Replay</span>
-</button>
-
-<button
-    on:click={() => handle_click(DeviceEnum.Desktop)}
-    class={cn(`btn h-max min-h-full !bg-transparent p-0 text-xs md:gap-[0.35vw] md:text-[0.9vw] hidden md:flex`)}
->
-    <Chat class="w-4 md:w-[1vw]" />
-    <span>Replay</span>
-</button>
+{#each button_mapping as item}
+    <button
+        on:click={() => handle_click(item.device)}
+        class={cn(item.class, "btn h-max min-h-full !bg-transparent p-0 text-xs md:gap-[0.35vw] md:text-[0.9vw]")}
+    >
+        <Chat class="w-4 md:w-[1vw]" />
+        <span>Replay</span>
+    </button>
+{/each}
