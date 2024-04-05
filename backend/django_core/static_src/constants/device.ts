b2bcfdf0bee49e2307206f6af1d3807e0eabd9dd
tokitouq <tokitousan2005@gmail.com>
2024-04-05 22:49:27 +0530

    add

--- backend/django_core/static_src/constants/device.ts
+++
@@ -1,7 +0,0 @@
-export const DEVICE_STATE = {
-    Mobile: "mobile",
-    Tablet: "tablet",
-    Desktop: "desktop",
-} as const;
-
-export type DeviceStateType = typeof DEVICE_STATE[keyof typeof DEVICE_STATE];
