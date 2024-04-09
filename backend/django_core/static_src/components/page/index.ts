1c68ccdd2dce826b16d80245d7de5159949c24f3
tokitouq <tokitousan2005@gmail.com>
2024-04-09 14:13:14 +0530

    move to svelte component

--- backend/django_core/static_src/components/page/index.ts
+++ backend/django_core/static_src/components/page/index.ts
@@ -4,6 +4,7 @@ import { register } from "$functions/register";
 const mapping = [
     { tagname: `explore`, component: await import("./explore/Index.svelte") },
     { tagname: `anime-info`, component: await import("./anime/info/Index.svelte") },
+    { tagname: `anime-episode`, component: await import("./anime/episode/Index.svelte") },
     { tagname: `upload`, component: await import("./upload/Index.svelte") },
     { tagname: `home`, component: await import("./home/Index.svelte") },
     { tagname: `anime`, component: await import("./anime/Index.svelte") }
