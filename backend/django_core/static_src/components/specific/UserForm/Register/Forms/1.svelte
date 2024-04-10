08208ab645cb47692a3597e8953cda7d87a0ce11
tokitouq <tokitousan2005@gmail.com>
2024-04-10 16:41:04 +0530

    fix: page 1

--- backend/django_core/static_src/components/specific/UserForm/Register/Forms/1.svelte
+++ backend/django_core/static_src/components/specific/UserForm/Register/Forms/1.svelte
@@ -3,6 +3,8 @@
     import Tick from "$icons/Tick/Index.svelte";
     import Info from "$icons/Info/Index.svelte";
     import ArrowUpRight from "$icons/ArrowUpRight/Index.svelte";
+    import Arrow from "$icons/Arrow/Index.svelte";
+
     import { beforeUpdate, createEventDispatcher } from "svelte";
     import Markdown from "$components/minor/Markdown/Index.svelte";
     import * as _ from "lodash-es";
@@ -14,6 +16,7 @@
     import { get_csrf_token } from "$functions/get_csrf_token";
     import { FETCH_TIMEOUT } from "$constants/fetch";
     import { enhance_anchor } from "$functions/anchor_enhancements";
+    import { autofocus } from "$functions/autofocus";
 
     export let pages_state: [{ email: string }, { password: string }, { confirm_password: string }];
 
@@ -171,16 +174,23 @@
 </script>
 
 <form
+    use:autofocus
     on:submit|preventDefault={handle_submit}
     class="flex h-full flex-col justify-between"
 >
-    <div class="flex flex-col gap-2 whitespace-nowrap font-bold uppercase leading-none tracking-widest text-white md:gap-[0.5vw] md:text-[1.2vw]">
-        <span class="inline-flex gap-2 md:gap-[0.5vw]">
+    <div class="flex flex-col items-start">
+        <span class="text-lg font-bold uppercase leading-none tracking-widest text-warning md:text-[1.5vw]">
             create your
             <CoreText />
             account
         </span>
-        <span class="text-xs font-medium uppercase text-white/90 md:text-[1vw]">unlock the anime world - join us!</span>
+        <button
+            on:click={() => window.history.back()}
+            class="btn btn-link p-0 h-max min-h-full md:text-[1.25vw] md:gap-[0.5vw]"
+        >
+            <Arrow variant="fill" class="md:size-[1.25vw] -rotate-90" />
+            Go Back
+        </button>
     </div>
     <div class="flex flex-col gap-5 md:gap-[1.5vw]">
         <div class="flex flex-col gap-1 md:gap-[0.5vw]">
@@ -195,7 +205,7 @@
                 on:input|preventDefault={handle_email_input}
                 on:input={check_if_form_is_submitable}
                 placeholder="Email address"
-                class="border-primary-500 focus:border-primary-400 h-12 w-full rounded-xl border-2 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-all placeholder:text-white/50 md:h-[3.125vw] md:rounded-[0.75vw] md:border-[0.2vw] md:px-[1vw] md:text-[1.1vw]"
+                class="border-neutral focus:border-primary w-full rounded-xl border-2 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-colors duration-300 placeholder:text-white/50 md:rounded-[0.75vw] md:border-[0.2vw] p-3.5 md:px-[1.1vw] md:py-[0.8vw] leading-none md:text-[1.1vw]"
             />
             <div class="flex items-center gap-2 text-xs leading-none md:gap-[0.5vw] md:text-[0.75vw]">
                 <Info class="w-3 opacity-70 md:w-[0.9vw]" />
@@ -223,7 +233,7 @@
                 on:input={handle_password_input}
                 on:input={check_if_form_is_submitable}
                 placeholder="Password"
-                class="border-primary-500 focus:border-primary-400 h-12 w-full rounded-xl border-2 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-all placeholder:text-white/50 md:h-[3.125vw] md:rounded-[0.75vw] md:border-[0.2vw] md:px-[1vw] md:text-[1.1vw]"
+                class="border-neutral focus:border-primary w-full rounded-xl border-2 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-colors duration-300 placeholder:text-white/50 md:rounded-[0.75vw] md:border-[0.2vw] p-3.5 md:px-[1.1vw] md:py-[0.8vw] leading-none md:text-[1.1vw]"
             />
             <div class="flex flex-col">
                 <div class="grid grid-cols-4 gap-[1.5vw] md:gap-[0.75vw]">
@@ -276,7 +286,7 @@
                 on:input={handle_confirm_password}
                 on:input={check_if_form_is_submitable}
                 placeholder="Confirm Password"
-                class="border-primary-500 focus:border-primary-400 h-12 w-full rounded-xl border-2 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-all placeholder:text-white/50 md:h-[3.125vw] md:rounded-[0.75vw] md:border-[0.2vw] md:px-[1vw] md:text-[1.1vw]"
+                class="border-neutral focus:border-primary w-full rounded-xl border-2 bg-transparent px-5 text-base font-medium outline-none !ring-0 transition-colors duration-300 placeholder:text-white/50 md:rounded-[0.75vw] md:border-[0.2vw] p-3.5 md:px-[1.1vw] md:py-[0.8vw] leading-none md:text-[1.1vw]"
             />
             <div class="flex items-center gap-2 text-xs leading-none md:gap-[0.5vw] md:text-[0.75vw]">
                 {#if _.isEmpty(confirm_password.error)}
@@ -307,7 +317,7 @@
         <button
             type="submit"
             class:btn-disabled={!form_is_submitable}
-            class={cn(`btn btn-primary h-max min-h-max rounded-lg p-4 text-base font-semibold leading-none text-accent md:rounded-[0.5vw] md:p-[1vw] md:text-[0.95vw]`)}
+            class={cn(`btn btn-primary h-max min-h-max rounded-lg p-4 text-base font-semibold leading-none text-accent md:rounded-[0.75vw] md:py-[1vw] md:px-[1.25vw] md:text-[0.95vw]`)}
         >
             <span>Continue</span>
             <ArrowUpRight class="w-4 rotate-45 md:w-[1vw]" />
