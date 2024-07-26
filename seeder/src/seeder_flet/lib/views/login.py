f68e464612fad2ff9481097843590e7bf2c38395
tokitouq <suneeth.since2005@gmail.com>
2024-07-26 23:52:57 +0530

    refactor: seeder login to async with asyncio

--- seeder/src/seeder_flet/lib/views/login.py
+++ seeder/src/seeder_flet/lib/views/login.py
@@ -1,12 +1,15 @@
+import asyncio
 import flet as ft
 
 __all__ = ["login_view"]
 
 
-def handle_login_click(e, username: str, password: str, backend: str):
+async def handle_login_click(username: str, password: str, backend: str):
     "Try to login with backend"
     ...
 
+def handle_login_click_wrapper(e, username: str, password: str, backend: str):
+    asyncio.run(handle_login_click(username, password, backend))
 
 async def login_view():
     username_or_email_field = ft.TextField(
@@ -61,7 +64,7 @@ async def login_view():
                             color={ft.MaterialState.DEFAULT: ft.colors.BLUE_100},
                             bgcolor=ft.colors.TERTIARY,
                         ),
-                        on_click=lambda e: handle_login_click(
+                        on_click=lambda e: handle_login_click_wrapper(
                             e,
                             username=username_or_email_field.value,
                             password=password_field.value,
