32f3a28ddf5911df21affdc14dfb084b1155850d
tokitouq <suneeth.since2005@gmail.com>
2024-07-28 22:06:06 +0530

    chore: mostly add perfect navbar

--- seeder/src/seeder_flet/lib/components/navbar.py
+++ seeder/src/seeder_flet/lib/components/navbar.py
@@ -1,4 +1,31 @@
 import flet as ft
 
+
 async def navbar(page: ft.Page):
-    return ft.Text("Hi I'm a cute navbar")
+    return ft.Container(
+        padding=10,
+        content=ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            controls=[
+                ft.Row(
+                    controls=[
+                        ft.Image(src="/icons/core_icon.svg", width=40, height=40),
+                        ft.Image(src="/icons/coreproject_text.svg", height=20),
+                    ]
+                ),
+                ft.FilledButton(
+                    content=ft.Text("Logout"),
+                    style=ft.ButtonStyle(
+                        shape=ft.RoundedRectangleBorder(radius=10),
+                        color={"": ft.colors.WHITE},
+                        bgcolor={
+                            "": ft.colors.with_opacity(0.25, ft.colors.ERROR),
+                            "hovered": ft.colors.with_opacity(0.5, ft.colors.ERROR),
+                        },
+                        animation_duration=300,
+                    ),
+                    on_click=lambda _: page.go("/login"),
+                ),
+            ],
+        ),
+    )
