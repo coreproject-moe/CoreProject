9ac11bb611423ccc3aa7e326749cc6a46702401d
tokitouq <suneeth.since2005@gmail.com>
2024-07-28 23:59:28 +0530

    feat: add perfect navbar

--- seeder/src/seeder_flet/lib/components/navbar.py
+++ seeder/src/seeder_flet/lib/components/navbar.py
@@ -1,35 +1,49 @@
 import asyncio
 import flet as ft
 
+
 async def handle_logout(e: ft.ControlEvent, page: ft.Page):
     "Logout me!!!"
     ...
     page.go("/login")
 
+
 async def navbar(page: ft.Page):
     return ft.Container(
         padding=10,
-        content=ft.Row(
-            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+        content=ft.Stack(
             controls=[
                 ft.Row(
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                     controls=[
-                        ft.Image(src="/icons/core_icon.svg", width=40, height=40),
-                        ft.Image(src="/icons/coreproject_text.svg", height=20),
-                    ]
+                        ft.Row(
+                            controls=[
+                                ft.Image(
+                                    src="/icons/core_icon.svg", width=40, height=40
+                                ),
+                            ]
+                        ),
+                        ft.ElevatedButton(
+                            content=ft.Text("Logout"),
+                            style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=10),
+                                color=ft.colors.BLUE_50,
+                                animation_duration=300,
+                            ),
+                            on_click=lambda e: asyncio.run(handle_logout(e, page)),
+                        ),
+                    ],
                 ),
-                ft.FilledButton(
-                    content=ft.Text("Logout"),
-                    style=ft.ButtonStyle(
-                        shape=ft.RoundedRectangleBorder(radius=10),
-                        color={"": ft.colors.WHITE},
-                        bgcolor={
-                            "": ft.colors.with_opacity(0.25, ft.colors.ERROR),
-                            "hovered": ft.colors.with_opacity(0.5, ft.colors.ERROR),
-                        },
-                        animation_duration=300,
-                    ),
-                    on_click=lambda e: asyncio.run(handle_logout(e, page))
+                ft.Row(
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    controls=[
+                        ft.Container(
+                            content=ft.Image(
+                                src="/icons/coreseeder_text.svg", height=15
+                            ),
+                            margin=ft.margin.only(top=5),
+                        ),
+                    ],
                 ),
             ],
         ),
