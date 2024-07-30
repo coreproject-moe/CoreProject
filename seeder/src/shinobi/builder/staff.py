1a5eb12b15327bcc31e160d2985f07766b54538d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-07-30 14:12:25 +0600

    Work on staff (#3892)
    * add
    
    * hm
    
    * add
    
    * Delete Test2.py
    
    * Update default.db
    
    * Update test.py
    
    * ad

--- seeder/src/shinobi/builder/staff.py
+++ seeder/src/shinobi/builder/staff.py
@@ -1,119 +1,61 @@
-import string
-
+import re
 from selectolax.parser import HTMLParser
-
+from typing import Set
 from shinobi.decorators.return_error_decorator import return_on_error
 from shinobi.utilities.regex import RegexHelper
 from shinobi.utilities.session import session
 
+import xml.etree.ElementTree as ET
+
 
 class StaffBuilder:
     def __init__(self) -> None:
-        self.anchors: list[str] = []
-        self.visited_urls: set[str] = set()
-
         # Reusuable clients
         self.client = session
 
         # Facades
         self.regex_helper = RegexHelper()
 
+        # Urls
+        self.urls_to_visit = self.__build_people()
+
     @staticmethod
     def get_parser(html: str) -> HTMLParser:
         return HTMLParser(html)
 
-    @staticmethod
-    def add_myanimelist_if_not_already_there(url: str) -> str:
-        if "myanimelist.net" not in url:
-            return "https://myanimelist.net" + url
-        else:
-            return url
-
-    @return_on_error("")
-    def has_next_page(self, html: str) -> bool:
-        parser = self.get_parser(html)
-        node = parser.css_first("div.normal_header > div.fl-r > div > span.bgColor1")
-
-        select_node_list = node.text().split(" ")
-        for item in select_node_list:
-            if self.regex_helper.check_if_string_contains_bracket(item):
-                bracketed_element_position = select_node_list.index(item)
-                break
-
-        if bracketed_element_position == len(select_node_list) - 1:
-            return False
-
-        return True
-
-    def get_all_pages_in_span_tag(self, html: str) -> list[str]:
-        parser = self.get_parser(html)
-        node = (
-            parser.css_first("div.normal_header > div.fl-r > div > span.bgColor1")
-            .select("a")
-            .matches
-        )
-        anchors = [anchor.attributes["href"] for anchor in node]
-        return [item for item in anchors if item is not None]
-
-    def _build_word_list(self) -> list[str]:
-        alphabet_list = list(string.ascii_uppercase)
-        return [
-            f"https://myanimelist.net/people.php?letter={letter}"
-            for letter in alphabet_list
-        ]
-
-    def __build_urls(self, url: str) -> None:
-        self.visited_urls.add(url)
-
+    def __build_people(self) -> Set[int]:
+        url = "https://myanimelist.net/sitemap/index.xml"
         res = self.client.get(url)
-        html = res.text
+        tree = ET.fromstring(res.content)
 
-        staff_nodes = self.get_parser(html).css("a[href*='/people/']")
+        people_urls = []
+        pattern = re.compile(r"people")
 
-        # MyAnimeList Blocked us
-        # Exponential delay
-        for staff_node in staff_nodes:
-            staff_href = staff_node.attributes["href"]
-            if (
-                staff_href
-                and staff_href not in self.anchors
-                and self.regex_helper.check_if_string_contains_integer(staff_href)
-            ):
-                self.anchors.append(
-                    self.add_myanimelist_if_not_already_there(staff_href)
-                )
-
-        if self.has_next_page(html):
-            all_pages = self.get_all_pages_in_span_tag(html)
-            for item in all_pages:
-                myanimelist_formated_url = "https://myanimelist.net" + item
-                if myanimelist_formated_url not in self.visited_urls:
-                    next_url = myanimelist_formated_url
-                    break
-
-            self.__build_urls(next_url)
-
-    def __build_ids(self) -> list[int]:
-        return [
-            self.regex_helper.get_first_integer_from_url(item) for item in self.anchors
+        people_urls = [
+            element.text
+            for sitemap in tree
+            for element in sitemap
+            if "loc" in element.tag and pattern.search(element.text)
         ]
+
+        return set(people_urls)
 
     def build_dictionary(
         self, excluded_ids: list[int] | None = None, sort: bool = False
     ) -> dict[int, str]:
-        for url in self._build_word_list():
-            self.__build_urls(url)
+        dictionary = {}
 
-        dictionary = dict(zip(self.__build_ids(), self.anchors))
+        for url in self.urls_to_visit:
+            res = self.client.get(url)
+            tree = ET.fromstring(res.content)
+
+            for entry in tree:
+                for element in entry:
+                    if "loc" in element.tag:
+                        content = element.text
+                        dictionary[self.regex_helper.get_id_from_url(content)] = content
 
         if sort:
             dictionary = dict(sorted(dictionary.items()))
-
-        if excluded_ids:
-            dictionary = {
-                key: value
-                for key, value in dictionary.items()
-                if key not in excluded_ids
-            }
 
         return dictionary
--- seeder/test.py
+++ seeder/test.py
@@ -1,11 +1,35 @@
-from shinobi.parser.character import CharacterParser
+import requests
+
+from shinobi.parser.staff import StaffParser
 from shinobi.utilities.session import session
 
-res = session.get("https://myanimelist.net/character/1")
+import flet as ft
+import os
+import asyncio
+
+os.environ["DATABASE_URL"] = "sqlite+aiosqlite:///./default.db"
+
+
+from src.seeder_flet.models._engine import Session
+from src.seeder_flet.models._models import Staff
+
+
+from shinobi.builder.staff import StaffBuilder
+
+
+builder = StaffBuilder()
+dictionary = builder.build_dictionary(sort=True)
 
 
 # noqa: E501
-parser = CharacterParser(res.text)
-data = parser.build_dictionary()
 
-print(data)
+
+async def create():
+    async with Session() as _session:
+        for i in dictionary:
+            db_obs = Staff(mal_id=i)
+            _session.add(db_obs)
+        await _session.commit()
+
+
+asyncio.run(create())
