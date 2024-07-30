6d3b228d0f2992d5400ed45e31df05fab8d407c1
tokitouq <suneeth.since2005@gmail.com>
2024-07-30 12:02:32 +0530

    basic rendering

--- seeder/src/seeder_flet/lib/views/people.py
+++ seeder/src/seeder_flet/lib/views/people.py
@@ -1,4 +1,71 @@
 import flet as ft
 
+
+async def get_people_data():
+    return [
+        {
+            "data": {
+                "mal_id": 1,
+                "url": "https:\/\/myanimelist.net\/people\/1\/Tomokazu_Seki",
+                "website_url": None,
+                "images": {
+                    "jpg": {
+                        "image_url": "https:\/\/cdn.myanimelist.net\/images\/voiceactors\/1\/55486.jpg"
+                    }
+                },
+                "name": "Tomokazu Seki",
+                "given_name": "\u667a\u4e00",
+                "family_name": "\u95a2",
+                "alternate_names": [
+                    "Seki Mondoya",
+                    "\u9580\u6238 \u958b",
+                    "Monto Hiraku",
+                ],
+                "birthday": "1972-09-08T00:00:00+00:00",
+                "favorites": 6188,
+                "about": "Hometown: Tokyo, Japan\nBlood type: AB\n\nTwitter: @seki0908\nInstagram: @sekitomokazu\nProfile: Atomic Monkey",
+            }
+        },
+        {
+            "data": {
+                "mal_id": 2,
+                "url": "https:\/\/myanimelist.net\/people\/2\/Tomokazu_Sugita",
+                "website_url": "https:\/\/agrs.co.jp\/",
+                "images": {
+                    "jpg": {
+                        "image_url": "https:\/\/cdn.myanimelist.net\/images\/voiceactors\/1\/81054.jpg"
+                    }
+                },
+                "name": "Tomokazu Sugita",
+                "given_name": "\u667a\u548c",
+                "family_name": "\u6749\u7530",
+                "alternate_names": [],
+                "birthday": "1980-10-11T00:00:00+00:00",
+                "favorites": 47217,
+                "about": "Hometown: Saitama, Japan\nBlood type: B\nHeight: 178 cm\nWeight: 57 kg\n\nTwitter: @sugitaLOV\nStaff Twitter: @AGRS_staff\nStaff Instagram: @agrs_staff\nYouTube: @agrs1011",
+            }
+        },
+    ]
+
+
 async def people_view():
-    return ft.Text("People page")
+    return ft.DataTable(
+        show_checkbox_column=True,
+        columns=[
+            ft.DataColumn(ft.Text("mal_id")),
+            ft.DataColumn(ft.Text("People")),
+            ft.DataColumn(ft.Text("Birthday")),
+            ft.DataColumn(ft.Text("Favorites")),
+        ],
+        rows=[
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(item["data"]["mal_id"])),
+                    ft.DataCell(ft.Text(item["data"]["name"])),
+                    ft.DataCell(ft.Text(item["data"]["birthday"])),
+                    ft.DataCell(ft.Text(item["data"]["favorites"])),
+                ]
+            )
+            for item in await get_people_data()
+        ],
+    )
