426fbf0f9501ca6a32bc12c7e66530dc93918e2d
tokitouq <suneeth.since2005@gmail.com>
2024-08-06 09:06:23 +0530

    style: left column common commands

--- seeder/src/renderer/components/navbar/Index.svelte
+++ seeder/src/renderer/components/navbar/Index.svelte
@@ -3,9 +3,9 @@
   import CoreSeederLogoPath from '@assets/icons/core/coreseeder.svg'
 </script>
 
-<div class="w-full md:h-[6vw] flex items-center justify-between relative md:p-[1.5vw]">
-  <div class="flex items-center md:gap-[1.5vw] h-full">
+<div class="w-full md:h-14 flex items-center justify-between relative md:p-3">
+  <div class="flex items-center md:gap-3 h-full">
     <img src={CoreLogoPath} class="h-full w-auto" alt="" />
-    <img src={CoreSeederLogoPath} class="md:h-[1.75vw] w-auto" alt="" />
+    <img src={CoreSeederLogoPath} class="md:h-4 w-auto" alt="" />
   </div>
 </div>
--- seeder/src/renderer/layouts/HomeLayout.svelte
+++ seeder/src/renderer/layouts/HomeLayout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { Snippet } from 'svelte'
   import Navbar from '@components/navbar/Index.svelte'
+  import { COMMANDS } from '../constants/commands'
 
   const {
     children
@@ -11,8 +12,18 @@
 
 <div class="h-screen w-screen flex flex-col">
   <Navbar />
-  <div class="flex-1 flex">
-    <div class="md:w-1/4"></div>
+  <div class="flex-1 flex md:p-3 md:gap-3">
+    <div class="md:w-80 flex flex-col md:gap-1">
+      {#each COMMANDS.common as command}
+        <a
+          href={command}
+          class="btn !bg-transparent border-none md:p-2 h-max min-h-max justify-start rounded hover:!bg-primary font-normal hover:text-accent transition-none"
+          ><span class="text-warning font-semibold uppercase">get</span>{command
+            .replaceAll('-', ' ')
+            .replace('get ', '')}</a
+        >
+      {/each}
+    </div>
     <div class="w-full overflow-y-scroll">
       {@render children()}
     </div>
