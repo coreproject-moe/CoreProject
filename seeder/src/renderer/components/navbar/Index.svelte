6d85d092923bafab8a52c114c3fd4c8f223ce0ae
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-06 14:50:24 +0600

    Run Prettier

--- seeder/src/renderer/components/navbar/Index.svelte
+++ seeder/src/renderer/components/navbar/Index.svelte
@@ -3,9 +3,9 @@
 	import CoreSeederLogoPath from "@assets/icons/core/coreseeder.svg";
 </script>
 
-<div class="w-full md:h-14 flex items-center justify-between relative md:p-3">
-	<div class="flex items-center md:gap-3 h-full">
+<div class="relative flex w-full items-center justify-between md:h-14 md:p-3">
+	<div class="flex h-full items-center md:gap-3">
 		<img src={CoreLogoPath} class="h-full w-auto" alt="" />
-		<img src={CoreSeederLogoPath} class="md:h-4 w-auto" alt="" />
+		<img src={CoreSeederLogoPath} class="w-auto md:h-4" alt="" />
 	</div>
 </div>
