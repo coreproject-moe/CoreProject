da34a534bd65fda14b29af29c810e41801a2f1cb
tokitouq <suneeth.since2005@gmail.com>
2024-08-06 09:46:08 +0530

    feat: COMMANDS sidebar rendering

--- seeder/src/renderer/layouts/HomeLayout.svelte
+++ seeder/src/renderer/layouts/HomeLayout.svelte
@@ -13,15 +13,25 @@
 <div class="h-screen w-screen flex flex-col">
   <Navbar />
   <div class="flex-1 flex md:p-3 md:gap-3">
-    <div class="md:w-80 flex flex-col md:gap-1">
-      {#each COMMANDS.common as command}
-        <a
-          href={command}
-          class="btn !bg-transparent border-none md:p-2 h-max min-h-max justify-start rounded hover:!bg-primary font-normal hover:text-accent transition-none"
-          ><span class="text-warning font-semibold uppercase">get</span>{command
-            .replaceAll('-', ' ')
-            .replace('get ', '')}</a
-        >
+    <div class="md:w-60 flex flex-col md:gap-1">
+      {#each Object.entries(COMMANDS) as item}
+        {@const command_cat = item[0]}
+        {@const commands = item[1]}
+
+        <details class="collapse outline-none border-none rounded-none" open>
+          <summary class="collapse-title min-h-max p-0 text-info">{command_cat}</summary>
+          <div class="collapse-content pl-2 pt-1 pr-0">
+            {#each commands as command}
+              <a
+                href={command}
+                class="btn w-full !bg-transparent border-none md:p-2 h-max min-h-max justify-start rounded hover:!bg-primary font-normal hover:text-accent transition-none"
+                ><span class="text-warning font-semibold uppercase">get</span>{command
+                  .replaceAll('-', ' ')
+                  .replace('get ', '')}</a
+              >
+            {/each}
+          </div>
+        </details>
       {/each}
     </div>
     <div class="w-full overflow-y-scroll">
