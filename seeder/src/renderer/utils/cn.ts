03662e9c11dec2dad4c0c745dbb6d434c459cf43
__moonlit_grace__ <moonlitgrace.gaia@gmail.com>
2024-08-06 14:03:16 +0530

    [seeder] style: run prettier format (#3912)
    style: prettier format

--- seeder/src/renderer/utils/cn.ts
+++ seeder/src/renderer/utils/cn.ts
@@ -1,14 +1,14 @@
 import { twMerge } from "tailwind-merge";
 
 type ClassValue =
-  | ClassValue[]
-  | Record<string, string | number | null | boolean | undefined>
-  | string
-  | number
-  | null
-  | boolean
-  | undefined;
+	| ClassValue[]
+	| Record<string, string | number | null | boolean | undefined>
+	| string
+	| number
+	| null
+	| boolean
+	| undefined;
 
 const clsx = (...args: ClassValue[]) =>
-  args.filter((arg) => arg && typeof arg === "string").join(" ");
+	args.filter((arg) => arg && typeof arg === "string").join(" ");
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
