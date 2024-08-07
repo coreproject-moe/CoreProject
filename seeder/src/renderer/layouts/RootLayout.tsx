c48850219d07d19b8ab7cec88cc1adfbe1db9426
tokitouq <suneeth.since2005@gmail.com>
2024-08-07 11:12:34 +0530

    add

--- seeder/src/renderer/layouts/RootLayout.tsx
+++ seeder/src/renderer/layouts/RootLayout.tsx
@@ -3,16 +3,16 @@ import Sidebar from "@components/sidebar";
 import { RouteSectionProps } from "@solidjs/router";
 import { Component } from "solid-js";
 
-const HomeLayout: Component<RouteSectionProps<unknown>> = ({ children }) => {
-	return (
-		<div class="flex h-screen w-screen flex-col">
-			<Navbar />
-			<div class="flex flex-1 md:gap-3 md:p-3">
-				<Sidebar />
-				<div class="w-full overflow-y-scroll">{children}</div>
-			</div>
-		</div>
-	);
+const HomeLayout: Component<RouteSectionProps> = ({ children }) => {
+  return (
+    <div class="flex h-screen w-screen flex-col">
+      <Navbar />
+      <div class="flex flex-1 md:gap-3 md:p-3">
+        <Sidebar />
+        <div class="w-full overflow-y-scroll">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeLayout;
