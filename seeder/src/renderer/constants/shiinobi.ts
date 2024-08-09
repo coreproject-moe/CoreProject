8a8483c57999350ed30f1993586440490792c4b9
__moonlit_grace__ <moonlitgrace.gaia@gmail.com>
2024-08-09 16:09:29 +0530

    refactor(seeder): workflow with new shiinobi version (#3928)
    add

---
+++ seeder/src/renderer/constants/shiinobi.ts
@@ -0,0 +1,6 @@
+export const PLATFORMS = ["myanimelist"];
+
+export const COMMANDS = {
+	common: ["character-urls", "demographics", "staff-urls"],
+	anime: ["anime-explicit-genres", "anime-genres", "anime-themes", "anime-urls"]
+};
