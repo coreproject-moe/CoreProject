656fa196c1384d331aadd34a27fd14cd473db078
tokitouq <suneeth.since2005@gmail.com>
2024-08-09 15:57:34 +0530

    add

--- seeder/src/renderer/constants/commands.ts
+++
@@ -1,4 +0,0 @@
-export const COMMANDS = {
-	common: ["get-character-urls", "get-demographics", "get-staff-urls"],
-	anime: ["get-anime-explicit-genres", "get-anime-genres", "get-anime-themes", "get-anime-urls"]
-};
--- seeder/src/renderer/routes/home/index.tsx
+++ seeder/src/renderer/routes/home/index.tsx
@@ -5,7 +5,7 @@ const Home: Component = () => {
 	return (
 		<div class="flex size-full flex-col md:gap-10 md:px-10">
 			<div>
-				<h1 class="font-bold text-accent md:text-3xl">Hello, Sora</h1>
+				<h1 class="font-bold text-warning md:text-3xl">Hello, Sora</h1>
 				<span class="md:text-sm">{get_humanize_time()}!</span>
 			</div>
 		</div>
