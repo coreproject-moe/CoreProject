d4c4c9fc3bee6bf59108af9360572e15d72b7eb5
tokitouq <suneeth.since2005@gmail.com>
2024-08-09 23:45:25 +0530

    fix

--- seeder/src/renderer/components/sidebar/index.tsx
+++ seeder/src/renderer/components/sidebar/index.tsx
@@ -4,46 +4,46 @@ import { A } from "@solidjs/router";
 import { Component, For } from "solid-js";
 
 const Sidebar: Component = () => {
-  return (
-    <div class="flex flex-col md:w-44 md:gap-1">
-      <div class="flex h-full flex-col md:gap-1">
-        <A
-          href="/"
-          class="btn h-max min-h-max w-full justify-start rounded border-none font-normal outline-none transition-none hover:bg-primary hover:bg-primary/10 md:p-2 md:gap-1"
-          activeClass="!bg-primary text-accent"
-          inactiveClass="bg-transparent"
-          end
-        >
-          <span class="text-warning text-xs font-bold">&sol;</span>
-          Home
-        </A>
-        <For each={Object.entries(COMMANDS_MAPPING)}>
-          {([command_cat, commands_obj]) => (
-            <details class="collapse rounded-none border-none outline-none collapse-arrow" open>
-              <summary class="collapse-title min-h-max p-0 text-sm text-info after:!top-1/2 after:!size-1.5 after:!right-1">
-                {command_cat}
-              </summary>
-              <div class="collapse-content pl-0 pr-0 pt-1">
-                <For each={Object.entries(commands_obj)}>
-                  {([command_title, command]) => (
-                    <A
-                      href={command}
-                      class="btn h-max min-h-max w-full justify-start rounded border-none bg-transparent font-normal outline-none transition-none hover:bg-primary/10 md:p-2 md:gap-1"
-                      activeClass="!bg-primary text-accent"
-                    >
-                      <span class="text-warning text-xs font-bold">&sol;</span>
-                      {command_title.replaceAll("-", " ")}
-                    </A>
-                  )}
-                </For>
-              </div>
-            </details>
-          )}
-        </For>
-      </div>
-      <span class="md:text-xs">version: {VERSION}</span>
-    </div>
-  );
+	return (
+		<div class="flex flex-col md:w-44 md:gap-1">
+			<div class="flex h-full flex-col md:gap-1">
+				<A
+					href="/"
+					class="btn h-max min-h-max w-full justify-start rounded border-none font-normal outline-none transition-none hover:bg-primary hover:bg-primary/10 md:gap-1 md:p-2"
+					activeClass="!bg-primary text-accent"
+					inactiveClass="bg-transparent"
+					end
+				>
+					<span class="text-xs font-bold text-warning">&sol;</span>
+					Home
+				</A>
+				<For each={Object.entries(COMMANDS_MAPPING)}>
+					{([command_cat, commands_obj]) => (
+						<details class="collapse collapse-arrow rounded-none border-none !outline-none" open>
+							<summary class="collapse-title min-h-max p-0 text-sm text-info after:!right-1 after:!top-1/2 after:!size-1.5">
+								{command_cat}
+							</summary>
+							<div class="collapse-content pl-0 pr-0 pt-1">
+								<For each={Object.entries(commands_obj)}>
+									{([command_title, command]) => (
+										<A
+											href={command}
+											class="btn h-max min-h-max w-full justify-start rounded border-none bg-transparent font-normal outline-none transition-none hover:bg-primary/10 md:gap-1 md:p-2"
+											activeClass="!bg-primary text-accent"
+										>
+											<span class="text-xs font-bold text-warning">&sol;</span>
+											{command_title.replaceAll("-", " ")}
+										</A>
+									)}
+								</For>
+							</div>
+						</details>
+					)}
+				</For>
+			</div>
+			<span class="md:text-xs">version: {VERSION}</span>
+		</div>
+	);
 };
 
 export default Sidebar;
--- seeder/src/renderer/constants/shiinobi.ts
+++ seeder/src/renderer/constants/shiinobi.ts
@@ -1,15 +1,15 @@
 export const PLATFORMS = ["myanimelist"];
 
 export const COMMANDS_MAPPING = {
-  common: {
-    "Characters": "character-urls",
-    "Staffs": "staff-urls",
-    "Demographics": "demographics"
-  },
-  anime: {
-    "Animes": "anime-urls",
-    "Genres": "anime-genres",
-    "Explicit Genres": "anime-explicit-genres",
-    "Themes": "anime-themes"
-  }
-}
+	common: {
+		Characters: "character-urls",
+		Staffs: "staff-urls",
+		Demographics: "demographics"
+	},
+	anime: {
+		Animes: "anime-urls",
+		Genres: "anime-genres",
+		"Explicit Genres": "anime-explicit-genres",
+		Themes: "anime-themes"
+	}
+};
