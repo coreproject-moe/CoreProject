cb435e8015cec3aeca3a446779b92a4c08051698
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-10 22:58:17 +0600

    Update index.tsx

--- seeder/src/renderer/components/sidebar/index.tsx
+++ seeder/src/renderer/components/sidebar/index.tsx
@@ -33,7 +33,8 @@ const Sidebar: Component = () => {
 											class="btn h-max min-h-max w-full justify-start rounded border-none bg-transparent font-normal outline-none transition-none hover:bg-primary/10 md:p-2"
 											activeClass="!bg-primary text-accent"
 										>
-											<span textContent={obj.icon} class="text-warning" />
+											{/** @ts-ignore: this is safe to do here */}
+											<span innerHTML={obj.icon} class="text-warning" />
 											{command_title.replaceAll("-", " ")}
 										</A>
 									)}
