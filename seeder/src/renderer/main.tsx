81386dfdbdfed4118a87740a5dbdfcb38c838781
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-10 12:01:32 +0600

    Add

--- seeder/src/renderer/main.tsx
+++ seeder/src/renderer/main.tsx
@@ -1,6 +1,48 @@
 import "./main.css";
 
 import { render } from "solid-js/web";
-import App from "./App";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+import { defineCustomElements } from "@coreproject-moe/icons/components";
+
+const routes: RouteDefinition[] = [
+	{
+		path: "/",
+		component: lazy(() => import("@layouts/RootLayout")),
+		children: [
+			{
+				path: "",
+				component: lazy(() => import("@routes/home"))
+			},
+			{
+				path: "/character-urls",
+				component: lazy(() => import("@routes/character"))
+			},
+			{
+				path: "/demographics",
+				component: lazy(() => import("@routes/demographics"))
+			},
+			{
+				path: "/staff-urls",
+				component: lazy(() => import("@routes/staff"))
+			},
+			{
+				path: "/anime-explicit-genres",
+				component: lazy(() => import("@routes/anime/explicit-genres"))
+			},
+			{
+				path: "/anime-genres",
+				component: lazy(() => import("@routes/anime/genres"))
+			},
+			{
+				path: "/anime-urls",
+				component: lazy(() => import("@routes/anime"))
+			},
+			{
+				path: "/anime-themes",
+				component: lazy(() => import("@routes/anime/themes"))
+			}
+		]
+	}
+];
+defineCustomElements();
+render(() => <Router>{routes}</Router>, document.getElementById("root") as HTMLElement);
