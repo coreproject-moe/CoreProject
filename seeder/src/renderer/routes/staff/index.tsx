000b5cc0d92dff2444e88d7c89ccc63dfd6e4b11
tokitouq <suneeth.since2005@gmail.com>
2024-08-12 21:46:18 +0530

    add

--- seeder/src/renderer/routes/staff/index.tsx
+++ seeder/src/renderer/routes/staff/index.tsx
@@ -1,16 +1,7 @@
 import { Component } from "solid-js";
 
 const Staff: Component = () => {
-	async function handle_fetch_click() {
-		const res = await window.api.get_staff_urls();
-		console.log(res);
-	}
-
-	return (
-		<button class="btn btn-primary" onclick={handle_fetch_click}>
-			Fetch
-		</button>
-	);
+	return "Staff";
 };
 
 export default Staff;
