51d6ed5e3c6b4ca9ee52f01916fdfad7332cac40
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-13 06:07:13 +0600

    Add cors (#3975)
    Add

--- seeder/src/main/backend/index.ts
+++ seeder/src/main/backend/index.ts
@@ -1,8 +1,10 @@
 // https://gist.github.com/maximilian-lindsey/a446a7ee87838a62099d
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.get("/", (_, res) => {
 	res.send("Hello World!");
