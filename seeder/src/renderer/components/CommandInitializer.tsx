891f0b68c61858028643b1c6ef4b2c2272583b44
tokitouq <suneeth.since2005@gmail.com>
2024-08-13 10:53:20 +0530

    icon

--- seeder/src/renderer/components/CommandInitializer.tsx
+++ seeder/src/renderer/components/CommandInitializer.tsx
@@ -9,7 +9,10 @@ const CommandInitializer: Component = () => {
       <div class="flex items-center gap-2">
         <span class="text-sm">Select Platform: </span>
         <div class="dropdown">
-          <div tabindex="0" role="button" class="text-warning">{platform()}</div>
+          <div tabindex="0" role="button" class="text-warning flex items-center gap-1">
+            {platform()}
+            <coreproject-shape-chevron class="size-3"></coreproject-shape-chevron>
+          </div>
           <ul tabindex="0" class="dropdown-content bg-neutral p-1 mt-1 rounded min-w-full">
             <For each={PLATFORMS.filter((pl) => pl !== platform())}>
               {(pl) => <li onClick={() => setPlatform(pl)} class="text-sm hover:bg-primary px-2 py-0.5 cursor-pointer rounded hover:text-accent">{pl}</li>}
