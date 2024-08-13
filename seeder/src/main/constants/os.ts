4fc7ba0d1f61a1b2a4fb5d7b269c21e25b31abbb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-13 06:13:11 +0600

    Add

--- seeder/src/main/constants/os.ts
+++ seeder/src/main/constants/os.ts
@@ -1,3 +1,5 @@
+import process from "node:process";
+
 export const IS_MAC = process.platform === "darwin";
 export const IS_WINDOWS = process.platform === "win32";
 export const IS_LINUX = process.platform === "linux";
