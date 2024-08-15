d17a9ca477a4aa77165653c5609e63d3dea88ea1
tokitouq <suneeth.since2005@gmail.com>
2024-08-15 23:56:09 +0530

    formart

--- seeder/src/renderer/components/RenderDiff.tsx
+++ seeder/src/renderer/components/RenderDiff.tsx
@@ -1,28 +1,32 @@
-import { Change } from "diff"
+import { Change } from "diff";
 import { Component } from "solid-js";
 
 type Props = {
-  diff: Change[];
-  options: {
-    type: "add" | "remove";
-  }
-}
+	diff: Change[];
+	options: {
+		type: "add" | "remove";
+	};
+};
 
 const RenderDiff: Component<Props> = (props) => {
-  return (
-    <span>
-      {props.diff.filter((part) => props.options.type === "add" ? !part.removed : !part.added).map((part) => (
-        <span classList={{
-          "text-accent w-max leading-none p-0.5 rounded":
-            part.removed || part.added,
-          "bg-error/50": part.removed,
-          "bg-success/50": part.added
-        }}>
-          {part.value}
-        </span>
-      ))}
-    </span>
-  )
+	return (
+		<span>
+			{props.diff
+				.filter((part) => (props.options.type === "add" ? !part.removed : !part.added))
+				.map((part) => (
+					<span
+						classList={{
+							"text-accent w-max leading-none p-0.5 rounded":
+								part.removed || part.added,
+							"bg-error/50": part.removed,
+							"bg-success/50": part.added
+						}}
+					>
+						{part.value}
+					</span>
+				))}
+		</span>
+	);
 };
 
 export default RenderDiff;
