5ed5f51cc2e876e7fc4e29cde8f50dbcdded22dc
tokitouq <suneeth.since2005@gmail.com>
2024-08-15 23:55:44 +0530

    add final

--- seeder/src/renderer/components/RenderDiff.tsx
+++ seeder/src/renderer/components/RenderDiff.tsx
@@ -13,10 +13,10 @@ const RenderDiff: Component<Props> = (props) => {
     <span>
       {props.diff.filter((part) => props.options.type === "add" ? !part.removed : !part.added).map((part) => (
         <span classList={{
-          "text-accent w-max leading-none p-1 rounded whitespace-pre-line break-words":
+          "text-accent w-max leading-none p-0.5 rounded":
             part.removed || part.added,
-          "bg-error/35": part.removed,
-          "bg-success/25": part.added
+          "bg-error/50": part.removed,
+          "bg-success/50": part.added
         }}>
           {part.value}
         </span>
