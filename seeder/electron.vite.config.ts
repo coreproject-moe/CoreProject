26f6860dc6f40f1bba4d3d01ba55561046db17be
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-17 00:46:01 +0530

    add

--- seeder/electron.vite.config.ts
+++ seeder/electron.vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig, swcPlugin, externalizeDepsPlugin } from "electron-vite";
 import solid from "vite-plugin-solid";
 import svg from "vite-plugin-solid-svg";
 
+const node_alias = {
+	$constants: resolve(__dirname, "./src/main/constants"),
+	$interfaces: resolve(__dirname, "./src/main/interfaces"),
+	$backend: resolve(__dirname, "./src/main/backend"),
+	$workers: resolve(__dirname, "./src/main/workers"),
+	$utils: resolve(__dirname, "./src/main/utils"),
+	$database: resolve(__dirname, "./src/main/database")
+};
+
 export default defineConfig({
 	main: {
 		plugins: [externalizeDepsPlugin(), swcPlugin()],
@@ -23,14 +32,7 @@ export default defineConfig({
 			}
 		},
 		resolve: {
-			alias: {
-				$constants: resolve(__dirname, "./src/main/constants"),
-				$interfaces: resolve(__dirname, "./src/main/interfaces"),
-				$backend: resolve(__dirname, "./src/main/backend"),
-				$workers: resolve(__dirname, "./src/main/workers"),
-				$utils: resolve(__dirname, "./src/main/utils"),
-				$database: resolve(__dirname, "./src/main/database")
-			}
+			alias: node_alias
 		}
 	},
 	preload: {
@@ -52,13 +54,7 @@ export default defineConfig({
 			}
 		},
 		resolve: {
-			alias: {
-				$constants: resolve(__dirname, "./src/main/constants"),
-				$interfaces: resolve(__dirname, "./src/main/interfaces"),
-				$backend: resolve(__dirname, "./src/main/backend"),
-				$workers: resolve(__dirname, "./src/main/workers"),
-				$utils: resolve(__dirname, "./src/main/utils")
-			}
+			alias: node_alias
 		}
 	},
 	renderer: {
