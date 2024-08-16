7293e794f9959556e3d8f392a908f96f82d05f8c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-16 21:25:14 +0600

    Fix

--- seeder/src/main/interfaces/express_urls.ts
+++ seeder/src/main/interfaces/express_urls.ts
@@ -1,15 +1,10 @@
 import { get_free_port } from "$utils/port";
 
-export type ExpressUrls = {
-	shiinobi_healthcheck: string;
-	staff: string;
-};
-
 export const express_port = await get_free_port();
 
 const base_url = `http://localhost:${express_port}`;
 
-export const EXPRESS_URLS: ExpressUrls = {
+export const EXPRESS_URLS = {
 	shiinobi_healthcheck: `${base_url}/shiinobi-healthcheck`,
 	staff: `${base_url}/staff`
 };
