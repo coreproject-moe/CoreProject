338e4412e61508161cf780da8b1d4e362227ec9a
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-16 20:50:57 +0530

    add

--- seeder/src/main/interfaces/express_urls.ts
+++ seeder/src/main/interfaces/express_urls.ts
@@ -1,10 +1,15 @@
 import { get_free_port } from "$utils/port";
 
+export type ExpressUrls = {
+	shiinobi_healthcheck: string;
+	staff: string;
+};
+
 export const express_port = await get_free_port();
 
 const base_url = `http://localhost:${express_port}`;
 
-export const EXPRESS_URLS = {
+export const EXPRESS_URLS: ExpressUrls = {
 	shiinobi_healthcheck: `${base_url}/shiinobi-healthcheck`,
 	staff: `${base_url}/staff`
 };
--- seeder/src/preload/index.d.ts
+++ seeder/src/preload/index.d.ts
@@ -1,15 +1,13 @@
 import { ElectronAPI } from "@electron-toolkit/preload";
 import { COMMANDS as SHIINOBI_COMMANDS } from "$interfaces/shiinobi";
+import type { ExpressUrls } from "$interfaces/express_urls";
 
 declare global {
 	interface Window {
 		electron: ElectronAPI;
 		api: {
 			get_app_version: () => Promise<string>;
-			get_express_urls: () => Promise<{
-				shiinobi_healthcheck: string;
-				staff: string;
-			}>;
+			get_express_urls: () => Promise<ExpressUrls>;
 			// Shiinobi
 			[key in typeof SHIINOBI_COMMANDS[number]]: (...args: any[]) => Promise<object>;
 		};
