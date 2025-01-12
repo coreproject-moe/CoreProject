61ef6f356272cd9bc24a98ebca32d1c93c0db56e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-12 11:29:10 +0600

    Remove tracker from django

--- packages/backend/django_core/apps/trackers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import AnilistModel, KitsuModel, MalModel
-
-# Register your models here.
-
-admin.site.register([AnilistModel, MalModel, KitsuModel])
