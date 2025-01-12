abd5db040df84346ae106e702895858bb41838b0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-12 11:29:49 +0600

    Remove tracker from django

--- packages/backend/django_core/apps/torrent/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import announce_view, TorrentView
-
-urlpatterns = [
-    path("announce/", announce_view, name="announce"),
-    path("list/", TorrentView.as_view(), name="list"),
-]
