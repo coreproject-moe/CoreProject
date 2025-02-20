16224e3991c6979986bd98baa774dd8a5f56a56f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-02-20 16:56:49 +0600

    Update test.py

--- tracker/test.py
+++ tracker/test.py
@@ -1,10 +1,2 @@
-import urllib.parse
-
-# Extract the info_hash parameter from the query
-info_hash_encoded = "%234%e7%ad%97%e1%83%7br%dd%5e%1a%0a%f2%03%d0%d9l%dbj"
-
-# Decode the URL encoding
-info_hash_bytes = urllib.parse.unquote_to_bytes(info_hash_encoded)
-
-# Print the result
-print(f"{repr(info_hash_bytes)}")
+twisted = b"\x00\x00\x00\x00\xb8\xcbBe\x00\x00\x04\x17'\x10\x19\x80"
+b"\x00\x00\x00\x00\xb8\xcbBe\x00\x00\x04\x17'\x10\x19\x80"
