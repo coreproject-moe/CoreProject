5333276739cfdd6f4d92d091f574a3feb902f04d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-03-02 18:32:07 +0600

    Add

--- tracker/src/run.py
+++ tracker/src/run.py
@@ -1,5 +1,16 @@
 import asyncio
 
+try:
+    import uvloop  # type: ignore
+
+    HAS_UVLOOP = True
+except ImportError:
+    HAS_UVLOOP = False
+
 from coreproject_tracker.main import main
 
-asyncio.run(main())
+if HAS_UVLOOP:
+    uvloop.run(main())
+
+else:
+    asyncio.run(main())
