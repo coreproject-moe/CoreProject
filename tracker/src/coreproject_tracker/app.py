a2a87ea6c9b98b56b44c4ee734a09acfd3f7fac0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-03-25 11:25:24 +0600

    Install flask orjson only on cpython (#4615)
    * Initial on redis based pub sub
    
    * Update websocket.py
    
    * Update websocket.py
    
    * Better codes
    
    * Part 1
    
    * part 2
    
    * Update cli.py
    
    * Add
    
    * Add
    
    * Add
    
    * Add some information
    
    * Update

--- tracker/src/coreproject_tracker/app.py
+++ tracker/src/coreproject_tracker/app.py
@@ -1,6 +1,13 @@
-from flask_orjson import OrjsonProvider
 from quart import Quart
 from quart_redis import RedisHandler
+
+# Use orjson
+try:
+    from flask_orjson import OrjsonProvider  # type: ignore
+
+    HAS_FLASK_ORJSON = True
+except ImportError:
+    HAS_FLASK_ORJSON = False
 
 from coreproject_tracker.envs import REDIS_DATABASE, REDIS_HOST, REDIS_PORT
 from coreproject_tracker.servers import http_blueprint, ws_blueprint
@@ -9,8 +16,8 @@ from coreproject_tracker.servers import http_blueprint, ws_blueprint
 def make_app() -> Quart:
     app = Quart(__name__)
 
-    # Use orjson
-    app.json = OrjsonProvider(app)
+    if HAS_FLASK_ORJSON:
+        app.json = OrjsonProvider(app)
 
     # Config
     app.config["REDIS_URI"] = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DATABASE}"
