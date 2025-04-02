325dd74a5c7d2c977180d84364c84760a4508414
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-02 14:07:31 +0600

    Move

---
+++ tracker/web/svelte.config.js
@@ -0,0 +1,9 @@
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
+};
+
+export default config;
