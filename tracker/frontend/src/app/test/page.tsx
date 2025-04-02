e7de5031037602263349ea50a337c08bed583d0e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-02 17:09:59 +0600

    add

--- tracker/frontend/src/app/test/page.tsx
+++ tracker/frontend/src/app/test/page.tsx
@@ -1,3 +1,16 @@
 export default function Page() {
-  return <>Hello</>;
+  return (
+    <div className="grid grid-cols-2 items-center justify-center">
+      <iframe
+        className="h-[90vh] w-full"
+        src="/torrent-page"
+        title="Torrent Client 1"
+      />
+      <iframe
+        className="h-[90vh] w-full"
+        src="/torrent-page"
+        title="Torrent Client 2"
+      />
+    </div>
+  );
 }
