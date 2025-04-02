f7aa64558b7c1f7e08a940b77fbe333a703c05ae
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-02 15:14:52 +0600

    Add

--- tracker/frontend/src/components/folder.tsx
+++ tracker/frontend/src/components/folder.tsx
@@ -1,7 +1,13 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 
-export function FolderDiv(): ReactNode {
+type FolderProps = {
+  children?: ReactNode;
+};
+
+export const FolderDiv: React.FC<FolderProps> = ({ children }) => {
   return (
-    <div className="relative mx-auto mt-20 mb-8 h-[310px] w-[420px] rounded-tl-[5px] rounded-tr-[25px] rounded-br-[25px] rounded-bl-[25px] bg-white before:absolute before:-top-[18px] before:h-[25px] before:w-[200px] before:content-['']"></div>
+    <div className="before:clip-path-[path('M_0_0_L_160_0_C_185_2,_175_16,_200_18_L_0_50_z')] relative h-[310px] w-[420px] rounded-tl-[5px] rounded-tr-[25px] rounded-br-[25px] rounded-bl-[25px] bg-white before:absolute before:-top-[18px] before:h-[25px] before:w-[200px] before:rounded-[25px_0_0_0] before:bg-white before:content-['']">
+      {children}
+    </div>
   );
-}
+};
