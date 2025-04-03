0ca43ece7679aed561e61b7acc668e690f24b201
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-03 10:05:28 +0600

    add

--- tracker/frontend/src/app/page.tsx
+++ tracker/frontend/src/app/page.tsx
@@ -7,11 +7,95 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { AudioLines, Layers2, LoaderCircle } from "lucide-react";
+import { AudioLines, Layers2, LoaderCircle, TrendingUp } from "lucide-react";
 import RedisIcon from "@/icons/redis.svg";
 
 import Image from "next/image";
 import { useBackendData } from "@/hooks/useBackendData";
+import React from "react";
+
+function CardComponent({ data }: { data: any }) {
+  const mapping = [
+    {
+      title: "Quart",
+      description:
+        "An async Python micro framework for building web applications.",
+      icon: "/quart.png",
+      version: data.quart_version,
+    },
+    {
+      title: "Python",
+      description:
+        "Python is a programming language that lets you work quickly and integrate systems more effectively.",
+      icon: "/python.svg",
+      version: data.python_version,
+    },
+    {
+      title: "Redis",
+      description: "Redis is an in-memory database that persists on disk.",
+      icon: RedisIcon,
+      version: {
+        client: data.redis_version.client,
+        server: data.redis_version.server,
+      },
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-1 items-center justify-center gap-10 md:grid-cols-3">
+      {mapping.map((value, index) => {
+        return (
+          <Card key={index} className="md:h-[22vh] lg:h-[19vh]">
+            <CardHeader>
+              <CardTitle>{value.title}</CardTitle>
+              <CardDescription>
+                <p>{value.description}</p>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="relative inset-0 flex h-[50px] items-center justify-center">
+                {typeof value.icon === "string" && (
+                  <Image
+                    src={value.icon}
+                    width="120"
+                    height="250"
+                    alt="quart"
+                    className="absolute z-20 h-[40px] object-fill"
+                  />
+                )}
+
+                {typeof value.icon === "function" && <value.icon />}
+
+                <div className="bg-primary absolute z-10 h-[45px] w-[125px] rounded-lg"></div>
+              </div>
+            </CardContent>
+            <CardFooter className="justify-center gap-2">
+              {typeof value.version === "string" && (
+                <>
+                  <p>Version:</p>
+                  <code>{value.version}</code>
+                </>
+              )}
+              {typeof value.version === "object" && (
+                <>
+                  <div className="space-x-2">
+                    <span>Server:</span>
+                    <code>{value.version.server}</code>
+                  </div>
+                  <div className="text-primary/30">|</div>
+                  <div className="space-x-2">
+                    <span>Client:</span>
+                    <code>{value.version.client}</code>
+                  </div>
+                </>
+              )}
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
 
 export default function Home() {
   const {
@@ -35,102 +119,14 @@ export default function Home() {
             </div>
           ) : (
             <>
-              <div className="flex flex-col gap-5">
+              <div className="mb-5 flex flex-col gap-5">
                 <div className="flex items-center gap-3">
                   <Layers2 />
                   <h1 className="text-3xl">Stack version:</h1>
                 </div>
               </div>
               {/* Grid to show version  */}
-              <div className="grid grid-cols-1 items-center justify-center gap-10 md:grid-cols-3">
-                <Card className="md:h-[22vh] lg:h-[17vh]">
-                  <CardHeader>
-                    <CardTitle>Quart</CardTitle>
-                    <CardDescription className="h-10">
-                      <p>
-                        An async Python micro framework for building web
-                        applications.
-                      </p>
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="relative inset-0 flex h-[50px] items-center justify-center">
-                      <Image
-                        src="/quart.png"
-                        width="120"
-                        height="250"
-                        alt="quart"
-                        className="absolute z-20 h-[40px] object-fill"
-                      />
-                      <div className="bg-primary absolute z-10 h-[45px] w-[125px] rounded-lg"></div>
-                    </div>
-                  </CardContent>
-                  <CardFooter className="justify-center gap-2">
-                    <p>Version:</p>
-                    <code>{backendData.quart_version}</code>
-                  </CardFooter>
-                </Card>
-
-                <Card className="md:h-[22vh] lg:h-[17vh]">
-                  <CardHeader>
-                    <CardTitle>Python</CardTitle>
-                    <CardDescription className="h-10">
-                      <p>
-                        Python is a programming language that lets you work
-                        quickly and integrate systems more effectively.
-                      </p>
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="relative inset-0 flex h-[50px] items-center justify-center">
-                      <Image
-                        src="/python.svg"
-                        width="120"
-                        height="250"
-                        alt="quart"
-                        className="absolute z-20 h-[40px] object-fill"
-                      />
-                      <div className="bg-primary absolute z-10 h-[45px] w-[125px] rounded-lg"></div>
-                    </div>
-                  </CardContent>
-                  <CardFooter className="justify-center gap-2">
-                    <p>Version:</p>
-                    <code>{backendData.python_version}</code>
-                  </CardFooter>
-                </Card>
-
-                <Card className="md:h-[22vh] lg:h-[17vh]">
-                  <CardHeader>
-                    <CardTitle>Redis</CardTitle>
-                    <CardDescription className="h-10">
-                      <p>
-                        Redis is an in-memory database that persists on disk.
-                      </p>
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="relative inset-0 flex h-[50px] items-center justify-center">
-                      <RedisIcon
-                        width="120"
-                        height="250"
-                        className="absolute z-20 h-[40px]"
-                      />
-                      <div className="bg-primary absolute z-10 h-[45px] w-[125px] rounded-lg"></div>
-                    </div>
-                  </CardContent>
-                  <CardFooter className="justify-center gap-2">
-                    <div className="space-x-2">
-                      <span>Server:</span>
-                      <code>{backendData.redis_version.server}</code>
-                    </div>
-                    <div className="text-primary/30">|</div>
-                    <div className="space-x-2">
-                      <span>Client:</span>
-                      <code>{backendData.redis_version.client}</code>
-                    </div>
-                  </CardFooter>
-                </Card>
-              </div>
+              <CardComponent data={backendData} />
 
               {/* Tracker information */}
               <div className="my-10 flex flex-col gap-5">
@@ -139,32 +135,23 @@ export default function Home() {
                   <h1 className="text-3xl">Tracker information:</h1>
                 </div>
               </div>
+
               {/* Grid to show tracker information  */}
               <div className="grid grid-cols-1 items-center justify-center gap-10 md:grid-cols-3">
                 <Card className="md:h-[22vh] lg:h-[17vh]">
                   <CardHeader>
-                    <CardTitle>Quart</CardTitle>
-                    <CardDescription className="h-10">
-                      <p>
-                        An async Python micro framework for building web
-                        applications.
-                      </p>
+                    <CardTitle>Total Torrents</CardTitle>
+                    <CardDescription>
+                      <p>The amount of torrents in the database.</p>
                     </CardDescription>
                   </CardHeader>
                   <CardContent>
                     <div className="relative inset-0 flex h-[50px] items-center justify-center">
-                      <Image
-                        src="/quart.png"
-                        width="120"
-                        height="250"
-                        alt="quart"
-                        className="absolute z-20 h-[40px] object-fill"
-                      />
-                      <div className="bg-primary absolute z-10 h-[45px] w-[125px] rounded-lg"></div>
+                      <TrendingUp />
                     </div>
                   </CardContent>
                   <CardFooter className="justify-center gap-2">
-                    <p>Version:</p>
+                    <p>Total Torrents:</p>
                     <code>{backendData.quart_version}</code>
                   </CardFooter>
                 </Card>
