ccabe4cf97924fa2d34f10e93194a0e2a8d5471d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-03 08:08:09 +0600

    Force always dark mode

--- tracker/frontend/src/app/layout.tsx
+++ tracker/frontend/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html className="dark" lang="en" suppressHydrationWarning>
       <body className={`antialiased`}>
         <ThemeProvider
           attribute="class"
