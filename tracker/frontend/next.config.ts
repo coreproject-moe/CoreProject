f33b002f7fc08f309e1bbe91732ac88bdc9ef8cf
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-03 13:53:28 +0600

    Configure docker file

--- tracker/frontend/next.config.ts
+++ tracker/frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // images: { unoptimized: true },
-  // output: "export",
+  output: "standalone",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
