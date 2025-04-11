fc5989db69da3190dd83ab1f6c6039ba59c22e7b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-12 00:16:39 +0600

    feat: Fix typing  (#4670)
    * add
    
    * Update Dockerfile

--- tracker/backend/coreproject_tracker/envs/workers.py
+++ tracker/backend/coreproject_tracker/envs/workers.py
@@ -3,11 +3,13 @@ import os
 
 __all__ = ["WORKERS_COUNT"]
 
-WORKERS_COUNT = os.environ.get(
-    "WORKERS_COUNT",
-    max(
-        2,  # 2 is here cause of `http and websocket server` and `UDP server` each should use 1 core
-        multiprocessing.cpu_count()
-        - 2,  # 2 is here cause of `redis` and `UDP server` each needs 1 core
-    ),
+WORKERS_COUNT = int(
+    os.environ.get(
+        "WORKERS_COUNT",
+        max(
+            2,  # 2 is here cause of `http and websocket server` and `UDP server` each should use 1 core
+            multiprocessing.cpu_count()
+            - 2,  # 2 is here cause of `redis` and `UDP server` each needs 1 core
+        ),
+    )
 )
--- tracker/backend/coreproject_tracker/functions/array.py
+++ tracker/backend/coreproject_tracker/functions/array.py
@@ -1,14 +1,15 @@
 import random
+from typing import Iterable
 
 __all__ = ["get_n_random_items"]
 
 
-async def get_n_random_items[T](array: T, n: int) -> T:
+async def get_n_random_items[T](array: Iterable[T], n: int) -> list[T]:
     """Given a set of array elements, returns n random elements from array"""
-
     # If we pass only one element to the array
     if not isinstance(array, list):
         array = list(array)
+    ret = []
 
     try:
         ret = random.sample(array, n)
