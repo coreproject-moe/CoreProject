19a541dc5f5b3899e5223eab52615ea37e914241
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-04-25 20:50:49 +0600

    Update udp.py

--- tracker/backend/coreproject_tracker/servers/udp.py
+++ tracker/backend/coreproject_tracker/servers/udp.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from typing import Any
 
 import anyio
 from quart import json
@@ -85,76 +86,7 @@ async def run_udp_server(server_host: str, server_port: int):
             data = UdpDatastructure(**_data)
 
             if data.action == ACTIONS.ANNOUNCE:
-                _data |= {
-                    "info_hash": packet[16:36],  # 20 bytes
-                    "peer_id": packet[36:56].hex(),  # 20 bytes
-                    "downloaded": from_uint64(
-                        packet[56:64]  # Convert 64-bit unsigned integer
-                    ),
-                    "left": from_uint64(
-                        packet[64:72]  # Convert 64-bit unsigned integer
-                    ),
-                    "uploaded": from_uint64(
-                        packet[72:80]  # Convert 64-bit unsigned integer
-                    ),
-                    "event_name": await convert_event_id_to_event_enum(
-                        await from_uint32(
-                            packet[80:84]  # Read 4-byte unsigned int (big-endian)
-                        ),
-                    ),
-                    "ip": await from_uint32(packet[84:88]) or host,
-                    "key": await from_uint32(packet[88:92]),
-                    "numwant": await from_uint32(packet[92:96]),
-                    "port": await from_uint16(packet[96:98]) or port,
-                }
-                data = UdpDatastructure(**_data)
-                redis_stroage = RedisDatastructure(
-                    info_hash=data.info_hash.hex(),
-                    type="udp",
-                    peer_id=data.peer_id,
-                    peer_ip=data.ip,
-                    port=data.port,
-                    left=data.left,
-                )
-
-                await redis_stroage.save()
-
-                redis_data = await hget(data.info_hash.hex()) or {}
-                peers_list = await get_n_random_items(redis_data.values(), data.numwant)
-
-                peers = []
-                seeders = 0
-                leechers = 0
-
-                for peer in peers_list:
-                    _peers = []
-                    _seeders = 0
-                    _leechers = 0
-
-                    try:
-                        peer_data = json.loads(peer)
-
-                        if peer_data["left"] == 0:
-                            _seeders += 1
-                        else:
-                            _leechers += 1
-
-                        _peers.append(f"{peer_data['peer_ip']}:{peer_data['port']}")
-
-                    except (ValueError, KeyError):
-                        continue
-
-                    # This is here to make undo possible
-                    peers.extend(_peers)
-                    seeders += _seeders
-                    leechers += _leechers
-
-                _data |= {
-                    "peers": await addrs_to_compact(peers),
-                    "complete": seeders,
-                    "incomplete": leechers,
-                }
-                data = UdpDatastructure(**_data)
+                _data, data = await handle_announce(_data, packet, host=host, port=port)
 
             if data.event_name == EVENT_NAMES.STOP:
                 await hdel(data.info_hash, f"{data.ip}:{data.port}")
@@ -162,3 +94,78 @@ async def run_udp_server(server_host: str, server_port: int):
             packet = await make_udp_packet(data)
             logging.info(f"Sent UDP packet for {host}:{port}")
             await udp.sendto(packet, host, port)
+
+
+async def handle_announce(
+    _data: dict[str, Any], packet: bytes, host: str, port: int
+) -> tuple[dict[str, Any], UdpDatastructure]:
+    _data |= {
+        "info_hash": packet[16:36],  # 20 bytes
+        "peer_id": packet[36:56].hex(),  # 20 bytes
+        "downloaded": from_uint64(
+            packet[56:64]  # Convert 64-bit unsigned integer
+        ),
+        "left": from_uint64(
+            packet[64:72]  # Convert 64-bit unsigned integer
+        ),
+        "uploaded": from_uint64(
+            packet[72:80]  # Convert 64-bit unsigned integer
+        ),
+        "event_name": await convert_event_id_to_event_enum(
+            await from_uint32(
+                packet[80:84]  # Read 4-byte unsigned int (big-endian)
+            ),
+        ),
+        "ip": await from_uint32(packet[84:88]) or host,
+        "key": await from_uint32(packet[88:92]),
+        "numwant": await from_uint32(packet[92:96]),
+        "port": await from_uint16(packet[96:98]) or port,
+    }
+    data = UdpDatastructure(**_data)
+    redis_stroage = RedisDatastructure(
+        info_hash=data.info_hash.hex(),
+        type="udp",
+        peer_id=data.peer_id,
+        peer_ip=data.ip,
+        port=data.port,
+        left=data.left,
+    )
+
+    await redis_stroage.save()
+
+    redis_data = await hget(data.info_hash.hex()) or {}
+    peers_list = await get_n_random_items(redis_data.values(), data.numwant)
+
+    peers = []
+    seeders = 0
+    leechers = 0
+
+    for peer in peers_list:
+        _peers = []
+        _seeders = 0
+        _leechers = 0
+
+        try:
+            peer_data = json.loads(peer)
+
+            if peer_data["left"] == 0:
+                _seeders += 1
+            else:
+                _leechers += 1
+
+            _peers.append(f"{peer_data['peer_ip']}:{peer_data['port']}")
+
+        except (ValueError, KeyError):
+            continue
+
+        # This is here to make undo possible
+        peers.extend(_peers)
+        seeders += _seeders
+        leechers += _leechers
+
+    _data |= {
+        "peers": await addrs_to_compact(peers),
+        "complete": seeders,
+        "incomplete": leechers,
+    }
+    return _data, UdpDatastructure(**_data)
